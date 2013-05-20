a27a7dec5aa3a3efb8778d90e0c1bc453eb6c526
Andrew Nacin <nacin@git.wordpress.org>
2013-05-20 16:25:21 +0000

    WP.org P2 child theme: Enqueue CSS on our own so we can bump CSS versions.
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@9 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-p2/functions.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-p2/functions.php
@@ -1,6 +1,13 @@
 <?php
 
-add_action( 'after_setup_theme', 'wporg_p2_after_setup_theme' );
+add_action( 'after_setup_theme', 'wporg_p2_after_setup_theme', 11 );
 function wporg_p2_after_setup_theme() {
 	register_nav_menu( 'wporg_header_subsite_nav', 'WP.org Header Sub-Navigation' );
+}
+
+add_action( 'wp_enqueue_scripts', 'wporg_p2_enqueue_scripts', 11 );
+function wporg_p2_enqueue_scripts() {
+	wp_deregister_style( 'p2' );
+	wp_register_style( 'p2', get_template_directory_uri() . '/style.css' );
+	wp_enqueue_style( 'wporg-p2', get_stylesheet_uri(), array( 'p2' ), '2013-05-20' );
 }
