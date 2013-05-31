53c3cc6920963f3770b03f5d0f6f95a55dcca417
Andrew Nacin <nacin@git.wordpress.org>
2013-05-31 19:21:25 +0000

    Add a GlotPress theme for translate.wordpress.org that uses the global WordPress.org header/footer.
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@17 74240141-8908-4e6f-9713-ba540dce6ec7

---
+++ translate.wordpress.org/public_html/gp-templates/helper-functions.php
@@ -0,0 +1,17 @@
+<?php
+
+wp_enqueue_style( 'wporg', gp_url_public_root() . 'gp-templates/style.css', array( 'base' ), '20130531' );
+
+add_action( 'tmpl_load_locations', function( $locations, $template, $args, $template_path ) {
+	$core_templates = GP_PATH . 'gp-templates/';
+	require_once $core_templates . 'helper-functions.php';
+	$locations[] = $core_templates;
+	return $locations;
+}, 50, 4 );
+
+add_filter( 'gp_breadcrumb', function( $breadcrumb ) {
+	$breadcrumb = preg_replace( '#<span class="separator">(.*?)</span>#', '', $breadcrumb, 1 );
+	if ( false !== strpos( $breadcrumb, '<span class="active bubble">' ) )
+		$breadcrumb = str_replace( '</span><span class="active bubble">', ' <span class="active bubble">', $breadcrumb ) . '</span>';
+	return $breadcrumb;
+}, 11 ); // After bubble is added by lamba() in gp-templates/project.php
