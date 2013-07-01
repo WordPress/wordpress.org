ff9aecee0198baf34fc11a40cbfec20ca477c39f
Andrew Nacin <nacin@git.wordpress.org>
2013-07-01 21:11:46 +0000

    CSS bump.
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@51 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-p2/functions.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-p2/functions.php
@@ -9,5 +9,5 @@ add_action( 'wp_enqueue_scripts', 'wporg_p2_enqueue_scripts', 11 );
 function wporg_p2_enqueue_scripts() {
 	wp_deregister_style( 'p2' );
 	wp_register_style( 'p2', get_template_directory_uri() . '/style.css' );
-	wp_enqueue_style( 'wporg-p2', get_stylesheet_uri(), array( 'p2' ), '2013-05-28' );
+	wp_enqueue_style( 'wporg-p2', get_stylesheet_uri(), array( 'p2' ), '2013-07-01' );
 }
