0098fdabde6d2f3380fb10a5a0592a754f13898a
Samuel Wood (Otto) <otto42@git.wordpress.org>
2013-09-11 20:36:35 +0000

    Correct function call to has_post_thumbnail
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@81 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-makehome/masthead.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-makehome/masthead.php
@@ -3,7 +3,7 @@
 	<?php while ( have_posts() ) : the_post(); ?>
 		<hgroup <?php post_class( 'wrap' ); ?>>
 			<?php
-				if ( have_post_thumbnail() ) {
+				if ( has_post_thumbnail() ) {
 					the_post_thumbnail( array( 400, 200 ) );
 				}
 				the_content();
