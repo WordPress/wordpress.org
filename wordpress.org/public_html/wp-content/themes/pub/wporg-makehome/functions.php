f45052583d573aa0dd7fcc29f1fbf54a35daa4fe
Samuel Wood (Otto) <otto42@git.wordpress.org>
2013-11-19 17:31:58 +0000

    Add versioning to the style.css for cachebusting reasons.
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@126 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-makehome/functions.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-makehome/functions.php
@@ -2,7 +2,7 @@
 
 add_action( 'wp_enqueue_scripts', 'make_enqueue_scripts' );
 function make_enqueue_scripts() {
-	wp_enqueue_style( 'make-style', get_stylesheet_uri() );
+	wp_enqueue_style( 'make-style', get_stylesheet_uri(), array(), '1.0' );
 	wp_enqueue_style( 'dashicons', get_template_directory_uri() . '/dashicons.css' );
 }
 
