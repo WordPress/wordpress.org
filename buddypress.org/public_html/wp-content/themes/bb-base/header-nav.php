dec87c45c1c1b9e819df3e525900f98ffad3a18f
John James Jacoby <johnjamesjacoby@git.wordpress.org>
2014-03-04 17:40:09 +0000

    BuddyPress.org: Allow "Documentation" link in `header-nav.php` to be URL agnostic in `bb-base` theme.
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@456 74240141-8908-4e6f-9713-ba540dce6ec7

--- buddypress.org/public_html/wp-content/themes/bb-base/header-nav.php
+++ buddypress.org/public_html/wp-content/themes/bb-base/header-nav.php
@@ -2,7 +2,7 @@
 	<li <?php if ( is_page( 'about'    ) ) : ?>class="current"<?php endif; ?>><a href="<?php echo home_url( 'about'   ); ?>">About</a></li>
 	<li <?php if ( is_page( 'plugins'  ) ) : ?>class="current"<?php endif; ?>><a href="<?php echo home_url( 'plugins' ); ?>">Plugins</a></li>
 	<li <?php if ( is_page( 'themes'   ) ) : ?>class="current"<?php endif; ?>><a href="<?php echo home_url( 'themes'  ); ?>">Themes</a></li>
-	<li><a href="http://codex.bbpress.org/">Documentation</a></li>
+	<li><a href="<?php echo esc_url( '//codex.' . parse_url( home_url(), PHP_URL_HOST ) . '/' ); ?>">Documentation</a></li>
 	<li <?php if ( is_post_type_archive( 'post' ) || is_singular( 'post' ) || is_date() || is_tag() || is_category() || is_home() ) : ?>class="current"<?php endif; ?>><a href="<?php echo home_url( 'blog' ); ?>">Blog</a></li>
 	<?php if ( function_exists( 'is_bbpress' ) ) : ?><li <?php if ( is_bbpress() ) : ?>class="current"<?php endif; ?>><a href="<?php bbp_forums_url(); ?>">Support</a></li><?php endif; ?>
 	<li class="download<?php if ( is_page( 'download' ) ) : ?> current<?php endif; ?>"><a href="<?php echo home_url( 'download' ); ?>">Download</a></li>
