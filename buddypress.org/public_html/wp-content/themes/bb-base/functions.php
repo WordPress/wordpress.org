4e012c450f1ae9fe113281f90d058682a59fbddb
John James Jacoby <johnjamesjacoby@git.wordpress.org>
2014-04-08 18:27:37 +0000

    BuddyPress.org: Lighter showcase header text for improved contrast and legibility.
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@534 74240141-8908-4e6f-9713-ba540dce6ec7

--- buddypress.org/public_html/wp-content/themes/bb-base/functions.php
+++ buddypress.org/public_html/wp-content/themes/bb-base/functions.php
@@ -33,7 +33,7 @@ if ( bb_base_is_codex() ) {
 function bb_base_register_stylesheets() {
 
 	// Version of CSS
-	$version = '20140407d';
+	$version = '20140408a';
 
 	// Base theme styling
 	wp_enqueue_style( 'bb-base',   get_template_directory_uri()   . '/style.css', false,                         $version, 'screen' );
