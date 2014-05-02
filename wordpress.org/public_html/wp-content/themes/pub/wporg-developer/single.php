91c2d63c2d42355841fde7e39b5febbc42c41b32
Scott Reilly <coffee2code@git.wordpress.org>
2014-05-02 21:50:14 +0000

    Code Reference: Disable use of wporg_developer_post_nav() in single.php
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@594 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-developer/single.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-developer/single.php
@@ -17,7 +17,7 @@ get_header(); ?>
 
 			<?php get_template_part( 'content', 'reference'); ?>
 
-			<?php wporg_developer_post_nav(); ?>
+			<?php //wporg_developer_post_nav(); ?>
 
 			<?php
 				// If comments are open or we have at least one comment, load up the comment template
