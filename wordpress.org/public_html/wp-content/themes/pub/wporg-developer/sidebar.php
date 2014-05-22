fd78f03e31cb5cf8f31c815a32b1196e9752a00b
Scott Reilly <coffee2code@git.wordpress.org>
2014-05-22 16:31:20 +0000

    Code Reference: handbook sidebar styles. props nicolealleyinteractivecom
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@637 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-developer/sidebar.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-developer/sidebar.php
@@ -8,7 +8,9 @@
 <?php if ( is_active_sidebar( get_post_type() ) ) : ?>
 	<div id="sidebar" class="widget-area sidebar section" role="complementary">
 		<?php do_action( 'before_sidebar' ); ?>
-		<?php if ( ! dynamic_sidebar( get_post_type() ) ) : ?>
-		<?php endif; // end sidebar widget area ?>
+		<ul>
+			<?php if ( ! dynamic_sidebar( get_post_type() ) ) : ?>
+			<?php endif; // end sidebar widget area ?>
+		</ul>
 	</div><!-- #secondary -->
 <?php endif; ?>
