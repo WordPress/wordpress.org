def2f60402f59dd3f8608b8e3c26d66d348ee8c2
Samuel Wood (Otto) <otto42@git.wordpress.org>
2014-05-25 21:12:38 +0000

    Add initial 404 page for makehome. Fixes #383
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@644 74240141-8908-4e6f-9713-ba540dce6ec7

---
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-makehome/404.php
@@ -0,0 +1,16 @@
+<?php get_header(); ?>
+
+<section class="get-involved">
+<div class="wrapper">
+
+    <div id="404" class="post 404">
+
+		<h2 class="section-title">Not Found</h2>
+		<article style="width:100%">
+			<p>Are you looking to contribute to WordPress? Check out the <a href="//make.wordpress.org/">many ways to get involved</a>.</p>
+		</article>
+	</div>
+</div><!-- /wrapper -->
+</section>
+
+<?php get_footer(); ?>
