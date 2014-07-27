fc78e2ca97b92620a2f0100a4beadfe8bd6f3f1d
Ian Dunn <iandunn@git.wordpress.org>
2014-07-27 20:55:40 +0000

    International Sites: Get local copy of returned variable instead of reference.
    Fixes #573
    props iamfriendly
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@763 74240141-8908-4e6f-9713-ba540dce6ec7

--- global.wordpress.org/public_html/wp-content/mu-plugins/roles/rosetta-roles.php
+++ global.wordpress.org/public_html/wp-content/mu-plugins/roles/rosetta-roles.php
@@ -34,7 +34,7 @@ function ros_role_modifications() {
 	if ( ! get_role( 'validator' ) ) {
 		add_role( 'validator', __( 'Validator', 'rosetta' ), array( 'read' => true, 'level_0' => true ) );
 	}
-	$editor_role = & get_role( 'editor' );
+	$editor_role = get_role( 'editor' );
 	if ( $editor_role && ! $editor_role->has_cap( 'remove_users' ) ) {
 		$editor_role->add_cap( 'edit_theme_options' );
 		$editor_role->add_cap( 'list_users' );
