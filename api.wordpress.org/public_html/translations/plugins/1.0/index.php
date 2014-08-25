ca99b54dd61434395d2a72c59715f7f2fcf35401
Andrew Nacin <nacin@git.wordpress.org>
2014-08-25 20:33:54 +0000

    Translations: Add theme and plugin endpoints.
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@811 74240141-8908-4e6f-9713-ba540dce6ec7

---
+++ api.wordpress.org/public_html/translations/plugins/1.0/index.php
@@ -0,0 +1,20 @@
+<?php
+
+$base_dir = dirname( dirname( dirname( __DIR__ ) ) );
+require( $base_dir . '/translations/lib.php' );
+require( $base_dir . '/init.php' );
+require( $base_dir . '/includes/hyperdb/bb-10-hyper-db.php' );
+require( $base_dir . '/includes/object-cache.php' );
+wp_cache_init();
+
+$slug = isset( $_REQUEST['slug'] ) ? $_REQUEST['slug'] : '';
+$version = isset( $_REQUEST['version'] ) ? $_REQUEST['version'] : null;
+
+$translations = find_all_translations_for_type_and_domain( 'plugin', $slug, $version );
+
+call_headers( 'application/json' );
+
+echo json_encode( array( 'translations' => $translations ) );
+
+exit;
+
