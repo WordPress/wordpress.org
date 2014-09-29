a0cd818a325a0cf1446a2cea0f341a097198c415
Scott Reilly <coffee2code@git.wordpress.org>
2014-09-29 06:24:35 +0000

    WP.org P2 theme: bump CSS version
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@878 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-p2/functions.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-p2/functions.php
@@ -9,7 +9,7 @@ add_action( 'wp_enqueue_scripts', 'wporg_p2_enqueue_scripts', 11 );
 function wporg_p2_enqueue_scripts() {
 	wp_deregister_style( 'p2' );
 	wp_register_style( 'p2', get_template_directory_uri() . '/style.css' );
-	wp_enqueue_style( 'wporg-p2', get_stylesheet_uri(), array( 'p2' ), '2014-08-19' );
+	wp_enqueue_style( 'wporg-p2', get_stylesheet_uri(), array( 'p2' ), '20140928' );
 }
 
 add_filter( 'get_comment_author_url', 'wporg_p2_comment_profile_urls' );
