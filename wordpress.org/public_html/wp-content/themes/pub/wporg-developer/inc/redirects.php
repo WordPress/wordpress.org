1623cecc13910e90d362690d1ad7a5458d30235f
Scott Reilly <coffee2code@git.wordpress.org>
2014-10-10 22:24:46 +0000

    Code Reference: redirect singularized slugs for parsed post types to correct pluralized version
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@898 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-developer/inc/redirects.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-developer/inc/redirects.php
@@ -25,6 +25,7 @@ class DevHub_Redirects {
 		add_action( 'template_redirect', array( __CLASS__, 'redirect_handbook' ) );
 		add_action( 'template_redirect', array( __CLASS__, 'redirect_resources' ) );
 		add_action( 'template_redirect', array( __CLASS__, 'redirect_pluralized_handbooks' ), 1 );
+		add_action( 'template_redirect', array( __CLASS__, 'redirect_pluralized_reference_post_types' ), 1 );
 	}
 
 	/**
@@ -83,6 +84,33 @@ class DevHub_Redirects {
 		if ( 'themes' == $name ) {
 			wp_redirect( get_post_type_archive_link( 'theme-handbook' ), 301 );
 			exit();
+		}
+	}
+
+	/**
+	 * Redirects requests for the pluralized slugs of the code reference parsed
+	 * post types.
+	 *
+	 * Note: this is a convenience redirect and not a fix for any officially
+	 * deployed links.
+	 */
+	public static function redirect_pluralized_reference_post_types() {
+		$path = trailingslashit( $_SERVER['REQUEST_URI'] );
+
+		$post_types = array(
+			'class'    => 'classes',
+			'function' => 'functions',
+			'hook'     => 'hooks',
+			'method'   => 'methods',
+		);
+
+		// '/reference/$singular(/*)?' => '/reference/$plural(/*)?'
+		foreach ( $post_types as $post_type_slug_singular => $post_type_slug_plural ) {
+			if ( 0 === stripos( $path, "/reference/{$post_type_slug_singular}/" ) ) {
+				$path = "/reference/{$post_type_slug_plural}/" . substr( $path, strlen( "/reference/{$post_type_slug_singular}/" ) );
+				wp_redirect( $path, 301 );
+				exit();
+			}
 		}
 	}
 
