3ad9287e335b1a7d95e85efb07e2da8f48ba84dd
Andrew Nacin <nacin@git.wordpress.org>
2014-12-17 18:45:09 +0000

    GlotPress: Disable profiles for now.
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@1046 74240141-8908-4e6f-9713-ba540dce6ec7

--- translate.wordpress.org/includes/gp-plugins/wporg-specifics.php
+++ translate.wordpress.org/includes/gp-plugins/wporg-specifics.php
@@ -9,10 +9,21 @@ class GP_WPorg_Specifics extends GP_Plugin {
 	function __construct() {
 		parent::__construct();
 		$this->add_action( 'before_login_form' );
+		$this->add_filter( 'gp_url_profile', array( 'args' => 2 ) );
+		$this->add_filter( 'routes' );
 	}
 
 	function before_login_form() {
 		echo '<span class="secondary">' . __( 'Log in with your wordpress.org forums account. If you don&#8217;t have one, you can <a href="http://wordpress.org/support/register.php">register at the forums.</a>' ) . '</span>';
+	}
+
+	function gp_url_profile( $url, $nicename ) {
+		return 'https://profiles.wordpress.org/' . $nicename;
+	}
+
+	function routes( $routes ) {
+		unset( $routes['get:/profile/(.+?)'] );
+		return $routes;
 	}
 }
 GP::$plugins->wporg_specifics = new GP_WPorg_Specifics;
