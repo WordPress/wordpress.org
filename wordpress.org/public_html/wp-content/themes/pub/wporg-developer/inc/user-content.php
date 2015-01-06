70b12887a7c10d62c9dd9599a5da7c9dd08655f9
Scott Reilly <coffee2code@git.wordpress.org>
2015-01-06 18:15:00 +0000

    developer.wordpress.org: Also enqueue user-notes.js and comment-related JS for wp-parser-hooks
    * Adds code-related quicktag buttons to comment form
    * Hides comment form behind toggle, as done everywhere else
    
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@1093 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-developer/inc/user-content.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-developer/inc/user-content.php
@@ -61,10 +61,12 @@ class DevHub_User_Submitted_Content {
 	 * Enqueues scripts and styles.
 	 */
 	public static function scripts_and_styles() {
-		if ( is_singular() && ( '0' != get_comments_number() || \DevHub\post_type_has_source_code() ) ) {
-			wp_enqueue_script( 'wporg-developer-function-reference', get_template_directory_uri() . '/js/function-reference.js', array( 'jquery', 'syntaxhighlighter-core', 'syntaxhighlighter-brush-php' ), '20140515', true );
-			wp_enqueue_style( 'syntaxhighlighter-core' );
-			wp_enqueue_style( 'syntaxhighlighter-theme-default' );
+		if ( is_singular() ) {
+			if ( '0' != get_comments_number() || \DevHub\post_type_has_source_code() ) {
+				wp_enqueue_script( 'wporg-developer-function-reference', get_template_directory_uri() . '/js/function-reference.js', array( 'jquery', 'syntaxhighlighter-core', 'syntaxhighlighter-brush-php' ), '20140515', true );
+				wp_enqueue_style( 'syntaxhighlighter-core' );
+				wp_enqueue_style( 'syntaxhighlighter-theme-default' );
+			}
 
 			wp_enqueue_script( 'wporg-developer-user-notes', get_template_directory_uri() . '/js/user-notes.js', array( 'quicktags' ), '20141117', true );
 			if ( get_option( 'thread_comments' ) ) {
