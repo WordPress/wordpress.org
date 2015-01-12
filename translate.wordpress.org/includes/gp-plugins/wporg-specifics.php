911455b18301d6f34cbd2a8fa818c940fb2ad69d
Dominik Schilling (ocean90) <ocean90@git.wordpress.org>
2015-01-12 17:14:29 +0000

    GlotPress WPorg plugin: Add natural sorting for WordPress and BuddyPress sub projects.
    fixes #813.
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@1133 74240141-8908-4e6f-9713-ba540dce6ec7

--- translate.wordpress.org/includes/gp-plugins/wporg-specifics.php
+++ translate.wordpress.org/includes/gp-plugins/wporg-specifics.php
@@ -11,6 +11,7 @@ class GP_WPorg_Specifics extends GP_Plugin {
 		$this->add_action( 'before_login_form' );
 		$this->add_filter( 'gp_url_profile', array( 'args' => 2 ) );
 		$this->add_filter( 'routes' );
+		$this->add_filter( 'projects', array( 'args' => 2 ) );
 	}
 
 	function before_login_form() {
@@ -24,6 +25,31 @@ class GP_WPorg_Specifics extends GP_Plugin {
 	function routes( $routes ) {
 		unset( $routes['get:/profile/(.+?)'] );
 		return $routes;
+	}
+
+	function projects( $sub_projects, $parent_id ) {
+		if ( 1 != $parent_id && 13 != $parent_id ) { // 1 = WordPress, 13 = BuddyPress
+			return $sub_projects;
+		}
+
+		/*
+		 * Natural sorting for sub projects.
+		 *
+		 * 3.0.x
+		 * 4.0.x
+		 * 3.1.x
+		 * 4.1.x
+		 *  =>
+		 * 4.1.x
+		 * 4.0.x
+		 * 3.1.x
+		 * 3.0.x
+		 */
+		usort( $sub_projects, function( $a, $b ) {
+			return - strcasecmp( $a->name, $b->name );
+		});
+
+		return $sub_projects;
 	}
 }
 GP::$plugins->wporg_specifics = new GP_WPorg_Specifics;
