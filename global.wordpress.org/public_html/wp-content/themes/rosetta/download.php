9bf0be058e451252cbd8dc28c493b9c2dd011511
Dion Hulse <dd32@git.wordpress.org>
2015-01-22 20:03:42 +0000

    Rosetta Downloads template: Code cleanup and removal of some PHP Notices
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@1165 74240141-8908-4e6f-9713-ba540dce6ec7

--- global.wordpress.org/public_html/wp-content/themes/rosetta/download.php
+++ global.wordpress.org/public_html/wp-content/themes/rosetta/download.php
@@ -9,29 +9,29 @@ $releases = $rosetta->rosetta->get_releases_breakdown();
 	<div class="wrapper">
 	<div class="section releases">
 <?php
-	if (!empty($releases)):
-		if (isset($releases['latest'])):
+	if ( ! empty( $releases ) ):
+		if ( isset( $releases['latest'] ) ):
 			release_row(null, null, null, true);
 ?>
 			<h3 id="latest"><?php _e('Latest release', 'rosetta'); ?></h3>
 			<table class="releases latest">
-				<?php echo release_row($releases['latest'], 'alt'); ?>
+				<?php echo release_row( $releases['latest'], 'alt' ); ?>
 			</table>
 <?php
 		endif;
-		if (is_array($releases['branches'])):
+		if ( ! empty( $releases['branches'] ) ):
 ?>
 			<a name="older" id="older"></a>
 <?php
-		foreach ($releases['branches'] as $branch => $branch_rels):
-			release_row(null, null, null, true);
+		foreach ( $releases['branches'] as $branch => $branch_rels ):
+			release_row( null, null, null, true );
 ?>
-			<h3><?php printf(__('%s Branch', 'rosetta'), $branch);?></h3>
+			<h3><?php printf( __('%s Branch', 'rosetta'), $branch );?></h3>
 			<table class="releases">
 <?php
-	foreach($branch_rels as $release):
+	foreach ( $branch_rels as $release ):
 ?>
-				<?php release_row($release, 'alt');?>
+				<?php release_row( $release, 'alt' );?>
 <?php
 	endforeach;
 ?>
@@ -39,15 +39,15 @@ $releases = $rosetta->rosetta->get_releases_breakdown();
 <?php
 		endforeach;
 		endif; # any branches
-		if (is_array($releases['betas'])):
+		if ( ! empty( $releases['betas'] ) ):
 ?>
 			<h3 id="betas"><?php _e('Beta &amp; RC releases', 'rosetta'); ?></h3>
 			<table id="beta" class="releases">
 <?php
-	release_row(null, null, null, true);
-	foreach($releases['betas'] as $release):
+	release_row( null, null, null, true );
+	foreach ( $releases['betas'] as $release ):
 ?>
-				<?php release_row($release, 'alt', 'beta-first');?>
+				<?php release_row( $release, 'alt', 'beta-first' ); ?>
 <?php
 	endforeach;
 ?>
