43de7771d2547acd58d13f2491f2d98ec86291d4
Andrew Nacin <nacin@git.wordpress.org>
2015-01-23 22:40:25 +0000

    Trac mentions: Handle special cases for core trac.  * Block mention notifications when the user has blocked the ticket.  * Subscribe users to tickets once they've been mentioned.
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@1183 74240141-8908-4e6f-9713-ba540dce6ec7

--- api.wordpress.org/public_html/dotorg/trac/mentions-handler.php
+++ api.wordpress.org/public_html/dotorg/trac/mentions-handler.php
@@ -1,5 +1,9 @@
 <?php
 
+define( 'BLOCKED',    0 );
+define( 'SUBSCRIBED', 1 );
+define( 'MENTIONED',  2 );
+
 require dirname( dirname( __DIR__ ) ) . '/includes/slack-config.php';
 
 if ( ! isset( $_POST['secret'] ) || $_POST['secret'] !== \Dotorg\Slack\Trac\URL_SECRET__MENTIONS ) {
@@ -27,11 +31,33 @@ function wporg_user_has_visited_trac( $user_login ) {
 	return (bool) $wpdb->get_var( $wpdb->prepare( "SELECT user_login FROM trac_users WHERE user_login = %s", $user_login ) );
 }
 
+function wporg_get_trac_ticket_subscription_status( $username, $ticket ) {
+	global $wpdb;
+	add_db_table( 'trac_core', '_ticket_subs' ); // HyperDB
+	$status = $wpdb->get_var( $wpdb->prepare(
+		"SELECT status FROM _ticket_subs WHERE username = %s AND ticket = %s",
+		$username, $ticket
+	) );
+
+	if ( is_numeric( $status ) ) {
+		return (int) $status;
+	}
+	return false;
+}
+
 if ( ! wporg_user_has_visited_trac( $user_login ) ) {
 	$wpdb->insert( 'trac_users', compact( 'user_login' ) );
 }
 
 add_filter( 'wporg_notifications_notify_username', function( $notify, $username ) use ( $type, $payload, $wpdb ) {
+	// Core Trac has notifications configured, see if the user has blocked the ticket.
+	if ( $payload->trac === 'core' ) {
+		$status = wporg_get_trac_ticket_subscription_status( $username, $payload->ticket_id );
+		if ( BLOCKED === $status ) {
+			return false;
+		}
+	}
+
 	if ( $type === 'ticket' ) {
 		// Don't need a query to say we can notify the owner and reporter.
 		if ( $username === $payload->owner || $username === $payload->reporter ) {
@@ -40,6 +66,15 @@ add_filter( 'wporg_notifications_notify_username', function( $notify, $username 
 	}
 
 	if ( wporg_user_has_visited_trac( $username ) ) {
+		// If on Core Trac, a user is not a reporter, owner, or subscriber, subscribe them.
+		if ( isset( $status ) && false === $status ) {
+			$wpdb->insert( '_ticket_subs', array(
+				'username' => $username,
+				'ticket'   => $payload->ticket_id,
+				'status'   => MENTIONED,
+			) );
+		}
+
 		return true;
 	}
 	return $notify;
