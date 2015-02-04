c70392586e77c28614c01ee56bdd7e0c11a70395
Konstantin Obenland <obenland@git.wordpress.org>
2015-02-04 19:53:05 +0000

    WP.org Themes: Don't escape theme names.
    It can break names like "It's a girl".
    https://wordpress.org/themes/its-a-girl
    
    H/t to melchoyce for reporting the bug.
    
    See #745.
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@1221 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-themes/view-templates/theme.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-themes/view-templates/theme.php
@@ -8,7 +8,7 @@
 	<# } #>
 	<a class="more-details url" href="{{{ data.permalink }}}" rel="bookmark"><?php _ex( 'More Info', 'theme' ); ?></a>
 	<div class="theme-author"><?php printf( __( 'By %s' ), '<span class="author">{{ data.author }}</span>' ); ?></div>
-	<h3 class="theme-name entry-title">{{ data.name }}</h3>
+	<h3 class="theme-name entry-title">{{{ data.name }}}</h3>
 
 	<div class="theme-actions">
 		<a class="button button-primary preview install-theme-preview" href="//downloads.wordpress.org/theme/{{ data.slug }}.{{ data.version }}.zip"><?php esc_html_e( 'Download' ); ?></a>
