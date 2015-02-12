d3649708e3f097bbd19cb982edfa4a80bbfdd947
Konstantin Obenland <obenland@git.wordpress.org>
2015-02-12 19:56:30 +0000

    WP.org Themes: First pass at revised modal styles.
    Props melchoyce.
    
    See http://make.wordpress.org/meta/2015/01/23/theme-directory-mockups/
    Fixes missing forum link for #852,
    Fixes #847.
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@1255 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-themes/content-single.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-themes/content-single.php
@@ -3,87 +3,44 @@
 	$theme = wporg_themes_photon_screen_shot( $theme );
 ?>
 <div class="theme-wrap">
-	<div class="theme-about">
+	<div class="theme-about hentry">
 
 		<?php if ( strtotime( '-2 years' ) > strtotime( $theme->last_updated ) ) : ?>
-			<div class="theme-notice notice notice-warning">
-				<p><?php _e( 'This theme <strong>hasn&#146;t been updated in over 2 years</strong>. It may no longer be maintained or supported and may have compatibility issues when used with more recent versions of WordPress.', 'wporg-themes' ); ?></p>
-			</div><!-- .theme-info -->
+		<div class="theme-notice notice notice-warning">
+			<p><?php _e( 'This theme <strong>hasn&#146;t been updated in over 2 years</strong>. It may no longer be maintained or supported and may have compatibility issues when used with more recent versions of WordPress.', 'wporg-themes' ); ?></p>
+		</div><!-- .theme-info -->
 		<?php endif; ?>
 
-		<div class="theme-screenshots">
-			<div class="screenshot"><?php echo esc_url( $theme->screenshot_url . '?w=732&strip=all' ); ?></div>
+		<div class="theme-head">
+			<h3 class="theme-name entry-title"><?php the_title(); ?></h3>
+			<h4 class="theme-author"><?php printf( __( 'By %s' ), sprintf( '<a href="https://profiles.wordpress.org/%s"><span class="author">%s</span><a/>', $theme->author ) ); ?></h4>
 
 			<div class="theme-actions">
 				<a href="<?php echo esc_url( '//downloads.wordpress.org/theme/' . $theme->slug . '.' . $theme->version . '.zip' ); ?>" class="button button-primary"><?php _e( 'Download' ); ?></a>
 				<a href="<?php echo esc_url( $theme->preview_url ); ?>" class="button button-secondary"><?php _e( 'Preview' ); ?></a>
 			</div>
-		</div><!-- .theme-screenshots -->
+
+			<?php if ( ! empty( $theme->parent ) ) : ?>
+			<div class="theme-notice notice notice-info">
+				<p class="parent-theme"><?php printf( __( 'This is a child theme of %s.' ), sprintf( '<a href="/%1$s">%2$s</a>', $theme->parent->slug, $theme->parent->name ) ); ?></p>
+			</div>
+			<?php endif; ?>
+		</div><!-- .theme-head -->
 
 		<div class="theme-info">
-			<div class="hentry">
-				<h3 class="theme-name entry-title"><?php the_title(); ?></h3>
-				<h4 class="theme-author"><?php printf( __( 'By %s' ), sprintf( '<a href="https://profiles.wordpress.org/%s"><span class="author">%s</span><a/>', $theme->author ) ); ?></h4>
+			<div class="screenshot"><?php echo esc_url( $theme->screenshot_url . '?w=732&strip=all' ); ?></div>
 
-				<div class="theme-description entry-summary"><?php the_content(); ?></div>
+			<div class="theme-description entry-summary"><?php the_content(); ?></div>
 
-				<?php if ( ! empty( $theme->parent ) ) : ?>
-				<div class="theme-notice notice notice-info">
-					<p class="parent-theme"><?php printf( __( 'This is a child theme of %s.' ), sprintf( '<a href="/%1$s">%2$s</a>', $theme->parent->slug, $theme->parent->name ) ); ?></p>
-				</div>
-				<?php endif; ?>
-			</div><!-- .theme-info -->
-
-			<div class="theme-ratings" itemprop="aggregateRating" itemscope itemtype="http://schema.org/AggregateRating">
-				<meta itemprop="ratingValue" content="<?php echo esc_attr( number_format_i18n( $theme->rating / 20, 1 ) ); ?>"/>
-				<meta itemprop="ratingCount" content="<?php echo esc_attr( $theme->num_ratings ); ?>"/>
-				<h4><?php _e( 'Ratings', 'wporg-themes' ); ?></h4>
-
-				<div class="star-holder">
-					<div class="star-rating" style="width: <?php echo esc_attr( number_format_i18n( $theme->rating, 1 ) ); ?>%">
-						<?php printf( __( '%d stars', 'wporg-themes' ), number_format_i18n( $theme->rating / 20 ) ); ?>
-					</div>
-				</div>
-				<span><?php printf( __( '%s out of 5 stars.', 'wporg-themes' ), number_format_i18n( $theme->rating / 20, 1 ) ); ?></span>
-
+			<div class="theme-tags">
+				<h4><?php _e( 'Tags:' ); ?></h4>
 				<?php
-					if ( ! empty( $theme->ratings ) && ! empty( $theme->num_ratings ) ) :
-						foreach ( $theme->ratings as $key => $rate_count ) :
+				foreach( $theme->tags as &$tag ) :
+					$tag = sprintf( '<a href="%1$s">%2$s</a>', esc_url( home_url( "/tag/{$tag}/" ) ), $tag );
+				endforeach;
+				echo implode( ', ', $theme->tags );
 				?>
-				<div class="counter-container">
-					<a href="//wordpress.org/support/view/theme-reviews/<?php echo esc_attr( $theme->slug ); ?>?filter=<?php echo $key; ?>" title="<?php printf( _n( 'Click to see reviews that provided a rating of %d star', 'Click to see reviews that provided a rating of %d stars', $key, 'wporg-themes' ), $key ); ?>">
-						<span class="counter-label"><?php printf( __( '%d stars', 'wporg-themes' ), $key ); ?></span>
-						<span class="counter-back">
-							<span class="counter-bar" style="width: <?php echo 92 * ( $rate_count / $theme->num_ratings ); ?>px;"></span>
-						</span>
-						<span class="counter-count"><?php echo $rate_count; ?></span>
-					</a>
-				</div>
-				<?php
-						endforeach;
-					endif;
-				?>
-			</div><!-- .theme-rating -->
-
-			<div class="theme-devs">
-				<h4><?php _e( 'Development', 'wporg-themes' ); ?></h4>
-				<h5><?php _e( 'Subscribe', 'wporg-themes' ); ?></h5>
-				<ul class="unmarked-list">
-					<li>
-						<a href="//themes.trac.wordpress.org/log/<?php echo esc_attr( $theme->slug ); ?>?limit=100&mode=stop_on_copy&format=rss">
-							<img src="//s.w.org/style/images/feedicon.png" style="vertical-align:text-top;"/>
-							<?php _e( 'Development Log', 'wporg' ); ?>
-						</a>
-					</li>
-				</ul>
-
-				<h5><?php _e( 'Browse the Code', 'wporg-themes' ); ?></h5>
-				<ul class="unmarked-list">
-					<li><a href="//themes.trac.wordpress.org/log/<?php echo esc_attr( $theme->slug ); ?>/" rel="nofollow"><?php _e( 'Development Log', 'wporg-themes' ); ?></a></li>
-					<li><a href="//themes.svn.wordpress.org/<?php echo esc_attr( $theme->slug ); ?>/" rel="nofollow"><?php _e( 'Subversion Repository', 'wporg-themes' ); ?></a></li>
-					<li><a href="//themes.trac.wordpress.org/browser/<?php echo esc_attr( $theme->slug ); ?>/" rel="nofollow"><?php _e( 'Browse in Trac', 'wporg-themes' ); ?></a></li>
-				</ul>
-			</div><!-- .theme-devs -->
+			</div><!-- .theme-tags -->
 
 			<div class="theme-downloads">
 				<h4><?php _e( 'Downloads', 'wporg-themes' ); ?></h4>
@@ -141,16 +98,59 @@
 				</script>
 				<p class="total-downloads"><?php printf( __( 'Total downloads: %s' ), '<strong>' . number_format_i18n( $theme->downloaded ) . '</strong>' ); ?></p>
 			</div><!-- .theme-downloads -->
+		</div><!-- .theme-info -->
 
-			<div class="theme-tags">
-				<h4><?php _e( 'Tags:' ); ?></h4>
-				<?php
-					foreach( $theme->tags as &$tag ) :
-						$tag = sprintf( '<a href="%1$s">%2$s</a>', esc_url( home_url( "/tag/{$tag}/" ) ), $tag );
-					endforeach;
-					echo implode( ', ', $theme->tags );
-				?>
-			</div><!-- .theme-tags -->
-		</div>
+		<div class="theme-meta">
+			<div class="theme-ratings" itemprop="aggregateRating" itemscope itemtype="http://schema.org/AggregateRating">
+				<meta itemprop="ratingValue" content="<?php echo esc_attr( number_format_i18n( $theme->rating / 20, 1 ) ); ?>"/>
+				<meta itemprop="ratingCount" content="<?php echo esc_attr( $theme->num_ratings ); ?>"/>
+				<h4><?php _e( 'Ratings', 'wporg-themes' ); ?></h4>
+
+				<div class="rating">
+					<div class="star-holder">
+						<div class="star-rating" style="width: <?php echo esc_attr( number_format_i18n( $theme->rating, 1 ) ); ?>%">
+							<?php printf( __( '%d stars', 'wporg-themes' ), number_format_i18n( $theme->rating / 20 ) ); ?>
+						</div>
+					</div>
+					<p class="description"><?php printf( __( '%s out of 5 stars.', 'wporg-themes' ), number_format_i18n( $theme->rating / 20, 1 ) ); ?></p>
+				</div>
+
+				<?php if ( ! empty( $theme->ratings ) && ! empty( $theme->num_ratings ) ) : ?>
+				<ul>
+					<?php foreach ( $theme->ratings as $key => $rate_count ) : ?>
+					<li class="counter-container">
+						<a href="//wordpress.org/support/view/theme-reviews/<?php echo esc_attr( $theme->slug ); ?>?filter=<?php echo $key; ?>" title="<?php printf( _n( 'Click to see reviews that provided a rating of %d star', 'Click to see reviews that provided a rating of %d stars', $key, 'wporg-themes' ), $key ); ?>">
+							<span class="counter-label"><?php printf( __( '%d stars', 'wporg-themes' ), $key ); ?></span>
+							<span class="counter-back">
+								<span class="counter-bar" style="width: <?php echo 100 * ( $rate_count / $theme->num_ratings ); ?>px;"></span>
+							</span>
+							<span class="counter-count"><?php echo $rate_count; ?></span>
+						</a>
+					</li>
+					<?php endforeach; ?>
+				</ul>
+				<?php endif; ?>
+			</div><!-- .theme-rating -->
+
+			<div class="theme-devs">
+				<h4><?php _e( 'Development', 'wporg-themes' ); ?></h4>
+				<h5><?php _e( 'Subscribe', 'wporg-themes' ); ?></h5>
+				<ul class="unmarked-list">
+					<li>
+						<a href="//themes.trac.wordpress.org/log/<?php echo esc_attr( $theme->slug ); ?>?limit=100&mode=stop_on_copy&format=rss">
+							<img src="//s.w.org/style/images/feedicon.png" />
+							<?php _e( 'Development Log', 'wporg' ); ?>
+						</a>
+					</li>
+				</ul>
+
+				<h5><?php _e( 'Browse the Code', 'wporg-themes' ); ?></h5>
+				<ul class="unmarked-list">
+					<li><a href="//themes.trac.wordpress.org/log/<?php echo esc_attr( $theme->slug ); ?>/" rel="nofollow"><?php _e( 'Development Log', 'wporg-themes' ); ?></a></li>
+					<li><a href="//themes.svn.wordpress.org/<?php echo esc_attr( $theme->slug ); ?>/" rel="nofollow"><?php _e( 'Subversion Repository', 'wporg-themes' ); ?></a></li>
+					<li><a href="//themes.trac.wordpress.org/browser/<?php echo esc_attr( $theme->slug ); ?>/" rel="nofollow"><?php _e( 'Browse in Trac', 'wporg-themes' ); ?></a></li>
+				</ul>
+			</div><!-- .theme-devs -->
+		</div><!-- .theme-meta -->
 	</div>
 </div>
--- wordpress.org/public_html/wp-content/themes/pub/wporg-themes/view-templates/theme-single.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-themes/view-templates/theme-single.php
@@ -6,131 +6,141 @@
 			<button class="left dashicons dashicons-no"><span class="screen-reader-text"><?php _e( 'Show previous theme' ); ?></span></button>
 			<button class="right dashicons dashicons-no"><span class="screen-reader-text"><?php _e( 'Show next theme' ); ?></span></button>
 		</div>
-		<div class="theme-about">
+
+		<div class="theme-about hentry">
 			<# if ( data.is_outdated ) { #>
 			<div class="theme-notice notice notice-warning">
 				<p><?php _e( 'This theme <strong>hasn&#146;t been updated in over 2 years</strong>. It may no longer be maintained or supported and may have compatibility issues when used with more recent versions of WordPress.', 'wporg-themes' ); ?></p>
 			</div><!-- .theme-notice -->
 			<# } #>
 
-				<div class="theme-screenshots">
-					<# if ( data.screenshot_url ) { #>
-						<div class="screenshot"><img src="{{ data.screenshot_url }}?w=732&strip=all" alt=""/></div>
-					<# } else { #>
-						<div class="screenshot blank"></div>
-					<# } #>
+			<div class="theme-head">
+				<h3 class="theme-name entry-title">{{{ data.name }}}</h3>
+				<h4 class="theme-author"><?php printf( __( 'By %s' ), '<a href="https://profiles.wordpress.org/{{ data.author }}"><span class="author">{{{ data.author }}}</span></a>' ); ?></h4>
 
+				<div class="theme-actions">
+					<a href="{{{ data.preview_url }}}" class="button button-secondary"><?php _e( 'Preview' ); ?></a>
+					<a href="//downloads.wordpress.org/theme/{{ data.slug }}.{{ data.version }}.zip" class="button button-primary"><?php _e( 'Download' ); ?></a>
+				</div>
 
-					<div class="theme-actions">
-						<a href="//downloads.wordpress.org/theme/{{ data.slug }}.{{ data.version }}.zip" class="button button-primary"><?php _e( 'Download' ); ?></a>
-						<a href="{{{ data.preview_url }}}" class="button button-secondary"><?php _e( 'Preview' ); ?></a>
-					</div>
-				</div><!-- .theme-screenshot -->
+				<# if ( data.parent ) { #>
+				<div class="theme-notice notice notice-info">
+					<p class="parent"><?php printf( __( 'This is a child theme of %s.' ), sprintf( '<a href="/%1$s">%2$s</a>', '{{{ data.parent.slug }}}', '{{{ data.parent.name }}}' ) ); ?></p>
+				</div>
+				<# } #>
+			</div><!-- .theme-head -->
 
-				<div class="theme-info">
-				<div class="theme hentry">
-					<h3 class="theme-name entry-title">{{{ data.name }}}</h3>
-					<h4 class="theme-author"><?php printf( __( 'By %s' ), '<a href="https://profiles.wordpress.org/{{ data.author }}"><span class="author">{{{ data.author }}}</span></a>' ); ?></h4>
+			<div class="theme-info">
+				<# if ( data.screenshot_url ) { #>
+				<div class="screenshot"><img src="{{ data.screenshot_url }}?w=798&strip=all" alt=""/></div>
+				<# } else { #>
+				<div class="screenshot blank"></div>
+				<# } #>
 
-					<p class="theme-description entry-summary">{{{ data.description }}}</p>
+				<div class="theme-description entry-summary"><p>{{{ data.description }}}</p></div>
 
-					<# if ( data.parent ) { #>
-					<div class="theme-notice notice notice-info">
-						<p class="parent"><?php printf( __( 'This is a child theme of %s.' ), sprintf( '<a href="/%1$s">%2$s</a>', '{{{ data.parent.slug }}}', '{{{ data.parent.name }}}' ) ); ?></p>
-					</div>
-					<# } #>
-				</div><!-- .theme-info -->
-
-				<div class="theme-ratings" itemprop="aggregateRating" itemscope itemtype="http://schema.org/AggregateRating">
-					<meta itemprop="ratingValue" content="{{ (data.rating/20).toFixed(1) }}"/>
-					<meta itemprop="ratingCount" content="{{ data.num_ratings }}"/>
-					<h4><?php _e( 'Ratings', 'wporg-themes' ); ?></h4>
-					<div class="star-holder">
-						<div class="star-rating" style="width: {{ (data.rating).toFixed(1) }}%"><?php printf( __( '%d stars', 'wporg-themes' ), '{{ Math.round( data.rating ) }}' ); ?></div>
-					</div>
-					<span><?php printf( __( '%s out of 5 stars.', 'wporg-themes' ), '{{ (data.rating/20).toFixed(1) }}' ); ?></span>
-
-					<# if ( data.ratings ) { #>
-						<div class="counter-container">
-							<a href="//wordpress.org/support/view/theme-reviews/{{ data.id }}?filter=5" title="<?php printf( __( 'Click to see reviews that provided a rating of %d stars', 'wporg-themes' ), 5 ); ?>">
-								<span class="counter-label"><?php printf( __( '%d stars', 'wporg-themes' ), 5 ); ?></span>
-								<span class="counter-back">
-									<span class="counter-bar" style="width: {{ 92 * data.ratings[5] / data.num_ratings }}px;"></span>
-								</span>
-								<span class="counter-count">{{ data.ratings[5] }}</span>
-							</a>
-						</div>
-						<div class="counter-container">
-							<a href="//wordpress.org/support/view/theme-reviews/{{ data.id }}?filter=4" title="<?php printf( __( 'Click to see reviews that provided a rating of %d stars', 'wporg-themes' ), 4 ); ?>">
-								<span class="counter-label"><?php printf( __( '%d stars', 'wporg-themes' ), 4 ); ?></span>
-								<span class="counter-back">
-									<span class="counter-bar" style="width: {{ 92 * data.ratings[4] / data.num_ratings }}px;"></span>
-								</span>
-								<span class="counter-count">{{ data.ratings[4] }}</span>
-							</a>
-						</div>
-						<div class="counter-container">
-							<a href="//wordpress.org/support/view/theme-reviews/{{ data.id }}?filter=3" title="<?php printf( __( 'Click to see reviews that provided a rating of %d stars', 'wporg-themes' ), 3 ); ?>">
-								<span class="counter-label"><?php printf( __( '%d stars', 'wporg-themes' ), 3 ); ?></span>
-								<span class="counter-back">
-									<span class="counter-bar" style="width: {{ 92 * data.ratings[3] / data.num_ratings }}px;"></span>
-								</span>
-							</a>
-							<span class="counter-count">{{ data.ratings[3] }}</span>
-						</div>
-						<div class="counter-container">
-							<a href="//wordpress.org/support/view/theme-reviews/{{ data.id }}?filter=2" title="<?php printf( __( 'Click to see reviews that provided a rating of %d stars', 'wporg-themes' ), 2 ); ?>">
-								<span class="counter-label"><?php printf( __( '%d stars', 'wporg-themes' ), 2 ); ?></span>
-								<span class="counter-back">
-									<span class="counter-bar" style="width: {{ 92 * data.ratings[2] / data.num_ratings }}px;"></span>
-								</span>
-								<span class="counter-count">{{ data.ratings[2] }}</span>
-							</a>
-						</div>
-						<div class="counter-container">
-							<a href="//wordpress.org/support/view/theme-reviews/{{ data.id }}?filter=1" title="<?php printf( __( 'Click to see reviews that provided a rating of %d stars', 'wporg-themes' ), 1 ); ?>">
-								<span class="counter-label"><?php printf( __( '%d stars', 'wporg-themes' ), 1 ); ?></span>
-								<span class="counter-back">
-									<span class="counter-bar" style="width: {{ 92 * data.ratings[1] / data.num_ratings }}px;"></span>
-								</span>
-								<span class="counter-count">{{ data.ratings[1] }}</span>
-							</a>
-						</div>
-					<# } #>
-				</div><!-- .theme-rating -->
-
-					<div class="theme-devs">
-						<h4><?php _e( 'Development', 'wporg-themes' ); ?></h4>
-						<h5><?php _e( 'Subscribe', 'wporg-themes' ); ?></h5>
-						<ul class="unmarked-list">
-							<li>
-								<a href="//themes.trac.wordpress.org/log/{{data.id}}?limit=100&mode=stop_on_copy&format=rss">
-									<img src="//s.w.org/style/images/feedicon.png" style="vertical-align:text-top;" />
-									<?php _e( 'Development Log', 'wporg' ); ?>
-								</a>
-							</li>
-						</ul>
-
-						<h5><?php _e( 'Browse the Code', 'wporg-themes' ); ?></h5>
-						<ul class="unmarked-list">
-							<li><a href="//themes.trac.wordpress.org/log/{{data.id}}/" rel="nofollow"><?php _e( 'Development Log', 'wporg-themes' ); ?></a></li>
-							<li><a href="//themes.svn.wordpress.org/{{data.id}}/" rel="nofollow"><?php _e( 'Subversion Repository', 'wporg-themes' ); ?></a></li>
-							<li><a href="//themes.trac.wordpress.org/browser/{{data.id}}/" rel="nofollow"><?php _e( 'Browse in Trac', 'wporg-themes' ); ?></a></li>
-						</ul>
-					</div><!-- .theme-devs -->
+				<# if ( data.tags ) { #>
+				<div class="theme-tags">
+					<h4><?php _e( 'Tags:' ); ?></h4>
+					{{{ data.tags }}}
+				</div><!-- .theme-tags -->
+				<# } #>
 
 				<div class="theme-downloads">
 					<h4><?php _e( 'Downloads Per Day' ); ?></h4>
 					<div id="theme-download-stats-{{data.id}}" class="chart"></div>
 					<p class="total-downloads"><?php printf( __( 'Total downloads: %s' ), '<strong>{{ new Number(data.downloaded).toLocaleString() }}</strong>' ); ?></p>
 				</div><!-- .theme-downloads -->
+			</div>
 
-				<# if ( data.tags ) { #>
-					<div class="theme-tags">
-						<h4><?php _e( 'Tags:' ); ?></h4>
-						{{{ data.tags }}}
-					</div><!-- .theme-tags -->
-				<# } #>
+			<div class="theme-meta">
+				<div class="theme-ratings" itemprop="aggregateRating" itemscope itemtype="http://schema.org/AggregateRating">
+					<meta itemprop="ratingValue" content="{{ (data.rating/20).toFixed(1) }}"/>
+					<meta itemprop="ratingCount" content="{{ data.num_ratings }}"/>
+					<h4><?php _e( 'Ratings', 'wporg-themes' ); ?></h4>
+					<div class="rating">
+						<div class="star-holder">
+							<div class="star-rating" style="width: {{ (data.rating).toFixed(1) }}%"><?php printf( __( '%d stars', 'wporg-themes' ), '{{ Math.round( data.rating ) }}' ); ?></div>
+						</div>
+						<p class="description"><?php printf( __( '%s out of 5 stars.', 'wporg-themes' ), '{{ (data.rating/20).toFixed(1) }}' ); ?></p>
+					</div>
+
+					<# if ( data.ratings ) { #>
+					<ul>
+						<li class="counter-container">
+							<a href="//wordpress.org/support/view/theme-reviews/{{ data.id }}?filter=5" title="<?php printf( __( 'Click to see reviews that provided a rating of %d stars', 'wporg-themes' ), 5 ); ?>">
+								<span class="counter-label"><?php printf( __( '%d stars', 'wporg-themes' ), 5 ); ?></span>
+								<span class="counter-back">
+									<span class="counter-bar" style="width: {{ 100 * data.ratings[5] / data.num_ratings }}%;"></span>
+								</span>
+								<span class="counter-count">{{ data.ratings[5] }}</span>
+							</a>
+						</li>
+						<li class="counter-container">
+							<a href="//wordpress.org/support/view/theme-reviews/{{ data.id }}?filter=4" title="<?php printf( __( 'Click to see reviews that provided a rating of %d stars', 'wporg-themes' ), 4 ); ?>">
+								<span class="counter-label"><?php printf( __( '%d stars', 'wporg-themes' ), 4 ); ?></span>
+								<span class="counter-back">
+									<span class="counter-bar" style="width: {{ 100 * data.ratings[4] / data.num_ratings }}%;"></span>
+								</span>
+								<span class="counter-count">{{ data.ratings[4] }}</span>
+							</a>
+						</li>
+						<li class="counter-container">
+							<a href="//wordpress.org/support/view/theme-reviews/{{ data.id }}?filter=3" title="<?php printf( __( 'Click to see reviews that provided a rating of %d stars', 'wporg-themes' ), 3 ); ?>">
+								<span class="counter-label"><?php printf( __( '%d stars', 'wporg-themes' ), 3 ); ?></span>
+								<span class="counter-back">
+									<span class="counter-bar" style="width: {{ 100 * data.ratings[3] / data.num_ratings }}%;"></span>
+								</span>
+								<span class="counter-count">{{ data.ratings[3] }}</span>
+							</a>
+						</li>
+						<li class="counter-container">
+							<a href="//wordpress.org/support/view/theme-reviews/{{ data.id }}?filter=2" title="<?php printf( __( 'Click to see reviews that provided a rating of %d stars', 'wporg-themes' ), 2 ); ?>">
+								<span class="counter-label"><?php printf( __( '%d stars', 'wporg-themes' ), 2 ); ?></span>
+								<span class="counter-back">
+									<span class="counter-bar" style="width: {{ 100 * data.ratings[2] / data.num_ratings }}%;"></span>
+								</span>
+								<span class="counter-count">{{ data.ratings[2] }}</span>
+							</a>
+						</li>
+						<li class="counter-container">
+							<a href="//wordpress.org/support/view/theme-reviews/{{ data.id }}?filter=1" title="<?php printf( __( 'Click to see reviews that provided a rating of %d stars', 'wporg-themes' ), 1 ); ?>">
+								<span class="counter-label"><?php printf( __( '%d stars', 'wporg-themes' ), 1 ); ?></span>
+								<span class="counter-back">
+									<span class="counter-bar" style="width: {{ 100 * data.ratings[1] / data.num_ratings }}%;"></span>
+								</span>
+								<span class="counter-count">{{ data.ratings[1] }}</span>
+							</a>
+						</li>
+					</ul>
+					<# } #>
+				</div><!-- .theme-rating -->
+
+				<div class="theme-devs">
+					<h4><?php _e( 'Development', 'wporg-themes' ); ?></h4>
+					<h5><?php _e( 'Subscribe', 'wporg-themes' ); ?></h5>
+					<ul class="unmarked-list">
+						<li>
+							<a href="//themes.trac.wordpress.org/log/{{data.id}}?limit=100&mode=stop_on_copy&format=rss">
+								<img src="//s.w.org/style/images/feedicon.png" />
+								<?php _e( 'Development Log', 'wporg' ); ?>
+							</a>
+						</li>
+					</ul>
+
+					<h5><?php _e( 'Browse the Code', 'wporg-themes' ); ?></h5>
+					<ul class="unmarked-list">
+						<li><a href="//themes.trac.wordpress.org/log/{{data.id}}/" rel="nofollow"><?php _e( 'Development Log', 'wporg-themes' ); ?></a></li>
+						<li><a href="//themes.svn.wordpress.org/{{data.id}}/" rel="nofollow"><?php _e( 'Subversion Repository', 'wporg-themes' ); ?></a></li>
+						<li><a href="//themes.trac.wordpress.org/browser/{{data.id}}/" rel="nofollow"><?php _e( 'Browse in Trac', 'wporg-themes' ); ?></a></li>
+					</ul>
+				</div><!-- .theme-devs -->
+
+				<div class="theme-support">
+					<h4><?php _e( 'Support', 'wporg-themes' ); ?></h4>
+					<p><?php _e( 'Got something to say? Neeed help?', 'wporg-themes' ); ?></p>
+					<a href="//wordpress.org/support/theme/{{ data.slug }}" class="button button-secondary"><?php _e( 'View support forum', 'wporg-themes' ); ?></a>
+				</div><!-- .theme-support -->
 			</div>
 		</div>
 	</div>
