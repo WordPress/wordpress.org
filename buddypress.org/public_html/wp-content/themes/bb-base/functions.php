a0eefc777919b5b796c38901dd839b6c09bb8e73
John James Jacoby <johnjamesjacoby@git.wordpress.org>
2015-02-16 21:40:38 +0000

    BuddyPress.org: Reorder @media queries and force widths for desktop sizes. See #691.
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@1260 74240141-8908-4e6f-9713-ba540dce6ec7

--- buddypress.org/public_html/wp-content/themes/bb-base/functions.php
+++ buddypress.org/public_html/wp-content/themes/bb-base/functions.php
@@ -33,7 +33,7 @@ if ( bb_base_is_codex() ) {
 function bb_base_register_stylesheets() {
 
 	// Version of CSS
-	$version = '20150216a';
+	$version = '20150216b';
 
 	// Base theme styling
 	wp_enqueue_style( 'bb-base',   get_template_directory_uri()   . '/style.css', false,                         $version, 'screen' );
