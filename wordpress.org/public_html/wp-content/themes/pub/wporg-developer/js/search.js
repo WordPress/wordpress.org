fa73769687246c2d0e0deb6678c4b938e6428cd3
Scott Reilly <coffee2code@git.wordpress.org>
2015-02-25 20:42:52 +0000

    developer.wordpress.org: Give focus to search input after revealing search bar. Fixes #882
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@1326 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-developer/js/search.js
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-developer/js/search.js
@@ -1,8 +1,26 @@
 ( function( $ ) {
-	// Toggle search
-	$( '#inner-search .search-section' ).toggle();
 
-	$( '#inner-search #inner-search-icon' ).on( 'click', function() {
+	function toggle_search_bar() {
+		// The search input field.
+		var search_input = $( '#inner-search .search-section .search-field' );
+
+		// Toggle display.
 		$( '#inner-search .search-section' ).toggle();
+
+		// Give input field focus if it is now visible, remove focus otherwise.
+		if ( search_input.is(':visible') ) {
+			search_input.focus();
+		} else {
+			search_input.blur();
+		}
+	}
+
+	// Toggle search bar on page load (it's shown by default).
+	toggle_search_bar();
+
+	// Toggle search bar when icon is clicked.
+	$( '#inner-search #inner-search-icon' ).on( 'click', function() {
+		toggle_search_bar();
 	});
+
 } )( jQuery );
