6dae8c1a2f2e6fad278c19a7bd6cd9158655de5e
Konstantin Obenland <obenland@git.wordpress.org>
2015-03-20 16:12:56 +0000

    WP.org Themes: Move action link outside of theme link.
    Nested anchors are a no-no and can cause layout bugs in older browsers.
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@1440 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-themes/content.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-themes/content.php
@@ -12,9 +12,8 @@ $theme = new WPORG_Themes_Repo_Package( $post );
 			<?php printf( _x( 'By %s', 'post author', 'wporg-themes' ), '<span class="author vcard">' . esc_html( get_the_author() ) . '</span>' ); ?>
 		</div>
 		<h3 class="theme-name entry-title"><?php the_title(); ?></h3>
-
-		<div class="theme-actions">
-			<a class="button button-primary preview install-theme-preview" href="<?php echo esc_url( '//downloads.wordpress.org/theme/' . $post->post_name . '.' . $theme->latest_version() . '.zip' ); ?>"><?php esc_html_e( 'Download' ); ?></a>
-		</div>
 	</a>
+	<div class="theme-actions">
+		<a class="button button-primary preview install-theme-preview" href="<?php echo esc_url( '//downloads.wordpress.org/theme/' . $post->post_name . '.' . $theme->latest_version() . '.zip' ); ?>"><?php esc_html_e( 'Download' ); ?></a>
+	</div>
 </article>
--- wordpress.org/public_html/wp-content/themes/pub/wporg-themes/view-templates/theme.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-themes/view-templates/theme.php
@@ -10,9 +10,8 @@
 		<span class="more-details"><?php _ex( 'More Info', 'theme' ); ?></span>
 		<div class="theme-author"><?php printf( __( 'By %s' ), '<span class="author">{{ data.author.display_name }}</span>' ); ?></div>
 		<h3 class="theme-name entry-title">{{{ data.name }}}</h3>
-
-		<div class="theme-actions">
-			<a class="button button-primary preview install-theme-preview" href="//downloads.wordpress.org/theme/{{ data.slug }}.{{ data.version }}.zip"><?php esc_html_e( 'Download' ); ?></a>
-		</div>
 	</a>
+	<div class="theme-actions">
+		<a class="button button-primary preview install-theme-preview" href="//downloads.wordpress.org/theme/{{ data.slug }}.{{ data.version }}.zip"><?php esc_html_e( 'Download' ); ?></a>
+	</div>
 </script>
