be523d463e25781242906a8e01642e65ba777a34
Dominik Schilling (ocean90) <ocean90@git.wordpress.org>
2015-04-23 21:05:50 +0000

    Rosetta: Remove an obsolete hint.
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@1507 74240141-8908-4e6f-9713-ba540dce6ec7

--- global.wordpress.org/public_html/wp-content/mu-plugins/downloads/rosetta-downloads.php
+++ global.wordpress.org/public_html/wp-content/mu-plugins/downloads/rosetta-downloads.php
@@ -155,7 +155,7 @@ class Rosetta_Downloads {
 					<tr>
 						<th scope="col" style="width:80px"><?php _e( 'Locale', 'rosetta' ); ?></th>
 						<th scope="col" style="text-align:right"><?php _e( 'Release Package', 'rosetta' ); ?></th>
-						<th scope="col" style="text-align:right"><?php _e( 'Language Pack', 'rosetta' ); ?> <abbr title="Since March 11, 2015.">*</abbr></th>
+						<th scope="col" style="text-align:right"><?php _e( 'Language Pack', 'rosetta' ); ?>></th>
 					</tr>
 				</thead>
 
@@ -178,7 +178,7 @@ class Rosetta_Downloads {
 					<tr>
 						<th scope="col" style="width:80px"><?php _e( 'Locale', 'rosetta' ); ?></th>
 						<th scope="col" style="text-align:right"><?php _e( 'Release Package', 'rosetta' ); ?></th>
-						<th scope="col" style="text-align:right"><?php _e( 'Language Pack', 'rosetta' ); ?> <abbr title="Since March 11, 2015.">*</abbr></th>
+						<th scope="col" style="text-align:right"><?php _e( 'Language Pack', 'rosetta' ); ?></th>
 					</tr>
 				</tfoot>
 			</table>
