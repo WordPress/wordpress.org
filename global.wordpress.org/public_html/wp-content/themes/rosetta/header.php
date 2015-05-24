6235941cbbd3fd3a6cf1380ccc1c83f113818822
Dominik Schilling (ocean90) <ocean90@git.wordpress.org>
2015-05-24 21:47:38 +0000

    Rosetta Theme: Update RTL stylesheet.
    This is the LTR stylesheet run through grunt-rtlcss.
    
    see #1049.
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@1619 74240141-8908-4e6f-9713-ba540dce6ec7

--- global.wordpress.org/public_html/wp-content/themes/rosetta/header.php
+++ global.wordpress.org/public_html/wp-content/themes/rosetta/header.php
@@ -1,14 +1,14 @@
 <?php
 
-wp_enqueue_style( 'rosetta', get_stylesheet_uri(), array(), 17, 'screen' );
-
+$stylesheet = get_stylesheet_uri();
 if ( is_rtl() ) {
-	wp_enqueue_style( 'rosetta-rtl', get_locale_stylesheet_uri(), array( 'rosetta' ), 2, 'screen' );
+	$stylesheet = str_replace( '.css', '-rtl.css', $stylesheet );
 }
+
+wp_enqueue_style( 'rosetta', $stylesheet, array(), 17 );
 
 if ( is_locale_css() ) {
 	wp_enqueue_style( 'rosetta-locale', get_locale_css_url(), array(), 1 );
 }
 
 require WPORGPATH . 'header.php';
-
