e14cfc5f2fd43e58be5ca95a4de49a6f49c37bb3
Dominik Schilling (ocean90) <ocean90@git.wordpress.org>
2015-05-26 18:47:30 +0000

    Rosetta Theme: Add comment reply script.
    see #1049.
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@1630 74240141-8908-4e6f-9713-ba540dce6ec7

--- global.wordpress.org/public_html/wp-content/themes/rosetta/header.php
+++ global.wordpress.org/public_html/wp-content/themes/rosetta/header.php
@@ -11,4 +11,8 @@ if ( is_locale_css() ) {
 	wp_enqueue_style( 'rosetta-locale', get_locale_css_url(), array(), 1 );
 }
 
+if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+	wp_enqueue_script( 'comment-reply' );
+}
+
 require WPORGPATH . 'header.php';
