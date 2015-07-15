5374f49394f4da935d3105cb22cc0cdb0e6b001a
Scott Reilly <coffee2code@git.wordpress.org>
2015-07-15 21:28:34 +0000

    W.org P2 child theme: Use the handbook landing page title for the title instead of a hardcoded 'Handbook'.
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@1742 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-p2/archive-handbook.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-p2/archive-handbook.php
@@ -15,7 +15,7 @@
 <div class="sleeve_main">
 
 	<div id="main">
-		<h2><?php _e( 'Handbook', 'p2' ); ?></h2>
+		<h2><?php the_title(); ?></h2>
 		<?php if ( have_posts() ) : ?>
 
 			<ul id="postlist">
