d776c84cced81a9cb551e8ec04b3d54612674572
Dominik Schilling (ocean90) <ocean90@git.wordpress.org>
2015-07-18 14:52:50 +0000

    Translate: Adjust route unregistration for new API in GlotPress.
    see #1124.
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@1752 74240141-8908-4e6f-9713-ba540dce6ec7

--- translate.wordpress.org/includes/gp-plugins/wporg-specifics.php
+++ translate.wordpress.org/includes/gp-plugins/wporg-specifics.php
@@ -10,21 +10,20 @@ class GP_WPorg_Specifics extends GP_Plugin {
 		parent::__construct();
 		$this->add_action( 'before_login_form' );
 		$this->add_filter( 'gp_url_profile', array( 'args' => 2 ) );
-		$this->add_filter( 'routes' );
+		$this->add_action( 'init' );
 		$this->add_filter( 'projects', array( 'args' => 2 ) );
 	}
 
 	function before_login_form() {
-		echo '<span class="secondary">' . __( 'Log in with your wordpress.org forums account. If you don&#8217;t have one, you can <a href="http://wordpress.org/support/register.php">register at the forums.</a>' ) . '</span>';
+		echo '<span class="secondary">' . __( 'Log in with your wordpress.org forums account. If you don&#8217;t have one, you can <a href="https://wordpress.org/support/register.php">register at the forums.</a>' ) . '</span>';
 	}
 
 	function gp_url_profile( $url, $nicename ) {
 		return 'https://profiles.wordpress.org/' . $nicename;
 	}
 
-	function routes( $routes ) {
-		unset( $routes['get:/profile/(.+?)'] );
-		return $routes;
+	function init() {
+		GP::$router->remove( '/profile/(.+?)' );
 	}
 
 	function projects( $sub_projects, $parent_id ) {
