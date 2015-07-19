b68f4cafa4554a7f9581aaa0af910b92ab805e71
Dominik Schilling (ocean90) <ocean90@git.wordpress.org>
2015-07-19 19:13:25 +0000

    Forums Theme: Fix Log Out URL and add some escaping.
    see #1126.
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@1754 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-forums/header.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-forums/header.php
@@ -2,20 +2,20 @@
 
 <div id="headline">
 	<div class="wrapper">
-		<h2><a href="<?php echo home_url( '/' ); ?>"><?php _e( 'Forums', 'wporg-forums' ); ?></a></h2>
+		<h2><a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php _e( 'Forums', 'wporg-forums' ); ?></a></h2>
 		<p class="login">
 			<?php if ( is_user_logged_in() ) : ?>
-				<?php echo sprintf( __( 'Howdy, %s', 'wporg-forums' ), '<a href="' . bbp_get_user_profile_url( bbp_get_current_user_id() ) . '">' . bbp_get_current_user_name() . '</a>' ); ?>
+				<?php echo sprintf( __( 'Howdy, %s', 'wporg-forums' ), '<a href="' . esc_url( bbp_get_user_profile_url( bbp_get_current_user_id() ) ) . '">' . bbp_get_current_user_name() . '</a>' ); ?>
 
 				<small>(
 				<?php if ( bbp_is_user_keymaster() ) : ?>
 					<a href="<?php echo esc_url( admin_url() ); ?>"><?php _e( 'Admin', 'wporg-forums' ); ?></a> |
 				<?php endif; ?>
-					<a href="<?php bbp_logout_url(); ?>"><?php _e( 'Sign Out', 'wporg-forums' ); ?></a>
+					<a href="<?php echo esc_url( wp_logout_url() ); ?>"><?php _e( 'Log Out', 'wporg-forums' ); ?></a>
 				)</small><br>
 			<?php else : ?>
 				<?php $current_url = esc_url_raw( 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] ); ?>
-				<a href="https://wordpress.org/support/bb-login.php?redirect_to=<?php echo urlencode( $current_url ); ?>"><?php _e( 'Login', 'wporg-forums' ); ?></a>
+				<a href="https://wordpress.org/support/bb-login.php?redirect_to=<?php echo urlencode( $current_url ); ?>"><?php _e( 'Log In', 'wporg-forums' ); ?></a>
 			<?php endif; ?>
 		</p>
 	</div>
