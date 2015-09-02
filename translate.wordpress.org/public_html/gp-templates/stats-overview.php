30e83e1d7eaf6af5aeedd90a0d458619a5518eb6
Dion Hulse <dd32@git.wordpress.org>
2015-09-02 22:38:03 +0000

    Translate: Stats overview: Update the styles. Props sam. See #1202
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@1859 74240141-8908-4e6f-9713-ba540dce6ec7

--- translate.wordpress.org/public_html/gp-templates/stats-overview.php
+++ translate.wordpress.org/public_html/gp-templates/stats-overview.php
@@ -7,62 +7,59 @@ $breadcrumb[] = __( 'Translation status overview' );
 gp_breadcrumb( $breadcrumb );
 gp_tmpl_header();
 
-function get_rgb_for_percent( $percent ) {
-	// This function does 100% = Green, < 100% = Red~Yellow
-	if ( $percent == 100 ) {
-		return 'rgb(0,153,0)';
-	}
-	$min = 0;
-	$max = 153;
-	$num = floor( $min + ( ($max - $min) * ( $percent/100 ) ) );
-	return "rgb(153,$num,0)";
-}
 
 ?>
-<div class="table">
-	<style>
-		<?php /* Temporary styling */ ?>
-		table.table {
-			text-align: center;
-		}
-		table.table thead th {
-			text-align: center;
-			font-weight: bold;
-		}
-		table.table .none {
-			color: #ccc;
-		}
-		table.table tbody th {
-			text-align: left;
-		}
-	</style>
+<div class="stats-table">
 	<table class="table">
 		<thead>
 			<tr>
-				<th><?php _e( 'Language' ); ?></th>
+				<th class="title"><?php _e( 'Locale' ); ?></th>
 				<?php foreach ( $projects as $slug => $project ) : ?>
-					<th><?php echo esc_html( str_replace( 'WordPress.org ', '', $project->name ) ); ?></th>
+					<th><?php
+						$namereplace = array( 'WordPress.org ', 'WordPress for ', 'WordPress ', 'ectory' );
+						echo esc_html( str_replace( $namereplace, '', $project->name ) );
+					?></th>
 				<?php endforeach; ?>
 			</tr>
 		</thead>
 		<tbody>
-			<?php foreach ( $translation_locale_complete as $locale_slug => $total_complete ) :
+			<?php
+			foreach ( $translation_locale_complete as $locale_slug => $total_complete ) :
 				$gp_locale = GP_Locales::by_slug( $locale_slug );
+				$set_slug = 'default';
 				// Variants (de/formal for example) don't have GP_Locales in this context
+				if ( ! $gp_locale && ( list( $base_locale_slug, $set_slug ) = explode( '/', $locale_slug ) ) ) {
+					$gp_locale = clone GP_Locales::by_slug( $base_locale_slug );
+					// Just append it for now..
+					$gp_locale->wp_locale .= '/' . $set_slug;
+				}
+				if ( ! $gp_locale || ! $gp_locale->wp_locale ) {
+					continue;
+				}
 			?>
 				<tr>
-					<th title="<?php echo esc_attr( $gp_locale->english_name ?: $locale_slug ); ?>"><?php echo esc_html( $gp_locale->wp_locale ?: $locale_slug ); ?></th>
-					<?php foreach ( $projects as $slug => $project ) :
+					<th title="<?php echo esc_attr( $gp_locale->english_name ); ?>">
+						<a href="<?php echo gp_url_join( 'locale', $gp_locale->slug, $set_slug ); ?>">
+							<?php echo esc_html( $gp_locale->wp_locale ); ?> 
+						</a>
+					</th>
+					<?php
+					foreach ( $projects as $slug => $project ) {
+						$projecturl = gp_url_join( 'locale', $gp_locale->slug, $set_slug, $project->path );
+
 						if ( isset( $translation_locale_statuses[ $locale_slug ][ $project->path ] ) ) {
 							$percent = $translation_locale_statuses[ $locale_slug ][ $project->path ];
-							$percent = '<span style="color:' . get_rgb_for_percent( $percent ) . '">' . $percent . '%</span>';
+
+							$percent_class = 'percent' . (int) ( $percent / 10 ) * 10;
+							echo '<td class="' . $percent_class .'"><a href="' . $projecturl . '">' . $percent . '%</a></td>';
 
 						} else {
-							$percent = '<span class="none">&mdash;</span>';
+							echo '<td class="none">&mdash;</td>';
 						}
+						
+						
+					}
 					?>
-						<td><?php echo $percent; ?></td>
-					<?php endforeach; ?>
 				</tr>
 			<?php endforeach; ?>
 		</tbody>
