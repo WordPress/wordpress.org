b6f95fc8a2da5f400db4be861f649b90bf1c7106
Dominik Schilling (ocean90) <ocean90@git.wordpress.org>
2015-09-18 21:18:06 +0000

    Translate: Bump CSS version for cache bust.
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@1897 74240141-8908-4e6f-9713-ba540dce6ec7

--- translate.wordpress.org/public_html/gp-templates/helper-functions.php
+++ translate.wordpress.org/public_html/gp-templates/helper-functions.php
@@ -1,6 +1,6 @@
 <?php
 
-wp_enqueue_style( 'wporg', gp_url_ssl( gp_url_public_root() ) . 'gp-templates/style.css', array( 'base' ), '20150918' );
+wp_enqueue_style( 'wporg', gp_url_ssl( gp_url_public_root() ) . 'gp-templates/style.css', array( 'base' ), '20150919' );
 
 add_action( 'tmpl_load_locations', function( $locations, $template, $args, $template_path ) {
 	$core_templates = GP_PATH . 'gp-templates/';
