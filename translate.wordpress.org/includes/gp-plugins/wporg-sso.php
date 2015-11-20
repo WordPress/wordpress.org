6da762ba21672404429633dec0fe3fca19ac236c
Stephane Daury (stephdau) <stephdau@git.wordpress.org>
2015-11-20 17:51:34 +0000

    translate.wordpress.org: Adding loader for our SSO plugin.
    Note: disabled for now, still iterating in dev, not live yet.
    
    See #1406
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@2100 74240141-8908-4e6f-9713-ba540dce6ec7

---
+++ translate.wordpress.org/includes/gp-plugins/wporg-sso.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Loads SSO GlotPress plugin from linked external
+ * 
+ * @author stephdau
+ */
+
+/**
+ * NOTE: Not enabled yet, iterating in dev.
+ * 
+ *
+$sso_plugin_path = __DIR__ . '/wporg-sso/gp-plugin.php';
+
+if ( file_exists( $sso_plugin_path ) ){
+	require_once( $sso_plugin_path );
+}
+ *
+ */
