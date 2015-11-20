9ca4988d7c9d197a674c8efa373ce019266c29b0
Stephane Daury (stephdau) <stephdau@git.wordpress.org>
2015-11-20 18:08:34 +0000

    login.wordpress.org: Changing labels to Log In and Log Out (WP's defaults).
    props ocean90
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@2101 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-login/index.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-login/index.php
@@ -13,9 +13,9 @@ get_header();
 		<h1><?php _e( 'Welcome!', 'wporg-login' ); ?></h1>
 		<ul>
 			<?php if ( is_user_logged_in() ) : ?>
-				<li class="button"><a href="<?php echo wp_logout_url(); ?>"><?php _e( 'Logout', 'wporg-login' ); ?></a></li>
+				<li class="button"><a href="<?php echo wp_logout_url(); ?>"><?php _e( 'Log Out' ); ?></a></li>
 			<?php else : ?>
-				<li class="button"><a href="<?php echo wp_login_url(); ?>"><?php _e( 'Login', 'wporg-login' ); ?></a></li>
+				<li class="button"><a href="<?php echo wp_login_url(); ?>"><?php _e( 'Log In' ); ?></a></li>
 				<li class="button"><a href="https://wordpress.org/support/register.php"><?php _e( 'Register', 'wporg-login' ); ?></a></li>
 			<?php endif; ?>
 		</ul>
