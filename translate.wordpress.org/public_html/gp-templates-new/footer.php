594abb5e5a99d9adc86cadf90fff2e264bee13b2
Dominik Schilling (ocean90) <ocean90@git.wordpress.org>
2015-11-22 17:18:58 +0000

    Translate: Don't call `gp_footer()` in the new `footer.php` template.
    See #1352.
    
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@2106 74240141-8908-4e6f-9713-ba540dce6ec7

--- translate.wordpress.org/public_html/gp-templates-new/footer.php
+++ translate.wordpress.org/public_html/gp-templates-new/footer.php
@@ -1,7 +1,6 @@
 	</div><!-- gp-content -->
 	<p id="gp-footer" class="secondary">
 		<?php printf( __('Proudly powered by <a href="%s" title="Found in translation">GlotPress</a>.'), 'http://glotpress.org/' ); ?>
-		<?php gp_footer(); ?>
 	</p>
 <?php require WPORGPATH . 'footer.php';
 
