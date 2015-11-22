d1609940202f87f04d69431ff7ce06ececde9c51
Dominik Schilling (ocean90) <ocean90@git.wordpress.org>
2015-11-22 15:29:18 +0000

    Translate: Copy "gp-templates" to "gp-templates-new".
    "gp-templates-new" is temporary and will be used to apply changes for GlotPress as a plugin.
    
    See #1352.
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@2104 74240141-8908-4e6f-9713-ba540dce6ec7

---
+++ translate.wordpress.org/public_html/gp-templates-new/header.php
@@ -0,0 +1,47 @@
+<?php
+wp_enqueue_style( 'base' );
+wp_enqueue_script( 'jquery' );
+
+global $pagetitle;
+$pagetitle = gp_title();
+require WPORGPATH . 'header.php';
+?>
+	<script type="text/javascript">document.body.className = document.body.className.replace('no-js','js');</script>
+<div id="headline">
+	<div class="wrapper">
+		<h2><a href="//make.wordpress.org/polyglots/">Translating WordPress</a></h2>
+		<span id="hello">
+		<a class="menu-link" href="//make.wordpress.org/polyglots/">Blog</a>
+		<a class="menu-link" href="//make.wordpress.org/polyglots/teams/">Teams</a>
+		<a class="menu-link" href="//make.wordpress.org/polyglots/handbook/">Translator Handbook</a>
+		<?php
+		if (GP::$user->logged_in()):
+			$user = GP::$user->current();
+
+			printf( __('Hi, %s.'), '<a href="'.gp_url( '/profile' ).'">'.$user->user_login.'</a>' );
+			?>
+			<a href="<?php echo gp_url('/logout')?>"><?php _e('Log out'); ?></a>
+		<?php else: ?>
+			<strong><a href="<?php echo gp_url_login(); ?>"><?php _e('Log in'); ?></a></strong>
+		<?php endif; ?>
+		<?php do_action( 'after_hello' ); ?>
+		</span>
+	</div>
+</div>
+<div class="gp-content">
+
+<div id="gp-js-message"></div>
+
+		<?php if (gp_notice('error')): ?>
+			<div class="error">
+				<?php echo gp_notice( 'error' ); //TODO: run kses on notices ?>
+			</div>
+		<?php endif; ?>
+		<?php if (gp_notice()): ?>
+			<div class="notice">
+				<?php echo gp_notice(); ?>
+			</div>
+		<?php endif; ?>
+		<?php echo gp_breadcrumb(); ?>
+		<?php do_action( 'after_notices' ); ?>
+
---
+++ translate.wordpress.org/public_html/gp-templates-new/locale-project.php
@@ -0,0 +1,190 @@
+<?php
+gp_title( sprintf( __( 'Projects translated to %s &lt; GlotPress' ),  esc_html( $locale->english_name ) ) );
+
+$breadcrumb   = array();
+$breadcrumb[] = gp_link_get( '/', __( 'Locales' ) );
+$breadcrumb[] = gp_link_get( gp_url_join( '/locale', $locale_slug, $set_slug ), esc_html( $locale->english_name ) );
+$breadcrumb[] = gp_link_get( gp_url_join( '/locale', $locale_slug, $set_slug, $project->path ), esc_html( $project->name ) );
+$breadcrumb[] = $sub_project->name;
+gp_breadcrumb( $breadcrumb );
+gp_tmpl_header();
+?>
+
+<div class="project-header">
+	<p class="project-description"><?php echo $sub_project->description; ?></p>
+
+	<div class="project-box percent-<?php echo $project_status->percent_complete; ?>">
+		<div class="project-box-header">
+			<div class="project-icon">
+				<?php echo $project_icon; ?>
+			</div>
+
+			<ul class="project-meta">
+				<li class="project-name"><?php echo $sub_project->name; ?></li>
+				<li class="locale-english"><?php echo $locale->english_name; ?></li>
+				<li class="locale-native"><?php echo $locale->native_name; ?></li>
+				<li class="locale-code">
+					<?php
+					echo $locale->wp_locale;
+
+					if ( count( $variants ) > 1 ) {
+						?>
+						<select id="variant-selector" name="variant">
+							<?php
+							foreach ( $variants as $variant ) {
+								$selected =
+								printf(
+									'<option name="%s" data-project-url="%s"%s>%s</option>',
+									$variant,
+									esc_url( gp_url_join( '/locale', $locale_slug, $variant, $sub_project->path ) ),
+									( $set_slug == $variant ) ? ' selected="selected"' : '',
+									ucfirst( $variant )
+								);
+							}
+							?>
+						</select>
+						<?php
+					}
+					?>
+				</li>
+			</ul>
+
+			<div class="project-status">
+				<?php echo $project_status->percent_complete . '%'; ?>
+			</div>
+		</div>
+
+		<div class="project-status-progress percent">
+			<div class="percent-complete" style="width:<?php echo $project_status->percent_complete; ?>%;"></div>
+		</div>
+
+		<div class="project-box-footer">
+			<ul class="projects-dropdown">
+				<li><span>All Sub-Projects</span>
+					<ul>
+						<?php
+						// Show the current project if it has strings.
+						if ( $sub_project_status->all_count ) {
+							printf(
+								'<li><a href="%s">%s <span>%s</span></a>',
+								gp_url_project( $sub_project->path, gp_url_join( $locale->slug, $set_slug ) ),
+								$sub_project->name,
+								$sub_project_status->percent_complete . '%'
+							);
+						}
+
+						foreach ( $sub_projects as $_sub_project ) {
+							$status = $sub_project_statuses[ $_sub_project->id ];
+
+							printf(
+								'<li><a href="%s">%s <span>%s</span></a>',
+								gp_url_project( $_sub_project->path, gp_url_join( $locale->slug, $set_slug ) ),
+								$_sub_project->name,
+								$status->percent_complete . '%'
+							);
+						}
+						?>
+					</ul>
+				</li>
+			</ul>
+		</div>
+	</div>
+</div>
+
+<div class="locale-project">
+	<table class="locale-sub-projects">
+		<thead>
+			<tr>
+				<th class="header"><?php _e( 'Set / Sub Project' ); ?></th>
+				<th><?php _e( 'Translated' ); ?></th>
+				<th><?php _e( 'Fuzzy' ); ?></th>
+				<th><?php _e( 'Untranslated' ); ?></th>
+				<th><?php _e( 'Waiting' ); ?></th>
+			</tr>
+		</thead>
+		<tbody>
+			<?php
+			// Show the current project if it has strings.
+			if ( $sub_project_status->all_count ) {
+				?>
+				<tr>
+					<td class="set-name">
+						<strong><?php gp_link( gp_url_project( $sub_project->path, gp_url_join( $locale->slug, $set_slug ) ), $sub_project->name ); ?></strong>
+						<?php if ( $sub_project_status->percent_complete > 90 ) : ?>
+							<span class="sub-project-status percent-90"><?php echo $sub_project_status->percent_complete; ?>%</span>
+						<?php else : ?>
+							<span class="sub-project-status"><?php echo $sub_project_status->percent_complete; ?>%</span>
+						<?php endif; ?>
+					</td>
+					<td class="stats translated">
+						<?php gp_link( gp_url_project( $sub_project->path, gp_url_join( $locale->slug, $set_slug ), array( 'filters[translated]' => 'yes', 'filters[status]' => 'current') ), absint( $sub_project_status->current_count ) ); ?>
+					</td>
+					<td class="stats fuzzy">
+						<?php gp_link( gp_url_project( $sub_project->path, gp_url_join( $locale->slug, $set_slug ), array( 'filters[status]' => 'fuzzy' ) ), absint( $sub_project_status->fuzzy_count ) ); ?>
+					</td>
+					<td class="stats untranslated">
+						<?php gp_link( gp_url_project( $sub_project->path, gp_url_join( $locale->slug, $set_slug ), array( 'filters[status]' => 'untranslated' ) ), absint( $sub_project_status->all_count ) -  absint( $sub_project_status->current_count ) ); ?>
+					</td>
+					<td class="stats waiting">
+						<?php gp_link( gp_url_project( $sub_project->path, gp_url_join( $locale->slug, $set_slug ), array( 'filters[translated]' => 'yes', 'filters[status]' => 'waiting' ) ), absint( $sub_project_status->waiting_count ) ); ?>
+					</td>
+					</tr>
+				</tr>
+				<?php
+			}
+			?>
+
+			<?php
+			foreach ( $sub_projects as $sub_project ) {
+				$status = $sub_project_statuses[ $sub_project->id ];
+				?>
+				<tr>
+					<td class="set-name">
+						<strong><?php gp_link( gp_url_project( $sub_project->path, gp_url_join( $locale->slug, $set_slug ) ), $sub_project->name ); ?></strong>
+						<?php if ( $status->percent_complete > 90 ) : ?>
+							<span class="sub-project-status percent-90"><?php echo $status->percent_complete; ?>%</span>
+						<?php else : ?>
+							<span class="sub-project-status"><?php echo $status->percent_complete; ?>%</span>
+						<?php endif; ?>
+					</td>
+					<td class="stats translated">
+						<?php gp_link( gp_url_project( $sub_project->path, gp_url_join( $locale->slug, $set_slug ), array( 'filters[translated]' => 'yes', 'filters[status]' => 'current') ), absint( $status->current_count ) ); ?>
+					</td>
+					<td class="stats fuzzy">
+						<?php gp_link( gp_url_project( $sub_project->path, gp_url_join( $locale->slug, $set_slug ), array( 'filters[status]' => 'fuzzy' ) ), absint( $status->fuzzy_count ) ); ?>
+					</td>
+					<td class="stats untranslated">
+						<?php gp_link( gp_url_project( $sub_project->path, gp_url_join( $locale->slug, $set_slug ), array( 'filters[status]' => 'untranslated' ) ), absint( $status->all_count ) -  absint( $status->current_count ) ); ?>
+					</td>
+					<td class="stats waiting">
+						<?php gp_link( gp_url_project( $sub_project->path, gp_url_join( $locale->slug, $set_slug ), array( 'filters[translated]' => 'yes', 'filters[status]' => 'waiting' ) ), absint( $status->waiting_count ) ); ?>
+					</td>
+					</tr>
+				</tr>
+				<?php
+			}
+			?>
+		</tbody>
+	</table>
+</div>
+
+<script>
+	jQuery( document ).ready( function( $ ) {
+		$( '#variant-selector' ).on( 'change', function( event ) {
+			event.preventDefault();
+
+			var $optionSelected = $( 'option:selected', this ),
+				projectUrl = $optionSelected.data( 'projectUrl' );
+
+			if ( projectUrl.length ) {
+				window.location = projectUrl;
+			}
+		});
+
+		$( '.projects-dropdown > li' ).on( 'click', function() {
+			$( this ).parent( '.projects-dropdown' ).toggleClass( 'open' );
+		});
+	});
+</script>
+
+<?php gp_tmpl_footer();
