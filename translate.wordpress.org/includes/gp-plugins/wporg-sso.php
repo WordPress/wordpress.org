4e2e09f66fab2105f36c3ab6f1b24905ad3e1a08
Stephane Daury (stephdau) <stephdau@git.wordpress.org>
2015-11-26 16:34:10 +0000

    translate.wordpress.org: Enabling the WPORG SSO plugin, to redirect logins to https://login.wordpress.org, and remove the built-in route.
    See:
     * https://make.wordpress.org/meta/2015/11/16/centralizing-logins-across-wordpress-org/
     * https://meta.trac.wordpress.org/ticket/1406
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@2136 74240141-8908-4e6f-9713-ba540dce6ec7

--- translate.wordpress.org/includes/gp-plugins/wporg-sso.php
+++ translate.wordpress.org/includes/gp-plugins/wporg-sso.php
@@ -5,14 +5,8 @@
  * @author stephdau
  */
 
-/**
- * NOTE: Not enabled yet, iterating in dev.
- * 
- *
 $sso_plugin_path = __DIR__ . '/wporg-sso/gp-plugin.php';
 
-if ( file_exists( $sso_plugin_path ) ){
+if ( file_exists( $sso_plugin_path ) ) {
 	require_once( $sso_plugin_path );
 }
- *
- */
