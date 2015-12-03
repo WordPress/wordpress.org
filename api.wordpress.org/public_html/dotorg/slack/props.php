6ddd4ef454a86f6caa25d9af5c2e0d1182322113
Michael Adams (mdawaffe) <mdawaffe@git.wordpress.org>
2015-12-03 16:41:41 +0000

    Slack: `/props` command endpoint.
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@2155 74240141-8908-4e6f-9713-ba540dce6ec7

---
+++ api.wordpress.org/public_html/dotorg/slack/props.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace {
+	require dirname( dirname( __DIR__ ) ) . '/includes/hyperdb/bb-10-hyper-db.php';
+	require dirname( dirname( __DIR__ ) ) . '/includes/slack-config.php';
+}
+
+namespace Dotorg\Slack\Props {
+
+require dirname( dirname( __DIR__ ) ) . '/includes/slack/props/lib.php';
+
+function get_avatar( $username, $slack_id, $team_id ) {
+	global $wpdb;
+
+	$wp_user_id = $wpdb->get_var( $wpdb->prepare(
+		"SELECT user_id FROM slack_users WHERE slack_id = %s",
+		$slack_id
+	) );
+
+	$email = $wpdb->get_var( $wpdb->prepare(
+		"SELECT user_email FROM $wpdb->users WHERE ID = %d",
+		$wp_user_id
+	) );
+
+	$hash = md5( strtolower( trim( $email ) ) );
+	return sprintf( 'https://secure.gravatar.com/avatar/%s?s=96d=mm&r=G&%s', $hash, time() );
+}
+
+if ( constant( __NAMESPACE__ . '\\WEBHOOK_TOKEN' ) === $_POST['token'] ) {
+	run( $_POST );
+}
+
+}
