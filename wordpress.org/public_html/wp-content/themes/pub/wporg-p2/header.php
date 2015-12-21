0f57bb11da2b2fc23f59cbc6944b6f9a440a4392
Scott Reilly <coffee2code@git.wordpress.org>
2015-12-21 22:01:12 +0000

    W.org P2 child theme:: Move 'Skip to content' a11y link higher up on page and provide styling.
    Adds `.skip-link` styles from Twenty Sixteen, giving the link visual styling and making it visible on focus.
    
    Fixes #1482, #1484.
    Props SergeyBiryukov.
    
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@2221 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-p2/header.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-p2/header.php
@@ -4,6 +4,7 @@ $pagetitle = wp_get_document_title();
 require WPORGPATH . 'header.php';
 ?>
 <div id="headline">
+		<a class="assistive-text skip-link" href="#main"><?php _e( 'Skip to content', 'p2' ); ?></a>
         <div class="wrapper">
                 <h2><a href="<?php echo home_url( '/' ); ?>"><?php bloginfo( 'name' ); ?></a></h2>
 				<?php wp_nav_menu( array( 'theme_location' => 'wporg_header_subsite_nav', 'fallback_cb' => '__return_false' ) ); ?>
@@ -15,7 +16,6 @@ require WPORGPATH . 'header.php';
 	<?php if ( has_nav_menu( 'primary' ) ) : ?>
 	<div role="navigation" class="site-navigation main-navigation">
 		<h1 class="assistive-text"><?php _e( 'Menu', 'p2' ); ?></h1>
-		<div class="assistive-text skip-link"><a href="#main" title="<?php esc_attr_e( 'Skip to content', 'p2' ); ?>"><?php _e( 'Skip to content', 'p2' ); ?></a></div>
 
 		<?php wp_nav_menu( array(
 			'theme_location' => 'primary',
