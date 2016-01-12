8c9050ca0cd7ab1cc0072822c88198de084377a9
Dominik Schilling (ocean90) <ocean90@git.wordpress.org>
2016-01-12 17:22:50 +0000

    Translate: Add the `no-js` class to the list of body classes.
    See #1352.
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@2279 74240141-8908-4e6f-9713-ba540dce6ec7

--- translate.wordpress.org/public_html/gp-templates-new/helper-functions.php
+++ translate.wordpress.org/public_html/gp-templates-new/helper-functions.php
@@ -2,6 +2,18 @@
 wp_register_style( 'wporg-translate', 'https://wordpress.org/translate/gp-templates-new/style.css', array( 'base' ), '20151123' );
 gp_enqueue_style( 'wporg-translate' );
 
+/**
+ * Adds custom classes to the array of body classes.
+ *
+ * @param array $classes An array of body classes.
+ * @return array Filtered body classes.
+ */
+function wporg_gp_template_body_classes( $classes ) {
+	$classes[] = 'no-js';
+	return $classes;
+}
+add_filter( 'body_class', 'wporg_gp_template_body_classes' );
+
 add_action( 'gp_tmpl_load_locations', function( $locations, $template, $args, $template_path ) {
 	$core_templates = GP_PATH . 'gp-templates/';
 	require_once $core_templates . 'helper-functions.php';
