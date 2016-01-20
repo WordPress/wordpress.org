6c6bb2e629c1cec1c1700afc8f3754c9cdd03708
Samuel Sidler <samuelsidler@git.wordpress.org>
2016-01-20 00:25:57 +0000

    WordPress.tv: Remove "From the blog" widget from frontpage of WordPress.tv
    Props SergeyBiryukov.
    Fixes #1480.
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@2335 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.tv/public_html/wp-content/themes/wptv2/sidebar.php
+++ wordpress.tv/public_html/wp-content/themes/wptv2/sidebar.php
@@ -1,30 +1,6 @@
 <div class="secondary-content">
 	<ul>
 		<li>
-			<h3><a href="http://blog.wordpress.tv"><?php esc_html_e( 'From the Blog', 'wptv' ); ?></a></h3>
-			<ul>
-				<?php
-					// Make it easier to contribute to this theme by not assuming multisite context.
-					if ( function_exists( 'switch_to_blog' ) ) {
-						switch_to_blog( 5310177 ); // blog.wordpress.tv
-					}
-
-					query_posts( 'posts_per_page=5' );
-
-					while ( have_posts() ) :
-						the_post();
-				?>
-					<li><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></li>
-				<?php
-					endwhile;
-
-					if ( function_exists( 'restore_current_blog' ) ) {
-						restore_current_blog();
-					}
-				?>
-			</ul>
-		</li>
-		<li>
 			<h3><?php esc_html_e( 'Resources', 'wptv' ); ?></h3>
 
 			<ul>
