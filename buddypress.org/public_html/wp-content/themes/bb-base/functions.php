c9f566e6a668f078968bea0df3c014933fb9ee7f
John James Jacoby <johnjamesjacoby@git.wordpress.org>
2016-01-20 21:23:33 +0000

    Decrease front page headline font-size to make room for more text.
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@2341 74240141-8908-4e6f-9713-ba540dce6ec7

--- buddypress.org/public_html/wp-content/themes/bb-base/functions.php
+++ buddypress.org/public_html/wp-content/themes/bb-base/functions.php
@@ -54,7 +54,7 @@ if ( bb_base_is_codex() ) {
 function bb_base_register_stylesheets() {
 
 	// Version of CSS
-	$version = '20151027a';
+	$version = '201601200001';
 
 	// Base theme styling
 	wp_enqueue_style( 'bb-base',   get_template_directory_uri()   . '/style.css', false,                         $version, 'screen' );
