8e081ebb99d3f1ae84c774a6bf0f77bce5d99f02
Scott Reilly <coffee2code@git.wordpress.org>
2016-01-26 23:53:27 +0000

    W.org Showcase: Bump CSS version for cache bust.
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@2378 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-showcase/header.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-showcase/header.php
@@ -3,7 +3,7 @@ $GLOBALS['pagetitle'] = wp_get_document_title();
 
 $prefix = is_ssl() ? 'https://' : 'http://s.';
 wp_enqueue_style( 'blog-wp4', $prefix.'wordpress.org/style/blog-wp4.css', array(), 4 );
-wp_enqueue_style( 'showcase', $prefix.'wordpress.org/wp-content/themes/pub/wporg-showcase/style.css', array(), 13 );
+wp_enqueue_style( 'showcase', $prefix.'wordpress.org/wp-content/themes/pub/wporg-showcase/style.css', array(), 14 );
 require WPORGPATH . 'header.php';
 ?>
 <div id="headline">
