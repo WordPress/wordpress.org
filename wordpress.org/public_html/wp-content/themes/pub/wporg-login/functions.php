1072baa23f2a6f2d87d45150b6e1caecaceeb646
Gary Pendergast <pento@git.wordpress.org>
2016-02-03 22:10:15 +0000

    Login.w.org: Add Google Analytics tracking for page views.
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@2453 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-login/functions.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-login/functions.php
@@ -50,3 +50,27 @@ function wporg_login_replace_css() {
 	wp_register_style( 'login', get_stylesheet_directory_uri() . $css_file, array( 'buttons', 'dashicons', 'open-sans' ), filemtime( __DIR__ . $css_file ) );
 }
 add_action( 'login_init', 'wporg_login_replace_css' );
+
+/**
+ * Add Google Analytics tracking to login pages.
+ */
+function wporg_login_analytics() {
+?>
+<script type="text/javascript">
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-52447-1']);
+_gaq.push(['_setDomainName', 'wordpress.org']);
+_gaq.push(['_trackPageview']);
+(function() {
+	var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+function recordOutboundLink(link, category, action) {
+	_gaq.push(['_trackEvent', category, action])
+	setTimeout('document.location = "' + link.href + '"', 100);
+}
+</script>
+<?php
+}
+add_action( 'wp_footer', 'wporg_login_analytics' );
