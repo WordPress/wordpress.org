df5c49a04dfdfb9f1dd576640a5bd85cced3215f
Stephane Daury (stephdau) <stephdau@git.wordpress.org>
2016-02-04 17:54:05 +0000

    WPORG Login: using `wp_lostpassword_url()` instead of a hard coded path. Now filtered for a custom route, as of r2465.
    See #1524
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@2466 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-login/partials/login.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-login/partials/login.php
@@ -1,6 +1,6 @@
 <?php wp_login_form(); ?>
 
 <p class="nav">
-	<a href="/lostpassword/" title="<?php _e( 'Password Lost and Found', 'wporg-login' ); ?>"><?php _e( 'Forgot password?', 'wporg-login' ); ?></a> &nbsp; • &nbsp; 
+	<a href="<?php echo wp_lostpassword_url( '/' ); ?>" title="<?php _e( 'Password Lost and Found', 'wporg-login' ); ?>"><?php _e( 'Forgot password?', 'wporg-login' ); ?></a> &nbsp; • &nbsp; 
 	<a href="https://wordpress.org/support/register.php" title="<?php _e( 'Create an account', 'wporg-login' ); ?>"><?php _e( 'Create an account', 'wporg-login' ); ?></a>
 </p>
