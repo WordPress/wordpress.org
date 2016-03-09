f9690fc3863068f6e3507487c013d852459af503
Scott Reilly <coffee2code@git.wordpress.org>
2016-03-09 19:51:34 +0000

    jobs.wordpress.net: Add greater emphasis to making note of the job token.
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@2720 74240141-8908-4e6f-9713-ba540dce6ec7

--- jobs.wordpress.net/public_html/wp-content/themes/jobswp/content-post-job-success.php
+++ jobs.wordpress.net/public_html/wp-content/themes/jobswp/content-post-job-success.php
@@ -1,11 +1,11 @@
 <div class="notice notice-success">
 	<h2><?php _e( 'Thank you for submitting your job posting!', 'jobswp' ); ?></h2>
 
-	<p><?php printf( __( 'Your job posting will be moderated in the next 24-48 hours to ensure it meets the criteria listed in our <a href="%s">FAQ</a>. After approval, your posting will stay on the job board for a total of 21 days.',
+	<p><?php printf( __( 'Your job posting will be reviewed in the next 24-48 hours to ensure it meets the criteria listed in our <a href="%s">FAQ</a>. After approval, your posting will stay on the job board for a total of 21 days.',
 	'jobswp' ),
 	'/faq/' ); ?></p>
 
-	<p><?php printf( __( 'Take note of the following job token. It can be used to remove your job posting from the site before it expires without having to make a request via the feedback form (which could take 24-48 hours to honor). The token can be used on the <a href="%s">job removal page</a>.', 'jobswp' ),
+	<p><?php printf( __( '<strong>Important!</strong> Take note of the following job token. It is your only means of removing the job from the site in a <em>timely</em> fashion prior to its automatic expiration. (Otherwise, you would have to make a request via the feedback form which could take 24-48 hours or longer to honor). The token is used on the <a href="%s">job removal page</a>.', 'jobswp' ),
 		'/remove-a-job/'
 	); ?></p>
 
