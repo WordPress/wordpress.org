c8773c3f0be35df0271e1ab7368039da41036add
Konstantin Obenland <obenland@git.wordpress.org>
2016-04-19 19:57:19 +0000

    Plugins Directory: Always show link to zip file if available.
    Restricting it to only `pending` does not reflect the review flow anymore.
    
    See #1570.
    
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@2978 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/plugins/plugin-directory/admin/metabox/class-review-tools.php
+++ wordpress.org/public_html/wp-content/plugins/plugin-directory/admin/metabox/class-review-tools.php
@@ -8,27 +8,21 @@ namespace WordPressdotorg\Plugin_Directory\Admin\Metabox;
  */
 class Review_Tools {
 	static function display() {
-		$post = get_post();
-		$plugin_slug = $post->post_name;
-
+		$post      = get_post();
 		$zip_files = get_attached_media( 'application/zip', $post );
-		if ( $zip_files && 'pending' == $post->post_status ) {
+
+		if ( $zip_files ) {
 			$zip_file = current( $zip_files );
-			$zip_url = wp_get_attachment_url( $zip_file->ID );
-			printf(
-				'<p>' . __( '<strong>Zip file:</strong> %s', 'wporg-plugins' ) . '</p>',
-				sprintf(
-					'<a href="%s">%s</a>',
-					esc_url( $zip_url ),
-					esc_html( $zip_url )
-				)
+			$zip_url  = wp_get_attachment_url( $zip_file->ID );
+			printf( '<p>' . __( '<strong>Zip file:</strong> %s', 'wporg-plugins' ) . '</p>',
+				sprintf( '<a href="%s">%s</a>', esc_url( $zip_url ), esc_html( $zip_url ) )
 			);
 		}
 
 		echo "<ul>
-			<li><a href='https://plugins.trac.wordpress.org/log/{$plugin_slug}/'>" . __( 'Development Log', 'wporg-plugins' ) . "</a></li>
-			<li><a href='https://plugins.svn.wordpress.org/{$plugin_slug}/'>" . __( 'Subversion Repository', 'wporg-plugins' ) . "</a></li>
-			<li><a href='https://plugins.trac.wordpress.org/browser/{$plugin_slug}/'>" . __( 'Browse in Trac', 'wporg-plugins' ) . '</a></li>
+			<li><a href='https://plugins.trac.wordpress.org/log/{$post->post_name}/'>" . __( 'Development Log', 'wporg-plugins' ) . "</a></li>
+			<li><a href='https://plugins.svn.wordpress.org/{$post->post_name}/'>" . __( 'Subversion Repository', 'wporg-plugins' ) . "</a></li>
+			<li><a href='https://plugins.trac.wordpress.org/browser/{$post->post_name}/'>" . __( 'Browse in Trac', 'wporg-plugins' ) . '</a></li>
 		</ul>';
 
 	}
