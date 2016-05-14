ea4524a2c016ba7aec53bde4791b28decf6cb37a
Dominik Schilling (ocean90) <ocean90@git.wordpress.org>
2016-05-14 13:27:51 +0000

    WordPress.org SSO: Remove trailing spaces.
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@3145 74240141-8908-4e6f-9713-ba540dce6ec7

--- common/includes/wporg-sso/class-wporg-sso.php
+++ common/includes/wporg-sso/class-wporg-sso.php
@@ -2,19 +2,19 @@
 if ( ! class_exists( 'WPOrg_SSO' ) ) {
 	/**
 	 * Single Sign-On (SSO) handling for WordPress/bbPress/GlotPress instances under *.wordpress.org.
-	 * 
+	 *
 	 * @author stephdau
 	 */
 	class WPOrg_SSO {
 		const SSO_HOST = 'login.wordpress.org';
-		
+
 		public $sso_host_url;
 		public $sso_login_url;
 		public $sso_signup_url;
-		
+
 		public $host;
 		public $script;
-		
+
 		/**
 		 * Constructor, instantiate common properties
 		 */
@@ -22,19 +22,19 @@ if ( ! class_exists( 'WPOrg_SSO' ) ) {
 			$this->sso_host_url   = 'https://' . self::SSO_HOST;
 			$this->sso_login_url  = $this->sso_host_url . '/';
 			$this->sso_signup_url = 'https://wordpress.org/support/register.php'; // For now
-			
+
 			if ( ! empty( $_SERVER['HTTP_HOST'] ) ) {
 				$this->host   = $_SERVER['HTTP_HOST'];
 				$this->script = $_SERVER['SCRIPT_NAME'];
 			}
 		}
-		
+
 		/**
 		 * Checks if the requested redirect_to URL is part of the wordpress.org empire, adds it as an redirect host if so.
 		 *
 		 * @param array $hosts Currently allowed hosts
 		 * @return array $hosts Edited lists of allowed hosts
-		 * 
+		 *
 		 * @example add_filter( 'allowed_redirect_hosts', array( &$this, 'add_allowed_redirect_host' ) );
 		*/
 		public function add_allowed_redirect_host( $hosts ) {
@@ -46,23 +46,23 @@ if ( ! class_exists( 'WPOrg_SSO' ) ) {
 				// If not on the SSO host, add login.wordpress.org, to be safe
 				$host = self::SSO_HOST;
 			}
-		
+
 			// If we got a host by now, it's a safe wordpress.org-based one, add it to the list of allowed redirects
 			if ( ! empty( $host ) && ! in_array( $host, $hosts ) ){
 				$hosts[] = $host;
 			}
-		
+
 			// Return list of allowed hosts
 			return $hosts;
 		}
-		
+
 		/**
 		 * Returns the SSO login URL, with redirect_to as requested, if deemed valid.
-		 * 
+		 *
 		 * @param string $redirect_to
 		 * @param string $filter_redirect_to When used with the WP login_url filter, the redirect_to is passed as a 2nd arg instead.
 		 * @return string
-		 * 
+		 *
 		 * @example Use directly, or through add_action( 'login_url', array( &$wporg_sso, 'login_url' ), 10, 2 );
 		 */
 		public function login_url( $redirect_to = '', $filter_redirect_to = '' ) {
@@ -76,31 +76,31 @@ if ( ! class_exists( 'WPOrg_SSO' ) ) {
 				$login_url = add_query_arg( 'redirect_to', urlencode( $redirect_to ), $login_url );
 			}
 			return $login_url;
-			
+
 		}
-		
-		
+
+
 		/**
 		 * Tests if the current process has $_SERVER['HTTP_HOST'] or not (EG: cron'd processes do not).
-		 * 
+		 *
 		 * @return boolean
 		 */
 		public function has_host() {
 			return ( ! empty( $this->host ) );
 		}
-		
+
 		/**
 		 * Get a safe redirect URL (ie: a wordpress.org-based one) from $_REQUEST['redirect_to'] or a safe alternative.
-		 * 
+		 *
 		 * @return string Safe redirect URL from $_REQUEST['redirect_to']
 		 */
 		protected function _get_safer_redirect_to() {
 			// Setup a default redirect to URL, with a safe version to only change if validation succeeds below.
 			$redirect_to = ! empty( $_GET['action'] ) && in_array( $_GET['action'], array( 'logout', 'loggedout' ) ) ? '/loggedout/' : 'https://wordpress.org/';
-			
+
 			if ( ! empty( $_REQUEST['redirect_to'] ) ) {
 				// User is requesting a further redirect afterward, let's make sure it's a legit target.
-				$redirect_to_requested = function_exists( 'wp_sanitize_redirect' ) ? wp_sanitize_redirect( $_REQUEST['redirect_to'] ) : $_REQUEST['redirect_to'];
+				$redirect_to_requested = function_exists( 'wp_sanitize_redirect' ) ? wp_sanitize_redirect( $redirect_to ) : $redirect_to;
 				if ( $this->_is_valid_targeted_domain( $redirect_to_requested ) ) {
 					$redirect_to = $redirect_to_requested;
 				}
@@ -117,13 +117,13 @@ if ( ! class_exists( 'WPOrg_SSO' ) ) {
 					$redirect_to = $redirect_to_source_parent;
 				}
 			}
-			
+
 			return $redirect_to;
 		}
-		
+
 		/**
 		 * Tests if the passed host/domain, or URL, is part of the WordPress.org domain.
-		 * 
+		 *
 		 * @param unknown $string A domain, hostname, or URL
 		 * @return boolean True is ok, false if not
 		 */
@@ -131,44 +131,44 @@ if ( ! class_exists( 'WPOrg_SSO' ) ) {
 			if ( empty( $string ) || ! is_string( $string ) ) {
 				$string = '';
 			}
-			
+
 			if ( strstr( $string , '/' ) ) {
 				$url = parse_url( $string );
 				$host = ( ! empty( $url['host'] ) ) ? $url['host'] : '';
 			} else {
 				$host = $string;
 			}
-			
+
 			if ( ! empty( $host ) && strstr( $host , '.' ) ) {
 				return ( preg_match( '/^(.+\.)?wordpress\.org$/', $host ) ) ? true : false;
 			}
-			
+
 			return false;
 		}
 
 		/**
 		 * Validates if target URL is within our bounds, then redirects to it if so, or to WP.org homepage (returns if headers already sent).
-		 * 
+		 *
 		 * @param string $to Destination URL
 		 * @param number $status HTTP redirect status, defaults to 302
-		 * 
+		 *
 		 * @note: using our own over wp_safe_redirect(), etc, because not all targeted platforms (WP/BB/GP/etc) implement an equivalent, we run early, etc.
 		 */
 		protected function _safe_redirect( $to, $status = 302 ) {
 			if ( headers_sent() ) {
 				return;
 			}
-			
+
 			if ( ! $this->_is_valid_targeted_domain( $to ) ) {
 				$to = $this->_get_safer_redirect_to();
 			}
-			
+
 			header(
 				'Location: ' . $to,
 				true,
 				preg_match( '/^30(1|2)$/', $status ) ? $status : 302
 			);
-			
+
 			die();
 		}
 	}
