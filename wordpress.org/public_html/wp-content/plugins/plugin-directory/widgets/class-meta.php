3b6879c65690c2cbf69f61c2c91f97cf16f5dc79
Konstantin Obenland <obenland@git.wordpress.org>
2016-05-20 15:59:04 +0000

    Plugin Directory: Merge the two Meta widgets.
    H/t dd32.
    See #1719.
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@3190 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/plugins/plugin-directory/widgets/class-meta.php
+++ wordpress.org/public_html/wp-content/plugins/plugin-directory/widgets/class-meta.php
@@ -30,7 +30,7 @@ class Meta extends \WP_Widget {
 		?>
 
 		<ul>
-			<li><?php printf( __( 'Last updated: %s ago', 'wporg-plugins' ), human_time_diff( get_post_modified_time() ) ); ?></li>
+			<li><?php printf( __( 'Last updated: %s ago', 'wporg-plugins' ), '<span itemprop="dateModified" content="' . esc_attr( get_post_modified_time( 'c' ) ) . '">' . human_time_diff( get_post_modified_time() ) . '</span>' ); ?></li>
 			<li><?php printf( __( 'Active installs: %s', 'wporg-plugins' ), Template::active_installs( false ) ); ?></li>
 			<li><?php printf( __( 'Category: %s', 'wporg-plugins' ), get_the_term_list( get_post()->ID, 'plugin_category', '', ', ' ) ); ?></li>
 			<li><?php printf( __( 'Designed to work with: %s', 'wporg-plugins' ), get_the_term_list( get_post()->ID, 'plugin_category', '', ', ' ) ); ?></li>
