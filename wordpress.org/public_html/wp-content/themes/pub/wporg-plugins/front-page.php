bece3ef1fde5f2fd107fb1a19d1e51af3202afae
Konstantin Obenland <obenland@git.wordpress.org>
2016-05-20 22:50:42 +0000

    Plugin Directory: Populate the three featured plugin groups.
    See #1719.
    
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@3208 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-plugins/front-page.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-plugins/front-page.php
@@ -10,8 +10,8 @@
 namespace WordPressdotorg\Plugin_Directory\Theme;
 
 $sections = array(
-	'popular ' => __( 'Popular Plugins', 'wporg-plugins' ),
-	'trending' => __( 'Trending Plugins', 'wporg-plugins' ),
+	'featured' => __( 'Featured Plugins', 'wporg-plugins' ),
+	'popular'  => __( 'Popular Plugins', 'wporg-plugins' ),
 	'beta'     => __( 'Beta Plugins', 'wporg-plugins' ),
 );
 
@@ -25,17 +25,31 @@ get_header();
 
 	<main id="main" class="site-main" role="main">
 
-		<?php foreach ( $sections as $section_slug => $section_title ) : ?>
+		<?php foreach ( $sections as $section_slug => $section_title ) :
+			$section_args = array(
+				'post_type'       => 'plugin',
+				'posts_per_page'  => 4,
+				'plugin_category' => $section_slug,
+			);
+
+			if ( 'popular' === $section_slug ) :
+				$section_args['meta_key'] = 'active_installs';
+				$section_args['orderby']  = 'meta_value_num';
+				unset( $section_args['plugin_category'] );
+			endif;
+
+			$section_query = new \WP_Query( $section_args );
+		?>
 
 			<section class="plugin-section">
 				<header class="section-header">
 					<h1 class="section-title"><?php echo esc_html( $section_title ); ?></h1>
-					<a class="section-link" href="<?php echo esc_url( home_url( $section_slug ) ); ?>"><?php _ex( 'See all', 'plugins', 'wporg-plugins' ); ?></a>
+					<a class="section-link" href="<?php echo esc_url( home_url( "browse/$section_slug/" ) ); ?>"><?php _ex( 'See all', 'plugins', 'wporg-plugins' ); ?></a>
 				</header>
 
 				<?php
-					while ( have_posts() ) :
-						the_post();
+					while ( $section_query->have_posts() ) :
+						$section_query->the_post();
 
 						get_template_part( 'template-parts/plugin', 'index' );
 					endwhile;
