2c3d1577718b6e45607c098e7ed217d3d830b718
Konstantin Obenland <obenland@git.wordpress.org>
2016-05-23 22:28:15 +0000

    Plugin Directory: Return shortcode content rather than displaying it.
    Give the shortcode handler the chance to insert the content at the right position.
    
    See #1719, [3219].
    
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@3222 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/plugins/plugin-directory/shortcodes/class-developers.php
+++ wordpress.org/public_html/wp-content/plugins/plugin-directory/shortcodes/class-developers.php
@@ -15,28 +15,25 @@ class Developers {
 	static function display() {
 		$post       = get_post();
 		$committers = Tools::get_plugin_committers( $post->post_name );
-	?>
-		<ul class="plugin-developers">
 
-		<?php
-			foreach ( $committers as $committer_slug ) :
-				$committer = get_user_by( 'slug', $committer_slug );
-		?>
-			<li><?php echo get_avatar( $committer->ID, 32 ) . $committer->display_name; ?></li>
-		<?php endforeach; ?>
+		$output = '<ul class="plugin-developers">';
+		foreach ( $committers as $committer_slug ) {
+			$committer = get_user_by( 'slug', $committer_slug );
 
-		</ul>
+			$output .= '<li>' . get_avatar( $committer->ID, 32 ) . $committer->display_name . '</li>';
+		}
+		$output .= '</ul>';
 
-		<h5><?php _e( 'Browse the code', 'wporg-plugins' ); ?></h5>
-		<ul>
-			<li>
-				<a href="<?php echo esc_url( "https://plugins.trac.wordpress.org/log/{$post->post_name}/" ); ?>" rel="nofollow"><?php _e( 'Development Log', 'wporg-plugins' ); ?></a>
-				<a href="<?php echo esc_url( "https://plugins.trac.wordpress.org/log/{$post->post_name}/?limit=100&mode=stop_on_copy&format=rss" ); ?>" rel="nofollow"><img src="//s.w.org/style/images/feedicon.png" /></a>
-			</li>
-			<li><a href="<?php echo esc_url( "https://plugins.svn.wordpress.org/{$post->post_name}/" ); ?>" rel="nofollow"><?php _e( 'Subversion Repository', 'wporg-plugins' ); ?></a></li>
-			<li><a href="<?php echo esc_url( "https://plugins.trac.wordpress.org/browser/{$post->post_name}/" ); ?>" rel="nofollow"><?php _e( 'Browse in Trac', 'wporg-plugins' ); ?></a></li>
-			<li><a href="<?php echo esc_url( "https://translate.wordpress.org/projects/wp-plugins/{$post->post_name}/" ); ?>" rel="nofollow"><?php _e( 'Translation Contributors', 'wporg-plugins' ); ?></a></li>
-		</ul>
-	<?php
+		return $output .
+			'<h5>' . __( 'Browse the code', 'wporg-plugins' ) . '</h5>' .
+			'<ul>' .
+				'<li>' .
+					'<a href="' . esc_url( "https://plugins.trac.wordpress.org/log/{$post->post_name}/" ) . '" rel="nofollow">' . __( 'Development Log', 'wporg-plugins' ) . '</a>' . "\n" .
+					'<a href="' . esc_url( "https://plugins.trac.wordpress.org/log/{$post->post_name}/?limit=100&mode=stop_on_copy&format=rss" ) . '" rel="nofollow"><img src="//s.w.org/style/images/feedicon.png" /></a>' .
+				'</li>' .
+				'<li><a href="' . esc_url( "https://plugins.svn.wordpress.org/{$post->post_name}/" ) . '" rel="nofollow">' . __( 'Subversion Repository', 'wporg-plugins' ) . '</a></li>' .
+				'<li><a href="' . esc_url( "https://plugins.trac.wordpress.org/browser/{$post->post_name}/" ) . '" rel="nofollow">' . __( 'Browse in Trac', 'wporg-plugins' ) . '</a></li>' .
+				'<li><a href="' . esc_url( "https://translate.wordpress.org/projects/wp-plugins/{$post->post_name}/" ) . '" rel="nofollow">' . __( 'Translation Contributors', 'wporg-plugins' ) . '</a></li>' .
+			'</ul>';
 	}
 }
