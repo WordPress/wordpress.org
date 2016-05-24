d1c3060200730f9585bb6b3b36eb4d59d2b3f52c
Scott Reilly <coffee2code@git.wordpress.org>
2016-05-24 21:10:54 +0000

    W.org P2 child theme: Add `searchform.php` template for context aware handbook/non-handbook search form submission URLs.
    Fixes #515.
    
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@3238 74240141-8908-4e6f-9713-ba540dce6ec7

---
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-p2/searchform.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Searchform template.
+ *
+ * @package P2
+ */
+?>
+<?php $search_url = wporg_is_handbook() ? wporg_get_current_handbook_home_url() : home_url( '/' ); ?>
+<form role="search" method="get" id="searchform" class="searchform" action="<?php echo esc_url( $search_url ); ?>">
+	<div>
+		<label class="screen-reader-text" for="s"><?php _ex( 'Search for:', 'label', 'p2' ); ?></label>
+		<input type="text" value="<?php echo get_search_query(); ?>" name="s" id="s" />
+		<input type="submit" id="searchsubmit" value="<?php echo esc_attr_x( 'Search', 'submit button', 'p2' ); ?>" />
+	</div>
+</form>
+
