33f6ef73b39dbbad5f4e819936ebca989ff6f273
Scott Reilly <coffee2code@git.wordpress.org>
2016-05-24 22:50:56 +0000

    developer.wordpress.org: Bump CSS version and autocomplete JS version for cache bust.
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@3247 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-developer/inc/autocomplete.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-developer/inc/autocomplete.php
@@ -43,7 +43,7 @@ class DevHub_Search_Form_Autocomplete {
 		wp_register_script( 'awesomplete', get_template_directory_uri() . '/js/awesomplete.min.js', array(), '20160322', true );
 		wp_enqueue_script( 'awesomplete' );
 
-		wp_register_script( 'autocomplete', get_template_directory_uri() . '/js/autocomplete.js', array( 'awesomplete' ), '20160322', true );
+		wp_register_script( 'autocomplete', get_template_directory_uri() . '/js/autocomplete.js', array( 'awesomplete' ), '20160524', true );
 		wp_localize_script( 'autocomplete', 'autocomplete', array(
 				'ajaxurl' => admin_url( 'admin-ajax.php' ),
 				'nonce'   => wp_create_nonce( 'autocomplete_nonce' ),
