c7dd499c5c3fab73c88d1a4f7eeb5312f1d070e9
Konstantin Obenland <obenland@git.wordpress.org>
2016-05-24 16:01:00 +0000

    Plugins Directory: Add a banner to show plugin queue volume.
    Also improves notices on the upload page in general.
    
    Fixes #1727.
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@3230 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/plugins/plugin-directory/shortcodes/class-upload.php
+++ wordpress.org/public_html/wp-content/plugins/plugin-directory/shortcodes/class-upload.php
@@ -7,21 +7,39 @@ class Upload {
 	 * Renders the upload shortcode.
 	 */
 	public static function display() {
+		ob_start();
+
 		if ( is_user_logged_in() ) :
 
-			if ( ! empty( $_POST['_wpnonce'] ) && wp_verify_nonce( $_POST['_wpnonce'], 'wporg-plugins-upload' ) && 'upload' === $_POST['action'] ) {
-				if ( UPLOAD_ERR_OK === $_FILES['zip_file']['error'] ) {
+			if ( ! empty( $_POST['_wpnonce'] ) && wp_verify_nonce( $_POST['_wpnonce'], 'wporg-plugins-upload' ) && 'upload' === $_POST['action'] ) :
+				if ( UPLOAD_ERR_OK === $_FILES['zip_file']['error'] ) :
 					$uploader = new Upload_Handler;
 					$message  = $uploader->process_upload();
-				}  else {
+				else :
 					$message = __( 'Error in file upload.', 'wporg-plugins' );
-				}
+				endif;
 
-				if ( ! empty( $message ) ) {
-					echo "<div class='notice notice-warning'><p>{$message}</p></div>\n";
-				}
-			}
-			?>
+				if ( ! empty( $message ) ) :
+					echo "<div class='notice notice-warning notice-alt'><p>{$message}</p></div>\n";
+				endif;
+
+			else :
+				$plugins = wp_count_posts( 'plugin', 'readable' );
+				?>
+
+				<div class="plugin-queue-message notice notice-info notice-alt">
+					<p>
+						<?php
+						printf( _n( 'Currently there is %1$s plugin in the review queue.', 'Currently there are %1$s plugins in the review queue, %2$s of which are awaiting their initial review.', ( $plugins->draft + $plugins->pending ), 'wporg-plugins' ),
+							'<strong>' . ( $plugins->draft + $plugins->pending ) . '</strong>',
+							'<strong>' . $plugins->draft . '</strong>'
+						);
+						?>
+					</p>
+				</div>
+
+			<?php endif; ?>
+
 			<form enctype="multipart/form-data" id="upload_form" method="POST" action="">
 				<?php wp_nonce_field( 'wporg-plugins-upload' ); ?>
 				<input type="hidden" name="action" value="upload"/>
@@ -32,10 +50,14 @@ class Upload {
 					<small><?php printf( __( 'Maximum allowed file size: %s', 'wporg-plugins' ), esc_html( self::get_max_allowed_file_size() ) ); ?></small>
 				</p>
 			</form>
+
 		<?php else : ?>
-			<p><?php printf( __( 'Before you can upload a new plugin, <a href="%s">please log in</a>.', 'wporg-plugins' ), esc_url( 'https://login.wordpress.org/' ) ); ?>
-			</p>
+
+			<p><?php printf( __( 'Before you can upload a new plugin, <a href="%s">please log in</a>.', 'wporg-plugins' ), esc_url( 'https://login.wordpress.org/' ) ); ?></p>
+
 		<?php endif;
+
+		return ob_get_clean();
 	}
 
 	/**
