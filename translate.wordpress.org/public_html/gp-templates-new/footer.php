074df66c3bbbf0b939694c9a2986f626590b1527
Dominik Schilling (ocean90) <ocean90@git.wordpress.org>
2016-05-28 22:37:51 +0000

    Translate: Remove translate.wordpress.org directory.
    Plugins and templates haven been converted to WordPress plugins which are located in https://meta.trac.wordpress.org/browser/sites/trunk/wordpress.org/public_html/wp-content/plugins.
    
    Fixes #1735.
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@3270 74240141-8908-4e6f-9713-ba540dce6ec7

--- translate.wordpress.org/public_html/gp-templates-new/footer.php
+++
@@ -1,6 +0,0 @@
-	</div><!-- gp-content -->
-	<p id="gp-footer" class="secondary">
-		<?php printf( __('Proudly powered by <a href="%s" title="Found in translation">GlotPress</a>.'), 'http://glotpress.org/' ); ?>
-	</p>
-<?php require WPORGPATH . 'footer.php';
-
--- translate.wordpress.org/public_html/gp-templates/footer.php
+++
@@ -1,7 +0,0 @@
-	</div><!-- gp-content -->
-	<p id="gp-footer" class="secondary">
-		<?php printf( __('Proudly powered by <a href="%s" title="Found in translation">GlotPress</a>.'), 'http://glotpress.org/' ); ?>
-		<?php gp_footer(); ?>
-	</p>
-<?php require WPORGPATH . 'footer.php';
-
