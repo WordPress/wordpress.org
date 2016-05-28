e42732c116abaef5ce2e934be70d23d7e59deb2e
Dominik Schilling (ocean90) <ocean90@git.wordpress.org>
2016-05-28 21:37:00 +0000

    Translate: Replace hardcoded URLs with `plugins_url()`.
    See #1735.
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@3268 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/plugins/wporg-gp-customizations/templates/helper-functions.php
+++ wordpress.org/public_html/wp-content/plugins/wporg-gp-customizations/templates/helper-functions.php
@@ -1,9 +1,9 @@
 <?php
-wp_register_style( 'wporg-translate', 'https://wordpress.org/translate/gp-templates-new/style.css', array( 'base' ), '20160425' );
+wp_register_style( 'wporg-translate', plugins_url( 'style.css', __FILE__ ), array( 'base' ), '20160425' );
 gp_enqueue_style( 'wporg-translate' );
 
-wp_register_style( 'chartist', 'https://wordpress.org/translate/gp-templates-new/css/chartist.min.css', array(), '0.9.5' );
-wp_register_script( 'chartist', 'https://wordpress.org/translate/gp-templates-new/js/chartist.min.js', array(), '0.9.5' );
+wp_register_style( 'chartist', plugins_url( 'css/chartist.min.css', __FILE__ ), array(), '0.9.5' );
+wp_register_script( 'chartist', plugins_url( 'js/chartist.min.js', __FILE__ ), array(), '0.9.5' );
 
 
 /**
