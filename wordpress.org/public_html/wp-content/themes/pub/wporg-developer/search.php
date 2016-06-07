691e01ae7f92a0d19c129183cefe69597f839dcd
Scott Reilly <coffee2code@git.wordpress.org>
2016-06-07 18:46:20 +0000

    developer.wordpress.org: Implement meaningful breadcrumbs for search results page.
    Not only makes the breadcrumbs helpful for navigation, it brings the breadcrumbs into consistency with its appearance everywhere else.
    
    * Use `:` instead of `/` to separate 'Search Results' prefix from breadcrumbs.
    * Add link to 'Home'.
    * Add link to either code reference or current handbook's home page.
    
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@3313 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-developer/search.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-developer/search.php
@@ -10,8 +10,16 @@ get_header(); ?>
 	<div id="content-area">
 
 		<div class="breadcrumb-trail breadcrumbs" itemprop="breadcrumb">
-			<span class="trail-browse"><span class="trail-begin"><?php _e( 'Search Results', 'wporg' ); ?></span></span>
-			<span class="sep">/</span> <span class="trail-end"><?php echo esc_html( get_search_query() ); ?></span>
+			<span class="trail-browse"><span class="trail-begin"><?php _e( 'Search Results', 'wporg' ); ?></span></span><span class="sep">:</span>
+			<span><a href="<?php echo home_url( '/' ); ?>"><?php _e( 'Home', 'wporg' ); ?></a></span>
+			<span class="sep">/</span>
+			<?php if ( get_query_var( 'is_handbook' ) ) : ?>
+			<span><a href="<?php echo esc_url( get_query_var( 'current_handbook_home_url' ) ); ?>"><?php echo get_query_var( 'current_handbook_name' ); ?></a></span>
+			<?php elseif ( $reference_page = get_page_by_path( 'reference' ) ) : ?>
+			<span><a href="<?php echo esc_url( get_permalink( $reference_page ) ); ?>"><?php echo get_the_title( $reference_page ); ?></a></span>
+			<?php endif; ?>
+			<span class="sep">/</span>
+			<span class="trail-end"><?php echo esc_html( get_search_query() ); ?></span>
 		</div>
 
 		<?php get_search_form(); ?>
