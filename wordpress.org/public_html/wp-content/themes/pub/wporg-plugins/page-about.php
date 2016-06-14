4b1917307f420531fa210893eef7b5315966951a
Konstantin Obenland <obenland@git.wordpress.org>
2016-06-14 17:38:15 +0000

    Plugin Directory: Add page templates for developer resources.
    Makes all strings available for translation.
    
    See #1719.
    
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@3362 74240141-8908-4e6f-9713-ba540dce6ec7

---
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-plugins/page-about.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * The template for the a out page.
+ *
+ * @package WordPressdotorg\Plugin_Directory\Theme
+ */
+
+namespace WordPressdotorg\Plugin_Directory\Theme;
+
+get_header(); ?>
+
+	<main id="main" class="site-main" role="main">
+
+		<?php while ( have_posts() ) : the_post(); ?>
+
+		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+			<header class="entry-header">
+				<h1 class="entry-title"><?php _e( 'Developer Information', 'wporg-plugins' ); ?></h1>
+			</header><!-- .entry-header -->
+
+			<div class="entry-content">
+				<p><?php printf( __( 'You’ve got a cool new plugin and are hoping to give it some exposure. You’re in the right place. Just <a href="%s">ask us to host it for you</a>. You’ll be able to:', 'wporg-plugins' ), esc_url( home_url( 'about/add' ) ) ); ?></p>
+				<ul>
+					<li><?php _e( 'Keep track of how many people have downloaded it.', 'wporg-plugins' ); ?></li>
+					<li><?php _e( 'Let people leave comments about your plugin.', 'wporg-plugins' ); ?></li>
+					<li><?php _e( 'Get your plugin rated against all the other cool WordPress plugins.', 'wporg-plugins' ); ?></li>
+					<li><?php _e( 'Give your plugin lots of exposure in this centralized repository.', 'wporg-plugins' ); ?></li>
+				</ul>
+
+				<h3><?php _e( 'There are some restrictions', 'wporg-plugins' ); ?></h3>
+				<ul>
+					<li><?php printf( __( 'Your plugin must be compatible with the <a href="%s">GNU General Public License v2</a>, or any later version. We strongly recommend using the same license as WordPress — “GPLv2 or later.” (If you don’t specify a compatible license, what you check in will considered GPLv2 or later.)', 'wporg-plugins' ), esc_url( 'http://www.gnu.org/licenses/license-list.html#GPLCompatibleLicenses' ) ); ?></li>
+					<li><?php _e( 'The plugin must not do anything illegal or be morally offensive (that’s subjective, we know).', 'wporg-plugins' ); ?></li>
+					<li><?php printf( __( 'You have to actually use the <a href="%s">Subversion</a> repository we give you in order for your plugin to show up on this site. The WordPress Plugins Directory is a hosting site, not a listing site.', 'wporg-plugins' ), esc_url( 'http://subversion.tigris.org/' ) ); ?></li>
+					<li><?php _e( 'The plugin must not embed external links on the public site (like a "powered by" link) without explicitly asking the user&#8217;s permission.', 'wporg-plugins' ); ?></li>
+					<li><?php printf( __( 'Your plugin must abide by our list of <a href="%s">detailed guidelines</a>, which include not being a spammer and not abusing the systems.', 'wporg-plugins' ), esc_url( 'https://developer.wordpress.org/plugins/wordpress-org/detailed-plugin-guidelines/' ) ); ?></li>
+				</ul>
+
+				<h3><?php _e( 'Submission is Simple', 'wporg-plugins' ); ?></h3>
+				<ol>
+				 	<li><?php printf( __( '<a href="%s">Sign up</a> for an account on WordPress.org.', 'wporg-plugins' ), esc_url( 'https://wordpress.org/support/register.php' ) ); ?></li>
+				 	<li><?php printf( __( '<a href="%s">Submit your plugin for review</a>.', 'wporg-plugins' ), esc_url( home_url( 'about/add' ) ) ); ?></li>
+				 	<li><?php printf( __( 'After your plugin is <a href="%s">manually reviewed</a>, you will either be approved or emailed and asked to provide more information and/or make corrections.', 'wporg-plugins' ), esc_url( 'https://developer.wordpress.org/plugins/wordpress-org/plugin-developer-faq/#questions-about-submissions-and-approval' ) ); ?></li>
+				 	<li><?php printf( __( 'Once approved, you’ll be given access to a <a id="subversion" href="%s">Subversion Repository</a> where you’ll store your plugin.', 'wporg-plugins' ), esc_url( 'https://developer.wordpress.org/plugins/wordpress-org/how-to-use-subversion/' ) ); ?></li>
+
+				 	<li>
+				 		<?php
+					 	/* translators: 1: URL to readme section; 2: URL to home page; */
+					 	printf( __( 'Shortly after you upload your plugin (and a <a href="%1$s">readme file</a>!) to that repository, it will be automatically displayed in the <a href="%2$s">plugins browser</a>.', 'wporg-plugins' ), '#readme', esc_url( home_url( '/' ) ) );
+					 	?>
+					 </li>
+				 	<li><?php printf( __( 'Check out the <strong><a href="%s">FAQ</a> </strong>for more information.', 'wporg-plugins' ), esc_url( 'https://developer.wordpress.org/plugins/wordpress-org/plugin-developer-faq/' ) ); ?></li>
+				</ol>
+
+				<h3 id="readme"><?php _e( 'Readme files', 'wporg-plugins' ); ?></h3>
+				<p>
+					<?php
+					/* translators: 1: URL to readme file; 2: URL to readme validator; */
+					printf( __( 'To make your entry in the plugin browser most useful, each plugin should have a readme file named <code>readme.txt</code> that adheres to the <a href="%1$s">WordPress plugin readme file standard</a>. You can put your readme file through the <a href="%2$s">readme validator</a> to check it.', 'wporg-plugins' ), esc_url( 'https://wordpress.org/plugins/files/2016/06/readme.txt' ), esc_url( 'https://wordpress.org/plugins/wp-admin/edit.php?post_type=plugin&page=readme_validator' ) );
+					?>
+				</p>
+			</div><!-- .entry-content -->
+
+			<footer class="entry-footer">
+				<?php
+				edit_post_link(
+					sprintf(
+					/* translators: %s: Name of current post */
+						esc_html__( 'Edit %s', 'wporg-plugins' ),
+						the_title( '<span class="screen-reader-text">"', '"</span>', false )
+					),
+					'<span class="edit-link">',
+					'</span>'
+				);
+				?>
+			</footer><!-- .entry-footer -->
+		</article><!-- #post-## -->
+
+		<?php endwhile;	?>
+
+	</main><!-- #main -->
+
+<?php
+get_footer();
---
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-plugins/page-add.php
@@ -0,0 +1,73 @@
+<?php
+/**
+ * The template for the Add Your Plugin.
+ *
+ * @package WordPressdotorg\Plugin_Directory\Theme
+ */
+
+namespace WordPressdotorg\Plugin_Directory\Theme;
+
+get_header(); ?>
+
+	<main id="main" class="site-main" role="main">
+
+		<?php while ( have_posts() ) : the_post(); ?>
+
+		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+			<header class="entry-header">
+				<h1 class="entry-title"><?php _e( 'Add Your Plugin', 'wporg-plugins' ); ?></h1>
+			</header><!-- .entry-header -->
+
+			<div class="entry-content">
+				<p><?php printf( __( 'Once submitted, your plugin will be manually reviewed for any common errors as well as ensuring it complies with <a href="%s">all the guidelines</a>.', 'wporg-plugins' ), esc_url( 'https://developer.wordpress.org/plugins/wordpress-org/detailed-plugin-guidelines/' ) ); ?></p>
+
+				<?php do_shortcode( '[wporg-plugin-upload]' ); ?>
+
+				<p><?php _e( 'Even if you&#8217;ve submitted a dozen plugins, take the time to refresh your memory with the following information:', 'wporg-plugins' ); ?>
+				<ul>
+				 	<li><a href="https://developer.wordpress.org/plugins/wordpress-org/how-to-use-subversion/"><?php _e( 'How to use SVN', 'wporg-plugins' ); ?></a></li>
+				 	<li><a href="https://developer.wordpress.org/plugins/wordpress-org/deploying-your-plugin/"><?php _e( 'Deploying your plugin', 'wporg-plugins' ); ?></a></li>
+				 	<li><a href="https://developer.wordpress.org/plugins/wordpress-org/plugin-assets/"><?php _e( 'Plugin Assets (and how to use them)', 'wporg-plugins' ); ?></a></li>
+				 	<li><a href="https://developer.wordpress.org/plugins/wordpress-org/plugin-developer-faq/"><?php _e( 'Developer FAQ', 'wporg-plugins' ); ?></a></li>
+				</ul>
+
+				<hr />
+
+				<h3 id="faq"><?php _e( 'FAQ', 'wporg-plugins' ); ?></h3>
+
+				<h4><?php _e( 'How long will the review process take?', 'wporg-plugins' ); ?></h4>
+				<p><?php printf( __( 'This is in the <a href="%s">Developer FAQ</a>. It takes anywhere between 1 and 10 days. We attempt to review all plugins within 5 business days of submission, but the process takes as long as it takes, depending on the complexity of your plugin.', 'wporg-plugins' ), esc_url( 'https://developer.wordpress.org/plugins/wordpress-org/plugin-developer-faq/' ) ); ?></p>
+
+				<h4><?php _e( 'What will my plugin URL be?', 'wporg-plugins' ); ?></h4>
+				<p><?php _e( 'Your plugin&#8217;s URL will be populated based on the value of <code>Plugin Name</code> in your main plugin file (the one with the plugin headers). If you set yours as <code>Plugin Name: Boaty McBoatface</code> then your URL will be <code>https://wordpress.org/plugins/boaty-mcboatface</code> and your slug will be <code>boaty-mcboatface</code> for example. If there is an existing plugin with your name, then you will be <code>boaty-mcboatface-2</code> and so on. It behaves exactly like WordPress post names.', 'wporg-plugins' ); ?></p>
+				<p><?php _e( 'Once your plugin is approved, it <em>cannot</em> be renamed.', 'wporg-plugins' ); ?></p>
+
+				<h4><?php _e( 'I made a mistake in my plugin name. Should I resubmit?', 'wporg-plugins' ); ?></h4>
+				<p><?php _e( 'Please don&#8217;t! Instead email <code>plugins@wordpress.org</code> and we can rename your plugin as long as it&#8217;s not approved. Since we check emails first, the odds are we&#8217;ll catch it. If we don&#8217;t, just email us and explain the mistake. We&#8217;ll explain what do to.', 'wporg-plugins' ); ?></p>
+
+				<h4><?php _e( 'Why was I told my plugin name was unacceptable?', 'wporg-plugins' ); ?></h4>
+				<p><?php printf( __( 'This is explained in detail in our <a href="%s">detailed plugin guidelines</a>, but currently we give you the chance to rename it during the review process if the plugin name violates the guideline. Some terms (like &#8217;plugin&#8217; and &#8217;wordpress&#8217;) will be removed for you, as those should not be used at all. We get it. You&#8217;re a WordPress Plugin.', 'wporg-plugins' ), esc_url( 'https://developer.wordpress.org/plugins/wordpress-org/detailed-plugin-guidelines/' ) ); ?></p>
+				<p><?php _e( 'Regarding other companies and plugins&#8217; names and trademarks, don&#8217;t use them to start your plugin name. If you&#8217;re not Facebook, you shouldn&#8217;t submit a plugin that uses <code>facebook</code> as the first term in your slug. "Facebook Like Sharer" (which would be <code>facebook-like-sharer</code>) is not acceptable, but "Like Sharer for Facebook (which would be <code>like-sharer-for-facebook</code>) would be alright.', 'wporg-plugins' ); ?></p>
+			</div><!-- .entry-content -->
+
+			<footer class="entry-footer">
+				<?php
+				edit_post_link(
+					sprintf(
+					/* translators: %s: Name of current post */
+						esc_html__( 'Edit %s', 'wporg-plugins' ),
+						the_title( '<span class="screen-reader-text">"', '"</span>', false )
+					),
+					'<span class="edit-link">',
+					'</span>'
+				);
+				?>
+			</footer><!-- .entry-footer -->
+		</article><!-- #post-## -->
+
+		<?php endwhile;	?>
+
+	</main><!-- #main -->
+
+<?php
+get_footer();
