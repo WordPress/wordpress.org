8b8930f8a37983813ae280d0a5eeab2d29f98cbc
Konstantin Obenland <obenland@git.wordpress.org>
2016-06-14 08:36:02 +0000

    Plugin Directory: Second pass at upload form styles.
    This time, let's be less intrusive on other styles.
    
    See #1573, [3345].
    
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@3346 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-plugins/searchform.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-plugins/searchform.php
@@ -2,9 +2,7 @@
 namespace WordPressdotorg\Plugin_Directory\Theme;
 ?>
 <form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-	<label>
-		<span class="screen-reader-text"><?php _ex( 'Search for:', 'label', 'wporg-plugins' ); ?></span>
-		<input type="search" class="search-field" placeholder="<?php echo esc_attr_x( 'Search plugins&hellip;', 'placeholder', 'wporg-plugins' ); ?>" value="<?php the_search_query(); ?>" name="s" />
-		<button class="button button-primary button-search"><i class="dashicons dashicons-search"></i></button>
-	</label>
+	<label for="s" class="screen-reader-text"><?php _ex( 'Search for:', 'label', 'wporg-plugins' ); ?></label>
+	<input type="search" id="s" class="search-field" placeholder="<?php echo esc_attr_x( 'Search plugins&hellip;', 'placeholder', 'wporg-plugins' ); ?>" value="<?php the_search_query(); ?>" name="s" />
+	<button class="button button-primary button-search"><i class="dashicons dashicons-search"></i></button>
 </form>
