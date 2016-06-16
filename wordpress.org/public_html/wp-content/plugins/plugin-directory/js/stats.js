38511cd7b2ca920902caafa257e9812443d76b94
Konstantin Obenland <obenland@git.wordpress.org>
2016-06-16 11:03:59 +0000

    Plugin Directory: Update version stats to handle the new data source.
    See #1596.
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@3384 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/plugins/plugin-directory/js/stats.js
+++ wordpress.org/public_html/wp-content/plugins/plugin-directory/js/stats.js
@@ -52,100 +52,51 @@ google.load( "visualization", "1", { packages: ["corechart"] } );
 				return;
 			}
 
-			var bar_headers = [],
-				bar_values = [],
-				version_list = [],
-				to_combine = [],
-				older_versions = 0,
-				combine_limit = 6,
-				combine_percentage = 1,
-				version,
-				index,
-				bar_table_versions,
-				formatter;
-
-			// Start of the Google Datatable requirements.
-			bar_headers.push( '' );
-			bar_values.push( '' );
+			var barHeaders  = [ '' ],
+				barValues   = [ '' ],
+				versionList = [],
+				index       = 0,
+				version, data, formatter;
 
 			// Gather and sort the list of versions.
-			for ( version in versions ) {
-				version_list.push( version );
-			}
+			$.each( versions, function( version ) {
+				versionList.push( version );
+			} );
 
 			// Sort the version list by version.
-			version_list.sort( function( a, b ) {
+			versionList.sort( function( a, b ) {
 				a = a.split( '.' );
 				b = b.split( '.' );
 				return ( a[0] != b[0] ) ? a[0]-b[0] : a[1]-b[1];
 			} );
 
-			// Move any versions (old or new) with < combine_percentage into the combined list.
-			for ( index in version_list ) {
-				version = version_list[ index ];
-				if ( versions[ version ] <= combine_percentage ) {
-					to_combine.push( version );
-				}
-			}
-
-			// If a plugin has more than 6 versions, combine the older ones into a "Older Versions" group.
-			if ( version_list.length > combine_limit ) {
-				for ( index in version_list ) {
-					version = version_list[ index ];
-					if ( ( version_list.length - to_combine.length ) <= combine_limit ) {
-						break;
-					}
-					if ( to_combine.indexOf( version ) > -1 ) {
-						continue; // Next.
-					}
-					to_combine.push( version );
-				}
-			}
-
-			// Only combine when there's multiples in there
-			if ( to_combine.length > 1 ) {
-				to_combine.forEach( function( version ) {
-					older_versions += versions[ version ];
-
-					delete versions[ version ];
-					delete version_list[ version_list.indexOf( version ) ];
-				} );
-			}
-
-			if ( older_versions ) {
-				bar_headers.push( settings.l10n.otherVersions );
-				bar_values.push( Math.round( older_versions * 100 ) / 100 );
-			}
+			// Move 'other' versions to the beginning.
+			versionList.unshift( versionList.pop() );
 
 			// Add all the versions
-			version_list.forEach( function( version ) {
-				bar_headers.push( version );
-				bar_values.push( versions[ version ] );
+			versionList.forEach( function( version ) {
+				barHeaders.push( version );
+				barValues.push( versions[ version ] );
 			} );
 
-			bar_headers.push( { role: 'annotation' } );
-			bar_values.push( '' );
-
-			bar_table_versions = google.visualization.arrayToDataTable([
-				bar_headers,
-				bar_values
+			data = google.visualization.arrayToDataTable([
+				barHeaders,
+				barValues
 			]);
 
 			// Format it as percentages
-			formatter = new google.visualization.NumberFormat({
+			formatter = new google.visualization.NumberFormat( {
 				fractionDigits: 1,
 				suffix: '%'
-			});
-
-			index = 0;
-			bar_values.forEach( function( value ) {
-				if ( bar_values[ value ] ) {
-					formatter.format( bar_table_versions, index );
-				}
-				index++;
 			} );
 
-			new google.visualization.BarChart( document.getElementById( 'plugin-version-stats' ) ).draw( bar_table_versions, {
+			$.each( barValues, function( value ) {
+				if ( barValues[ value ] ) {
+					formatter.format( data, ++index );
+				}
+			} );
+
+			new google.visualization.BarChart( document.getElementById( 'plugin-version-stats' ) ).draw( data, {
 				legend: {
 					position: 'bottom'
 				},
