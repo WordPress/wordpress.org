846e1128d5417976502fec0a463366912f5e5f64
Konstantin Obenland <obenland@git.wordpress.org>
2016-06-17 09:11:50 +0000

    Plugin Directory: More semantic screenshots markup.
    See #1719.
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@3401 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/plugins/plugin-directory/shortcodes/class-screenshots.php
+++ wordpress.org/public_html/wp-content/plugins/plugin-directory/shortcodes/class-screenshots.php
@@ -1,6 +1,5 @@
 <?php
 namespace WordPressdotorg\Plugin_Directory\Shortcodes;
-
 use WordPressdotorg\Plugin_Directory\Template;
 
 /**
@@ -18,32 +17,25 @@ class Screenshots {
 		$output = '';
 
 		// All indexed from 1.
-		$screenshot_descriptions = get_post_meta( $plugin->ID, 'screenshots', true );
-		$assets_screenshots      = get_post_meta( $plugin->ID, 'assets_screenshots', true );
+		$descriptions = get_post_meta( $plugin->ID, 'screenshots', true );
+		$screen_shots = get_post_meta( $plugin->ID, 'assets_screenshots', true );
 
-		foreach ( $screenshot_descriptions as $index => $description ) {
+		/*
+		 * Find the image that corresponds with the text.
+		 * The image numbers are stored within the 'resolution' key.
+		 */
+		foreach ( $screen_shots as $image ) {
+			$screen_shot = sprintf( '<a href="%1$s" rel="nofollow"><img class="screenshot" src="%1$s" /></a>',
+				Template::get_asset_url( $plugin->post_name, $image )
+			);
 
-			/*
-			 * Find the image that corresponds with the text.
-			 * The image numbers are stored within the 'resolution' key.
-			 */
-			foreach ( $assets_screenshots as $image ) {
-				if ( $index == $image['resolution'] ) {
-					$output .= sprintf(
-						'<li>
-							<a href="%1$s" rel="nofollow">
-								<img class="screenshot" src="%1$s">
-							</a>
-							<p>%2$s</p>
-						</li>',
-						Template::get_asset_url( $plugin->post_name, $image ),
-						$description
-					);
-					break;
-				}
+			if ( array_key_exists( $image['resolution'], $descriptions ) ) {
+				$screen_shot .= '<figcaption>' . esc_html( $descriptions[ $image['resolution'] ] ) . '</figcaption>';
 			}
+
+			$output .= '<li><figure>' . $screen_shot . '</figure></li>';
 		}
 
-		return '<ol class="screenshots">' . $output . '</ol>';
+		return '<ul class="plugin-screenshots">' . $output . '</ul>';
 	}
 }
