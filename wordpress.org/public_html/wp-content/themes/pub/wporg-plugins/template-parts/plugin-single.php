47abef2acc0deca4dd44b78cde7f8c4018b0b3ae
Konstantin Obenland <obenland@git.wordpress.org>
2016-06-18 13:04:01 +0000

    Plugin Directory: Have reviews section always explanded.
    See #1719.
    
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@3428 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-plugins/template-parts/plugin-single.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-plugins/template-parts/plugin-single.php
@@ -75,7 +75,7 @@ $widget_args = array(
 		the_widget( 'WordPressdotorg\Plugin_Directory\Widgets\Meta',    array(), $widget_args );
 		the_widget( 'WordPressdotorg\Plugin_Directory\Widgets\Ratings', array(), $widget_args );
 		the_widget( 'WordPressdotorg\Plugin_Directory\Widgets\Support', array(), $widget_args );
-		the_widget( 'WordPressdotorg\Plugin_Directory\Widgets\Donate', array(), $widget_args );
+		the_widget( 'WordPressdotorg\Plugin_Directory\Widgets\Donate',  array(), $widget_args );
 		?>
 	</div><!-- .entry-meta -->
 </article><!-- #post-## -->
