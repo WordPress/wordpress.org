fc5d558041e492edfd2efddd3909af1441921d29
Scott Reilly <coffee2code@git.wordpress.org>
2016-06-19 22:08:49 +0000

    W.org P2 child theme: For handbook-related templates, use the current post type instead of hard-coding 'handbook' when getting archive url and dynamic sidebar.
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@3488 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-p2/sidebar-handbook.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-p2/sidebar-handbook.php
@@ -5,12 +5,11 @@
  * @package P2
  */
 
-
-if ( is_active_sidebar( 'handbook' ) ) : ?>
+if ( is_active_sidebar( wporg_get_current_handbook() ) ) : ?>
 	<div id="sidebar">
 		<?php do_action( 'before_sidebar' ); ?>
 		<ul>
-			<?php dynamic_sidebar( 'handbook' ); ?>
+			<?php dynamic_sidebar( wporg_get_current_handbook() ); ?>
 		</ul>
 		<div class="clear"></div>
 	</div> <!-- // sidebar -->
--- wordpress.org/public_html/wp-content/themes/pub/wporg-p2/single-handbook.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-p2/single-handbook.php
@@ -7,7 +7,7 @@
 ?>
 <?php get_header(); ?>
 
-<div class="handbook-name"><span><a href="<?php echo esc_url( get_post_type_archive_link( 'handbook' ) ); ?>"><?php echo esc_html( WPorg_Handbook::get_name() ); ?></a></span></div>
+<div class="handbook-name"><span><a href="<?php echo esc_url( get_post_type_archive_link( wporg_get_current_handbook() ) ); ?>"><?php echo esc_html( wporg_get_current_handbook_name() ); ?></a></span></div>
 
 <!-- Also called on in footer but will not display the second time. -->
 <?php get_sidebar( 'handbook' ); ?> 
