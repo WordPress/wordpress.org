e42b89174335ecb1c5975e26ea8d324a8e1a2411
Jennifer M. Dodd <jmdodd@git.wordpress.org>
2016-06-26 10:25:28 +0000

    Breathe: Use left border instead of top border on mobile to denote post resolved state.
    See #1778.
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@3580 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-breathe/functions.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-breathe/functions.php
@@ -5,6 +5,6 @@ function styles() {
 	wp_enqueue_style( 'p2-breathe', get_template_directory_uri() . '/style.css' );
 
 	// Cacheing hack
-	wp_enqueue_style( 'wporg-breathe', get_stylesheet_uri(), array( 'p2-breathe' ), '20160626' );
+	wp_enqueue_style( 'wporg-breathe', get_stylesheet_uri(), array( 'p2-breathe' ), '20160626a' );
 }
 add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\styles', 9 );
