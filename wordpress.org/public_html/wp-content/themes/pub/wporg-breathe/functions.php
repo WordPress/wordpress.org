0fe0e04bd48b29738d794580e21993657cbdf8c8
Gary Pendergast <pento@git.wordpress.org>
2016-07-08 02:06:12 +0000

    Breathe: remove horizontal scroll in Safari.
    Fixes #1815.
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@3647 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-breathe/functions.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-breathe/functions.php
@@ -6,7 +6,7 @@ function styles() {
 	wp_enqueue_style( 'p2-breathe', get_template_directory_uri() . '/style.css' );
 
 	// Cacheing hack
-	wp_enqueue_style( 'wporg-breathe', get_stylesheet_uri(), array( 'p2-breathe' ), '20160707g' );
+	wp_enqueue_style( 'wporg-breathe', get_stylesheet_uri(), array( 'p2-breathe' ), '20160708' );
 }
 add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\styles', 11 );
 
