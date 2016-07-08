f1f6cd614bd34859da70398052e4405572634cff
Dominik Schilling <ocean90@git.wordpress.org>
2016-07-08 23:20:21 +0000

    Rosetta Showcase: Move logic into a class so it can be initialized only when needed.
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@3655 74240141-8908-4e6f-9713-ba540dce6ec7

--- global.wordpress.org/public_html/wp-content/mu-plugins/showcase/rosetta-showcase.php
+++ global.wordpress.org/public_html/wp-content/mu-plugins/showcase/rosetta-showcase.php
@@ -1,147 +1,255 @@
 <?php
-/*
+/**
  * Plugin Name: Rosetta Showcase
+ * Plugin URI: https://wordpress.org/
+ * Description: Showcase for local sites.
  * Author: Zé Fontainhas and Andrew Nacin
+ * Version: 2.0
  */
 
+class Rosetta_Showcase {
 
-add_action( 'init', 'rosetta_showcase_register' );
+	/**
+	 * Name of the custom post type.
+	 *
+	 * @var string
+	 */
+	public $post_type = 'showcase';
 
-function rosetta_showcase_register() {
-	$labels = array(
-		'name' => _x( 'Showcase', 'post type general name', 'rosetta' ),
-		'singular_name' => _x( 'Showcase Site', 'post type singular name', 'rosetta' ),
-		'add_new' => _x( 'Add New', 'showcase item', 'rosetta' ),
-		'add_new_item' => __( 'Add New Site', 'rosetta' ),
-		'edit_item' => __( 'Edit Site', 'rosetta' ),
-		'new_item' => __( 'New Site', 'rosetta' ),
-		'view_item' => __( 'View Site', 'rosetta' ),
-		'search_items' => __( 'Search Showcase', 'rosetta' ),
-		'not_found' =>  __( 'Nothing found', 'rosetta' ),
-		'not_found_in_trash' => __( 'Nothing found in Trash', 'rosetta' ),
-	);
+	/**
+	 * Constructor.
+	 */
+	public function __construct() {
+		add_action( 'plugins_loaded', array( $this, 'plugins_loaded' ) );
+	}
 
-	$args = array(
-		'labels' => $labels,
-		'public' => true,
-		'show_ui' => true,
-		'rewrite' => false,
-		'has_archive' => true,
-		'capability_type' => 'post',
-		'map_meta_cap' => true,
-		'hierarchical' => false,
-		'supports' => array( 'title','excerpt' ),
-		'menu_icon' => 'dashicons-slides',
-	);
+	/**
+	 * Attaches hooks once plugins are loaded.
+	 */
+	public function plugins_loaded() {
+		add_action( 'init', [ $this, 'register_post_type' ] );
+		add_filter( 'post_type_link', [ $this, 'filter_showcase_permalink' ], 10, 2 );
+		add_action( 'save_post', [ $this, 'save_showcase_url' ], 10, 2 );
+		add_action( 'manage_' . $this->post_type . '_posts_columns',  [ $this, 'showcase_custom_columns' ] );
+		add_filter( 'manage_' . $this->post_type . '_posts_custom_column', [ $this, 'showcase_custom_column' ], 10, 2 );
+		add_filter( 'manage_edit-' . $this->post_type . '_sortable_columns', [ $this, 'showcase_sortable_columns' ], 10, 0 );
+		add_filter( 'sharing_meta_box_show', [ $this, 'disable_sharing_meta_box' ], 10, 2 );
+	}
 
-	register_post_type( 'showcase', $args );
-}
+	/**
+	 * Registers the custom post type used for the showcase.
+	 */
+	public function register_post_type() {
+		$labels = array(
+			'name'               => _x( 'Showcase', 'post type general name', 'rosetta' ),
+			'singular_name'      => _x( 'Showcase Site', 'post type singular name', 'rosetta' ),
+			'add_new'            => _x( 'Add New', 'showcase item', 'rosetta' ),
+			'add_new_item'       => __( 'Add New Site', 'rosetta' ),
+			'edit_item'          => __( 'Edit Site', 'rosetta' ),
+			'new_item'           => __( 'New Site', 'rosetta' ),
+			'view_item'          => __( 'View Site', 'rosetta' ),
+			'search_items'       => __( 'Search Showcase', 'rosetta' ),
+			'not_found'          => __( 'Nothing found', 'rosetta' ),
+			'not_found_in_trash' => __( 'Nothing found in Trash', 'rosetta' ),
+		);
 
-add_filter( 'post_type_link', 'rosetta_filter_showcase_permalink', 10, 2 );
+		$args = array(
+			'labels'               => $labels,
+			'public'               => true,
+			'show_ui'              => true,
+			'rewrite'              => false,
+			'has_archive'          => false,
+			'capability_type'      => 'post',
+			'map_meta_cap'         => true,
+			'hierarchical'         => false,
+			'show_in_nav_menus'    => false,
+			'can_export'           => false,
+			'exclude_from_search'  => true,
+			'supports'             => array( 'title', 'excerpt' ),
+			'menu_icon'            => 'dashicons-slides',
+			'register_meta_box_cb' => [ $this, 'register_showcase_meta_box' ],
+		);
 
-function rosetta_filter_showcase_permalink( $post_link, $post ) {
-	if ( 'showcase' == $post->post_type ) {
+		register_post_type( $this->post_type, $args );
+	}
+
+	/**
+	 * Replaces the default permalink with the URL to the website.
+	 *
+	 * @param string  $post_link The post's permalink.
+	 * @param WP_Post $post      The post in question.
+	 * @return string The filtered URL.
+	 */
+	public function filter_showcase_permalink( $post_link, $post ) {
+		if ( $this->post_type === $post->post_type ) {
+			$url = get_post_meta( $post->ID, '_rosetta_showcase_url', true );
+			if ( $url ) {
+				return $url;
+			}
+		}
+
+		return $post_link;
+	}
+
+	/**
+	 * Replaces the default excerpt metabox with a custom one.
+	 */
+	public function register_showcase_meta_box() {
+		remove_meta_box(
+			'postexcerpt',
+			$this->post_type,
+			'normal'
+		);
+
+		add_meta_box(
+			'rosetta_showcase_meta',
+			__( 'Description and URL', 'rosetta' ),
+			[ $this, 'showcase_meta_box' ],
+			$this->post_type,
+			'normal',
+			'default'
+		);
+	}
+
+	/**
+	 * Prints the metabox for the URL and description of a website.
+	 *
+	 * @param WP_Post $post Post object.
+	 */
+	public function showcase_meta_box( $post ) {
 		$url = get_post_meta( $post->ID, '_rosetta_showcase_url', true );
-		if ( $url ) {
-			return $url;
+		?>
+		<p><label for="rosetta_showcase_url"><?php _e( 'URL', 'rosetta' ); ?></label>
+			<input style="margin-left: 0; width: 98%" name="rosetta_showcase_url" type="text" value="<?php echo esc_url( $url ); ?>" /></p>
+		<label for="excerpt"><?php _e( 'Description', 'rosetta' ); ?></label>
+		<textarea rows="1" cols="40" name="excerpt" tabindex="6" id="excerpt"><?php echo $post->post_excerpt; // textarea_escaped ?></textarea>
+		<?php
+	}
+
+	/**
+	 * Saves the URL of a website as a post meta.
+	 *
+	 * @param int     $post_id Post ID.
+	 * @param WP_Post $post    Post object.
+	 */
+	public function save_showcase_url( $post_id, $post ) {
+		if ( $this->post_type !== $post->post_type ) {
+			return;
+		}
+
+		if ( ! current_user_can( 'edit_post', $post_id ) ) {
+			return;
+		}
+
+		if ( defined( 'DOING_AUTOSAVE' ) || defined( 'DOING_AJAX' ) ) {
+			return;
+		}
+
+		if ( ! isset( $_POST['rosetta_showcase_url'] ) ) {
+			return;
+		}
+
+		$url = esc_url_raw( $_POST['rosetta_showcase_url'] );
+
+		update_post_meta( $post_id, '_rosetta_showcase_url', $url );
+	}
+
+	/**
+	 * Disables Jetpack's sharing meta box for a showcase item.
+	 *
+	 * @param $enable Whether the metabox is visible
+	 * @param WP_Post $post    Post object.
+	 * @return bool True if metabox is visible, false if not.
+	 */
+	public function disable_sharing_meta_box( $enable, $post ) {
+		if ( $this->post_type === $post->post_type ) {
+			return false;
+		}
+
+		return $enable;
+	}
+
+	/**
+	 * Filters the columns displayed in the list table.
+	 *
+	 * @param array $columns An array of column names.
+	 * @return array An array of column names.
+	 */
+	public function showcase_custom_columns( $columns ) {
+		$columns = [
+			'cb'          => $columns['cb'],
+			'shot'        => __( 'Image', 'rosetta' ),
+			'website'     => __( 'Website', 'rosetta' ),
+			'description' => __( 'Description', 'rosetta' ),
+			'url'         => __( 'URL', 'rosetta' ),
+		];
+
+		return $columns;
+	}
+
+	/**
+	 * Prints the content for each custom column in the list table.
+	 *
+	 * @param string $column   The name of the column to display.
+	 * @param int    $post_id  The current post ID.
+	 */
+	public function showcase_custom_column( $column, $post_id ) {
+		switch ( $column ) {
+			case 'website' :
+				$title = _draft_or_post_title();
+				printf(
+					'<a class="row-title" href="%s" aria-label="%s">%s</a>',
+					get_edit_post_link( $post_id ),
+					/* translators: %s: post title */
+					esc_attr( sprintf( __( '&#8220;%s&#8221; (Edit)', 'rosetta' ), $title ) ),
+					$title
+				);
+				break;
+
+			case 'description' :
+				the_excerpt();
+				break;
+
+			case 'url' :
+				$url = get_post_meta( $post_id, '_rosetta_showcase_url', true );
+				echo '<a href="' . esc_url( $url ) . '">' . esc_url_raw( $url ) . '</a>';
+				break;
+
+			case 'shot' :
+				$url = esc_url( get_post_meta( $post_id, '_rosetta_showcase_url', true ) );
+				if ( $url ) {
+					echo '<a href="' . esc_url( $url ) . '" target="_blank"><img width="200" src="https://wordpress.com/mshots/v1/' . urlencode( $url ) . '?w=400" /></a>';
+				}
+				break;
 		}
 	}
-	return $post_link;
-}
 
-add_action( 'add_meta_boxes_showcase', 'rosetta_add_showcase_meta_box' );
+	/**
+	 * Filters the list table sortable columns.
+	 *
+	 * @return array An array of sortable columns.
+	 */
+	public function showcase_sortable_columns() {
+		$columns = [
+			'website' => 'title',
+		];
 
-function rosetta_add_showcase_meta_box() {
-	remove_meta_box( 'postexcerpt', 'showcase', 'normal' );
-	add_meta_box( 'rosetta_showcase_meta', __( 'Description and URL', 'rosetta' ), 'rosetta_showcase_meta_box', 'showcase', 'normal', 'default' );
-}
-
-function rosetta_showcase_meta_box( $post ) {
-	$url = get_post_meta( $post->ID, '_rosetta_showcase_url', true );
-	?>
-	<p><label for="rosetta_showcase_url"><?php _e( 'URL' ) // core ?></label>
-	<input style="margin-left: 0; width: 98%" name="rosetta_showcase_url" type="text" value="<?php echo esc_url( $url ); ?>" /></p>
-	<label for="excerpt"><?php _e( 'Description' ) // core ?></label>
-	<textarea rows="1" cols="40" name="excerpt" tabindex="6" id="excerpt"><?php echo $post->post_excerpt; // textarea_escaped ?></textarea>
-	<?php
-}
-
-add_action( 'save_post', 'save_showcase_url', 10, 2 );
-
-function save_showcase_url( $post_id, $post ) {
-
-	if ( $post->post_type != 'showcase' ) {
-		return;
+		return $columns;
 	}
 
-	if ( ! current_user_can( 'edit_post', $post_id ) ) {
-		return;
-	}
+	/**
+	 * Retrieves four random showcase posts to be used on the front end.
+	 *
+	 * @return array List of showcase posts.
+	 */
+	public function front() {
+		$posts = get_posts( [
+			'post_type'   => $this->post_type,
+			'numberposts' => - 1,
+		] );
 
-	if ( defined( 'DOING_AUTOSAVE' ) || defined( 'DOING_AJAX' ) ) {
-		return;
-	}
-
-	if ( ! isset( $_POST['rosetta_showcase_url' ] ) ) {
-		return;
-	}
-
-	$url = esc_url_raw( $_POST['rosetta_showcase_url'] );
-
-	update_post_meta( $post_id, '_rosetta_showcase_url', $url );
-}
-
-/* Show columns on the 'All Sites' page */
-
-add_action( 'manage_showcase_posts_columns',  'rosetta_showcase_custom_columns' );
-add_filter( 'manage_showcase_posts_custom_column', 'rosetta_showcase_custom_column', 10, 2 );
-
-function rosetta_showcase_custom_columns( $columns ) {
-	$columns = array(
-		'cb' => $columns['cb'],
-		'shot' => __( 'Image' ), // core
-		'title' => __( 'Website' ), // core
-		'description' => __( 'Description' ), // core
-		'url' => __( 'URL' ), // core
-	);
-	return $columns;
-}
-
-function rosetta_showcase_custom_column( $column, $post_id ) {
-	$post = get_post( $post_id );
-
-	switch ( $column ) {
-		case 'description' :
-			the_excerpt();
-			break;
-		case 'url' :
-			$url = get_post_meta( $post_id, '_rosetta_showcase_url', true );
-			echo '<a href="' . esc_url( $url ) . '">' . esc_url_raw( $url ) . '</a>';
-			break;
-		case 'shot' :
-			$url = esc_url( get_post_meta( $post_id, '_rosetta_showcase_url', true ) );
-			if ( $url ) {
-				echo '<a href="' . $url . '" target="_blank"><img width="100" src="https://wordpress.com/mshots/v1/' . urlencode( $url ) . '?w=100" /></a>';
-			}
-			break;
-	}
-}
-
-/* Add Showcase posts to the main loop and feed */
-//add_filter( 'pre_get_posts', 'rosetta_get_posts_with_showcase' );
-function rosetta_get_posts_with_showcase( $query ) {
-	global $wp_the_query;
-	if ( ( is_home() && $wp_the_query === $query ) || is_feed() ) {
-		$query->set( 'post_type', array( 'post', 'showcase' ) );
-	}
-
-	return $query;
-}
-
-class Rosetta_Showcase {
-	function front() {
-		$posts = get_posts( array( 'post_type' => 'showcase', 'numberposts' => -1 ) );
 		shuffle( $posts );
+
 		return array_slice( $posts, 0, 4 );
 	}
 }
