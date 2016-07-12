15f1614ffc1583df17859109fd4e44945e642a97
Drew Jaynes <drewapicture@git.wordpress.org>
2016-07-12 00:06:42 +0000

    developer.wordpress.org: Display changelog data in a table, ordered newest-to-oldest.
    See #1819. See #1820.
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@3668 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-developer/reference/template-changelog.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-developer/reference/template-changelog.php
@@ -14,19 +14,44 @@ if ( ! empty( $changelog_data ) ) :
 	<hr />
 	<section class="changelog">
 		<h3><?php _e( 'Changelog', 'wporg' ); ?></h3>
-		<ul>
-			<?php foreach ( $changelog_data as $version => $data ) : ?>
-				<li>
-					<strong><?php _e( 'Since:', 'wporg' ); ?></strong>
-					<?php printf(
-					/* translators: %s: WordPress version */
-						__( 'WordPress %s', 'wporg' ),
-						sprintf( '<a href="%1$s">%2$s</a>', esc_url( $data['since_url'] ), esc_html( $version ) )
-					); ?>
-					<?php echo $data['description']; // escaped in get_changelog_data() ?>
-				</li>
-			<?php endforeach; ?>
-		</ul>
+
+		<table>
+			<caption class="screen-reader-text"><?php _e( 'Changelog', 'wporg' ); ?></caption>
+			<thead>
+				<tr>
+					<th class="changelog-version"><?php _e( 'Version', 'wporg' ); ?></th>
+					<th class="changelog-desc"><?php _e( 'Description', 'wporg' ); ?></th>
+				</tr>
+			</thead>
+
+			<tbody>
+				<?php
+				$count = count( $changelog_data );
+				$i = 0;
+
+				$changelog_data = array_reverse( $changelog_data );
+
+				foreach ( $changelog_data as $version => $data ) : ?>
+					<?php
+					// Add "Introduced." for the initial version description, last since the array is reversed.
+					$data['description'] = ( $i == ( $count - 1 ) ) ? __( 'Introduced.', 'wporg' ) : $data['description'];
+
+					$version_link = sprintf( '<a href="%1$s" alt="%2$s">%3$s</a>',
+						esc_url( $data['since_url'] ),
+						esc_attr( "WordPress {$version}" ),
+						esc_html( $version )
+					);
+
+					$i++;
+					?>
+
+					<tr>
+						<td><?php echo $version_link; ?></td>
+						<td><?php echo $data['description']; ?></td>
+					</tr>
+				<?php endforeach; ?>
+			</tbody>
+		</table>
 	</section>
 <?php endif; ?>
 
