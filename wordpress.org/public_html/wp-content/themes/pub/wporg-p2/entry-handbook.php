c65ae27da4ba2defc0057bfc6aa1bbda9d715b5d
Scott Reilly <coffee2code@git.wordpress.org>
2016-07-15 19:00:48 +0000

    W.org P2 child theme: Discontinue display of comments for handbook pages.
    Fixes #79.
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@3684 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-p2/entry-handbook.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-p2/entry-handbook.php
@@ -43,49 +43,6 @@
 	<?php the_content( __( '(More ...)' , 'p2' ) ); ?>
 	</div>
 
-	<?php
-	/*
-	 * Comments
-	 */
-
-	$comment_field = '<div class="form"><textarea id="comment" class="expand50-100" name="comment" cols="45" rows="3"></textarea></div> <label class="post-error" for="comment" id="commenttext_error"></label>';
-
-	$comment_notes_before = '<p class="comment-notes">' . ( get_option( 'require_name_email' ) ? sprintf( ' ' . __( 'Required fields are marked %s', 'p2' ), '<span class="required">*</span>' ) : '' ) . '</p>';
-
-	$p2_comment_args = array(
-		'title_reply'           => __( 'Reply', 'p2' ),
-		'comment_field'         => $comment_field,
-		'comment_notes_before'  => $comment_notes_before,
-		'comment_notes_after'   => '<span class="progress spinner-comment-new"></span>',
-		'label_submit'          => __( 'Reply', 'p2' ),
-		'id_submit'             => 'comment-submit',
-	);
-
-	?>
-
-	<?php if ( get_comments_number() > 0 && ! post_password_required() ) : ?>
-		<div class="discussion" style="display: none">
-			<p>
-				<?php p2_discussion_links(); ?>
-				<a href="#" class="show-comments"><?php _e( 'Toggle Comments', 'p2' ); ?></a>
-			</p>
-		</div>
-	<?php endif;
-
-	wp_link_pages( array( 'before' => '<p class="page-nav">' . __( 'Pages:', 'p2' ) ) ); ?>
-
 	<div class="bottom-of-entry">&nbsp;</div>
 
-	<?php if ( p2_is_ajax_request() ) : ?>
-		<ul id="comments-<?php the_ID(); ?>" class="commentlist inlinecomments"></ul>
-	<?php else :
-		comments_template();
-		$pc = 0;
-		if ( p2_show_comment_form() && $pc == 0 && ! post_password_required() ) :
-			$pc++; ?>
-			<div class="respond-wrap">
-				<?php comment_form( $p2_comment_args ); ?>
-			</div><?php
-		endif;
-	endif; ?>
 </li>
