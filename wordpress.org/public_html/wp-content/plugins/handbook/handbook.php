bd3e00e3c04bba82fe835933e48dd12c97bec9a8
Scott Reilly <coffee2code@git.wordpress.org>
2016-07-15 19:20:10 +0000

    Handbook plugin: Bump callout-boxes CSS version for cache bust.
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@3689 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/plugins/handbook/handbook.php
+++ wordpress.org/public_html/wp-content/plugins/handbook/handbook.php
@@ -40,7 +40,7 @@ class WPorg_Handbook_Init {
 	}
 
 	static public function enqueue_styles() {
-		wp_enqueue_style( 'wporg-handbook-css', plugins_url( '/stylesheets/callout-boxes.css', __FILE__ ), array(), '20160224' );
+		wp_enqueue_style( 'wporg-handbook-css', plugins_url( '/stylesheets/callout-boxes.css', __FILE__ ), array(), '20160715' );
 	}
 
 	static public function enqueue_scripts() {
