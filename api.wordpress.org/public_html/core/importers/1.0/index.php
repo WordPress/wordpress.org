a5f9f16095b53df8131fc2b23773562c737c4c22
Dominik Schilling <ocean90@git.wordpress.org>
2016-07-16 15:42:29 +0000

    Importers API: Add support for a `version` parameter.
    #core35191 is going to change the layout of the Importer screen. It uses new strings to make the description consistent with the plugin state.
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@3690 74240141-8908-4e6f-9713-ba540dce6ec7

--- api.wordpress.org/public_html/core/importers/1.0/index.php
+++ api.wordpress.org/public_html/core/importers/1.0/index.php
@@ -1,15 +1,77 @@
 <?php
 
-$popular_importers = wporg_api_get_popular_importers();
-# unset( $popular_importers['tumblr'] );
+$version = '';
+if ( isset( $_REQUEST['version'] ) ) { // Introduced in WordPress 4.6.
+	$version = str_replace( '-src', '', $_REQUEST['version'] );
+}
+
+if ( version_compare( $version, '4.6-beta', '>=' ) ) {
+	$popular_importers = wporg_api_get_popular_importers_46();
+} else {
+	$popular_importers = wporg_api_get_popular_importers();
+}
 
 $response = array( 'importers' => $popular_importers, 'translated' => false );
 echo defined( 'JSON_RESPONSE' ) ? json_encode( $response ) : serialize( $response );
 
 function __( $string ) { return $string; }
 
-// This function is synced in wp-admin/includes/import.php.
+// This function is synced in wp-admin/includes/import.php of >= 4.6
+function wporg_api_get_popular_importers_46() {
+	return array(
+		// slug => name, description, plugin slug, and register_importer() slug
+		'blogger' => array(
+			'name' => __( 'Blogger' ),
+			'description' => __( 'Import posts, comments, and users from a Blogger blog.' ),
+			'plugin-slug' => 'blogger-importer',
+			'importer-id' => 'blogger',
+		),
+		'wpcat2tag' => array(
+			'name' => __( 'Categories and Tags Converter' ),
+			'description' => __( 'Convert existing categories to tags or tags to categories, selectively.' ),
+			'plugin-slug' => 'wpcat2tag-importer',
+			'importer-id' => 'wp-cat2tag',
+		),
+		'livejournal' => array(
+			'name' => __( 'LiveJournal' ),
+			'description' => __( 'Import posts from LiveJournal using their API.' ),
+			'plugin-slug' => 'livejournal-importer',
+			'importer-id' => 'livejournal',
+		),
+		'movabletype' => array(
+			'name' => __( 'Movable Type and TypePad' ),
+			'description' => __( 'Import posts and comments from a Movable Type or TypePad blog.' ),
+			'plugin-slug' => 'movabletype-importer',
+			'importer-id' => 'mt',
+		),
+		'opml' => array(
+			'name' => __( 'Blogroll' ),
+			'description' => __( 'Import links in OPML format.' ),
+			'plugin-slug' => 'opml-importer',
+			'importer-id' => 'opml',
+		),
+		'rss' => array(
+			'name' => __( 'RSS' ),
+			'description' => __( 'Import posts from an RSS feed.' ),
+			'plugin-slug' => 'rss-importer',
+			'importer-id' => 'rss',
+		),
+		'tumblr' => array(
+			'name' => __( 'Tumblr' ),
+			'description' => __( 'Import posts &amp; media from Tumblr using their API.' ),
+			'plugin-slug' => 'tumblr-importer',
+			'importer-id' => 'tumblr',
+		),
+		'wordpress' => array(
+			'name' => 'WordPress',
+			'description' => __( 'Import posts, pages, comments, custom fields, categories, and tags from a WordPress export file.' ),
+			'plugin-slug' => 'wordpress-importer',
+			'importer-id' => 'wordpress',
+		),
+	);
+}
 
+// This function is synced in wp-admin/includes/import.php of <= 4.5
 function wporg_api_get_popular_importers() {
 	return array(
 		// slug => name, description, plugin slug, and register_importer() slug
@@ -63,4 +125,3 @@ function wporg_api_get_popular_importers() {
 		),
 	);
 }
-
