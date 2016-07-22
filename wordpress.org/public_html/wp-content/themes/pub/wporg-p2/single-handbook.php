b40f268add10df195e868d64523e5bd2f5942ac6
Scott Reilly <coffee2code@git.wordpress.org>
2016-07-22 17:38:13 +0000

    W.org P2 child theme: Use `h1` tag for handbook page title.
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@3711 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-p2/single-handbook.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-p2/single-handbook.php
@@ -17,7 +17,7 @@
 <div class="sleeve_main">
 
 	<div id="main">
-		<h2 class="handbook-page-title"><?php the_title(); ?></h2>
+		<h1 class="handbook-page-title"><?php the_title(); ?></h1>
 		<?php if ( have_posts() ) : ?>
 
 			<ul id="postlist">
