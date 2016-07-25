e6e51343c818e726c278356334d74a98e0484b4a
Scott Reilly <coffee2code@git.wordpress.org>
2016-07-25 21:23:37 +0000

    developer.wordpress.org: Fix sidebar display after changes to handbook sidebar markup in [3686].
    Props SergeyBiryukov.
    Fixes #1857.
    
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@3722 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-developer/sidebar.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-developer/sidebar.php
@@ -8,9 +8,9 @@
 <?php if ( is_active_sidebar( get_post_type() ) ) : ?>
 	<div id="sidebar" class="widget-area sidebar section" role="complementary">
 		<?php do_action( 'before_sidebar' ); ?>
-		<ul>
-			<?php if ( ! dynamic_sidebar( get_post_type() ) ) : ?>
-			<?php endif; // end sidebar widget area ?>
-		</ul>
+
+		<?php if ( ! dynamic_sidebar( get_post_type() ) ) : ?>
+		<?php endif; // end sidebar widget area ?>
+
 	</div><!-- #secondary -->
 <?php endif; ?>
