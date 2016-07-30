400c35a560d335512fd27e7941e3c5a9ff226e43
Dominik Schilling <ocean90@git.wordpress.org>
2016-07-30 20:30:37 +0000

    Forums Theme: Use `get_header()` in the 404 template.
    Ensures that the correct navigation is loaded.
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@3756 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-forums/404.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-forums/404.php
@@ -1,17 +1,12 @@
-<?php wporg_get_global_header(); ?>
-
-<div id="headline">
-	<div class="wrapper">
-		<h2><?php _e( 'Page Not Found', 'wporg-forums' ); ?></h2>
-	</div>
-</div>
+<?php get_header(); ?>
 
 <div id="pagebody">
 	<div class="wrapper">
 		<div class="col-12">
+			<h3><?php _e( 'Page Not Found', 'wporg-forums' ); ?></h3>
 			<p class="intro"><?php _e( 'The page you were looking for could not be found. I&#8217;m sorry, it&#8217;s not your fault&hellip; probably.', 'wporg-forums' ); ?></p>
 		</div>
 	</div>
 </div>
 
-<?php wporg_get_global_footer(); ?>
+<?php get_footer(); ?>
