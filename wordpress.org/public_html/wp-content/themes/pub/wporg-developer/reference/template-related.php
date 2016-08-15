ce9864d600b1883420e92558386c0e7f26c5ef26
Scott Reilly <coffee2code@git.wordpress.org>
2016-08-15 19:52:04 +0000

    developer.wordpress.org: Don't show the 'Related' section, or either the 'Uses' or the 'Used By' subsections, if nothing would be listed.
    Fixes #1907.
    
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@3811 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-developer/reference/template-related.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-developer/reference/template-related.php
@@ -9,19 +9,23 @@
 namespace DevHub;
 
 if ( show_usage_info() ) :
+
+	$has_uses    = ( post_type_has_uses_info()  && ( $uses    = get_uses()    ) && $uses->have_posts()    );
+	$has_used_by = ( post_type_has_usage_info() && ( $used_by = get_used_by() ) && $used_by->have_posts() );
+
+	if ( $has_uses || $has_used_by ) :
 	?>
 	<hr />
 	<section class="related">
 		<h2><?php _e( 'Related', 'wporg' ); ?></h2>
 
-		<?php if ( post_type_has_uses_info() ) : ?>
+		<?php if ( $has_uses ) : ?>
 			<article class="uses">
 				<h3><?php _e( 'Uses', 'wporg' ); ?></h3>
 				<ul>
 					<?php
-					$uses = get_uses();
 					$uses_to_show = 5;
-					if ( $uses ) : while ( $uses->have_posts() ) : $uses->the_post()
+					while ( $uses->have_posts() ) : $uses->the_post()
 						?>
 						<li>
 							<span><?php echo esc_attr( get_source_file() ); ?>:</span>
@@ -36,17 +40,19 @@ if ( show_usage_info() ) :
 							);
 							?></a>
 						<a href="#" class="hide-more"><?php _e( 'Hide more uses', 'wporg' ); ?></a>
-					<?php endif; endif; ?>
+					<?php endif; ?>
 				</ul>
 			</article>
 		<?php endif; ?>
 
-		<hr />
+		<?php if ( $has_used_by ) : ?>
+
+		<?php if ( $has_uses ) { ?><hr /><?php } ?>
+
 		<article class="used-by">
 			<h3><?php _e( 'Used By', 'wporg' ); ?></h3>
 			<ul>
 				<?php
-				$used_by = get_used_by();
 				$used_by_to_show = 5;
 				while ( $used_by->have_posts() ) : $used_by->the_post();
 					?>
@@ -66,6 +72,8 @@ if ( show_usage_info() ) :
 				<?php endif; ?>
 			</ul>
 		</article>
-	</section>
-<?php endif; ?>
+		<?php endif; ?>
 
+	</section>
+	<?php endif; ?>
+<?php endif; ?>
