4ae51f7949282d2866de77a81f238bcf1c98d458
Dominik Schilling (ocean90) <ocean90@git.wordpress.org>
2016-08-17 11:12:54 +0000

    Trac: Update `mentionsRegEx` to support usernames with a `-`.
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@3821 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/style/trac/wp-trac.js
+++ wordpress.org/public_html/style/trac/wp-trac.js
@@ -98,7 +98,7 @@ var wpTrac, coreKeywordList, gardenerKeywordList, coreFocusesList;
 
 		linkMentions: function() {
 			// See https://github.com/regexps/mentions-regex/blob/master/index.js#L21
-			var mentionsRegEx = /(^|[^a-zA-Z0-9_＠!@#$%&*])(?:(?:@|＠)(?!\/))([a-zA-Z0-9/_.]{1,20})(?:\b(?!@|＠)|$)/g,
+			var mentionsRegEx = /(^|[^a-zA-Z0-9_＠!@#$%&*])(?:(?:@|＠)(?!\/))([a-zA-Z0-9/_\-.]{1,20})(?:\b(?!@|＠)|$)/g,
 				mentionsInAttrRegEx = new RegExp( '="[^"]*?' + mentionsRegEx.source + '[\\s\\S]*?"' );
 
 			$( 'div.change .comment, #ticket .description' ).each( function() {
