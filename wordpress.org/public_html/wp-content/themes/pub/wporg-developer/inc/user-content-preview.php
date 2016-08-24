b4d4949f455d9806b44ca08758cb3cd312458ed1
Scott Reilly <coffee2code@git.wordpress.org>
2016-08-24 21:02:04 +0000

    developer.wordpress.org: Bump CSS and JS versions for cache bust.
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@3855 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-developer/inc/user-content-preview.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-developer/inc/user-content-preview.php
@@ -35,8 +35,8 @@ class DevHub_Note_Preview {
 	 */
 	public static function scripts_and_styles() {
 		if ( is_singular() ) {
-			wp_enqueue_script( 'wporg-developer-tabs', get_template_directory_uri() . '/js/tabs.js', array( 'jquery' ), '20160809', true );
-			wp_enqueue_script( 'wporg-developer-preview', get_template_directory_uri() . '/js/user-notes-preview.js', array( 'jquery', 'wporg-developer-function-reference', 'wporg-developer-tabs'  ), '20160809', true );
+			wp_enqueue_script( 'wporg-developer-tabs', get_template_directory_uri() . '/js/tabs.js', array( 'jquery' ), '20160824', true );
+			wp_enqueue_script( 'wporg-developer-preview', get_template_directory_uri() . '/js/user-notes-preview.js', array( 'jquery', 'wporg-developer-function-reference', 'wporg-developer-tabs'  ), '20160824', true );
 			wp_localize_script( 'wporg-developer-preview', 'wporg_note_preview', array(
 				'ajaxurl'       => admin_url( 'admin-ajax.php' ),
 				'nonce'         => wp_create_nonce( 'preview_nonce' ),
--- wordpress.org/public_html/wp-content/themes/pub/wporg-developer/inc/user-content.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-developer/inc/user-content.php
@@ -83,11 +83,11 @@ class DevHub_User_Submitted_Content {
 	 */
 	public static function scripts_and_styles() {
 		if ( is_singular() ) {
-			wp_enqueue_script( 'wporg-developer-function-reference', get_template_directory_uri() . '/js/function-reference.js', array( 'jquery', 'syntaxhighlighter-core', 'syntaxhighlighter-brush-php' ), '20160809', true );
+			wp_enqueue_script( 'wporg-developer-function-reference', get_template_directory_uri() . '/js/function-reference.js', array( 'jquery', 'syntaxhighlighter-core', 'syntaxhighlighter-brush-php' ), '20160824', true );
 			wp_enqueue_style( 'syntaxhighlighter-core' );
 			wp_enqueue_style( 'syntaxhighlighter-theme-default' );
 
-			wp_enqueue_script( 'wporg-developer-user-notes', get_template_directory_uri() . '/js/user-notes.js', array( 'quicktags' ), '20160809', true );
+			wp_enqueue_script( 'wporg-developer-user-notes', get_template_directory_uri() . '/js/user-notes.js', array( 'quicktags' ), '20160824', true );
 			if ( get_option( 'thread_comments' ) ) {
 				wp_enqueue_script( 'comment-reply' );
 			}
