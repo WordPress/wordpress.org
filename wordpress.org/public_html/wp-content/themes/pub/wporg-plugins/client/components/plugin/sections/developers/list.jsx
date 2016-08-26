cbdafebc9b68bd50269c2610d81dc81d9c0fe1ed
Konstantin Obenland <obenland@git.wordpress.org>
2016-08-26 21:54:15 +0000

    Plugin Directory: Properly loop over developers array.
    See #1719.
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@3866 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-plugins/client/components/plugin/sections/developers/list.jsx
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-plugins/client/components/plugin/sections/developers/list.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import values from 'lodash/values';
 
 export default React.createClass( {
 	displayName: 'DeveloperList',
@@ -9,9 +10,9 @@ export default React.createClass( {
 		}
 
 		return (
-			<ul>
-				{ this.props.contributors.map( contributor =>
-					<li>
+			<ul className="plugin-developers">
+				{ values( this.props.contributors ).map( ( contributor, index ) =>
+					<li key={ index }>
 						<img className="avatar avatar-32 photo" height="32" width="32" src={ contributor.avatar } />
 						<a href={ contributor.profile }>{ contributor.display_name }</a>
 					</li>
