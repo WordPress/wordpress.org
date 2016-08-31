e6a48a7f7909dbd227a7eefa4d1970e9cc8337a9
Jennifer M. Dodd <jmdodd@git.wordpress.org>
2016-08-31 19:52:23 +0000

    Support Forums: Stop using SQL_CALC_FOUND_ROWS on single forum queries.
    A better search is more valuable than completely accurate pagination, as the page content will always be in flux as more topics are added to the beginning of the stack.
    
    Order by PRIMARY KEY whenever possible.
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@3895 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/plugins/support-forums/inc/class-performance-optimizations.php
+++ wordpress.org/public_html/wp-content/plugins/support-forums/inc/class-performance-optimizations.php
@@ -4,19 +4,22 @@ namespace WordPressdotorg\Forums;
 
 class Performance_Optimizations {
 
+	var $term = null;
+	var $query = null;
+
 	function __construct() {
 		// Gravatar suppression on lists of topics.
-		add_filter( 'bbp_after_get_topic_author_link_parse_args', array( __CLASS__, 'get_author_link' ) );
-		add_filter( 'bbp_after_get_reply_author_link_parse_args', array( __CLASS__, 'get_author_link' ) );
+		add_filter( 'bbp_after_get_topic_author_link_parse_args', array( $this, 'get_author_link' ) );
+		add_filter( 'bbp_after_get_reply_author_link_parse_args', array( $this, 'get_author_link' ) );
 
 		// Query simplification.
-		add_filter( 'bbp_after_has_topics_parse_args', array( __CLASS__, 'has_topics' ) );
+		add_filter( 'bbp_after_has_topics_parse_args', array( $this, 'has_topics' ) );
 	}
 
 	/**
 	 * Remove unnecessary Gravatar display on lists of topics.
 	 */
-	public static function get_author_link( $r ) {
+	public function get_author_link( $r ) {
 		if ( ! bbp_is_single_topic() || bbp_is_topic_edit() ) {
 			$r['type'] = 'name';
 		}
@@ -24,54 +27,72 @@ class Performance_Optimizations {
 	}
 
 	/**
-	 * Optimize queries for has_topics as much as possible to avoid breaking things.
+	 * Optimize queries for has_topics as much as possible.
 	 */
-	public static function has_topics( $r ) {
+	public function has_topics( $r ) {
 		/**
-		 * Filter view queries so they only look at the last N days of topics.
-		 */
-		if ( bbp_is_single_view() ) {
-			$view = bbp_get_view_id();
-			// Exclude plugin and theme views from this restriction.
-			// @todo Update date to a reasonable range once we're done importing.
-			if ( ! in_array( $view, array( 'plugin', 'theme', 'review' ) ) ) {
-				$r['date_query'] = array( 'after' => '19 months ago' );
-			} else {
-				$term = self::get_term();
-
-				// If there are a lot of results for a single plugin or theme,
-				// order by post_date to avoid an INNER JOIN ON.
-				if ( $term && ! is_wp_error( $term ) && property_exists( $term, 'count' ) ) {
-					if ( $term->count > 10000 ) {
-						unset( $r['meta_key'] );
-						unset( $r['meta_type'] );
-
-						$r['orderby'] = 'post_date';
-					}
-				}
-			}
-
-		/**
-		 * Filter forum queries so they are not sorted by the post meta value of
+		 * Filter queries so they are not sorted by the post meta value of
 		 * `_bbp_last_active_time`. This query needs additional optimization
 		 * to run over large sets of posts.
 		 * See also:
 		 * - https://bbpress.trac.wordpress.org/ticket/1925
 		 */
-		} elseif ( bbp_is_single_forum() ) {
-			unset( $r['meta_key'] );
-			unset( $r['meta_type'] );
+		if ( isset( $r['post_type'] ) && 'topic' == $r['post_type'] ) {
+			// Theme and plugin views rely on taxonomy queries.
+			if ( isset( $r['tax_query'] ) ) {
+				return $r;
+			}
 
-			// This only works because we don't edit dates on forum topics.
-			$r['orderby'] = 'post_date';
+			// has_topics() uses this by default.
+			if ( isset( $r['meta_key'] ) && '_bbp_last_active_time' == $r['meta_key'] ) {
+				unset( $r['meta_key'] );
+				unset( $r['meta_type'] );
+				$r['orderby'] = 'ID';
+			}
+
+			// If this is a forum, limit the number of pages we're dealing with.
+			if ( isset( $r['post_parent'] ) && get_post_type( $r['post_parent'] ) === bbp_get_forum_post_type() ) {
+				$r['no_found_rows'] = true;
+				add_filter( 'bbp_topic_pagination', array( $this, 'forum_pagination' ) );
+				$this->query = $r;
+			}
 		}
+		return $r;
+	}
+
+	public function forum_pagination( $r ) {
+		global $wpdb;
+
+		// Try the stored topic count.
+		$count = get_post_meta( $this->query['post_parent'], '_bbp_topic_count', true );
+		if ( ! empty( $count ) ) {
+			$r['total'] = $count / bbp_get_topics_per_page();
+			return $r;
+		}
+
+		// Try SQL.
+		if ( ! is_null( $this->query ) ) {
+			$count = $wpdb->query( $wpdb->prepare( "SELECT COUNT(*) FROM $wpdb->posts WHERE post_parent = %d AND post_type = 'topic' AND post_status = 'publish' LIMIT 1", $this->query['post_parent'] ) );
+			if ( $count ) {
+				$r['total'] = $count / bbp_get_topics_per_page();
+				return $r;
+			}
+		}
+
+		// If all else fails...
+		$r['total'] = 10;
 		return $r;
 	}
 
 	/**
 	 * Get the term for a plugin or theme view from query_var.
 	 */
-	public static function get_term() {
+	public function get_term() {
+		if ( null !== $this->term ) {
+			return $this->term;
+		}
+
+		$slug = false;
 		if ( ! empty( get_query_var( Plugin::get_instance()->plugins->query_var() ) ) ) {
 			$slug = Plugin::get_instance()->plugins->slug();
 			$tax  = Plugin::get_instance()->plugins->taxonomy();
@@ -79,7 +100,11 @@ class Performance_Optimizations {
 			$slug = Plugin::get_instance()->themes->slug();
 			$tax  = Plugin::get_instance()->themes->taxonomy();
 		}
-		$term = get_term( $slug, $tax );
+		if ( $slug ) {
+			$term = get_term_by( 'slug', $slug, $tax );
+		} else {
+			return false;
+		}
 		return $term;
 	}
 }
