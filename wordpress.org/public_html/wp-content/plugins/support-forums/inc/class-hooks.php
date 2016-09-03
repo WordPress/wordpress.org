9228fb7517df2247f6eed9fd4086e933ca2d1dad
Dominik Schilling (ocean90) <ocean90@git.wordpress.org>
2016-09-03 19:24:57 +0000

    Support Forums: Disable oEmbed for bbPress' post types.
    There is currently no template support for these post types which results in an iframe for the full sites.
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@3946 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/plugins/support-forums/inc/class-hooks.php
+++ wordpress.org/public_html/wp-content/plugins/support-forums/inc/class-hooks.php
@@ -11,6 +11,10 @@ class Hooks {
 		// Display-related filters and actions.
 		add_filter( 'bbp_get_topic_admin_links', array( $this, 'get_admin_links' ), 10, 3 );
 		add_filter( 'bbp_get_reply_admin_links', array( $this, 'get_admin_links' ), 10, 3 );
+
+		// oEmbed.
+		add_filter( 'oembed_discovery_links', array( $this, 'disable_oembed_discovery_links' ) );
+		add_filter( 'oembed_response_data', array( $this, 'disable_oembed_response_data' ), 10, 2 );
 	}
 
 	/**
@@ -25,5 +29,35 @@ class Hooks {
 		$retval = $r['before'] . $links . $r['after'];
 
 		return $retval;
+	}
+
+	/**
+	 * Removes oEmbed discovery links for bbPress' post types.
+	 *
+	 * @param string $output HTML of the discovery links.
+	 * @return string Empty string for bbPress' post types, HTML otherwise.
+	 */
+	public function disable_oembed_discovery_links( $output ) {
+		$post_type = get_post_type();
+		if ( $post_type && in_array( $post_type, [ bbp_get_forum_post_type(), bbp_get_topic_post_type(), bbp_get_reply_post_type() ] ) ) {
+			return '';
+		}
+
+		return $output;
+	}
+
+	/**
+	 * Prevents retrieving oEmbed data for bbPress' post types.
+	 *
+	 * @param array   $data The response data.
+	 * @param WP_Post $post The post object.
+	 * @return array|false False for bbPress' post types, array otherwise.
+	 */
+	public function disable_oembed_response_data( $data, $post ) {
+		if ( in_array( $post->post_type, [ bbp_get_forum_post_type(), bbp_get_topic_post_type(), bbp_get_reply_post_type() ] ) ) {
+			return false;
+		}
+
+		return $data;
 	}
 }
