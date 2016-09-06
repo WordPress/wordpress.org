235c3edec50f44226e3aa087d34af54dc67e1ed4
Scott Reilly <coffee2code@git.wordpress.org>
2016-09-06 20:27:56 +0000

    Support: User Badges initial commit.
    Adds badges to user replies. Currently assigns one of three potential badges:
    
    * Plugin Author : replies made in the support/review forum for a plugin the reply author has commit access to
    * Plugin Contributor : replies made in the support/review forum for a plugin the reply author is listed as a contributor
    * Theme Author : replies made in the support/review forum for a theme the reply author has commit access to
    
    See #1544.
    Fixes #1993.
    
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@3978 74240141-8908-4e6f-9713-ba540dce6ec7

---
+++ wordpress.org/public_html/wp-content/plugins/wporg-bbp-user-badges/inc/class-plugin.php
@@ -0,0 +1,253 @@
+<?php
+
+namespace WordPressdotorg\Forums\User_Badges;
+
+defined( 'ABSPATH' ) or die();
+
+class Plugin {
+
+	/**
+	 * @access private
+	 * @var string The prefix for the plugin directory tables.
+	 */
+	private static $plugins_table_prefix;
+
+	/**
+	 * @access private
+	 * @var string The prefix for the theme directory tables.
+	 */
+	private static $themes_table_prefix;
+
+	/**
+	 * @access private
+	 * @var Plugin The singleton instance.
+	 */
+	private static $instance;
+
+	/**
+	 * Returns always the same instance of this plugin.
+	 *
+	 * @return Plugin
+	 */
+	public static function get_instance() {
+		if ( ! ( self::$instance instanceof self ) ) {
+			self::$instance = new self();
+		}
+		return self::$instance;
+	}
+
+	/**
+	 * Instantiates a new Plugin object.
+	 */
+	private function __construct() {
+		self::$plugins_table_prefix = 'wporg_' . WPORG_PLUGIN_DIRECTORY_BLOGID . '_';
+		self::$themes_table_prefix  = 'wporg_' . WPORG_THEME_DIRECTORY_BLOGID . '_';
+
+		add_action( 'bbp_loaded', array( $this, 'bbp_loaded' ) );
+	}
+
+	/**
+	 * Initializes the plugin.
+	 */
+	public function bbp_loaded() {
+		// Add class to div containing reply.
+		add_filter( 'bbp_get_reply_class', array( $this, 'bbp_get_reply_class' ), 10, 2 );
+
+		// Add badge before reply author info.
+		add_action( 'bbp_theme_before_reply_author_details', array( $this, 'wporg_support_add_author_badges' ) );
+	}
+
+	protected function get_author_badge_info() {
+		$badgeable_forums = array(
+			\WordPressdotorg\Forums\Plugin::PLUGINS_FORUM_ID,
+			\WordPressdotorg\Forums\Plugin::REVIEWS_FORUM_ID,
+			\WordPressdotorg\Forums\Plugin::THEMES_FORUM_ID,
+		);
+
+		if ( ! in_array( bbp_get_forum_id(), $badgeable_forums ) ) {
+			return;
+		}
+
+		$slug = $type = null;
+
+		$user_id = bbp_get_reply_author_id();
+		if ( ! $user_id ) {
+			return;
+		}
+
+		$user_login = get_user_by( 'id', $user_id )->user_login;
+		$reply_id   = bbp_get_reply_id();
+		$topic_id   = bbp_get_reply_topic_id();
+
+		$support_forum = \WordPressdotorg\Forums\Plugin::get_instance();
+
+		// Check if the thread is associated with a plugin.
+		if ( $slug = $support_forum->plugins->slug ) {
+			$type = 'plugin';
+		}
+		// Else check if the thread is associated with a theme.
+		elseif ( $slug = $support_forum->themes->slug ) {
+			$type = 'theme';
+		}
+		// Else not a type of concern.
+		else {
+			return;
+		}
+
+		return array(
+			'type'       => $type,
+			'slug'       => $slug,
+			'user_login' => $user_login
+		);
+	}
+
+	public function bbp_get_reply_class( $classes, $reply_id ) {
+		// Class related to plugin and theme authors/contributors.
+		if ( $info = $this->get_author_badge_info() ) {
+			if ( $this->is_user_author( $info['user_login'], $info['type'], $info['slug'] ) ) {
+				$contrib_type = 'author';
+			} elseif ( $this->is_user_contributor( $info['user_login'], $info['type'], $info['slug'] ) ) {
+				$contrib_type = 'contributor';
+			} else {
+				$contrib_type = '';
+			}
+
+			if ( $contrib_type ) {
+				$classes[] = 'by-' . $info['type'] . '-' . $contrib_type;
+			}
+		}
+
+		return $classes;
+	}
+
+	/**
+	 * Display author badge if reply author is in support or reviews forum for
+	 * the plugin/theme they contribute to.
+	 */
+	public function wporg_support_add_author_badges() {
+		if ( ! $info = $this->get_author_badge_info() ) {
+			return;
+		}
+
+		$label = $help = null;
+
+		// Determine strings to use based on user being an author or contributor.
+		if ( $this->is_user_author( $info['user_login'], $info['type'], $info['slug'] ) ) {
+			if ( 'plugin' == $info['type'] ) {
+				$label = __( 'Plugin Author', 'wporg-forums' );
+				$help  = __( 'This person is the author of this plugin', 'wporg-forums' );
+			} else {
+				$label = __( 'Theme Author', 'wporg-forums' );
+				$help  = __( 'This person is the author of this theme', 'wporg-forums' );
+			}
+		}
+		elseif ( $this->is_user_contributor( $info['user_login'], $info['type'], $info['slug'] ) ) {
+			if ( 'plugin' == $info['type'] ) {
+				$label = __( 'Plugin Contributor', 'wporg-forums' );
+				$help  = __( 'This person is a contributor to this plugin', 'wporg-forums' );
+			} else {
+				$label = __( 'Theme Contributor', 'wporg-forums' );
+				$help  = __( 'This person is a contributor to this theme', 'wporg-forums' );
+			}
+		}
+
+		// Build the markup.
+		$output = '';
+		if ( $label ) {
+			$output .= sprintf(
+				'<span class="author-badge %s-author-badge" title="%s">%s</span>',
+				esc_attr( $info['type'] ),
+				esc_attr( $help ),
+				$label
+			);
+		}
+
+		// Output the markup.
+		echo $output;
+	}
+
+	/**
+	 * Checks if the specified user is an author to the specified plugin/theme.
+	 *
+	 * An author is defined as someone who has commit access to a plugin, or is
+	 * the designated author for a theme.
+	 *
+	 * @param string $user_login User login.
+	 * @param string $type       Either 'plugin' or 'theme'.
+	 * @param string $slug       Slug for the plugin or theme.
+	 * @return bool              True if user is an author, false otherwise.
+	 */
+	public function is_user_author( $user_login, $type, $slug ) {
+		global $wpdb;
+
+		$authors = wp_cache_get( $slug, $type . '_authors' );
+
+		if ( false === $authors ) {
+			if ( 'plugin' === $type ) {
+				// Get users who have commit access.
+				$authors = $wpdb->get_col( $wpdb->prepare(
+					"SELECT user FROM plugin_2_svn_access WHERE `path` = %s",
+					'/' . $slug
+				) );
+			}
+			else {
+				// TODO: Change this if themes support having more than one author.
+				$author_id = $wpdb->get_var( $wpdb->prepare(
+					"SELECT post_author FROM " . self::$themes_table_prefix . "posts WHERE post_name = %s LIMIT 1",
+					$slug
+				) );
+				if ( $author_id ) {
+					$author = get_user_by( 'id', $author_id );
+					$authors = array( $author->user_login );
+				}
+			}
+
+			wp_cache_add( $slug, $authors, $type . '_authors', HOUR_IN_SECONDS );
+		}
+
+		return in_array( $user_login, $authors );
+	}
+
+	/**
+	 * Checks if the specified user is a contributor to the specified plugin/theme.
+	 *
+	 * A plugin contributor is someone listed as a contributor in the plugin's readme.txt.
+	 * Currently, themes do not support having contirbutors.
+	 *
+	 * @param string $user_login User login.
+	 * @param string $type       Either 'plugin' or 'theme'.
+	 * @param string $slug       Slug for the plugin or theme.
+	 * @return bool              True if user is a contributor, false otherwise.
+	 */
+	public function is_user_contributor( $user_login, $type, $slug ) {
+		global $wpdb;
+
+		$contributors = wp_cache_get( $slug, $type . '_contributors' );
+
+		if ( false === $contributors ) {
+			if ( 'plugin' === $type ) {
+				// TODO: Change this when the Plugin Directory switches over to WordPress.
+				$contributors = $wpdb->get_var( $wpdb->prepare(
+					'SELECT meta_value FROM plugin_2_meta m LEFT JOIN plugin_2_topics t ON m.object_id = t.topic_id WHERE t.topic_slug = %s AND m.object_type = %s AND m.meta_key = %s',
+					$slug,
+					'bb_topic',
+					'contributors'
+				) );
+
+				if ( $contributors ) {
+					$contributors = unserialize( $contributors );
+				}
+			}
+			else {
+				// Themes have no additional contributors at the moment.
+				// TODO: Change this if themes support specifying contributors.
+				$contributors = array();
+			}
+
+			wp_cache_add( $slug, $contributors, $type . '_contributors', HOUR_IN_SECONDS );
+		}
+
+		return in_array( $user_login, $contributors );
+	}
+
+}
---
+++ wordpress.org/public_html/wp-content/plugins/wporg-bbp-user-badges/wporg-bbp-user-badges.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Plugin Name: bbPress: Show User Badges
+ * Description: Display a badge for plugin and theme authors and contributors for replies in their own support forums.
+ * Version:     1.0
+ * Author:      WordPress.org
+ * Author URI:  https://wordpress.org/
+ * License:     GPLv2 or later
+ * Text Domain: wporg-forums
+ */
+
+namespace WordPressdotorg\Forums\User_Badges;
+
+defined( 'ABSPATH' ) or die();
+
+// Store the root plugin file for usage with functions which use the plugin basename
+define( __NAMESPACE__ . '\PLUGIN_FILE', __FILE__ );
+
+// Includes
+include( dirname( __FILE__ ) . '/inc/class-plugin.php' );
+
+// Instantiate the Plugin
+Plugin::get_instance();
