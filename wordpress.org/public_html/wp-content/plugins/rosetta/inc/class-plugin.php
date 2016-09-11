c8669a70d7f337289fa77660b6baf16706d26443
Dominik Schilling (ocean90) <ocean90@git.wordpress.org>
2016-09-11 20:08:22 +0000

    Rosetta: Add filters for date options.
    See #2003
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@4033 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/plugins/rosetta/inc/class-plugin.php
+++ wordpress.org/public_html/wp-content/plugins/rosetta/inc/class-plugin.php
@@ -60,5 +60,78 @@ class Plugin {
 		}
 
 		// Customizations for all sites.
+		$this->filter_date_options();
+	}
+
+	/**
+	 * Adds filters for all date options.
+	 */
+	private function filter_date_options() {
+		$options = new Filter\Options();
+
+		$options->add_option(
+			( new Filter\Option() )
+				->set_name( 'timezone_string' )
+				->set_callback( function() {
+					/* translators: default GMT offset or timezone string. Must be either a valid offset (-12 to 14)
+					 * or a valid timezone string (America/New_York). See https://secure.php.net/manual/timezones.php
+					 * for all timezone strings supported by PHP.
+					 */
+					$offset_or_tz = _x( '0', 'default GMT offset or timezone string', 'rosetta' );
+					if ( $offset_or_tz && ! is_numeric( $offset_or_tz ) && in_array( $offset_or_tz, timezone_identifiers_list() ) ) {
+						return $offset_or_tz;
+					} else {
+						return '';
+					}
+				} )
+				->set_priority( 9 ) // Before `wp_timezone_override_offset()`
+		);
+
+		$options->add_option(
+			( new Filter\Option() )
+				->set_name( 'gmt_offset' )
+				->set_callback( function() {
+					/* translators: default GMT offset or timezone string. Must be either a valid offset (-12 to 14)
+					 * or a valid timezone string (America/New_York). See https://secure.php.net/manual/timezones.php
+					 * for all timezone strings supported by PHP.
+					 */
+					$offset_or_tz = _x( '0', 'default GMT offset or timezone string', 'rosetta' );
+					if ( $offset_or_tz && is_numeric( $offset_or_tz ) ) {
+						return $offset_or_tz;
+					} else {
+						return 0;
+					}
+				} )
+				->set_priority( 9 ) // Before `wp_timezone_override_offset()`
+		);
+
+		$options->add_option(
+			( new Filter\Option() )
+				->set_name( 'date_format' )
+				->set_callback( function() {
+					/* translators: default date format, see https://secure.php.net/date */
+					return __( 'F j, Y', 'rosetta' );
+				} )
+		);
+
+		$options->add_option(
+			( new Filter\Option() )
+				->set_name( 'time_format' )
+				->set_callback( function() {
+					/* translators: default time format, see https://secure.php.net/date */
+					return __( 'g:i a', 'rosetta' );
+				} )
+		);
+
+		$options->add_option(
+			( new Filter\Option() )
+				->set_name( 'start_of_week' )
+				->set_callback( function() {
+					/* translators: default start of the week. 0 = Sunday, 1 = Monday */
+					return _x( '1', 'start of week', 'rosetta' );
+				} )
+		);
+
+		$options->setup();
 	}
 }
