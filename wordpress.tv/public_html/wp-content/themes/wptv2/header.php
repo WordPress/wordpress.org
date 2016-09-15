0919ad4cd08856a85179574925daeded4dad6c6d
Scott Reilly <coffee2code@git.wordpress.org>
2016-09-15 17:14:05 +0000

    WordPress.tv: Fix mixed content warning for Typekit JS.
    Props SergeyBiryukov, targz-1.
    Fixes #2037.
    
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@4071 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.tv/public_html/wp-content/themes/wptv2/header.php
+++ wordpress.tv/public_html/wp-content/themes/wptv2/header.php
@@ -21,7 +21,7 @@ global $wptv;
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 
-	<script type="text/javascript" src="http://use.typekit.com/mgi6udv.js"></script>
+	<script type="text/javascript" src="//use.typekit.com/mgi6udv.js"></script>
 	<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
 	<?php wp_head(); ?>
 </head>
