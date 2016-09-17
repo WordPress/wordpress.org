a6bedceb4ac1e624b0fe05e5eb60a936a31d7566
Stephen Edgar <netweb@git.wordpress.org>
2016-09-17 02:28:07 +0000

    Forums Theme: Restore topics count column in the forums loop.
    This change restores what was previously removed in [2428] which was an attempt to make the sidebar look better with the forum list squeezed into it, we're no longer trying to do that.
    
    Fixes #2053
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@4085 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-forums/bbpress/loop-single-forum.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-forums/bbpress/loop-single-forum.php
@@ -3,5 +3,6 @@
 		<a class="bbp-forum-title" href="<?php bbp_forum_permalink(); ?>" title="<?php bbp_forum_title(); ?>"><?php bbp_forum_title(); ?></a>
 		<br><?php bbp_forum_content(); ?>
 	</li>
+	<li class="bbp-forum-topic-count"><?php bbp_forum_topic_count(); ?></li>
 	<li class="bbp-forum-reply-count"><?php bbp_forum_post_count(); ?></li>
 </ul><!-- #bbp-forum-<?php bbp_forum_id(); ?> -->
