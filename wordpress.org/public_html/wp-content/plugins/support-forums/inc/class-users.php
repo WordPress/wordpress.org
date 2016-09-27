370975ccec1578102e1182ffd8e552325ddcd79e
Scott Reilly <coffee2code@git.wordpress.org>
2016-09-27 19:21:03 +0000

    Support Forums: Don't override display value of user role with custom title.
    Props netweb.
    See #1972.
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@4144 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/plugins/support-forums/inc/class-users.php
+++ wordpress.org/public_html/wp-content/plugins/support-forums/inc/class-users.php
@@ -5,9 +5,6 @@ namespace WordPressdotorg\Forums;
 class Users {
 
 	public function __construct() {
-		// If the user has a custom title, use that instead of the forum role.
-		add_filter( 'bbp_get_user_display_role', array( $this, 'display_role' ), 10, 2 );
-
 		// Add a Custom Title input to user's profile.
 		add_action( 'bbp_user_edit_after_name', array( $this, 'add_custom_title_input' ) );
 
@@ -17,22 +14,6 @@ class Users {
 
 		// Custom user contact methods.
 		add_filter( 'user_contactmethods', array( $this, 'custom_contact_methods' ) );
-	}
-
-	/**
-	 * If the user has a custom title, use that instead of the forum role.
-	 *
-	 * @param string  $role    The user's forum role.
-	 * @param int     $user_id The user ID.
-	 * @return string The user's custom forum title, or their forum role.
-	 */
-	public function display_role( $role, $user_id ) {
-		$title = get_user_option( 'title', $user_id );
-		if ( ! empty( $title ) ) {
-			return esc_html( $title );
-		}
-
-		return $role;
 	}
 
 	/**
