181f0edf88f29453895de54de9eb28806a95f4d6
Jennifer M. Dodd <jmdodd@git.wordpress.org>
2016-10-04 04:10:20 +0000

    Support Forums: Add plugin-committer view and feed.
    See #2071.
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@4184 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/plugins/support-forums/inc/class-support-compat.php
+++ wordpress.org/public_html/wp-content/plugins/support-forums/inc/class-support-compat.php
@@ -7,10 +7,22 @@ namespace WordPressdotorg\Forums;
 
 class Support_Compat {
 
-	var $loaded = false;
+	var $loaded     = false;
+	var $query      = null;
+	var $user_login = null;
 
 	public function __construct() {
 		if ( ! $this->loaded ) {
+			// Intercept feed requests prior to bbp_request_feed_trap.
+			add_filter( 'bbp_request', array( $this, 'request' ), 9 );
+
+			// Add views for plugin committer/contributor.
+			add_filter( 'query_vars',            array( $this, 'add_query_var' ) );
+			add_action( 'bbp_add_rewrite_rules', array( $this, 'add_rewrite_rules' ) );
+
+			// We have to add the custom view before bbPress runs its own action
+			// on parse_query at priority 2.
+			add_action( 'parse_query',           array( $this, 'parse_query' ), 0 );
 
 			// Exclude compat forums from forum dropdown.
 			add_filter( 'bbp_after_get_dropdown_parse_args', array( $this, 'get_dropdown' ) );
@@ -23,6 +35,134 @@ class Support_Compat {
 
 			$this->loaded = true;
 		}
+	}
+
+	public function request( $query_vars ) {
+		if ( isset( $query_vars['feed'] ) && isset( $query_vars['wporg_user_login'] ) ) {
+			if ( isset( $query_vars['bbp_view'] ) && in_array( $query_vars['bbp_view'], array( 'plugin-committer' ) ) ) {
+				$this->query = $query_vars;
+				add_filter( 'bbp_get_view_query_args', array( $this, 'get_view_query_args_for_feed' ), 10, 2 );
+
+				// Override bbPress topic pubDate handling to show topic time and not last active time
+				add_filter( 'get_post_metadata', array( $this, 'topic_pubdate_correction_for_feed' ), 10, 4 );
+			}
+		}
+		return $query_vars;
+	}
+
+	public function topic_pubdate_correction_for_feed( $value, $object_id, $meta_key, $single ) {
+		// We only care about _bbp_last_active_time in this particular context
+		if( $meta_key == '_bbp_last_active_time' ) {
+			$value = get_post_time( 'Y-m-d H:i:s', true, $object_id );
+		}
+		return $value;
+	}
+
+	public function get_view_query_args_for_feed( $retval, $view ) {
+		switch( $this->query['bbp_view'] ) {
+			case 'plugin-committer' :
+				return array(
+					'post_parent__in' => array( Plugin::PLUGINS_FORUM_ID, Plugin::REVIEWS_FORUM_ID ),
+					'post_status'     => 'publish',
+					'tax_query'       => array( array(
+						'taxonomy'    => 'topic-plugin',
+						'field'       => 'slug',
+						'terms'       => $this->get_plugin_slugs_by_committer( $this->query['wporg_user_login'] ),
+					) ),
+					'show_stickies'   => false,
+					'orderby'         => 'ID',
+				);
+				break;
+		}
+		return $retval;
+	}
+
+	public function parse_query() {
+		$user_login = get_query_var( 'wporg_user_login' );
+		$view = get_query_var( 'bbp_view' );
+		if ( ! $user_login || ! $view ) {
+			return;
+		}
+
+		// Basic setup.
+		$this->user_login = $user_login;
+
+		if ( $view == 'plugin-committer' ) {
+
+			$slugs = $this->get_plugin_slugs_by_committer( $user_login );
+
+			// Add plugin-committer view.
+			bbp_register_view(
+				'plugin-committer',
+				sprintf( __( 'Plugin Committer &raquo; %s', 'wporg-forums' ), esc_html( $user_login ) ),
+				array(
+					'post_parent__in' => array( Plugin::PLUGINS_FORUM_ID, Plugin::REVIEWS_FORUM_ID ),
+					'post_status'     => 'publish',
+					'tax_query'       => array( array(
+						'taxonomy'    => 'topic-plugin',
+						'field'       => 'slug',
+						'terms'       => $slugs,
+					) ),
+					'show_stickies'   => false,
+				)
+			);
+
+			// Add output filters and actions.
+			add_filter( 'bbp_get_view_link', array( $this, 'get_view_link' ), 10, 2 );
+		}
+	}
+
+	public function add_query_var( $query_vars ) {
+		$query_vars[] = 'wporg_user_login';
+		return $query_vars;
+	}
+
+	public function add_rewrite_rules() {
+		$priority   = 'top';
+
+		$plugin_committer_rule = bbp_get_view_slug() . '/plugin-committer/([^/]+)/';
+
+		$feed_id    = 'feed';
+		$view_id    = bbp_get_view_rewrite_id();
+		$paged_id   = bbp_get_paged_rewrite_id();
+
+		$feed_slug  = 'feed';
+		$paged_slug = bbp_get_paged_slug();
+
+		$base_rule  = '?$';
+		$feed_rule  = $feed_slug . '/?$';
+		$paged_rule = $paged_slug . '/?([0-9]{1,})/?$';
+
+		// Add plugin committer rewrite rules.
+		add_rewrite_rule( $plugin_committer_rule . $base_rule,  'index.php?' . $view_id . '=plugin-committer&wporg_user_login=$matches[1]',                               $priority );
+		add_rewrite_rule( $plugin_committer_rule . $paged_rule, 'index.php?' . $view_id . '=plugin-committer&wporg_user_login=$matches[1]&' . $paged_id . '=$matches[2]', $priority );
+		add_rewrite_rule( $plugin_committer_rule . $feed_rule,  'index.php?' . $view_id . '=plugin-committer&wporg_user_login=$matches[1]&' . $feed_id  . '=$matches[2]', $priority );
+	}
+
+	/**
+	 * Filter view links to provide prettier links for the custom view structure.
+	 */
+	public function get_view_link( $url, $view ) {
+		global $wp_rewrite;
+
+		$view = bbp_get_view_id( $view );
+		if ( ! in_array( $view, array( 'plugin-committer' ) ) ) {
+			return $url;
+		}
+
+		// Pretty permalinks.
+		if ( $wp_rewrite->using_permalinks() ) {
+			$url = $wp_rewrite->root . 'view/plugin-committer/' . $this->user_login;
+			$url = home_url( user_trailingslashit( $url ) );
+
+		// Unpretty permalinks.
+		} else {
+			$url = add_query_arg( array(
+				bbp_get_view_rewrite_id() => $view,
+				'wporg_user_login'        => $this->user_login,
+			) );
+		}
+		return $url;
 	}
 
 	/**
@@ -127,5 +267,11 @@ class Support_Compat {
 			return false;
 		}
 		return in_array( $post_id, self::get_compat_forums() );
+	}
+
+	public static function get_plugin_slugs_by_committer( $user_login ) {
+		global $wpdb;
+		$slugs = (array) $wpdb->get_col( $wpdb->prepare( "SELECT `path` FROM `" . PLUGINS_TABLE_PREFIX . "svn_access` WHERE `user` = %s AND `access` = 'rw' LIMIT 4", $user_login ) );
+		return $slugs;
 	}
 }
