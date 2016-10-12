27784a8b5710c900b27b1710fafe78cdfcd68b9f
Scott Reilly <coffee2code@git.wordpress.org>
2016-10-12 18:44:45 +0000

    Forums Theme: Show context for posts listed in special views (e.g. spam/pending/archived).
    * Always show linked forum name.
    * Show topic name for topic posts.
    * Show linked topic name for reply posts.
    
    Props netweb, coffee2code.
    Fixes #2101.
    
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@4225 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-forums/bbpress/loop-posts.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-forums/bbpress/loop-posts.php
@@ -23,10 +23,32 @@
 
 			<?php if ( 'topic' == get_post_type() ) : ?>
 
+				<div id="post-<?php bbp_topic_id(); ?>" class="bbp-topic-header">
+					<div class="bbp-meta">
+							<span class="bbp-header">
+								<?php esc_html_e( 'Forum:', 'wporg-forums' ); ?>
+								<a class="bbp-forum-permalink" href="<?php bbp_forum_permalink( bbp_get_topic_forum_id() ); ?>"><?php bbp_forum_title( bbp_get_topic_forum_id() ); ?></a><br>
+								<?php esc_html_e( 'As the topic:', 'wporg-forums' ); ?>
+								<?php bbp_topic_title( bbp_get_topic_id() ); ?>
+							</span>
+					</div><!-- .bbp-meta -->
+				</div><!-- #post-<?php bbp_topic_id(); ?> -->
+
 				<?php bbp_get_template_part( 'content', 'single-topic-lead' ); ?>
 
 			<?php // This actually works. ?>
 			<?php else : bbpress()->reply_query = bbpress()->topic_query; ?>
+
+				<div id="post-<?php bbp_reply_id(); ?>" class="bbp-reply-header">
+					<div class="bbp-meta">
+							<span class="bbp-header">
+								<?php esc_html_e( 'Forum:', 'wporg-forums' ); ?>
+								<a class="bbp-forum-permalink" href="<?php bbp_forum_permalink( bbp_get_reply_forum_id() ); ?>"><?php bbp_forum_title( bbp_get_reply_forum_id() ); ?></a><br>
+								<?php esc_html_e( 'In reply to:', 'wporg-forums' ); ?>
+								<a class="bbp-topic-permalink" href="<?php bbp_topic_permalink( bbp_get_reply_topic_id() ); ?>"><?php bbp_topic_title( bbp_get_reply_topic_id() ); ?></a>
+							</span>
+					</div><!-- .bbp-meta -->
+				</div><!-- #post-<?php bbp_reply_id(); ?> -->
 
 				<?php bbp_get_template_part( 'loop', 'single-reply' ); ?>
 
