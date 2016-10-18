4fe546c2b64ac51988dcc661055c93305ffbdd1e
Dominik Schilling (ocean90) <ocean90@git.wordpress.org>
2016-10-18 22:01:10 +0000

    Trac/Slack: Add davidakennedy to the committers list.
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@4258 74240141-8908-4e6f-9713-ba540dce6ec7

--- common/includes/slack/announce/config.php
+++ common/includes/slack/announce/config.php
@@ -206,6 +206,6 @@ function get_committers() {
 		'jorbin', 'boone', 'jeremyfelt', 'pento', 'obenland', 'iseulde',
 		'westonruter', 'afercia', 'karmatosed', 'rmccue', 'michaelarestad',
 		'swissspidy', 'rachelbaker', 'joehoyle', 'melchoyce', 'eric', 'mike',
-		'peterwilsoncc', 'joemcgill',
+		'peterwilsoncc', 'joemcgill', 'davidakennedy'
 	);
 }
