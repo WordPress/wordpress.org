0d50606d026d89ae1183500662a87c41b6fdb39a
Mark Uraine <mapk@git.wordpress.org>
2016-11-01 18:38:42 +0000

    Reference ticket #1824
    Added text to beginning of Contributor section.
    Float contributor avatar to left so name doesn't wrap under it.
    Added text to bottom of list in a paragraph format including links to codebase,etc.
    Removed letter-spacing on FAQs.
    
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@4313 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-plugins/client/components/plugin/sections/developers/index.jsx
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-plugins/client/components/plugin/sections/developers/index.jsx
@@ -9,21 +9,12 @@ export default React.createClass( {
 		return (
 			<div>
 				<div id="developers" className="read-more" aria-expanded="false">
-					<h2>Authors</h2>
+					<h2>Contributors & Developers</h2>
+					<p>This is open source software. The following people have contributed to this plugin.</p>
 					<DeveloperList contributors={ this.props.contributors } />
 
-					<h5>Browse the Code</h5>
-					<ul>
-						<li>
-							<a href={ `https://plugins.trac.wordpress.org/log/${ this.props.slug }/` } rel="nofollow">Development Log</a>
-							<a href={ `https://plugins.trac.wordpress.org/log/${ this.props.slug }/?limit=100&mode=stop_on_copy&format=rss` } rel="nofollow">
-								<img src="https://s.w.org/style/images/feedicon.png" />
-							</a>
-						</li>
-						<li><a href={ `https://plugins.svn.wordpress.org/${ this.props.slug }/` } rel="nofollow">Subversion Repository</a></li>
-						<li><a href={ `https://plugins.trac.wordpress.org/browser/${ this.props.slug }/` } rel="nofollow">Browse in Trac</a></li>
-						<li><a href={ `https://translate.wordpress.org/projects/wp-plugins/${ this.props.slug }/` } rel="nofollow">Translation Contributors</a></li>
-					</ul>
+					<h5>Interested in development?</h5>
+					<p><a href={ `https://plugins.svn.wordpress.org/${ this.props.slug }/` } rel="nofollow">Browse the code</a> or subscribe to the <a href={ `https://plugins.trac.wordpress.org/log/${ this.props.slug }/` } rel="nofollow">development log</a> by <a href={ `https://plugins.trac.wordpress.org/log/${ this.props.slug }/?limit=100&mode=stop_on_copy&format=rss` } rel="nofollow">RSS</a>.</p>
 				</div>
 				<button type="button" className="button-link section-toggle" aria-controls="developers">Read more</button>
 			</div>
