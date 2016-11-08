a5ebbdaea96060c87180921e267e9e319921628f
Scott Reilly <coffee2code@git.wordpress.org>
2016-11-08 19:47:46 +0000

    developer.wordpress.org: Add single page template for REST API handbook.
    See #2201.
    
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@4343 74240141-8908-4e6f-9713-ba540dce6ec7

---
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-developer/single-rest-api-handbook.php
@@ -0,0 +1,27 @@
+<?php namespace DevHub;
+/**
+ * The Template for displaying all single posts.
+ *
+ * @package wporg-developer
+ */
+
+get_header(); ?>
+
+	<div id="content-area" class="has-sidebar">
+
+		<?php breadcrumb_trail(); ?>
+
+		<main id="main" <?php post_class( 'site-main' ); ?> role="main">
+
+		<?php while ( have_posts() ) : the_post(); ?>
+
+			<?php get_template_part( 'content', 'handbook'); ?>
+
+			<?php \WPorg_Handbook_Navigation::show_nav_links( 'REST API Table of Contents' ); ?>
+
+		<?php endwhile; // end of the loop. ?>
+
+		</main><!-- #main -->
+		<?php get_sidebar(); ?>
+	</div><!-- #primary -->
+<?php get_footer(); ?>
