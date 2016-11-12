f86f1ea3d2071076046416ea335b2747870023c3
Sergey Biryukov <sergeybiryukov@git.wordpress.org>
2016-11-12 01:00:06 +0000

    Support Theme: Bump CSS version for cache bust.
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@4352 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-support/functions.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-support/functions.php
@@ -31,7 +31,7 @@ function wporg_support_scripts() {
 		'forum-wp4-style',
 		get_template_directory_uri() . '/style.css',
 		array( 'bb-base' ),
-		'20161102'
+		'20161112'
 	);
 
 	wp_style_add_data( 'forum-wp4-style', 'rtl', 'replace' );
