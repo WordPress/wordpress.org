7f8dbf557b852dc39c44dfa343377e0c1f982a3a
John James Jacoby <johnjamesjacoby@git.wordpress.org>
2016-11-16 16:16:43 +0000

    BuddyPress.org: Update codex theme header navigation to match primary site.
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@4371 74240141-8908-4e6f-9713-ba540dce6ec7

--- buddypress.org/public_html/wp-content/themes/codex-buddypress-org/header-nav.php
+++ buddypress.org/public_html/wp-content/themes/codex-buddypress-org/header-nav.php
@@ -1,9 +1,8 @@
 <ul id="nav" class="menu">
-	<li><a href="https://buddypress.org/about/">About</a></li>
-	<li><a href="https://buddypress.org/plugins/">Plugins</a></li>
-	<li><a href="https://buddypress.org/themes/">Themes</a></li>
-	<li class="current"class="current"><a href="https://codex.buddypress.org/">Documentation</a></li>
-	<li><a href="https://buddypress.org/blog/">Blog</a></li>
-	<li><a href="https://buddypress.org/support/">Support</a></li>
-	<li><a href="https://buddypress.org/download/">Download</a></li>
+	<li id="menu-item-251086" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-251086"><a href="https://buddypress.org/about/">About</a></li>
+	<li id="menu-item-251087" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-251087"><a href="https://codex.buddypress.org/">Documentation</a></li>
+	<li id="menu-item-252086" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-252086"><a href="https://codex.buddypress.org/participate-and-contribute/">Get Involved</a></li>
+	<li id="menu-item-251088" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-251088"><a href="https://buddypress.org/blog/">News</a></li>
+	<li id="menu-item-251089" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-251089"><a href="https://buddypress.org/support/">Support</a></li>
+	<li id="menu-item-251090" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-251090"><a href="https://buddypress.org/download/">Download</a></li>
 </ul>
