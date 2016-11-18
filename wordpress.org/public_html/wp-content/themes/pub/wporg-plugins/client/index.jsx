3c15765a49ef94c5ee8cfd66d130ed562099ce13
Alex Shiels <tellyworth@git.wordpress.org>
2016-11-18 03:00:24 +0000

    Plugin directory: tentatively use React for the screenshot slider only.
    React is still disabled by default till this is confirmed.
    
    See #2246
    
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@4373 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-plugins/client/index.jsx
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-plugins/client/index.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Provider } from 'react-redux';
 
-import Router from 'modules/router';
-import getStore from 'modules/store';
+import Screenshots from 'components/plugin/sections/screenshots';
+
+// Temporary hack to use the srceenshot viewer without the full React client
+var elements = document.querySelectorAll( '#screenshots figure' );
+var images = [];
+for ( var i=0; i < elements.length; i++ ) {
+	var item = { 
+		src: elements[i].querySelector('img.screenshot').src,
+		caption: elements[i].querySelector('figcaption').textContent,
+	}
+	images.push( item );
+}
 
 render(
-	<Provider store={ getStore() }>
-		{ Router }
-	</Provider>,
-	document.getElementById( 'content' )
+	<Screenshots screenshots={images}>
+	</Screenshots>,
+	document.getElementById( 'screenshots' )
 );
