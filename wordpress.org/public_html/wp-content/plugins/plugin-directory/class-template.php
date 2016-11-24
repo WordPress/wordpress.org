875fc8d5fee40f7d5ea711894ec130d34983e075
dd32 <dd32@74240141-8908-4e6f-9713-ba540dce6ec7>
2016-11-24 01:42:02 +0000

    Plugin Directory: The `developers` tab is called `Contributors & Developers` not `Authors`.
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@4411 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/plugins/plugin-directory/class-template.php
+++ wordpress.org/public_html/wp-content/plugins/plugin-directory/class-template.php
@@ -182,7 +182,7 @@ class Template {
 					break;
 
 				case 'developers':
-					$title = _x( 'Authors', 'plugin tab title', 'wporg-plugins' );
+					$title = _x( 'Contributors &amp; Developers', 'plugin tab title', 'wporg-plugins' );
 					$url   = trailingslashit( $permalink ) . '/' . $section_slug . '/';
 					break;
 
