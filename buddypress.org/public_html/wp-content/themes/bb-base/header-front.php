f352d2d725ab796cd7e8673fb264231d305a6c12
John James Jacoby <johnjamesjacoby@git.wordpress.org>
2016-11-30 13:37:08 +0000

    BuddyPress.org/bbPress.org: Add comma's to srcset attributes, missed in r4447.
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@4448 74240141-8908-4e6f-9713-ba540dce6ec7

--- buddypress.org/public_html/wp-content/themes/bb-base/header-front.php
+++ buddypress.org/public_html/wp-content/themes/bb-base/header-front.php
@@ -4,7 +4,7 @@
 		<p>This software is really great. Go check it out for yourself and tell us how much you like it in the forums!</p>
 		<div>
 			<a href="<?php bloginfo( 'url' ); ?>/download/" id="big-demo-button" class="button">Download &rarr;</a>
-			<img src="<?php bloginfo( 'template_url' ); ?>/images/screenshots.png?v=6" srcset="<?php bloginfo( 'template_url' ); ?>/images/screenshots.png?v=6 1x <?php bloginfo( 'template_url' ); ?>/images/screenshots-2x.png?v=6 2x" alt="Screenshots">
+			<img src="<?php bloginfo( 'template_url' ); ?>/images/screenshots.png?v=6" srcset="<?php bloginfo( 'template_url' ); ?>/images/screenshots.png?v=6 1x, <?php bloginfo( 'template_url' ); ?>/images/screenshots-2x.png?v=6 2x" alt="Screenshots">
 		</div>
 	</div></div>
 	<hr class="hidden" />
