3edaa1ee38798538821e50abd95e920fa496a06f
Scott Reilly <coffee2code@git.wordpress.org>
2016-12-06 20:56:33 +0000

    Handbook plugin: Bump CSS version for cache bust.
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@4498 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/plugins/handbook/handbook.php
+++ wordpress.org/public_html/wp-content/plugins/handbook/handbook.php
@@ -40,7 +40,7 @@ class WPorg_Handbook_Init {
 	}
 
 	static public function enqueue_styles() {
-		wp_enqueue_style( 'wporg-handbook-css', plugins_url( '/stylesheets/callout-boxes.css', __FILE__ ), array(), '20160715' );
+		wp_enqueue_style( 'wporg-handbook-css', plugins_url( '/stylesheets/callout-boxes.css', __FILE__ ), array(), '20161206' );
 	}
 
 	static public function enqueue_scripts() {
