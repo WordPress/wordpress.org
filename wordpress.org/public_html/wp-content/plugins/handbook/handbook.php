eede808628484a8dbe6980109dab39c993cde192
Scott Reilly <coffee2code@git.wordpress.org>
2017-01-11 19:08:32 +0000

    Handbook plugin: Use `h2` as tag for widget titles instead of `h1`.
    Props SergeyBiryukov.
    Fixes #2371.
    
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@4626 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/plugins/handbook/handbook.php
+++ wordpress.org/public_html/wp-content/plugins/handbook/handbook.php
@@ -353,8 +353,8 @@ class WPorg_Handbook {
 			'description' => sprintf( __( 'Used on %s pages', 'wporg' ), $this->label ),
 			'before_widget' => '<aside id="%1$s" class="widget %2$s">',
 			'after_widget'  => '</aside>',
-			'before_title'  => '<h1 class="widget-title">',
-			'after_title'   => '</h1>',
+			'before_title'  => '<h2 class="widget-title">',
+			'after_title'   => '</h2>',
 		);
 
 		$sidebar_args = apply_filters( 'wporg_handbook_sidebar_args', $sidebar_args, $this );
