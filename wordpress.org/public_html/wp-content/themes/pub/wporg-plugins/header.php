8dad5f3f2136062f253fbf7cd9ce8bd2595b6be5
Alex Shiels <tellyworth@git.wordpress.org>
2017-01-13 00:17:16 +0000

    Plugin directory: switch survey link to wordpressdotorg PollDaddy account.
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@4649 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-plugins/header.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-plugins/header.php
@@ -58,7 +58,7 @@ require WPORGPATH . 'header.php';
 						</div>
 					</nav><!-- #site-navigation -->
 				<?php endif; ?>
-				<div class="feedback-survey"><a href="http://mapk.polldaddy.com/s/new-plugin-directory"><span class="dashicons dashicons-megaphone"></span><span class="survey-msg">
+				<div class="feedback-survey"><a href="http://wordpressdotorg.polldaddy.com/s/new-plugin-directory"><span class="dashicons dashicons-megaphone"></span><span class="survey-msg">
 					<?php _e( 'Share your feedback!' ); ?>
 				</span></a></div>
 			</div><!-- .site-branding -->
