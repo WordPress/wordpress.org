2d223d722996bcc0ea29da3c424f04dae82cc44d
Sergey Biryukov <sergeybiryukov@git.wordpress.org>
2017-01-14 03:51:51 +0000

    Support Theme: Internationalize the text added in [4683].
    See #2357.
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@4687 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-support/bbpress/loop-forums-homepage.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-support/bbpress/loop-forums-homepage.php
@@ -14,8 +14,16 @@
 
 <div class="themes-plugins">
 	
-	<h3>Themes and Plugins</h3>
-	<p>Looking for help with a specific <a href="https://wordpress.org/themes/"><span class="dashicons dashicons-admin-appearance"></span> theme</a> or <a href="https://wordpress.org/plugins/"><span class="dashicons dashicons-admin-plugins"></span> plugin</a>? Head to the theme or plugin's page and find the "View support forum" link to visit the theme or plugin's individual forum.</p>
+	<h3><?php _e( 'Themes and Plugins', 'wporg-forums' ); ?></h3>
+	<p><?php
+		/* translators: 1: Theme Directory URL, 2: Appearance icon, 3: Plugin Directory URL, 4: Plugins icon */
+		printf( __( 'Looking for help with a specific <a href="%1$s">%2$s theme</a> or <a href="%3$s">%4$s plugin</a>? Head to the theme or plugin\'s page and find the "View support forum" link to visit the theme or plugin\'s individual forum.', 'wporg-forums' ),
+			esc_url( __( 'https://wordpress.org/themes/', 'wporg-forums' ) ),
+			'<span class="dashicons dashicons-admin-appearance"></span>',
+			esc_url( __( 'https://wordpress.org/plugins/', 'wporg-forums' ) ),
+			'<span class="dashicons dashicons-admin-plugins"></span>'
+		);
+	?></p>
 
 </div>
 
