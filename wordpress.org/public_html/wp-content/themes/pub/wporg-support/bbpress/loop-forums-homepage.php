46573e81fe9d4d537b9371519288c26e748f7afb
Mark Uraine <mapk@git.wordpress.org>
2017-01-14 03:00:56 +0000

    Component: Added theme and plugin msg to Support Homepage
    There's been confusion for users about where to submit questions regarding specific themes and plugins, so I added a piece explaining this on the homepage.
    
    Props @zoonini @samuelsidler
    Fixes #2357
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@4683 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-support/bbpress/loop-forums-homepage.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-support/bbpress/loop-forums-homepage.php
@@ -12,4 +12,11 @@
 
 </div><!-- .forums-directory -->
 
+<div class="themes-plugins">
+	
+	<h3>Themes and Plugins</h3>
+	<p>Looking for help with a specific <a href="https://wordpress.org/themes/"><span class="dashicons dashicons-admin-appearance"></span> theme</a> or <a href="https://wordpress.org/plugins/"><span class="dashicons dashicons-admin-plugins"></span> plugin</a>? Head to the theme or plugin's page and find the "View support forum" link to visit the theme or plugin's individual forum.</p>
+
+</div>
+
 <?php do_action( 'bbp_template_after_forums_loop' ); ?>
