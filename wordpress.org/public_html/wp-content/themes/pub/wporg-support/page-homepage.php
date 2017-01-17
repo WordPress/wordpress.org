14083d2fb57f5cba195625db76f576494bd94af0
Sergey Biryukov <sergeybiryukov@git.wordpress.org>
2017-01-17 17:42:20 +0000

    Support Theme: Internationalize info boxes in `page-homepage.php` template.
    See #2151.
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@4704 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-support/page-homepage.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-support/page-homepage.php
@@ -19,26 +19,35 @@ get_header(); ?>
 		<section class="three-up" id="forum-welcome">
 			<div>
 				<div class="info-box">
-					<span class="dashicons dashicons-sos"></span>
-					<h3>Welcome to Support</h3>
-					<p>Our community-based Support Forums are a great place to learn, share, and troubleshoot.</p>
-					<p><a href="https://codex.wordpress.org/Getting_Started_with_WordPress">Start learning</a></p>
+					<span class="dashicons <?php
+						/* translators: dashicon class name for 'Welcome to Support' section. Do not translate into your own language. */
+						esc_attr_e( 'dashicons-sos', 'wporg-forums' );
+					?>"></span>
+					<h3><?php _e( 'Welcome to Support', 'wporg-forums' ); ?></h3>
+					<p><?php _e( 'Our community-based Support Forums are a great place to learn, share, and troubleshoot.', 'wporg-forums' ); ?></p>
+					<p><?php _e( '<a href="https://codex.wordpress.org/Getting_Started_with_WordPress">Start learning</a>', 'wporg-forums' ); ?></p>
 				</div>
 			</div>
 			<div>
 				<div class="info-box">
-					<span class="dashicons dashicons-portfolio"></span>
-					<h3>Documentation</h3>
-					<p>Your first stop where you'll find information on everything from installing to creating plugins.</p>
-					<p><a href="https://codex.wordpress.org/">Explore documentation</a></p>
+					<span class="dashicons <?php
+						/* translators: dashicon class name for 'Documentation' section. Do not translate into your own language. */
+						esc_attr_e( 'dashicons-portfolio', 'wporg-forums' );
+					?>"></span>
+					<h3><?php _e( 'Documentation', 'wporg-forums' ); ?></h3>
+					<p><?php _e( 'Your first stop where you\'ll find information on everything from installing to creating plugins.', 'wporg-forums' ); ?></p>
+					<p><?php _e( '<a href="https://codex.wordpress.org/">Explore documentation</a>', 'wporg-forums' ); ?></p>
 				</div>
 			</div>
 			<div>
 				<div class="info-box">
-					<span class="dashicons dashicons-hammer"></span>
-					<h3>Get Involved</h3>
-					<p>The Support Handbook is great for tips, tricks, and advice regarding giving the best support possible.</p>
-					<p><a href="https://make.wordpress.org/support/handbook/">Explore the Handbook</a></p>
+					<span class="dashicons <?php
+						/* translators: dashicon class name for 'Get Involved' section. Do not translate into your own language. */
+						esc_attr_e( 'dashicons-hammer', 'wporg-forums' );
+					?>"></span>
+					<h3><?php _e( 'Get Involved', 'wporg-forums' ); ?></h3>
+					<p><?php _e( 'The Support Handbook is great for tips, tricks, and advice regarding giving the best support possible.', 'wporg-forums' ); ?></p>
+					<p><?php _e( '<a href="https://make.wordpress.org/support/handbook/">Explore the Handbook</a>', 'wporg-forums' ); ?></p>
 				</div>
 			</div>
 		</section>
