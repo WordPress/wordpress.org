420fa4c284682dc429bf48a646e42d38b76f932c
Scott Reilly <coffee2code@git.wordpress.org>
2017-01-18 19:30:21 +0000

    Support Theme: Bump CSS version for cache bust.
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@4723 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-support/functions.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-support/functions.php
@@ -31,7 +31,7 @@ function wporg_support_scripts() {
 		'forum-wp4-style',
 		get_template_directory_uri() . '/style.css',
 		array( 'bb-base' ),
-		'20170118'
+		'20170118a'
 	);
 
 	wp_style_add_data( 'forum-wp4-style', 'rtl', 'replace' );
