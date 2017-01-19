9ba88b064689f6d8e18afbe7cafc5b97384b8d52
Daniel Bachhuber <danielbachhuber@git.wordpress.org>
2017-01-19 15:08:33 +0000

    WP-CLI: Stub a new plugin for general customizations
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@4732 74240141-8908-4e6f-9713-ba540dce6ec7

---
+++ wordpress.org/public_html/wp-content/plugins/wporg-cli/wporg-cli.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Plugin name: WP-CLI: WordPress.org Customizations
+ * Description: Provides general customizations for WP-CLI's presence on WordPress.org
+ * Version:     0.1.0
+ * Author:      WordPress.org
+ * Author URI:  http://wordpress.org/
+ * License:     GPLv2 or later
+ */
