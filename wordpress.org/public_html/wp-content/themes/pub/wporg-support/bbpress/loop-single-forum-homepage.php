5376ee3f78989a666e3a100ab81c4814e9099dd1
Sergey Biryukov <sergeybiryukov@git.wordpress.org>
2017-01-24 08:22:10 +0000

    Support Theme: Make "View forum" links on home page translatable.
    See #2151.
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@4763 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-support/bbpress/loop-single-forum-homepage.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-support/bbpress/loop-single-forum-homepage.php
@@ -2,6 +2,6 @@
 	
 	<a class="bbp-forum-title" href="<?php bbp_forum_permalink(); ?>" title="<?php bbp_forum_title(); ?>"><h3><?php bbp_forum_title(); ?></h3></a>
 	<p><?php bbp_forum_content(); ?></p>
-	<p><a href="<?php bbp_forum_permalink(); ?>" title="<?php bbp_forum_title(); ?>" class="viewmore">View forum</a></p>
+	<p><a href="<?php bbp_forum_permalink(); ?>" title="<?php bbp_forum_title(); ?>" class="viewmore"><?php _e( 'View forum', 'wporg-forums' ); ?></a></p>
 
 </div><!-- #bbp-forum-<?php bbp_forum_id(); ?> -->
