6fdfcb00ec866c7df816cafb49e12cf87963c7a7
Dion Hulse <dd32@git.wordpress.org>
2017-01-28 09:56:10 +0000

    Plugin Directory: Tweak wording of the "please don't submit plugins here" message.
    Props SergeyBiryukov.
    Fixes #2457
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@4817 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/plugins/plugin-directory/shortcodes/class-upload.php
+++ wordpress.org/public_html/wp-content/plugins/plugin-directory/shortcodes/class-upload.php
@@ -41,7 +41,7 @@ class Upload {
 				<?php
 				if ( '/plugins-wp' === parse_url( home_url(), PHP_URL_PATH ) ) {
 					echo '<div class="notice notice-error notice-alt">
-						<p>Please submit all new plugin requests through the existing form <a href="https://wordpress.org/plugins/add/">available here</a>, You\'re currently viewing the beta version of the upcoming plugin directory, and is only for tesiting purposes.</p>
+						<p>Please submit all new plugin requests through the existing form <a href="https://wordpress.org/plugins/add/">available here</a>, You\'re currently viewing the beta version of the upcoming plugin directory, and this form is only for testing purposes.</p>
 					</div>';
 				}
 				?>
