1f24af3973e9d080af8606b8e21223eef9051750
Dominik Schilling (ocean90) <ocean90@git.wordpress.org>
2017-02-18 11:39:32 +0000

    Translate: Render WordPress' toolbar on front end.
    Since GlotPress has its own header/footer actions we have to call WordPress' render callbacks on them.
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@4923 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/plugins/wporg-gp-customizations/inc/class-plugin.php
+++ wordpress.org/public_html/wp-content/plugins/wporg-gp-customizations/inc/class-plugin.php
@@ -44,12 +44,76 @@ class Plugin {
 		add_filter( 'body_class', array( $this, 'wporg_add_make_site_body_class' ) );
 		add_filter( 'wporg_translate_language_pack_theme_args', array( $this, 'set_version_for_twentyseventeen_language_pack' ), 10, 2 );
 
+		// Toolbar.
+		add_action( 'admin_bar_menu', array( $this, 'add_profile_settings_to_admin_bar' ) );
+		add_action( 'admin_bar_init', array( $this, 'show_admin_bar' ) );
+		add_action( 'add_admin_bar_menus', array( $this, 'remove_admin_bar_menus' ) );
+
+		add_action( 'template_redirect', array( $this, 'jetpack_stats' ), 1 );
+
 		// Load the API endpoints.
 		add_action( 'rest_api_init', array( __NAMESPACE__ . '\REST_API\Base', 'load_endpoints' ) );
 
 		if ( defined( 'WP_CLI' ) && WP_CLI ) {
 			$this->register_cli_commands();
 		}
+	}
+
+	/**
+	 * Adds support for Jetpack Stats.
+	 */
+	public function jetpack_stats() {
+		if ( ! function_exists( 'stats_hide_smile_css' ) ) {
+			return;
+		}
+
+		add_action( 'gp_head', 'stats_hide_smile_css' );
+		add_action( 'gp_head', 'stats_admin_bar_head', 100 );
+		add_action( 'gp_footer', 'stats_footer', 101 );
+	}
+
+	/**
+	 * Renders the toolbar.
+	 */
+	public function show_admin_bar() {
+		add_action( 'gp_head', 'wp_admin_bar_header' );
+		add_action( 'gp_head', '_admin_bar_bump_cb' );
+
+		gp_enqueue_script( 'admin-bar' );
+		gp_enqueue_style( 'admin-bar' );
+
+		add_action( 'gp_footer', 'wp_admin_bar_render', 1000 );
+	}
+
+	/**
+	 * Adds the linkt to profile settings to the user actions toolbar menu.
+	 *
+	 * @param \WP_Admin_Bar $wp_admin_bar
+	 */
+	public function add_profile_settings_to_admin_bar( $wp_admin_bar ) {
+		$logout_node = $wp_admin_bar->get_node( 'logout' );
+		$wp_admin_bar->remove_node( 'logout' );
+
+		$wp_admin_bar->add_node( [
+			'parent' => 'user-actions',
+			'id'     => 'gp-profile-settings',
+			'title'  => 'Translate Settings',
+			'href'   => gp_url( '/settings' ),
+		] );
+
+		if ( $logout_node ) {
+			$wp_admin_bar->add_node( $logout_node ); // Ensures that logout is the last action.
+		}
+	}
+
+	/**
+	 * Removes default toolbar menus which are not needed.
+	 */
+	public function remove_admin_bar_menus() {
+		remove_action( 'admin_bar_menu', 'wp_admin_bar_search_menu', 4 );
+		remove_action( 'admin_bar_menu', 'wp_admin_bar_comments_menu', 60 );
+		remove_action( 'admin_bar_menu', 'wp_admin_bar_new_content_menu', 70 );
+		remove_action( 'admin_bar_menu', 'wp_admin_bar_edit_menu', 80 );
 	}
 
 	/**
