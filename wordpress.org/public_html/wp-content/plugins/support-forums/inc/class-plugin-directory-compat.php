6fd609c5c687c38b03df5844d67fabf4a4121e3e
Sergey Biryukov <sergeybiryukov@git.wordpress.org>
2017-02-18 02:57:05 +0000

    Support Forums: Check if `wporg_get_plugin_icon()` exists before calling it.
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@4916 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/plugins/support-forums/inc/class-plugin-directory-compat.php
+++ wordpress.org/public_html/wp-content/plugins/support-forums/inc/class-plugin-directory-compat.php
@@ -92,7 +92,10 @@ class Plugin_Directory_Compat extends Directory_Compat {
 	}
 
 	public function do_topic_sidebar() {
-		include_once WPORGPATH . 'extend/plugins-plugins/_plugin-icons.php';
+		if ( file_exists( WPORGPATH . 'extend/plugins-plugins/_plugin-icons.php' ) ) {
+			include_once WPORGPATH . 'extend/plugins-plugins/_plugin-icons.php';
+		}
+
 		$plugin  = sprintf( '<a href="//wordpress.org/plugins/%s/">%s</a>', esc_attr( $this->slug() ), esc_html( $this->plugin->post_title ) );
 		$faq     = sprintf( '<a href="//wordpress.org/plugins/%s/faq/">%s</a>', esc_attr( $this->slug() ), __( 'Frequently Asked Questions', 'wporg-forums' ) );
 		$support = sprintf( '<a href="//wordpress.org/support/plugin/%s/">%s</a>', esc_attr( $this->slug() ), __( 'Support Threads', 'wporg-forums' ) );
@@ -116,7 +119,9 @@ class Plugin_Directory_Compat extends Directory_Compat {
 		<div>
 			<h3><?php _e( 'About this Plugin', 'wporg-forums' ); ?></h3>
 			<ul>
+				<?php if ( function_exists( 'wporg_get_plugin_icon' ) ) : ?>
 				<li><?php echo wporg_get_plugin_icon( $this->slug, 128 ); ?></li>
+				<?php endif; ?>
 				<li style="clear:both;"><?php echo $plugin; ?></li>
 				<?php if ( ! empty( $this->plugin->post_content ) && false !== strpos( $this->plugin->post_content, '<!--section=faq-->' ) ) : ?>
 				<li><?php echo $faq; ?></li>
