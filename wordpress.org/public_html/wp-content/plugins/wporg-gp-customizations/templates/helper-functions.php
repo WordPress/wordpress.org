aa721bbd561109645cfb0c1a4446390c4eab79a9
Dominik Schilling (ocean90) <ocean90@git.wordpress.org>
2017-03-01 23:04:36 +0000

    Translate: Bump stylesheet version for cache bust.
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@5032 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/plugins/wporg-gp-customizations/templates/helper-functions.php
+++ wordpress.org/public_html/wp-content/plugins/wporg-gp-customizations/templates/helper-functions.php
@@ -1,5 +1,5 @@
 <?php
-wp_register_style( 'wporg-translate', plugins_url( 'style.css', __FILE__ ), array( 'gp-base' ), '20170218a' );
+wp_register_style( 'wporg-translate', plugins_url( 'style.css', __FILE__ ), array( 'gp-base' ), '20170302' );
 gp_enqueue_style( 'wporg-translate' );
 
 wp_register_style( 'chartist', plugins_url( 'css/chartist.min.css', __FILE__ ), array(), '0.9.5' );
