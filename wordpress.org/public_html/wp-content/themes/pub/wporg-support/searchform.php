d6e51e0f95ea6c8a8e75166a6803296ba76e4321
Sergey Biryukov <sergeybiryukov@git.wordpress.org>
2017-03-01 04:30:36 +0000

    Support Theme: Properly set up bbPress search form for international forums.
    Fixes #2450.
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@5022 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-support/searchform.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-support/searchform.php
@@ -1,8 +1,22 @@
 <?php
 namespace WordPressdotorg\Forums;
 ?>
+
+<?php if ( bb_is_intl_forum() ) : ?>
+
+<form role="search" method="get" class="search-form" action="<?php bbp_search_url(); ?>">
+	<label for="s" class="screen-reader-text"><?php _ex( 'Search for:', 'label', 'wporg-forums' ); ?></label>
+	<input type="hidden" name="action" value="bbp-search-request" />
+	<input type="search" id="s" class="search-field" placeholder="<?php echo esc_attr_x( 'Search forums', 'placeholder', 'wporg-forums' ); ?>" value="<?php echo esc_attr( bbp_get_search_terms() ); ?>" name="bbp_search" />
+	<button class="button button-primary button-search"><i class="dashicons dashicons-search"></i><span class="screen-reader-text"><?php _e( 'Search forums', 'wporg-forums' ); ?></span></button>
+</form>
+
+<?php else : ?>
+
 <form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
 	<label for="s" class="screen-reader-text"><?php _ex( 'Search for:', 'label', 'wporg-forums' ); ?></label>
 	<input type="search" id="s" class="search-field" placeholder="<?php echo esc_attr_x( 'Search forums', 'placeholder', 'wporg-forums' ); ?>" value="<?php the_search_query(); ?>" name="s" />
 	<button class="button button-primary button-search"><i class="dashicons dashicons-search"></i><span class="screen-reader-text"><?php _e( 'Search forums', 'wporg-forums' ); ?></span></button>
 </form>
+
+<?php endif; ?>
