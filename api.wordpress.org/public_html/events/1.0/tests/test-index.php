e9341e6f59855616105e56ffb299a945c7f92468
Ian Dunn <iandunn@git.wordpress.org>
2017-03-01 23:13:18 +0000

    Events API: Add automated tests for `get_location()`
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@5034 74240141-8908-4e6f-9713-ba540dce6ec7

---
+++ api.wordpress.org/public_html/events/1.0/tests/test-index.php
@@ -0,0 +1,252 @@
+<?php
+
+namespace Dotorg\API\Events;
+
+/**
+ * Main entry point
+ */
+function run_tests() {
+	define( 'RUNNING_TESTS', true );
+	require_once( dirname( __DIR__ ) . '/index.php' );
+
+	$failed = 0;
+	$failed += test_get_location();
+
+	printf( "\n\nFinished running all tests. %d failed.\n", $failed );
+}
+
+/**
+ * Output the results of an individual test
+ *
+ * @param int   $case_id
+ * @param bool  $passed
+ * @param mixed $expected_result
+ * @param mixed $actual_result
+ */
+function output_results( $case_id, $passed, $expected_result, $actual_result ) {
+	printf(
+		"\n* %s: %s",
+		$case_id,
+		$passed ? 'PASSED' : '_FAILED_'
+	);
+
+	if ( ! $passed ) {
+		$expected_output = is_scalar( $expected_result ) ? var_export( $expected_result, true ) : print_r( $expected_result, true );
+		$actual_output   = is_scalar( $actual_result   ) ? var_export( $actual_result,   true ) : print_r( $actual_result,   true );
+
+		printf(
+			"\n\nExpected result: %s\nActual result: %s",
+			$expected_output,
+			$actual_output
+		);
+	}
+}
+
+/**
+ * Test `get_location()`
+ *
+ * @return bool The number of failures
+ */
+function test_get_location() {
+	$failed = 0;
+	$cases  = get_location_test_cases();
+
+	printf( "\nRunning %d location tests\n", count( $cases ) );
+
+	foreach ( $cases as $case_id => $case ) {
+		$actual_result = get_location( $case['input'] );
+
+		// Normalize to lowercase to account for inconsistency in the IP database
+		if ( isset( $actual_result['description'] ) && is_string( $actual_result['description'] ) ) {
+			$actual_result['description'] = strtolower( $actual_result['description'] );
+		}
+
+		$differences = array_diff_assoc( $case['expected'], $actual_result );
+		$passed      = empty( $differences );
+
+		output_results( $case_id, $passed, $case['expected'], $actual_result );
+
+		if ( ! $passed ) {
+			$failed++;
+		}
+	}
+
+	return $failed;
+}
+
+/**
+ * Get the cases for testing `get_location()`
+ *
+ * @return array
+ */
+function get_location_test_cases() {
+	 $cases = array(
+		/*
+		 * Only the country is given
+		 */
+		'country-australia' => array(
+			'input' => array(
+				'country' => 'AU',
+			),
+			'expected' => array(
+				'country' => 'AU'
+			),
+		),
+
+
+		/*
+		 * The city, locale, and timezone are given
+		 */
+		'city-africa' => array(
+			'input' => array(
+				'location_name' => 'Nairobi',
+				'locale'        => 'en_GB',
+				'timezone'      => 'Africa/Nairobi',
+			),
+			'expected' => array(
+				'description' => 'nairobi',
+				'latitude'    => '-1.2833300',
+				'longitude'   => '36.8166700',
+				'country'     => 'KE',
+			),
+		),
+
+		'city-asia' => array(
+			'input' => array(
+				'location_name' => 'Tokyo',
+				'locale'        => 'ja',
+				'timezone'      => 'Asia/Tokyo',
+			),
+			'expected' => array(
+				'description' => 'tokyo',
+				'latitude'    => '35.6895000',
+				'longitude'   => '139.6917100',
+				'country'     => 'JP',
+			),
+		),
+
+		'city-europe' => array(
+			'input' => array(
+				'location_name' => 'Berlin',
+				'locale'        => 'de_DE',
+				'timezone'      => 'Europe/Berlin',
+			),
+			'expected' => array(
+				'description' => 'berlin',
+				'latitude'    => '52.5243700',
+				'longitude'   => '13.4105300',
+				'country'     => 'DE',
+			),
+		),
+
+		'city-north-america' => array(
+			'input' => array(
+				'location_name' => 'Vancouver',
+				'locale'        => 'en_CA',
+				'timezone'      => 'America/Vancouver',
+			),
+			'expected' => array(
+				'description' => 'vancouver',
+				'latitude'    => '49.2496600',
+				'longitude'   => '-123.1193400',
+				'country'     => 'CA',
+			),
+		),
+
+		'city-oceania' => array(
+			'input' => array(
+				'location_name' => 'Brisbane',
+				'locale'        => 'en_AU',
+				'timezone'      => 'Australia/Brisbane',
+			),
+			'expected' => array(
+				'description' => 'brisbane',
+				'latitude'    => '-27.4679400',
+				'longitude'   => '153.0280900',
+				'country'     => 'AU',
+			),
+		),
+
+		'city-south-america' => array(
+			'input' => array(
+				'location_name' => 'Sao Paulo',
+				'locale'        => 'pt_BR',
+				'timezone'      => 'America/Sao_Paulo',
+			),
+			'expected' => array(
+				'description' => 'são paulo',
+				'latitude'    => '-23.5475000',
+				'longitude'   => '-46.6361100',
+				'country'     => 'BR',
+			),
+		),
+
+
+		/*
+		 * Only the IP is given
+		 */
+		'ip-africa' => array(
+			'input' => array( 'ip' => '41.191.232.22' ),
+			'expected' => array(
+				'description' => 'harare',
+				'latitude'    => '-17.82935',
+				'longitude'   => '31.05389',
+				'country'     => 'ZW',
+			),
+		),
+
+		'ip-asia' => array(
+			'input' => array( 'ip' => '86.108.55.28' ),
+			'expected' => array(
+				'description' => 'amman',
+				'latitude'    => '31.95522',
+				'longitude'   => '35.94503',
+				'country'     => 'JO',
+			),
+		),
+
+		'ip-europe' => array(
+			'input' => array( 'ip' => '80.95.186.144' ),
+			'expected' => array(
+				'description' => 'belfast',
+				'latitude'    => '54.58333',
+				'longitude'   => '-5.93333',
+				'country'     => 'GB',
+			),
+		),
+
+		'ip-north-america' => array(
+			'input' => array( 'ip' => '189.147.186.0' ),
+			'expected' => array(
+				'description' => 'mexico city',
+				'latitude'    => '19.42847',
+				'longitude'   => '-99.12766',
+				'country'     => 'MX',
+			),
+		),
+
+		'ip-oceania' => array(
+			'input' => array( 'ip' => '116.12.57.122' ),
+			'expected' => array(
+				'description' => 'auckland',
+				'latitude'    => '-36.86667',
+				'longitude'   => '174.76667',
+				'country'     => 'NZ',
+			),
+		),
+
+		'ip-south-america' => array(
+			'input' => array( 'ip' => '189.147.186.0' ),
+			'expected' => array(
+				'description' => 'mexico city',
+				'latitude'    => '19.42847',
+				'longitude'   => '-99.12766',
+				'country'     => 'MX',
+			),
+		),
+	);
+
+	 return $cases;
+}
+
+run_tests();
