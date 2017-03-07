7d6710a35c5785cc5ecd63ea8a5aa1aebcc75a76
Sergey Biryukov <sergeybiryukov@git.wordpress.org>
2017-03-07 15:54:12 +0000

    Support Forums: In `Moderators::archive_post()` and `::unarchive_post()`, store `user_nicename` instead of `user_login`, for consistency with [5002].
    See #2474, #2493.
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@5111 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/plugins/support-forums/inc/class-moderators.php
+++ wordpress.org/public_html/wp-content/plugins/support-forums/inc/class-moderators.php
@@ -268,7 +268,7 @@ class Moderators {
 			), true );
 			if ( $post_id ) {
 				update_post_meta( $post->ID, self::ARCHIVED_META, $post->post_status );
-				update_post_meta( $post->ID, self::MODERATOR_META, wp_get_current_user()->user_login );
+				update_post_meta( $post->ID, self::MODERATOR_META, wp_get_current_user()->user_nicename );
 
 				if ( bbp_is_reply( $post->ID ) ) {
 					bbp_increase_topic_reply_count_hidden( bbp_get_reply_topic_id( $post->ID ) );
@@ -302,7 +302,7 @@ class Moderators {
 			) );
 			if ( $post_id ) {
 				delete_post_meta( $post->ID, self::ARCHIVED_META );
-				update_post_meta( $post->ID, self::MODERATOR_META, wp_get_current_user()->user_login );
+				update_post_meta( $post->ID, self::MODERATOR_META, wp_get_current_user()->user_nicename );
 
 				if ( bbp_is_reply( $post->ID ) ) {
 					bbp_decrease_topic_reply_count_hidden( bbp_get_reply_topic_id( $post->ID ) );
