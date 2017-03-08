f8582a4604bef72958c92555684f20b81123fc22
Sergey Biryukov <sergeybiryukov@git.wordpress.org>
2017-03-08 11:53:41 +0000

    Support Theme: After [5122], use correct relative link to the forums list.
    See #2455.
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@5126 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-support/header.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-support/header.php
@@ -13,7 +13,7 @@ namespace WordPressdotorg\Forums;
 
 $menu_items = array(
 	/* translators: relative link to the forums home page */
-	_x( '/support/forums/', 'header menu', 'wporg-forums' )                             => _x( 'Forums', 'header menu', 'wporg-forums' ),
+	_x( '/forums/', 'header menu', 'wporg-forums' )                                     => _x( 'Forums', 'header menu', 'wporg-forums' ),
 	_x( 'https://codex.wordpress.org/Main_Page', 'header menu', 'wporg-forums' )        => _x( 'Documentation', 'header menu', 'wporg-forums' ),
 	_x( 'https://make.wordpress.org/support/handbook/', 'header menu', 'wporg-forums' ) => _x( 'Get Involved', 'header menu', 'wporg-forums' ),
 );
