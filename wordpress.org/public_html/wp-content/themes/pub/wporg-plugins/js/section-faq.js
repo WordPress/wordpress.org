ca49bf105359747fc94a72cd09a99a9289dfbad3
Konstantin Obenland <obenland@git.wordpress.org>
2017-03-23 15:37:52 +0000

    Plugin Directory: Add back compat with existing FAQ markup
    Fixes a bug where existing FAQs were not toggle-able due to the missing button.
    
    See #2296.
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@5167 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-plugins/js/section-faq.js
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-plugins/js/section-faq.js
@@ -12,8 +12,19 @@
 			$question.siblings( '.open' ).toggleClass( 'open' ).attr( 'aria-expanded', false ).parent().next( 'dd' ).slideToggle( 200 );
 		}
 
+		$question.parent().toggleClass( 'open' ).attr( 'aria-expanded', function( index, attribute ) {
+			return 'true' !== attribute;
+		} ).next( 'dd' ).slideToggle( 200 );
+	})
+		.on( 'click', 'dt', function( event ) {
+		var $question = $( event.target );
+
+		if ( ! $question.is( '.open' ) ) {
+			$question.siblings( '.open' ).toggleClass( 'open' ).attr( 'aria-expanded', false ).next( 'dd' ).slideToggle( 200 );
+		}
+
 		$question.toggleClass( 'open' ).attr( 'aria-expanded', function( index, attribute ) {
 			return 'true' !== attribute;
-		} ).parent().next( 'dd' ).slideToggle( 200 );
+		} ).next( 'dd' ).slideToggle( 200 );
 	});
 } )( jQuery );
