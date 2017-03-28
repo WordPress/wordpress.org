3cd4f1509098d8b960bdaa9d3767d0547597ce43
Dion Hulse <dd32@git.wordpress.org>
2017-03-28 10:32:58 +0000

    Plugin Directory: Reviewer tools: Add support for plugin zip URLs in postmeta and expose the excerpt of pending/approved plugins.
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@5182 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/plugins/plugin-directory/admin/metabox/class-review-tools.php
+++ wordpress.org/public_html/wp-content/plugins/plugin-directory/admin/metabox/class-review-tools.php
@@ -10,19 +10,31 @@ class Review_Tools {
 	static function display() {
 		$post = get_post();
 
+		$zip_url = '';
 		foreach ( get_attached_media( 'application/zip', $post ) as $zip_file ) {
 			$zip_url = wp_get_attachment_url( $zip_file->ID );
+			break;
+		}
+		if ( ! $zip_url ) {
+			$zip_url = get_post_meta( $post->ID, '_submitted_zip', true );
+		}
 
+		if ( $zip_url ) {
 			printf( '<p>' . __( '<strong>Zip file:</strong> %s', 'wporg-plugins' ) . '</p>',
 				sprintf( '<a href="%s">%s</a>', esc_url( $zip_url ), esc_html( $zip_url ) )
 			);
 		}
 
-		echo "<ul>
-			<li><a href='https://plugins.trac.wordpress.org/log/{$post->post_name}/'>" . __( 'Development Log', 'wporg-plugins' ) . "</a></li>
-			<li><a href='https://plugins.svn.wordpress.org/{$post->post_name}/'>" . __( 'Subversion Repository', 'wporg-plugins' ) . "</a></li>
-			<li><a href='https://plugins.trac.wordpress.org/browser/{$post->post_name}/'>" . __( 'Browse in Trac', 'wporg-plugins' ) . '</a></li>
-		</ul>';
+		if ( 'pending' != $post->post_status ) {
+			echo "<ul>
+				<li><a href='https://plugins.trac.wordpress.org/log/{$post->post_name}/'>" . __( 'Development Log', 'wporg-plugins' ) . "</a></li>
+				<li><a href='https://plugins.svn.wordpress.org/{$post->post_name}/'>" . __( 'Subversion Repository', 'wporg-plugins' ) . "</a></li>
+				<li><a href='https://plugins.trac.wordpress.org/browser/{$post->post_name}/'>" . __( 'Browse in Trac', 'wporg-plugins' ) . '</a></li>
+			</ul>';
+		}
+		if ( $post->post_excerpt && in_array( $post->post_status, array( 'pending', 'approved' ) ) ) {
+			echo '<p>' . strip_tags( $post->post_excerpt ) . '</p>';
+		}
 
 		add_filter( 'wp_comment_reply', function( $string ) use ( $post ) {
 			$author = get_user_by( 'id', $post->post_author );
