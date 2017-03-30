c241144167b08021002284347db44ebbc8c40e86
Dion Hulse <dd32@git.wordpress.org>
2017-03-30 04:39:59 +0000

    Plugin Directory: Use the `Author` and `Author URI` plugin headers in the API response instead of the original plugin submitters details. This doesn't change `author_profile` which still references the original uploaders profile, as it always has. This field doesn't appear to be used by WordPress though.
    Props obenland for initial patch.
    Fixes #2585.
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@5204 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/plugins/plugin-directory/api/routes/class-plugin.php
+++ wordpress.org/public_html/wp-content/plugins/plugin-directory/api/routes/class-plugin.php
@@ -67,12 +67,12 @@ class Plugin extends Base {
 		$result['slug'] = $post->post_name;
 		$result['version'] = get_post_meta( $post_id, 'version', true ) ?: '0.0';
 
-		$author = get_user_by( 'id', $post->post_author );
-		$result['author'] = $author->display_name;
-		if ( $author->user_url ) {
-			$result['author'] = sprintf( '<a href="%s">%s</a>', $author->user_url, $result['author'] );
+		$result['author'] = strip_tags( get_post_meta( $post_id, 'header_author', true ) ) ?: get_user_by( 'id', $post->post_author )->display_name;
+		if ( false != ( $author_url = get_post_meta( $post_id, 'header_author_uri', true ) ) ) {
+			$result['author'] = sprintf( '<a href="%s">%s</a>', esc_url_raw( $author_url ), $result['author'] );
 		}
 
+		// Profile of the original plugin submitter
 		$result['author_profile'] = $this->get_user_profile_link( $post->post_author );
 		$result['contributors'] = array();
 
@@ -223,6 +223,10 @@ class Plugin extends Base {
 			if ( ! $u ) {
 				$u = get_user_by( 'slug', $user );
 			}
+		}
+
+		if ( ! $u ) {
+			return false;
 		}
 
 		return 'https://profiles.wordpress.org/' . $u->user_nicename;
