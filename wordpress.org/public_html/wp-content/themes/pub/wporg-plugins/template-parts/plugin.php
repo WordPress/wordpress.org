3b881e211663be24d62eb039e5565f5212184e24
Dion Hulse <dd32@git.wordpress.org>
2017-03-31 10:05:39 +0000

    Plugin Directory: Strip tags from author headers prior to escaping. This fixes cases where `<a href="..">Author</a>` was shown.
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@5224 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-plugins/template-parts/plugin.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-plugins/template-parts/plugin.php
@@ -30,7 +30,7 @@ use WordPressdotorg\Plugin_Directory\Template;
 	<hr>
 	<footer>
 		<span class="plugin-author">
-			<i class="dashicons dashicons-admin-users"></i> <?php echo esc_html( get_post_meta( get_the_ID(), 'header_author', true ) ?: get_the_author() ); ?>
+			<i class="dashicons dashicons-admin-users"></i> <?php echo esc_html( strip_tags( get_post_meta( get_the_ID(), 'header_author', true ) ) ?: get_the_author() ); ?>
 		</span>
 		<span class="active-installs">
 			<i class="dashicons dashicons-chart-area"></i> <?php printf( __( '%s active installs', 'wporg-plugins' ), Template::active_installs(false) ); ?>
