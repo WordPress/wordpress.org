bcd86f8191d844b332509da5e336166613b94fa0
Dominik Schilling (ocean90) <ocean90@git.wordpress.org>
2017-04-29 14:40:52 +0000

    Plugin Directory: Bump script version for cache bust.
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@5429 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-plugins/functions.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-plugins/functions.php
@@ -77,7 +77,7 @@ function scripts() {
 
 	if ( get_query_var( 'plugin_advanced' ) ) {
 		wp_enqueue_script( 'google-charts-loader', 'https://www.gstatic.com/charts/loader.js', array(), false, true );
-		wp_enqueue_script( 'wporg-plugins-stats', get_template_directory_uri() . '/js/stats.js', array( 'jquery', 'google-charts-loader' ), '20170328', true );
+		wp_enqueue_script( 'wporg-plugins-stats', get_template_directory_uri() . '/js/stats.js', array( 'jquery', 'google-charts-loader' ), '20170429', true );
 
 		wp_localize_script( 'wporg-plugins-stats', 'pluginStats', array(
 			'slug' => is_singular( 'plugin' ) ? get_queried_object()->post_name : '',
