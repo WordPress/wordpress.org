e28ea08b67dbdc9f9e6a1fbf4ad8b12e139c7f3c
Dion Hulse <dd32@git.wordpress.org>
2017-05-07 02:16:08 +0000

    Plugin Directory: Correctly set the 'page' value in the response, the method name is `get()` not `get_query_var()`.
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@5484 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/plugins/plugin-directory/api/routes/class-query-plugins.php
+++ wordpress.org/public_html/wp-content/plugins/plugin-directory/api/routes/class-query-plugins.php
@@ -67,9 +67,9 @@ class Query_Plugins extends Base {
 		// Use the main query so that is_main_query() is triggered for the filters.
 		$wp_query->query( $query );
 
-		$response['info']['page']    = (int) $wp_query->get_query_var( 'paged' ) ?: 1;
-		$response['info']['pages']   = (int) $wp_query->max_num_pages            ?: 0;
-		$response['info']['results'] = (int) $wp_query->found_posts              ?: 0;
+		$response['info']['page']    = (int) $wp_query->get( 'paged' ) ?: 1;
+		$response['info']['pages']   = (int) $wp_query->max_num_pages  ?: 0;
+		$response['info']['results'] = (int) $wp_query->found_posts    ?: 0;
 
 		foreach ( $wp_query->posts as $post ) {
 			$response['plugins'][] = $post->post_name ?: get_post( $post->ID )->post_name;
