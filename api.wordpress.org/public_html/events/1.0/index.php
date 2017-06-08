18abf60682803f7499e65d84ca4a6993b49b1b24
iandunn <iandunn@74240141-8908-4e6f-9713-ba540dce6ec7>
2017-06-08 00:32:02 +0000

    Events: Convert commas to spaces to prevent unwanted concatenation
    Previously input like `seattle, wa` would be converted to `seattlewa` instead of `seattle wa`, which would produce a false-negative.
    
    This bug was introduced in r5102.
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@5550 74240141-8908-4e6f-9713-ba540dce6ec7

--- api.wordpress.org/public_html/events/1.0/index.php
+++ api.wordpress.org/public_html/events/1.0/index.php
@@ -64,8 +64,7 @@ function parse_request() {
 
 	// Simplified parameters for lookup by location (city) name, with optional timezone and locale params for extra context.
 	if ( isset( $_REQUEST['location'] ) ) {
-		$location_args['location_name'] = trim( $_REQUEST['location'] );
-		$location_args['location_name'] = str_replace( ',', '', $location_args['location_name'] );
+		$location_args['location_name'] = trim( str_replace( ',', ' ', $_REQUEST['location'] ) );
 	}
 
 	if ( isset( $_REQUEST['timezone'] ) ) {
