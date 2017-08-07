ba87cce2f59f946bbf396dba8cd39f8c3a8ce122
Scott Reilly <coffee2code@git.wordpress.org>
2017-08-07 18:05:57 +0000

    Browse Happy: Use HTTPS for Opera's URL.
    See https://github.com/WordPress/browsehappy/pull/35
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@5760 74240141-8908-4e6f-9713-ba540dce6ec7

--- browsehappy.com/public_html/functions.php
+++ browsehappy.com/public_html/functions.php
@@ -43,7 +43,7 @@ function browsehappy_get_browser_data( $browser = false ) {
 			'wikidata' => 'Q41242',
 			'normalized' => 1, // just first number
 			'facebook' => 'Opera',
-			'url' => 'http://www.opera.com/',
+			'url' => 'https://www.opera.com/',
 			'info' => __( '&#8220;The fastest browser on Earth—secure, powerful and easy to use, with excellent privacy protection. And&nbsp;it&nbsp;is&nbsp;free.&#8221;', 'browsehappy' ),
 		),
 		'edge' => (object) array(
