9e20fc65454f5d31155020d9c980142d9352ee0e
Daniel Bachhuber <danielbachhuber@git.wordpress.org>
2017-08-17 21:59:35 +0000

    devhub/cli: Indicate when a command implements subcommands
    See https://github.com/wp-cli/wp-cli/issues/4297
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@5816 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-developer/content-command.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-developer/content-command.php
@@ -5,7 +5,16 @@
 ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<header class="entry-header">
-		<h2 class="entry-title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a></h2>
+		<?php
+			$children = get_children( array(
+				'post_parent'    => get_the_ID(),
+				'post_type'      => 'command',
+				'posts_per_page' => 1,
+				'orderby'        => 'title',
+				'order'          => 'ASC',
+			) );
+		?>
+		<h2 class="entry-title"><a href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?><?php if ( $children ) : ?> <span>&#60;command&#62;<?php endif; ?></a></h2>
 	</header><!-- .entry-header -->
 
 	<div class="entry-content">
