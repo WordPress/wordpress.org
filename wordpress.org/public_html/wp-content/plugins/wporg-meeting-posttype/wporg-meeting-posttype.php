5e21344d870164b7de827430702c1625920c6251
Sergey Biryukov <sergeybiryukov@git.wordpress.org>
2017-08-31 12:34:12 +0000

    Make/Meetings: Add "Occurrence in a month" option for recurrent meetings, allowing for a more flexible schedule like the first and third Thursday of every month.
    The occurrence day is based on the day of week that the Start Date falls on.
    
    Fixes #2218.
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@5850 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/plugins/wporg-meeting-posttype/wporg-meeting-posttype.php
+++ wordpress.org/public_html/wp-content/plugins/wporg-meeting-posttype/wporg-meeting-posttype.php
@@ -29,8 +29,12 @@ class Meeting_Post_Type {
 		add_action( 'admin_head', array( $mpt, 'meeting_column_width' ) );
 	}
 
-	public function meeting_column_width() {
-		echo '<style type="text/css">.column-team { width:10em !important; overflow:hidden }</style>';
+	public function meeting_column_width() { ?>
+		<style type="text/css">
+			.column-team { width: 10em !important; overflow: hidden; }
+			#meeting-info .recurring label { padding-right: 10px; }
+		</style>
+		<?php
 	}
 
 	public function meeting_add_custom_columns( $columns ) {
@@ -64,7 +68,7 @@ class Meeting_Post_Type {
 					'relation'=>'AND',
 					array(
 						'key'=>'recurring',
-						'value'=>array( 'weekly', 'biweekly', 'monthly', '1' ),
+						'value'=>array( 'weekly', 'biweekly', 'occurrence', 'monthly', '1' ),
 						'compare'=>'NOT IN',
 					),
 					array(
@@ -79,7 +83,7 @@ class Meeting_Post_Type {
 					'relation'=>'AND',
 					array(
 						'key'=>'recurring',
-						'value'=>array( 'weekly', 'biweekly', 'monthly', '1' ),
+						'value'=>array( 'weekly', 'biweekly', 'occurrence', 'monthly', '1' ),
 						'compare'=>'IN',
 					),
 					array(
@@ -114,53 +118,84 @@ class Meeting_Post_Type {
 
 		// for each entry, set a fake meta value to show the next date for recurring meetings
 		array_walk( $posts, function ( &$post ) {
-			if ( $post->recurring == 'weekly' || $post->recurring === '1' ) {
+			if ( 'weekly' === $post->recurring || '1' === $post->recurring ) {
 				try {
 					// from the start date, advance the week until it's past now
-					$start = new DateTime( $post->start_date.' '.$post->time.' GMT' );
-					$next = $start;
-					$now = new DateTime();
-					if ( $now > $next ) {
-						$interval = $start->diff($now);
+					$start = new DateTime( $post->start_date . ' ' . $post->time . ' GMT' );
+					$next  = $start;
+					$now   = new DateTime();
+
+					if ( $next < $now ) {
+						$interval = $start->diff( $now );
 						// add one to days to account for events that happened earlier today
-						$weekdiff = ceil( ($interval->days+1) / 7 );
-						$next->modify('+ '.$weekdiff.' weeks');
+						$weekdiff = ceil( ( $interval->days + 1 ) / 7 );
+						$next->modify( '+ ' . $weekdiff . ' weeks' );
 					}
+
 					$post->next_date = $next->format( 'Y-m-d' );
-				} catch (Exception $e) {
+				} catch ( Exception $e ) {
 					// if the datetime is invalid, then set the post->next_date to the start date instead
 					$post->next_date = $post->start_date;
 				}
-			} else if ( $post->recurring == 'biweekly' ) {
+			} else if ( 'biweekly' === $post->recurring ) {
 				try {
 					// advance the start date 2 weeks at a time until it's past now
-					$start = new DateTime( $post->start_date.' '.$post->time.' GMT' );
-					$next = $start;
-					$now = new DateTime();
-					while ( $now > $next ) {
-						$next->modify('+2 weeks');
+					$start = new DateTime( $post->start_date . ' ' . $post->time . ' GMT' );
+					$next  = $start;
+					$now   = new DateTime();
+
+					while ( $next < $now ) {
+						$next->modify( '+2 weeks' );
 					}
-					$post->next_date = $next->format('Y-m-d');
-				} catch (Exception $e) {
+
+					$post->next_date = $next->format( 'Y-m-d' );
+				} catch ( Exception $e ) {
 					// if the datetime is invalid, then set the post->next_date to the start date instead
 					$post->next_date = $post->start_date;
 				}
-			} else if ( $post->recurring == 'monthly' ) {
+			} else if ( 'occurrence' === $post->recurring ) {
+				try {
+					// advance the start date in the current month until it's past now
+					$start = new DateTime( $post->start_date . ' ' . $post->time . ' GMT' );
+					$next  = $start;
+					$now   = new DateTime();
+
+					$day_index = date( 'w', strtotime( $post->start_date . ' ' . $post->time . ' GMT' ) );
+					$day_name  = $GLOBALS['wp_locale']->get_weekday( $day_index );
+					$numerals  = array( 'first', 'second', 'third', 'fourth' );
+					$months    = array( 'this month', 'next month' );
+
+					foreach ( $post->occurrence as $index ) {
+						foreach ( $months as $month ) {
+							$next->modify( $numerals[ $index - 1 ] . ' ' . $day_name . ' of ' . $month );
+							if ( $next > $now ) {
+								break 2;
+							}
+						}
+					}
+
+					$post->next_date = $next->format( 'Y-m-d' );
+				} catch ( Exception $e ) {
+					// if the datetime is invalid, then set the post->next_date to the start date instead
+					$post->next_date = $post->start_date;
+				}
+			} else if ( 'monthly' === $post->recurring ) {
 				try {
 					// advance the start date 1 month at a time until it's past now
-					$start = new DateTime( $post->start_date.' '.$post->time.' GMT' );
-					$next = $start;
-					$now = new DateTime();
-					while ( $now > $next ) {
-						$next->modify('+1 month');
+					$start = new DateTime( $post->start_date . ' ' . $post->time . ' GMT' );
+					$next  = $start;
+					$now   = new DateTime();
+
+					while ( $next < $now ) {
+						$next->modify( '+1 month' );
 					}
-					$post->next_date = $next->format('Y-m-d');
-				} catch (Exception $e) {
+
+					$post->next_date = $next->format( 'Y-m-d' );
+				} catch ( Exception $e ) {
 					// if the datetime is invalid, then set the post->next_date to the start date instead
 					$post->next_date = $post->start_date;
 				}
-			}
-			else {
+			} else {
 				$post->next_date = $post->start_date;
 			}
 		});
@@ -227,7 +262,7 @@ class Meeting_Post_Type {
 
 	public function add_meta_boxes() {
 		add_meta_box(
-			'meeting_info',
+			'meeting-info',
 			'Meeting Info',
 			array( $this, 'render_meta_boxes' ),
 			'meeting',
@@ -240,73 +275,105 @@ class Meeting_Post_Type {
 		wp_enqueue_script( 'jquery-ui-datepicker' );
 		wp_enqueue_style( 'jquery-ui-style', 'https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.4/themes/smoothness/jquery-ui.css', true);
 
-		$meta = get_post_custom( $post->ID );
-		$team = ! isset( $meta['team'][0] ) ? '' : $meta['team'][0];
-		$start = ! isset( $meta['start_date'][0] ) ? '' : $meta['start_date'][0];
-		$end = ! isset( $meta['end_date'][0] ) ? '' : $meta['end_date'][0];
-		$time = ! isset( $meta['time'][0] ) ? '' : $meta['time'][0];
-		$recurring = ! isset( $meta['recurring'][0] ) ? '' : $meta['recurring'][0];
-		if ( $recurring === '1' ) {
+		$meta       = get_post_custom( $post->ID );
+		$team       = isset( $meta['team'][0] ) ? $meta['team'][0] : '';
+		$start      = isset( $meta['start_date'][0] ) ? $meta['start_date'][0] : '';
+		$end        = isset( $meta['end_date'][0] ) ? $meta['end_date'][0] : '';
+		$time       = isset( $meta['time'][0] ) ? $meta['time'][0] : '';
+		$recurring  = isset( $meta['recurring'][0] ) ? $meta['recurring'][0] : '';
+		if ( '1' === $recurring ) {
 			$recurring = 'weekly';
 		}
-		$link = ! isset( $meta['link'][0] ) ? '' : $meta['link'][0];
-		$location = ! isset( $meta['location'][0] ) ? '' : $meta['location'][0];
+		$occurrence = isset( $meta['occurrence'][0] ) ? unserialize( $meta['occurrence'][0] ) : array();
+		$link       = isset( $meta['link'][0] ) ? $meta['link'][0] : '';
+		$location   = isset( $meta['location'][0] ) ? $meta['location'][0] : '';
 		wp_nonce_field( 'save_meeting_meta_'.$post->ID , 'meeting_nonce' );
 		?>
 
 		<p>
-		<label for="team"><?php _e( 'Team: ', 'wporg' ); ?>
-			<input type="text" id="team" name="team" class="regular-text wide" value="<?php echo esc_attr($team); ?>">
+		<label for="team">
+			<?php _e( 'Team: ', 'wporg' ); ?>
+			<input type="text" id="team" name="team" class="regular-text wide" value="<?php echo esc_attr( $team ); ?>">
 		</label>
 		</p>
 		<p>
-		<label for="start_date"><?php _e( 'Start Date', 'wporg' ); ?>
-			<input type="text" name="start_date" class="date" value="<?php echo esc_attr($start); ?>">
+		<label for="start_date">
+			<?php _e( 'Start Date', 'wporg' ); ?>
+			<input type="text" name="start_date" id="start_date" class="date" value="<?php echo esc_attr( $start ); ?>">
 		</label>
-		<label for="end_date"><?php _e( 'End Date', 'wporg' ); ?>
-			<input type="text" name="end_date" class="date" value="<?php echo esc_attr($end); ?>">
+		<label for="end_date">
+			<?php _e( 'End Date', 'wporg' ); ?>
+			<input type="text" name="end_date" id="end_date" class="date" value="<?php echo esc_attr( $end ); ?>">
 		</label>
 		</p>
 		<p>
-		<label for="time"><?php _e( 'Time (UTC)', 'wporg' ); ?>
-			<input type="text" name="time" class="time" value="<?php echo esc_attr($time); ?>">
+		<label for="time">
+			<?php _e( 'Time (UTC)', 'wporg' ); ?>
+			<input type="text" name="time" id="time" class="time" value="<?php echo esc_attr( $time ); ?>">
 		</label>
-		<label for="recurring"><?php _e( 'Recurring: ', 'wporg' ); ?>
+		</p>
+		<p class="recurring">
+		<?php _e( 'Recurring: ', 'wporg' ); ?><br />
+		<label for="weekly">
 			<input type="radio" name="recurring" value="weekly" id="weekly" class="regular-radio" <?php checked( $recurring, 'weekly' ); ?>>
-			<label for="weekly"><?php _e( 'Weekly', 'wporg' ); ?></label>
+			<?php _e( 'Weekly', 'wporg' ); ?>
+		</label><br />
+
+		<label for="biweekly">
 			<input type="radio" name="recurring" value="biweekly" id="biweekly" class="regular-radio" <?php checked( $recurring, 'biweekly' ); ?>>
-			<label for="biweekly"><?php _e( 'Biweekly', 'wporg' ); ?></label>
+			<?php _e( 'Biweekly', 'wporg' ); ?>
+		</label><br />
+
+		<label for="occurrence">
+			<input type="radio" name="recurring" value="occurrence" id="occurrence" class="regular-radio" <?php checked( $recurring, 'occurrence' ); ?>>
+			<?php _e( 'Occurrence in a month:', 'wporg' ); ?>
+		</label>
+		<label for="week-1">
+			<input type="checkbox" name="occurrence[]" value="1" id="week-1" <?php checked( in_array( 1, $occurrence ) ); ?>>
+			<?php _e( '1st', 'wporg' ); ?>
+		</label>
+		<label for="week-2">
+			<input type="checkbox" name="occurrence[]" value="2" id="week-2" <?php checked( in_array( 2, $occurrence ) ); ?>>
+			<?php _e( '2nd', 'wporg' ); ?>
+		</label>
+		<label for="week-3">
+			<input type="checkbox" name="occurrence[]" value="3" id="week-3" <?php checked( in_array( 3, $occurrence ) ); ?>>
+			<?php _e( '3rd', 'wporg' ); ?>
+		</label>
+		<label for="week-4">
+			<input type="checkbox" name="occurrence[]" value="4" id="week-4" <?php checked( in_array( 4, $occurrence ) ); ?>>
+			<?php _e( '4th', 'wporg' ); ?>
+		</label><br />
+
+		<label for="monthly">
 			<input type="radio" name="recurring" value="monthly" id="monthly" class="regular-radio" <?php checked( $recurring, 'monthly' ); ?>>
-			<label for="monthly"><?php _e( 'Monthly', 'wporg' ); ?></label>
+			<?php _e( 'Monthly', 'wporg' ); ?>
 		</label>
 		</p>
 		<p>
 		<label for="link"><?php _e( 'Link: ', 'wporg' ); ?>
-			<input type="text" name="link" class="regular-text wide" value="<?php echo esc_url($link); ?>">
+			<input type="text" name="link" id="link" class="regular-text wide" value="<?php echo esc_url( $link ); ?>">
 		</label>
 		</p>
 		<p>
 		<label for="location"><?php _e( 'Location: ', 'wporg' ); ?>
-			<input type="text" id="location" name="location" class="regular-text wide" value="<?php echo esc_attr($location); ?>">
+			<input type="text" name="location" id="location" class="regular-text wide" value="<?php echo esc_attr( $location ); ?>">
 		</label>
 		</p>
 		<script>
-		jQuery(document).ready(function($){
+		jQuery(document).ready( function($) {
 			$('.date').datepicker({
-				dateFormat: "yy-mm-dd"
+				dateFormat: 'yy-mm-dd'
 			});
-			$(document).on( 'keydown', '#title, #location', function( e ) {
-				var keyCode = e.keyCode || e.which;
-				if ( 9 == keyCode){
-					e.preventDefault();
-					var target = $(this).attr('id') == 'title' ? '#team' : 'textarea#content';
-					if ( (target === '#team') || $('#wp-content-wrap').hasClass('html-active') ) {
-						$(target).focus();
-					} else {
-						tinymce.execCommand('mceFocus',false,'content');
-					}
-				}
+
+			$('input[name="recurring"]').change( function() {
+				var disabled = ( 'occurrence' !== $(this).val() );
+				$('#meeting-info').find('[name^="occurrence"]').prop('disabled', disabled);
 			});
+
+			if ( 'occurrence' !== $('input[name="recurring"]:checked').val() ) {
+				$('#meeting-info').find('[name^="occurrence"]').prop('disabled', true);
+			}
 		});
 		</script>
 	<?php
@@ -327,7 +394,7 @@ class Meeting_Post_Type {
 		}
 
 		// Don't save for revisions
-		if ( isset( $post->post_type ) && $post->post_type == 'revision' ) {
+		if ( isset( $post->post_type ) && 'revision' === $post->post_type ) {
 			return $post_id;
 		}
 
@@ -336,20 +403,18 @@ class Meeting_Post_Type {
 			return $post_id;
 		}
 
-		$team = ! isset( $meta['team'][0] ) ? '' : $meta['team'][0];
-		$start = ! isset( $meta['start_date'][0] ) ? '' : $meta['start_date'][0];
-		$end = ! isset( $meta['end_date'][0] ) ? '' : $meta['end_date'][0];
-		$recurring = ! isset( $meta['recurring'][0] ) ? '' : $meta['recurring'][0];
-		$link = ! isset( $meta['link'][0] ) ? '' : $meta['link'][0];
-		$location = ! isset( $meta['location'][0] ) ? '' : $meta['location'][0];
-
-		$meta['team'] = ( isset( $_POST['team'] ) ? esc_textarea( $_POST['team'] ) : '' );
-		$meta['start_date'] = ( isset( $_POST['start_date'] ) ? esc_textarea( $_POST['start_date'] ) : '' );
-		$meta['end_date'] = ( isset( $_POST['end_date'] ) ? esc_textarea( $_POST['end_date'] ) : '' );
-		$meta['time'] = ( isset( $_POST['time'] ) ? esc_textarea( $_POST['time'] ) : '' );
-		$meta['recurring'] = ( isset ( $_POST['recurring'] ) && ( in_array( $_POST['recurring'], array( 'weekly', 'biweekly', 'monthly' ) ) ) ? ( $_POST['recurring'] ) : '' );
-		$meta['link'] = ( isset( $_POST['link'] ) ? esc_url( $_POST['link'] ) : '' );
-		$meta['location'] = ( isset( $_POST['location'] ) ? esc_textarea( $_POST['location'] ) : '' );
+		$meta['team']        = ( isset( $_POST['team'] ) ? esc_textarea( $_POST['team'] ) : '' );
+		$meta['start_date']  = ( isset( $_POST['start_date'] ) ? esc_textarea( $_POST['start_date'] ) : '' );
+		$meta['end_date']    = ( isset( $_POST['end_date'] ) ? esc_textarea( $_POST['end_date'] ) : '' );
+		$meta['time']        = ( isset( $_POST['time'] ) ? esc_textarea( $_POST['time'] ) : '' );
+		$meta['recurring']   = ( isset( $_POST['recurring'] )
+		                         && in_array( $_POST['recurring'], array( 'weekly', 'biweekly', 'occurrence', 'monthly' ) )
+		                         ? ( $_POST['recurring'] ) : '' );
+		$meta['occurrence']  = ( isset( $_POST['occurrence'] ) && 'occurrence' === $meta['recurring']
+		                         && is_array( $_POST['occurrence'] )
+		                         ? array_map( 'intval', $_POST['occurrence'] ) : array() );
+		$meta['link']        = ( isset( $_POST['link'] ) ? esc_url( $_POST['link'] ) : '' );
+		$meta['location']    = ( isset( $_POST['location'] ) ? esc_textarea( $_POST['location'] ) : '' );
 
 		foreach ( $meta as $key => $value ) {
 			update_post_meta( $post->ID, $key, $value );
