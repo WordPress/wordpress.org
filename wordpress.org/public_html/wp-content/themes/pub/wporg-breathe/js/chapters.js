1ca27b9c39f07da60283fa3f9bbdb346f084a512
Sergey Biryukov <sergeybiryukov@git.wordpress.org>
2017-09-04 03:21:47 +0000

    Breathe: Make handbook Chapters menu accessible from keyboard, add screen reader feedback and visible focus style.
    Props Kau-Boy.
    Fixes #3048.
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@5871 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-breathe/js/chapters.js
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-breathe/js/chapters.js
@@ -4,7 +4,7 @@ jQuery(document).ready(function() {
 	// Add our expandable button
 	jQuery( '.menu-table-of-contents-container > ul > .menu-item-has-children > a' )
 		.wrap( '<div class="expandable"></div>' )
-		.before( '<span class="dashicons dashicons-arrow-down-alt2"></span>' );
+		.after( '<button class="dashicons dashicons-arrow-down-alt2" aria-expanded="false"></button>' );
 
 	// Invisibly open all of the submenus
 	jQuery( '.menu-item-has-children > ul ul' ).addClass( 'default-open' );
@@ -17,8 +17,12 @@ jQuery(document).ready(function() {
 
 	// Or if wrapped in a div.expandable
 	jQuery( '.menu-item-has-children > div > .dashicons' ).click( function() {
+		var menuToggle = jQuery( this ).closest( '.menu-item-has-children' );
+
 		jQuery( this ).parent().siblings( '.children' ).slideToggle();
-		jQuery( this ).closest( '.menu-item-has-children' ).toggleClass( 'open' );
+
+		menuToggle.toggleClass( 'open' );
+		menuToggle.attr( 'aria-expanded', menuToggle.hasClass( 'open' ) );
 	} );
 } );
 
