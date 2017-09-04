4973a3f3c4fcbffb56b4572e4989c8851ee74bdf
Konstantin Obenland <obenland@git.wordpress.org>
2017-09-04 15:16:05 +0000

    Main: Include parent theme css changes in `watch`.
    See #2861.
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@5878 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-main/Gruntfile.js
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-main/Gruntfile.js
@@ -140,7 +140,7 @@ module.exports = function( grunt ) {
 				tasks: ['jshint']
 			},
 			css: {
-				files: ['**/*.scss'],
+				files: ['**/*.scss', '../wporg/css/**/*scss'],
 				tasks: ['css']
 			}
 		}
