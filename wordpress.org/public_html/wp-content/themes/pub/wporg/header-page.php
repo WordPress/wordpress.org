9aaef8412329a39447c77a22d7444e7bdfbf5431
Konstantin Obenland <obenland@git.wordpress.org>
2017-09-05 14:12:56 +0000

    WPorg: Add page template.
    Uses the site title band for page titles.
    
    See #2861.
    
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@5884 74240141-8908-4e6f-9713-ba540dce6ec7

---
+++ wordpress.org/public_html/wp-content/themes/pub/wporg/header-page.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * The template for displaying all pages.
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package WordPressdotorg\Plugin_Directory\Theme
+ */
+
+namespace WordPressdotorg\Theme;
+
+require WPORGPATH . 'header.php';
+?>
+<div id="page" class="site">
+	<a class="skip-link screen-reader-text" href="#main"><?php esc_html_e( 'Skip to content', 'wporg' ); ?></a>
+
+	<div id="content" class="site-content">
---
+++ wordpress.org/public_html/wp-content/themes/pub/wporg/page.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * The template for displaying all pages.
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package WordPressdotorg\Theme
+ */
+
+namespace WordPressdotorg\Theme;
+
+get_header( 'page' ); ?>
+
+	<main id="main" class="site-main" role="main">
+
+		<?php
+		while ( have_posts() ) : the_post();
+
+			get_template_part( 'template-parts/content', 'page' );
+		endwhile; // End of the loop.
+		?>
+
+	</main><!-- #main -->
+
+	<?php
+get_footer();
