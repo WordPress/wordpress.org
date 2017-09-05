a7acb83935657dedb5f6db9410644e0ceff58154
Konstantin Obenland <obenland@git.wordpress.org>
2017-09-05 14:11:23 +0000

    WPorg: Add basic 404 page.
    Borrows most of it from the Plugin Directory.
    
    See #2861.
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@5883 74240141-8908-4e6f-9713-ba540dce6ec7

---
+++ wordpress.org/public_html/wp-content/themes/pub/wporg/404.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * The template for displaying 404 pages (not found).
+ *
+ * @link https://codex.wordpress.org/Creating_an_Error_404_Page
+ *
+ * @package WordPressdotorg\Plugin_Directory\Theme
+ */
+
+namespace WordPressdotorg\Theme;
+
+get_header(); ?>
+
+	<main id="main" class="site-main" role="main">
+
+		<section class="error-404 not-found">
+			<header class="page-header">
+				<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'wporg' ); ?></h1>
+			</header><!-- .page-header -->
+
+			<div class="page-content">
+				<p><?php printf( __( 'Try searching from the field above, or go to the <a href="%s">home page</a>.', 'wporg' ), get_home_url() ); ?></p>
+
+				<div class="logo-swing">
+					<img src="<?php echo get_theme_file_uri( '/images/wp-logo-blue-trans-blur.png' ); ?>" class="wp-logo" />
+					<img src="<?php echo get_theme_file_uri( '/images/wp-logo-blue.png' ); ?>" class="wp-logo" />
+				</div>
+			</div><!-- .page-content -->
+		</section><!-- .error-404 -->
+
+	</main><!-- #main -->
+
+<?php
+get_footer();
