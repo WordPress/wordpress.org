9ab896e79d3f8ff8d4d0956007f65589c4884ec3
Konstantin Obenland <obenland@git.wordpress.org>
2017-09-06 17:44:00 +0000

    WPorg/Main: Pull teaser post from blog.
    Also fixes to visual bugs where the pointer to the current menu item had the
    wrong color and the divider to home below didn't show up.
    
    See #2861.
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@5891 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-main/front-page.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-main/front-page.php
@@ -12,30 +12,10 @@
  * @package WordPressdotorg\Theme
  */
 
-namespace WordPressdotorg\Theme;
-function get_latest_devblog_post() {
-	$cached = wp_cache_get('feed', 'wporg');
-	if (is_array($cached)) {
-		echo '<!-- feed is cached -->';
-		return $cached;
-	}
-	$feed = file_get_contents('http://wordpress.org/news/feed/');
-	preg_match('|<item>\s*<title>(.*?)</title>.*?<link>(.*?)</link>.*?<description><!\[CDATA\[(.*?)\]\]>|s', $feed, $matches);
-	$matches = array_map('trim', $matches);
-	$matches[3] = preg_replace('|\s*\[\.\.\.\]$|', '&hellip;', $matches[3]);
-	$latest_post = array('title' => $matches[1], 'link' => $matches[2], 'content' => $matches[3]);
-	wp_cache_set('feed', $latest_post, 'wporg', 3600);
-	return $latest_post;
-}
-
-$latest_post = [];//get_latest_devblog_post();
-
-$headadd = '<link rel="alternate" type="application/rss+xml" title="WordPress Blog RSS" href="http://wordpress.org/news/feed/" />';
-$pagetitle = 'Blog Tool, Publishing Platform, and CMS';
-$titleover = 'Welcome to WordPress';
+namespace WordPressdotorg\MainTheme;
 
 require WPORGPATH . 'header.php';
- ?>
+?>
 	<header id="masthead" class="site-header" role="banner">
 		<div class="site-branding">
 			<p class="site-title"><?php _e( 'Meet WordPress', 'wporg-main' ); ?></p>
@@ -44,135 +24,141 @@ require WPORGPATH . 'header.php';
 		</div><!-- .site-branding -->
 	</header><!-- #masthead -->
 
-	<main id="main" class="site-main home-welcome" role="main">
-		<div id="lang-guess-wrap"></div>
+	<main id="main" class="site-main " role="main">
+		<div class="home-welcome">
+			<div id="lang-guess-wrap"></div>
 
-		<section class="intro">
-			<p class="subheading"><?php _e( 'Beautiful designs, powerful features, and the freedom to build anything you want. WordPress is both free and priceless at the same time.' ); ?></p>
-			<div class="screenshots">
-				<img src="https://s.w.org/images/home/screen-themes.png?1" class="dashboard" />
-				<img src="https://s.w.org/images/home/iphone-themes.png?1" class="dashboard-mobile" />
-			</div>
-		</section>
-
-		<section class="showcase">
-			<h2><?php _e( 'Trusted by the Best' ); ?></h2>
-			<p class="subheading"><?php _e( '28% of the web uses WordPress, from hobby blogs to the biggest news sites online.' ); ?></p>
-			<div class="collage">
-
-			</div>
-			<p class="cta-link"><a href="https://wordpress.org/showcase/"><?php _e( 'Discover more sites built with WordPress' ); ?></a>.</p>
-		</section>
-
-		<section class="features">
-			<h2><?php _e( 'Powerful Features' ); ?></h2>
-			<p class="subheading"><?php _e( 'Limitless possibilities. What will you create?' ); ?></p>
-			<ul>
-				<li>
-					<span class="dashicons dashicons-admin-customizer"></span>
-					<?php _e( 'Customizable<br />Designs' ); ?>
-				</li>
-				<li>
-					<span class="dashicons dashicons-welcome-widgets-menus"></span>
-					<?php _e( 'SEO<br />Friendly' ); ?>
-				</li>
-				<li>
-					<span class="dashicons dashicons-smartphone"></span>
-					<?php _e( 'Responsive<br />Mobile Sites' ); ?>
-				</li>
-				<li>
-					<span class="dashicons dashicons-chart-line"></span>
-					<?php _e( 'High<br />Performance' ); ?>
-				</li>
-				<li>
-					<a href="https://wordpress.org/mobile/"><img src="https://s.w.org/images/home/icon-run-blue.svg" />
-						<?php _e( 'Manage<br />on the Go' ); ?></a>
-				</li>
-				<li>
-					<span class="dashicons dashicons-lock"></span>
-					<?php _e( 'High<br />Security' ); ?>
-				</li>
-				<li>
-					<span class="dashicons dashicons-images-alt2"></span>
-					<?php _e( 'Powerful<br />Media Management' ); ?>
-				</li>
-				<li>
-					<span class="dashicons dashicons-universal-access"></span>
-					<?php _e( 'Easy and<br />Accessible' ); ?>
-				</li>
-			</ul>
-			<p><?php
-				/* translators: %s: Link to Plugin Directory. */
-				printf( __( 'Extend WordPress with over 45,000 plugins to help your website meet your needs. Add an online store, galleries, mailing lists, forums, analytics, and <a href="%s">much more</a>.' ), esc_url( 'https://wordpress.org/plugins/' ) ); ?></p>
-		</section>
-
-		<section class="community-2">
-			<div class="screen"></div>
-			<div class="container">
-				<h2><?php _e( 'Community' ); ?></h2>
-				<p class="subheading"><?php _e( 'Hundreds of thousands of developers, content creators, and site owners gather at monthly meetups in 436 cities worldwide' ); ?>.</p>
-			</div>
-		</section>
-
-		<section class="get">
-			<h2><?php _e( 'Get Started with WordPress' ); ?></h2>
-			<p class="subheading"><?php _e( 'Over 60 million people have chosen WordPress to power the place on the web they call &ldquo;home&rdquo; &mdash; join the family.' ); ?></p>
-			<div class="cta-wrapper">
-				<a href="https://wordpress.org/download/" class="button button-primary button-xl"><?php _e( 'Get WordPress' ); ?></a>
-			</div>
-		</section>
-
-		<div id="home-below">
-			<div class="wrapper">
-				<div class="col-2">
-					<h4><a href="https://wordpress.org/about/swag/"><?php _e( 'WordPress&nbsp;Swag' ); ?></a></h4>
-					<a href="https://wordpress.org/about/swag/"><img width="132" height="177" src="https://wpdotorg.files.wordpress.com/2015/10/gray-tshirt-swag.jpg" alt="<?php esc_attr_e( 'WordPress Swag' ); ?>" /></a>
+			<section class="intro">
+				<p class="subheading"><?php _e( 'Beautiful designs, powerful features, and the freedom to build anything you want. WordPress is both free and priceless at the same time.' ); ?></p>
+				<div class="screenshots">
+					<img src="https://s.w.org/images/home/screen-themes.png?1" class="dashboard" />
+					<img src="https://s.w.org/images/home/iphone-themes.png?1" class="dashboard-mobile" />
 				</div>
+			</section>
 
-				<div class="col-4">
-					<h4><a href="https://wordpress.org/news/"><?php _e( 'News From Our Blog' ); ?></a></h4>
+			<section class="showcase">
+				<h2><?php _e( 'Trusted by the Best' ); ?></h2>
+				<p class="subheading"><?php _e( '28% of the web uses WordPress, from hobby blogs to the biggest news sites online.' ); ?></p>
+				<div class="collage">
 
-					<h5><a href="<?php echo $latest_post['link']; ?>"><?php echo $latest_post['title']; ?></a></h5>
-					<p><?php echo $latest_post['content']; ?></p>
 				</div>
+				<p class="cta-link"><a href="https://wordpress.org/showcase/"><?php _e( 'Discover more sites built with WordPress' ); ?></a>.</p>
+			</section>
 
-				<div class="col-4">
-					<h4><?php _e( 'It&rsquo;s Easy&nbsp;As&hellip;' ); ?></h4>
+			<section class="features">
+				<h2><?php _e( 'Powerful Features' ); ?></h2>
+				<p class="subheading"><?php _e( 'Limitless possibilities. What will you create?' ); ?></p>
+				<ul>
+					<li>
+						<span class="dashicons dashicons-admin-customizer"></span>
+						<?php _e( 'Customizable<br />Designs' ); ?>
+					</li>
+					<li>
+						<span class="dashicons dashicons-welcome-widgets-menus"></span>
+						<?php _e( 'SEO<br />Friendly' ); ?>
+					</li>
+					<li>
+						<span class="dashicons dashicons-smartphone"></span>
+						<?php _e( 'Responsive<br />Mobile Sites' ); ?>
+					</li>
+					<li>
+						<span class="dashicons dashicons-chart-line"></span>
+						<?php _e( 'High<br />Performance' ); ?>
+					</li>
+					<li>
+						<a href="https://wordpress.org/mobile/"><img src="https://s.w.org/images/home/icon-run-blue.svg" />
+							<?php _e( 'Manage<br />on the Go' ); ?></a>
+					</li>
+					<li>
+						<span class="dashicons dashicons-lock"></span>
+						<?php _e( 'High<br />Security' ); ?>
+					</li>
+					<li>
+						<span class="dashicons dashicons-images-alt2"></span>
+						<?php _e( 'Powerful<br />Media Management' ); ?>
+					</li>
+					<li>
+						<span class="dashicons dashicons-universal-access"></span>
+						<?php _e( 'Easy and<br />Accessible' ); ?>
+					</li>
+				</ul>
+				<p><?php
+					/* translators: %s: Link to Plugin Directory. */
+					printf( __( 'Extend WordPress with over 45,000 plugins to help your website meet your needs. Add an online store, galleries, mailing lists, forums, analytics, and <a href="%s">much more</a>.' ), esc_url( 'https://wordpress.org/plugins/' ) ); ?></p>
+			</section>
 
-					<ol class="steps">
-						<li class="one"><span></span><?php printf( __( '<a href="%s">Find a Web Host</a> and get great hosting while supporting WordPress at the same&nbsp;time.' ), esc_url( 'https://wordpress.org/hosting/' ) ); ?></li>
-						<li class="two"><span></span><?php printf( __( '<a href="%s">Download &amp; Install WordPress</a> with our famous 5-minute&nbsp;installation. Feel like a rock star.' ), esc_url( 'https://wordpress.org/download/' ) ); ?></li>
-						<li class="three"><span></span><?php printf( __( '<a href="%s">Read the Documentation</a> and become a WordPress expert yourself, impress your friends.' ), esc_url( 'https://developer.wordpress.org' ) ); ?></li>
-
-					</ol>
+			<section class="community-2">
+				<div class="screen"></div>
+				<div class="container">
+					<h2><?php _e( 'Community' ); ?></h2>
+					<p class="subheading"><?php _e( 'Hundreds of thousands of developers, content creators, and site owners gather at monthly meetups in 436 cities worldwide' ); ?>.</p>
 				</div>
+			</section>
 
-				<div class="col-2">
-					<h4><a href="https://wordpress.org/showcase/"><?php _e( 'WordPress&nbsp;Users' ); ?></a></h4>
-					<ul id="notable-users">
-						<?php
-							$links = array(
-								'nytimes'       => 'https://wordpress.org/showcase/tag/new-york-times/',
-								'cnn'           => 'https://wordpress.org/showcase/tag/cnn/',
-								'rollingstones' => 'https://wordpress.org/showcase/the-rolling-stones/',
-								'people'        => 'https://wordpress.org/showcase/stylewatch-off-the-rack/',
-								'playstation'   => 'https://wordpress.org/showcase/playstationblog/',
-								'motleycrue'    => 'https://wordpress.org/showcase/motley-crue/',
-								'blondie'       => 'https://wordpress.org/showcase/blondie/',
-								'marthastewart' => 'https://wordpress.org/showcase/themarthablog/',
+			<section class="get">
+				<h2><?php _e( 'Get Started with WordPress' ); ?></h2>
+				<p class="subheading"><?php _e( 'Over 60 million people have chosen WordPress to power the place on the web they call &ldquo;home&rdquo; &mdash; join the family.' ); ?></p>
+				<div class="cta-wrapper">
+					<a href="https://wordpress.org/download/" class="button button-primary button-xl"><?php _e( 'Get WordPress' ); ?></a>
+				</div>
+			</section>
+		</div>
+
+		<div id="home-below" class="home-below">
+			<div class="col-2">
+				<h4><a href="https://wordpress.org/about/swag/"><?php _e( 'WordPress&nbsp;Swag' ); ?></a></h4>
+				<a href="https://wordpress.org/about/swag/"><img width="132" height="177" src="https://wpdotorg.files.wordpress.com/2015/10/gray-tshirt-swag.jpg" alt="<?php esc_attr_e( 'WordPress Swag' ); ?>" /></a>
+			</div>
+
+			<div class="col-4">
+				<h4><a href="https://wordpress.org/news/"><?php _e( 'News From Our Blog' ); ?></a></h4>
+
+				<?php
+				foreach ( get_posts( [ 'numberposts' => 1 ] ) as $featured_post ) :
+					setup_postdata( $featured_post );
+					the_title( sprintf( '<h5><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h5>' );
+					echo '<p>' . apply_filters( 'the_excerpt', get_the_excerpt() ) . '</p>';
+				endforeach;
+				wp_reset_postdata();
+				?>
+			</div>
+
+			<div class="col-4">
+				<h4><?php _e( 'It&rsquo;s Easy&nbsp;As&hellip;' ); ?></h4>
+
+				<ol class="steps">
+					<li class="one"><span></span><?php printf( __( '<a href="%s">Find a Web Host</a> and get great hosting while supporting WordPress at the same&nbsp;time.' ), esc_url( 'https://wordpress.org/hosting/' ) ); ?></li>
+					<li class="two"><span></span><?php printf( __( '<a href="%s">Download &amp; Install WordPress</a> with our famous 5-minute&nbsp;installation. Feel like a rock star.' ), esc_url( 'https://wordpress.org/download/' ) ); ?></li>
+					<li class="three"><span></span><?php printf( __( '<a href="%s">Read the Documentation</a> and become a WordPress expert yourself, impress your friends.' ), esc_url( 'https://developer.wordpress.org' ) ); ?></li>
+				</ol>
+			</div>
+
+			<div class="col-2">
+				<h4><a href="https://wordpress.org/showcase/"><?php _e( 'WordPress&nbsp;Users' ); ?></a></h4>
+
+				<ul id="notable-users">
+					<?php
+						$links = array(
+							'nytimes'       => 'https://wordpress.org/showcase/tag/new-york-times/',
+							'cnn'           => 'https://wordpress.org/showcase/tag/cnn/',
+							'rollingstones' => 'https://wordpress.org/showcase/the-rolling-stones/',
+							'people'        => 'https://wordpress.org/showcase/stylewatch-off-the-rack/',
+							'playstation'   => 'https://wordpress.org/showcase/playstationblog/',
+							'motleycrue'    => 'https://wordpress.org/showcase/motley-crue/',
+							'blondie'       => 'https://wordpress.org/showcase/blondie/',
+							'marthastewart' => 'https://wordpress.org/showcase/themarthablog/',
+						);
+
+						foreach ( array_rand( $links, 3 ) as $slug ) :
+							printf(
+								'<li><a href="%1$s"><img src="https://s.w.org/images/notableusers/%2$s-2x.png" alt="%2$s" width="130" height="57" /></a></li>',
+								$links[ $slug ],
+								$slug
 							);
-
-							foreach ( array_rand( $links, 3 ) as $slug ) :
-								printf(
-									'<li><a href="%1$s"><img src="https://s.w.org/images/notableusers/%2$s-2x.png" alt="%2$s" width="130" height="57" /></a></li>',
-									$links[ $slug ],
-									$slug
-								);
-							endforeach;
-						?>
-					</ul>
-					<p id="showcase-link"><a href="https://wordpress.org/showcase/"><?php _e( '&hellip; and hundreds more' ); ?></a></p>
-				</div>
+						endforeach;
+					?>
+				</ul>
+				<p id="showcase-link"><a href="https://wordpress.org/showcase/"><?php _e( '&hellip; and hundreds more' ); ?></a></p>
 			</div>
 		</div>
 
