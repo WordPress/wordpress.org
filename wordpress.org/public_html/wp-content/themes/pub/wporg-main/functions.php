e5885d11f18687218838850eeafa5a00e9cd1844
Konstantin Obenland <obenland@git.wordpress.org>
2017-09-06 15:33:54 +0000

    Main: Add child theme-specific functions file
    See [5889].
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@5890 74240141-8908-4e6f-9713-ba540dce6ec7

---
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-main/functions.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * WordPress.org functions and definitions.
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package WordPressdotorg\Theme\Main
+ */
+
+namespace WordPressdotorg\MainTheme;
+
+/**
+ * Sets up theme defaults and registers support for various WordPress features.
+ *
+ * Note that this function is hooked into the after_setup_theme hook, which
+ * runs before the init hook. The init hook is too late for some features, such
+ * as indicating support for post thumbnails.
+ */
+function setup() {
+
+	// This theme uses wp_nav_menu() in one location.
+	register_nav_menus( array(
+		'rosetta_main' => esc_html__( 'Rosetta', 'wporg-main' ),
+	) );
+
+}
+add_action( 'after_setup_theme', __NAMESPACE__ . '\setup' );
