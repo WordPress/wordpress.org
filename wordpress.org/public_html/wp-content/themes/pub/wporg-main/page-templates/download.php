3d81ac99b1ab397808b145a4c0c0207389d3dbb7
Konstantin Obenland <obenland@git.wordpress.org>
2017-11-17 14:32:49 +0000

    Main: Add a Download page template.
    Allows localization of the download page slug.
    
    Props DeFries.
    See #2861.
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@6145 74240141-8908-4e6f-9713-ba540dce6ec7

---
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-main/page-templates/download.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Template Name: Downloads
+ *
+ * Page template for displaying a downloads page.
+ *
+ * @package WordPressdotorg\MainTheme
+ */
+
+require_once get_stylesheet_directory() . '/page-txt-download.php';
