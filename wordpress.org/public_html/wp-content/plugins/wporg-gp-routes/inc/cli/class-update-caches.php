97a0ed2006cfce3ab6ed429e85296bb012cdd2f4
Dominik Schilling (ocean90) <ocean90@git.wordpress.org>
2017-11-22 11:31:00 +0000

    Translate: Don't fill the caches with empty values on query failures.
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@6157 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/plugins/wporg-gp-routes/inc/cli/class-update-caches.php
+++ wordpress.org/public_html/wp-content/plugins/wporg-gp-routes/inc/cli/class-update-caches.php
@@ -10,13 +10,17 @@ class Update_Caches extends WP_CLI_Command {
 
 	private $cache_group = 'wporg-translate';
 
+	private $running_all = false;
+
 	/**
 	 * Update all caches.
 	 */
 	public function all() {
+		$this->running_all = true;
 		$this->contributors_count();
 		$this->translation_status();
 		$this->existing_locales();
+		$this->running_all = false;
 	}
 
 	/**
@@ -32,11 +36,20 @@ class Update_Caches extends WP_CLI_Command {
 			return;
 		}
 
-		$locales = GP::$translation_set->existing_locales();
+		$locales   = GP::$translation_set->existing_locales();
 		$db_counts = $wpdb->get_results(
 			"SELECT `locale`, COUNT( DISTINCT user_id ) as `count` FROM {$wpdb->user_translations_count} WHERE `accepted` > 0 GROUP BY `locale`",
 			OBJECT_K
 		);
+
+		if ( ! $db_counts || ! $locales ) {
+			if ( $this->running_all ) {
+				WP_CLI::warning( 'Retrieving contributors count failed.' );
+				return;
+			} else {
+				WP_CLI::error( 'Retrieving contributors count failed.' );
+			}
+		}
 
 		$counts = array();
 		foreach ( $locales as $locale ) {
@@ -60,8 +73,12 @@ class Update_Caches extends WP_CLI_Command {
 		global $wpdb;
 
 		if ( ! isset( $wpdb->project_translation_status ) ) {
-			WP_CLI::error( 'The stats plugin seems not to be activated.' );
-			return;
+			if ( $this->running_all ) {
+				WP_CLI::warning( 'Retrieving translation status failed: The stats plugin seems not to be activated.' );
+				return;
+			} else {
+				WP_CLI::error( 'Retrieving translation status failed: The stats plugin seems not to be activated.' );
+			}
 		}
 
 		$translation_status = $wpdb->get_results( $wpdb->prepare(
@@ -70,6 +87,15 @@ class Update_Caches extends WP_CLI_Command {
 			WHERE `project_id` = %d AND `locale_slug` = %s",
 			2, 'default' // 2 = wp/dev
 		), OBJECT_K );
+
+		if ( ! $translation_status ) {
+			if ( $this->running_all ) {
+				WP_CLI::warning( 'Retrieving translation status failed.' );
+				return;
+			} else {
+				WP_CLI::error( 'Retrieving translation status failed.' );
+			}
+		}
 
 		wp_cache_set( 'translation-status', $translation_status, $this->cache_group );
 		WP_CLI::success( 'Translation status was updated.' );
@@ -82,6 +108,15 @@ class Update_Caches extends WP_CLI_Command {
 	 */
 	public function existing_locales() {
 		$existing_locales = GP::$translation_set->existing_locales();
+
+		if ( ! $existing_locales ) {
+			if ( $this->running_all ) {
+				WP_CLI::warning( 'Retrieving existing locales failed.' );
+				return;
+			} else {
+				WP_CLI::error( 'Retrieving existing locales failed.' );
+			}
+		}
 
 		wp_cache_set( 'existing-locales', $existing_locales, $this->cache_group );
 		WP_CLI::success( 'Existing locales were updated.' );
