79cd2146bd5367d3123e9fdc634c69c99cedb60c
Scott Reilly <coffee2code@git.wordpress.org>
2017-12-11 20:45:14 +0000

    developer.wordpress.org: Set global post object to explanation post while obtaining explanation.
    Negates need for explicitly decoding HTML entities in code blocks, which was also inadvertantly (and unnecessarily) being applied to REST API examples.
    
    Fixes #3292.
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@6258 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-developer/reference/template-explanation.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-developer/reference/template-explanation.php
@@ -17,7 +17,11 @@ if ( $explanation = get_explanation_field( 'post_content', get_the_ID() ) ) :
 		<?php
 		// Output the explanation. Passed through 'the_content' on retrieval, thus no escaping.
 		remove_filter( 'the_content', array( 'DevHub_Formatting', 'fix_unintended_markdown' ), 1 );
+		global $post;
+		$orig = $post;
+		$post = get_explanation( get_the_ID() );
 		echo apply_filters( 'the_content', apply_filters( 'get_the_content', $explanation ) );
+		$post = $orig;
 		add_filter( 'the_content', array( 'DevHub_Formatting', 'fix_unintended_markdown' ), 1 );
 		?>
 	</section>
