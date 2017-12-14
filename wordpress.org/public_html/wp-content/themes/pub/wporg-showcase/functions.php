fe2b0d2efe7d0d4de0709c81b8eb87c4022ec96e
Scott Reilly <coffee2code@git.wordpress.org>
2017-12-14 23:33:51 +0000

    W.org Showcase: Properly set script/stylesheet versions when enqueueing.
    See #1173.
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@6274 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-showcase/functions.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-showcase/functions.php
@@ -223,7 +223,7 @@ function extras_feed_init() {
 function wpsc_scripts() {
 	wp_enqueue_script( 'jquery' ); // explicit enqueue
 	wp_enqueue_script( 'jquery-cycle', get_template_directory_uri() . '/js/jquery.cycle2.min.js', array( 'jquery' ) );
-	wp_enqueue_script( 'wpsc-scripts', get_template_directory_uri() . '/js/scripts.js', array( 'jquery', 'jquery-cycle' ), 'aaa' );
+	wp_enqueue_script( 'wpsc-scripts', get_template_directory_uri() . '/js/scripts.js', array( 'jquery', 'jquery-cycle' ), '20171214' );
 }
 add_action('wp_enqueue_scripts', 'wpsc_scripts');
 
