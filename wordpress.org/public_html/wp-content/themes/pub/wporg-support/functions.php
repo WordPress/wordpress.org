862dab31635e8e93a2212fcbc1b2a612706700f3
Konstantin Obenland <obenland@git.wordpress.org>
2018-01-17 18:48:41 +0000

    Support Theme: Bump cache.
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@6390 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-support/functions.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-support/functions.php
@@ -31,7 +31,7 @@ function wporg_support_scripts() {
 		'forum-wp4-style',
 		get_template_directory_uri() . '/style.css',
 		array( 'bb-base' ),
-		'20180108'
+		'20180117'
 	);
 
 	wp_style_add_data( 'forum-wp4-style', 'rtl', 'replace' );
