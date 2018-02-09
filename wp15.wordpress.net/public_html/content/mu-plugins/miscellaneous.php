911f58e60b196c5c2619a7e45bfd19b0eeb2c176
Ian Dunn <iandunn@git.wordpress.org>
2018-02-09 18:33:03 +0000

    WP15: Setup basic installation.
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@6579 74240141-8908-4e6f-9713-ba540dce6ec7

---
+++ wp15.wordpress.net/public_html/content/mu-plugins/miscellaneous.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+Plugin Name: WP15 - Miscellaneous
+Description: Miscellaneous functionality for WP15
+Version:     0.1
+Author:      WordPress Meta Team
+Author URI:  https://make.wordpress.org/meta
+*/
+
+namespace WP15\Updates;
+defined( 'WPINC' ) or die();
+
+add_filter( 'tggr_end_date', __NAMESPACE__ . '\set_tagregator_cutoff_date' );
+
+/**
+ * Tell Tagregator when to stop fetching new items.
+ *
+ * The #wp15 hashtag will collect spam, etc, after the event is over, and we want to
+ * avoid publishing those.
+ *
+ * @param false|int $date
+ *
+ * @return false|int
+ */
+function set_tagregator_cutoff_date( $date ) {
+	// A few weeks after the event ends, so that wrap-up posts, etc are included.
+	return strtotime( 'June 15, 2018' );
+}
---
+++ wp15.wordpress.net/public_html/content/mu-plugins/updates.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+Plugin Name: WP15 - Updates
+Description: Automatically update Core, plugins, themes, and translations
+Version:     0.1
+Author:      WordPress Meta Team
+Author URI:  https://make.wordpress.org/meta
+*/
+
+namespace WP15\Updates;
+defined( 'WPINC' ) or die();
+
+/*
+ * Core will short-circuit auto-updates if it detects a version control system, so that needs to be
+ * overriden.
+ */
+add_filter( 'automatic_updates_is_vcs_checkout', '__return_false' );
+
+/*
+ * Auto update everything, even major Core releases, to minimize the maintenance burden.
+ */
+add_filter( 'allow_minor_auto_core_updates',     '__return_true'  );
+add_filter( 'allow_major_auto_core_updates',     '__return_true'  );
+add_filter( 'auto_update_plugin',                '__return_true'  );
+add_filter( 'auto_update_theme',                 '__return_true'  );
+add_filter( 'auto_update_translation',           '__return_true'  );
