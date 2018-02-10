2b6a5304a45a7225c6451e12419e20e90fa4e769
Ian Dunn <iandunn@git.wordpress.org>
2018-02-10 10:07:37 +0000

    WP15: Explicitly name config files based on their security context.
    Using "wp-config.php" for public config is counter-intuitive, since that file traditionally stores sensitive information. This makes it obvious to anyone that there are separate files for public and private configuration, and reduces the risk of a developer unintentionally adding private configuration to the public file.
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@6589 74240141-8908-4e6f-9713-ba540dce6ec7

---
+++ wp15.wordpress.net/config-public.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+ * SECURITY WARNING: This file is _public_. Add passwords, etc to `config-private.php`.
+ */
+
+$table_prefix = 'wp_';
+
+define( 'DB_CHARSET', 'utf8mb4' );
+define( 'DB_COLLATE', ''        );
+
+define( 'WP_CONTENT_URL', WP_HOME . '/content'                        );
+define( 'WP_SITEURL',     WP_HOME . '/wordpress'                      );
+define( 'WP_CONTENT_DIR', __DIR__ . '/public_html/content'            );
+//define( 'WPCACHEHOME',    WP_CONTENT_DIR . '/plugins/wp-super-cache/' );
+//define( 'WP_CACHE',       true                                        );
+
+define( 'DISALLOW_FILE_EDIT',       true );
+define( 'DISALLOW_UNFILTERED_HTML', true );
+
+if ( 'production' === WP15_ENVIRONMENT ) {
+	ini_set( 'display_errors', 0 );
+
+	define( 'WP_DEBUG_DISPLAY', false );
+	define( 'WP_DEBUG_LOG',     true  );
+	define( 'SCRIPT_DEBUG',     false );
+	define( 'FORCE_SSL_ADMIN',  true  );
+} else {
+	define( 'SAVEQUERIES',  true );
+	define( 'WP_DEBUG',     true );
+	define( 'SCRIPT_DEBUG', true );
+}
+
+if ( ! defined( 'ABSPATH' ) ) {
+	define( 'ABSPATH', dirname( __FILE__ ) . '/' );
+}
+
+require_once( ABSPATH . 'wp-settings.php' );
