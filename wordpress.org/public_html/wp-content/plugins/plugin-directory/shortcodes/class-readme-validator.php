50ff8757612485ada91d89506273eb6d860715bc
Dominik Schilling (ocean90) <ocean90@git.wordpress.org>
2018-02-15 16:27:17 +0000

    Plugin Directory: Remove extra whitespace in textarea for readme validation.
    Fixes #3458.
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@6647 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/plugins/plugin-directory/shortcodes/class-readme-validator.php
+++ wordpress.org/public_html/wp-content/plugins/plugin-directory/shortcodes/class-readme-validator.php
@@ -17,37 +17,23 @@ class Readme_Validator {
 			if ( $_POST ) {
 				self::validate_readme();
 			}
+
+			$readme_url      = $_POST['readme_url'] ?? '';
+			$readme_contents = $_POST['readme_contents'] ?? '';
+			$readme_contents = base64_decode( wp_unslash( $readme_contents ) );
 			?>
 
 			<form method="post" action="">
 				<p>
-					<input type="text" name="readme_url" size="70" placeholder="https://" value="
-					<?php
-					if ( isset( $_POST['readme_url'] ) ) {
-						echo esc_attr( $_POST['readme_url'] );
-					}
-					?>
-					" />
+					<input type="text" name="readme_url" size="70" placeholder="https://" value="<?php echo esc_attr( $readme_url ); ?>" />
 					<input type="submit" class="button button-secondary" value="<?php esc_attr_e( 'Validate!', 'wporg-plugins' ); ?>" />
 				</p>
 			</form>
 
 			<p><?php _e( '... or paste your <code>readme.txt</code> here:', 'wporg-plugins' ); ?></p>
-				<textarea rows="20" cols="100" name="readme_visible" placeholder="=== Plugin Name ===">
-					<?php
-					if ( isset( $_POST['readme_contents'] ) ) {
-						echo esc_textarea( base64_decode( wp_unslash( $_POST['readme_contents'] ) ) );
-					}
-					?>
-				</textarea>
+				<textarea rows="20" cols="100" name="readme_visible" placeholder="=== Plugin Name ==="><?php echo esc_textarea( $readme_contents ); ?></textarea>
 				<form id="readme-data" method="post" action="">
-					<textarea class="screen-reader-text" rows="20" cols="100" name="readme_contents">
-						<?php
-						if ( isset( $_POST['readme_contents'] ) ) {
-							echo esc_textarea( base64_decode( wp_unslash( $_POST['readme_contents'] ) ) );
-						}
-						?>
-					</textarea>
+					<textarea class="screen-reader-text" rows="20" cols="100" name="readme_contents"><?php echo esc_textarea( $readme_contents ); ?></textarea>
 				<p><input type="submit" class="button button-secondary" value="<?php esc_attr_e( 'Validate!', 'wporg-plugins' ); ?>" /></p>
 			</form>
 			<script>
