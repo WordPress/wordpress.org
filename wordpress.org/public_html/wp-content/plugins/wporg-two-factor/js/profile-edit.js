f08744dad2aa45457bdd84071871ddc18fc94ed5
Kónståntîn Obénland <obenland@git.wordpress.org>
2018-02-25 11:27:40 +0000

    2FA: First pass at adding backup codes.
    See #77.
    
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@6740 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/plugins/wporg-two-factor/js/profile-edit.js
+++ wordpress.org/public_html/wp-content/plugins/wporg-two-factor/js/profile-edit.js
@@ -80,4 +80,47 @@ jQuery( function( $ ) {
 				}
 			);
 		} );
+
+	$( '#generate-backup-codes' ).on( 'click', function() {
+		$.post(
+			ajaxurl,
+			{
+				action: 'two_factor_backup_codes_generate',
+				user_id: $( '#user_id' ).val(),
+				nonce: $( '#_nonce-backup-codes' ).val(),
+			},
+			function( response ) {
+				if ( response.success ) {
+					var $codesList = $( '.two-factor-backup-codes-unused-codes' );
+
+					$( '#generate-backup-codes' ).remove();
+					$( '.two-factor-backup-codes-wrapper' ).show();
+					$codesList.html( '' );
+
+					// Append the codes.
+					for ( i = 0; i < response.data.codes.length; i++ ) {
+						$codesList.append( '<li>' + response.data.codes[ i ] + '</li>' );
+					}
+
+					// Update counter.
+					$( '.two-factor-backup-codes-count' ).html( response.data.i18n.count );
+
+					// Build the download link
+					var txt_data = 'data:application/text;charset=utf-8,' + '\n';
+					txt_data += response.data.i18n.title.replace( /%s/g, document.domain ) + '\n\n';
+
+					for ( i = 0; i < response.data.codes.length; i++ ) {
+						txt_data += i + 1 + '. ' + response.data.codes[ i ] + '\n';
+					}
+
+					$( '#two-factor-backup-codes-download-link' ).attr( 'href', encodeURI( txt_data ) );
+				}
+			}
+		);
+	} );
+
+	$( '#print-agreement' ).on( 'change', function() {
+		$( '.two-factor-backup-codes-wrapper button[type="submit"]' ).prop( 'disabled', ! $( this ).prop( 'checked' ) );
+	} );
+
 } );
