83d90aa6659e350d941ee1cb5ff6060833a753ba
Sergey Biryukov <sergeybiryukov@git.wordpress.org>
2018-03-08 02:51:43 +0000

    Breathe: Convert close button for Welcome box to a toggle.
    Props dotrex, boemedia.
    Fixes #3393.
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@6841 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-breathe/functions.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-breathe/functions.php
@@ -46,7 +46,7 @@ function styles() {
 	wp_enqueue_style( 'p2-breathe', get_template_directory_uri() . '/style.css' );
 
 	// Cacheing hack
-	wp_enqueue_style( 'wporg-breathe', get_stylesheet_uri(), array( 'p2-breathe' ), '20180207a' );
+	wp_enqueue_style( 'wporg-breathe', get_stylesheet_uri(), array( 'p2-breathe' ), '20180208a' );
 }
 add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\styles', 11 );
 
@@ -59,14 +59,25 @@ function inline_scripts() {
 	$current_site = get_site();
 	?>
 	<script type="text/javascript">
-		var el = document.getElementById( 'make-welcome-hide' );
+		var el = document.getElementById( 'make-welcome-toggle' );
 		if ( el ) {
 			el.addEventListener( 'click', function( e ) {
-				document.cookie = el.dataset.cookie + '=' + el.dataset.hash +
-					'; expires=Fri, 31 Dec 9999 23:59:59 GMT' +
-					'; domain=<?php echo esc_js( $current_site->domain ); ?>' +
-					'; path=<?php echo esc_js( $current_site->path ); ?>';
-				jQuery( '.make-welcome' ).slideUp();
+				if ( jQuery( '.make-welcome .entry-content' ).is( ':hidden' ) ) {
+					document.cookie = el.dataset.cookie + '=' +
+						'; expires=Thu, 01 Jan 1970 00:00:00 GMT' +
+						'; domain=<?php echo esc_js( $current_site->domain ); ?>' +
+						'; path=<?php echo esc_js( $current_site->path ); ?>';
+					jQuery( '#make-welcome-toggle' ).text( '<?php esc_attr_e( 'Hide welcome box', 'wporg' ); ?>' );
+				} else {
+					document.cookie = el.dataset.cookie + '=' + el.dataset.hash +
+						'; expires=Fri, 31 Dec 9999 23:59:59 GMT' +
+						'; domain=<?php echo esc_js( $current_site->domain ); ?>' +
+						'; path=<?php echo esc_js( $current_site->path ); ?>';
+					jQuery( '#make-welcome-toggle' ).text( '<?php esc_attr_e( 'Show welcome box', 'wporg' ); ?>' );
+				}
+
+				jQuery( '.make-welcome .entry-content' ).slideToggle();
+				jQuery( '.make-welcome .post-edit-link' ).toggle();
 			} );
 		}
 	</script>
@@ -75,35 +86,45 @@ function inline_scripts() {
 add_action( 'wp_footer', __NAMESPACE__ . '\inline_scripts' );
 
 function welcome_box() {
-	$welcome = get_page_by_path( 'welcome' );
-	$cookie  = 'welcome-' . get_current_blog_id();
-	$hash    = isset( $_COOKIE[ $cookie ] ) ? $_COOKIE[ $cookie ] : '';
+	$welcome      = get_page_by_path( 'welcome' );
+	$cookie       = 'welcome-' . get_current_blog_id();
+	$hash         = isset( $_COOKIE[ $cookie ] ) ? $_COOKIE[ $cookie ] : '';
 	$content_hash = $welcome ? md5( $welcome->post_content ) : '';
 
-	if ( $welcome && ( empty( $hash ) || $content_hash !== $hash ) ) :
-		$columns = preg_split( '|<hr\s*/?>|', $welcome->post_content );
-		if ( count( $columns ) === 2 ) {
-			$welcome->post_content = "<div class='content-area'>\n\n{$columns[0]}</div><div class='widget-area'>\n\n{$columns[1]}</div>";
-		}
+	if ( ! $welcome ) {
+		return;
+	}
 
-		setup_postdata( $welcome );
+	if ( ! $hash || $content_hash !== $hash ) {
+		$class = '';
+		$label = __( 'Hide welcome box', 'wporg' );
+	} else {
+		$class = 'hidden';
+		$label = __( 'Show welcome box', 'wporg' );
+	}
 
-		// Disable Jetpack sharing buttons
-		add_filter( 'sharing_show', '__return_false' );
+	$columns = preg_split( '|<hr\s*/?>|', $welcome->post_content );
+	if ( count( $columns ) === 2 ) {
+		$welcome->post_content = "<div class='content-area'>\n\n{$columns[0]}</div><div class='widget-area'>\n\n{$columns[1]}</div>";
+	}
+
+	setup_postdata( $welcome );
+
+	// Disable Jetpack sharing buttons
+	add_filter( 'sharing_show', '__return_false' );
 	?>
 	<div class="make-welcome">
 		<div class="entry-meta">
-			<?php edit_post_link( __( 'Edit', 'wporg' ), '', '', $welcome->ID ); ?>
-			<button type="button" id="make-welcome-hide" class="toggle dashicons dashicons-no" data-hash="<?php echo $content_hash; ?>" data-cookie="<?php echo $cookie; ?>" title="<?php esc_attr_e( 'Hide this message', 'wporg' ); ?>"></button>
+			<?php edit_post_link( __( 'Edit', 'wporg' ), '', '', $welcome->ID, 'post-edit-link ' . $class ); ?>
+			<button type="button" id="make-welcome-toggle" data-hash="<?php echo $content_hash; ?>" data-cookie="<?php echo $cookie; ?>"><?php echo $label; ?></button>
 		</div>
-		<div class="entry-content clear">
+		<div class="entry-content clear <?php echo $class; ?>"">
 			<?php the_content(); ?>
 		</div>
 	</div>
 	<?php
-		remove_filter( 'sharing_show', '__return_false' );
-		wp_reset_postdata();
-	endif;
+	remove_filter( 'sharing_show', '__return_false' );
+	wp_reset_postdata();
 }
 add_action( 'wporg_breathe_after_header', __NAMESPACE__ . '\welcome_box' );
 
