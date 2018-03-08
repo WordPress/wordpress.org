33fec718c5b195d30f855839d2cc97fa811f7002
Ian Dunn <iandunn@git.wordpress.org>
2018-03-08 17:41:07 +0000

    WP15: Document the reasoning behind configuration structure.
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@6844 74240141-8908-4e6f-9713-ba540dce6ec7

--- wp15.wordpress.net/public_html/wp-config.php
+++ wp15.wordpress.net/public_html/wp-config.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * Everything that can be open-sourced should be, including configuration. That promotes transparency and
+ * collaboration; keeps the VCS layout simple, and makes dev environments simple and consistent.
+ *
+ * All of the files for this site are stored in `meta.svn`, except for `config-private.php`, which is ignored.
+ *
+ * Naming the config files explicitly with `public` and `private` removes any ambiguity that could lead to sensitive
+ * information accidentally being added to the public file, or to `wp-config.php`, which is also public.
+ */
 require_once( dirname( __DIR__ ). '/config-private.php' );
 require_once( dirname( __DIR__ ). '/config-public.php'  );
 
