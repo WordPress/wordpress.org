4fa4a02430b262900c9d703185ccd3e4630cfcd0
Ian Dunn <iandunn@git.wordpress.org>
2018-03-12 22:21:07 +0000

    WPF Stripe: Add a button for giving single donations.
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@6866 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpressfoundation.org/public_html/content/plugins/wpf-stripe/wpf-stripe.php
+++ wordpressfoundation.org/public_html/content/plugins/wpf-stripe/wpf-stripe.php
@@ -10,6 +10,7 @@ License:     GPLv2 or later
 
 namespace WordPress_Foundation\Stripe;
 use Exception;
+use Stripe\Stripe, Stripe\Customer, Stripe\Charge;
 
 defined( 'WPINC' ) || die();
 
@@ -93,48 +94,75 @@ function render_donate_shortcode() {
 		</form>
 	</div>
 
+	<p>Or make a one-time donation in the amount of your choosing:</p>
+
+	<div class="one-time-donation">
+		<form action="" method="POST">
+			<label>$
+				<input type="number" class="amount" name="amount" min="1" />
+			</label>
+
+			<script
+				src="https://checkout.stripe.com/checkout.js"
+				class="stripe-button"
+				data-key="<?php echo esc_html( STRIPE_PUBLIC_KEY ); ?>"
+				data-image="<?php echo esc_url( $image ); ?>"
+				data-name="WordPress Foundation"
+				data-description="One-Time Donation"
+				data-label="Give once"
+				data-zip-code="true"
+				data-billing-address="true">
+			</script>
+		</form>
+	</div>
+
 	<?php
 
 	return ob_get_clean();
 }
 add_shortcode( 'wpfstripe', __NAMESPACE__ . '\render_donate_shortcode' );
 
-add_action( 'init', __NAMESPACE__ . '\check_subscribe' );
-function check_subscribe() {
-	if ( !empty( $_POST ) && isset( $_POST['stripeToken'] ) ) {
-		process_payments();
-	}
-}
-
 function process_payments() {
-	global $wpf_success_url, $wpf_fail_url;
-
-	// no token, nothing to do
-	if ( empty( $_POST['stripeToken'] ) ) {
+	if ( ! isset( $_POST['stripeEmail'], $_POST['stripeToken'] ) ) {
 		return;
 	}
 
-	require_once('stripe-php/init.php');
+	require_once( __DIR__ . '/stripe-php/init.php' );
+	Stripe::setApiKey( STRIPE_SECRET_KEY );
 
-	\Stripe\Stripe::setApiKey( STRIPE_SECRET_KEY );
+	try {
+		if ( isset( $_POST['wpf_plan'] ) ) {
+			$params = array(
+				'source' => $_POST['stripeToken'],
+				'email'  => $_POST['stripeEmail'],
+				'plan'   => $_POST['wpf_plan'],	// Subscribe the new Customer to the plan at the same time.
+			);
 
-	try
-	{
-		$customer = \Stripe\Customer::create(array(
-			'email' => $_POST['stripeEmail'],
-			'source'  => $_POST['stripeToken'],
-			'plan' => $_POST['wpf_plan'],
-	  		));
+			Customer::create( $params );
+		} elseif ( isset( $_POST['amount'] ) ) {
+			$params = array(
+				'source'        => $_POST['stripeToken'],
+				'receipt_email' => $_POST['stripeEmail'],
+				'amount'        => $_POST['amount'] * 100,	// Convert dollars to pennies.
+				'currency'      => 'USD',
+				'description'   => 'WordPress Foundation donation',
+			);
 
-	  	wp_redirect( $wpf_success_url );
-		exit;
-	}
-	catch(Exception $e)
-	{
-		wp_redirect( $wpf_fail_url );
+			Charge::create( $params );
+		} else {
+			wp_die( 'Unsupported form action' );
+		}
+
+		$redirect_url = home_url( 'successful-donation/'  );
+	} catch ( Exception $exception ) {
+		log( $exception->getMessage(), $params );
+		$redirect_url = home_url( 'unsuccessful-donation/');
+	} finally {
+		wp_safe_redirect( $redirect_url );
 		exit;
 	}
 }
+add_action( 'init', __NAMESPACE__ . '\process_payments' );
 
 /**
  * Output CSS for the `[wpfstripe]` shortcode.
@@ -151,11 +179,54 @@ function custom_styles() {
 	<style>
 		.stripe-button-el {
 			text-transform: none;
-			float: left;
-			margin: 8px;
 		}
+
+		.subscription-plans .stripe-button-el {
+			margin: 0 12px 12px 0;
+		}
+
+			@media screen and ( min-width: 415px ) {
+				.subscription-plans {
+					display: flex;
+					flex-wrap: wrap;
+					justify-content: space-around;
+				}
+			}
+
+		.one-time-donation {
+			margin-top: .5em;
+		}
+
+		.one-time-donation form {
+			margin-top: .5em;
+			text-align: center;
+		}
+
+			.amount {
+				position: relative;
+				top: 3px;
+				width: 5em;
+				margin-right: .5em;
+				padding: 1px 5px;
+				text-align: center;
+			}
 	</style>
 
 	<?php
 }
 add_action( 'wp_head', __NAMESPACE__ . '\custom_styles' );
+
+/**
+ * Log an error message.
+ *
+ * @param string $error_message
+ * @param array  $data
+ */
+function log( $error_message, $data ) {
+	trigger_error( sprintf(
+		'%s error: %s. Data: %s',
+		__FUNCTION__,
+		$error_message,
+		wp_json_encode( $data )
+	) );
+}
