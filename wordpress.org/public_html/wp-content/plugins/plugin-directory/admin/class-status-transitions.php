04a66e27eceb93c734e02a5edeb4497e89cd661e
Dion Hulse <dd32@git.wordpress.org>
2018-03-14 05:14:59 +0000

    Plugin Directory: Attempt to avoid storing invalid dates in postmeta for status changes. If the post modified date isn't available, assume now.
    I'm not entirely sure `post_modified_gmt` is the correct field to use here, I suspect `post_modified` would be better, or perhaps even just using `time()` in all cases, but once again, I'm not sure why post_modified_gmt was used here instead.
    
    Fixes #3511.
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@6873 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/plugins/plugin-directory/admin/class-status-transitions.php
+++ wordpress.org/public_html/wp-content/plugins/plugin-directory/admin/class-status-transitions.php
@@ -150,7 +150,12 @@ class Status_Transitions {
 		}
 
 		// Record the time a plugin was transitioned into a specific status.
-		update_post_meta( $post->ID, "_{$new_status}", strtotime( $post->post_modified_gmt ) );
+		if ( '0000-00-00 00:00:00' === $post->post_modified_gmt ) {
+			// Assume now.
+			update_post_meta( $post->ID, "_{$new_status}", time() );
+		} else {
+			update_post_meta( $post->ID, "_{$new_status}", strtotime( $post->post_modified_gmt ) );
+		}
 	}
 
 	/**
