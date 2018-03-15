c3a39afd73a7ce07fc82c9368512a8950a1ab346
Ian Dunn <iandunn@git.wordpress.org>
2018-03-15 23:26:48 +0000

    WP15: Add site logo.
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@6881 74240141-8908-4e6f-9713-ba540dce6ec7

--- wp15.wordpress.net/public_html/content/themes/twentyseventeen-wp15/functions.php
+++ wp15.wordpress.net/public_html/content/themes/twentyseventeen-wp15/functions.php
@@ -5,6 +5,7 @@ namespace WP15\Theme;
 defined( 'WPINC' ) || die();
 
 add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\enqueue_scripts' );
+add_filter( 'get_custom_logo',    __NAMESPACE__ . '\set_custom_logo' );
 
 /**
  * Enqueue scripts and styles
@@ -15,3 +16,23 @@ function enqueue_scripts() {
 		get_template_directory_uri() . '/style.css'
 	);
 }
+
+/**
+ * Set the custom logo.
+ *
+ * @return string
+ */
+function set_custom_logo() {
+	ob_start();
+
+	?>
+
+	<a href="<?php echo esc_url( home_url() ); ?>" class="custom-logo-link" rel="home" itemprop="url">
+		<img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/images/wp15-logo.svg" class="custom-logo" alt="WordPress 15th anniversary logo" itemprop="logo" />
+	</a>
+
+
+	<?php
+
+	return ob_get_clean();
+};
