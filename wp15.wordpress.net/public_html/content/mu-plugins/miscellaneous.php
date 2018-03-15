5a8ac37d2440b4c47b682d703ab0cab7c05e1a14
Ian Dunn <iandunn@git.wordpress.org>
2018-03-15 23:26:52 +0000

    WP15: Enable the Additional CSS section for convenience.
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@6882 74240141-8908-4e6f-9713-ba540dce6ec7

--- wp15.wordpress.net/public_html/content/mu-plugins/miscellaneous.php
+++ wp15.wordpress.net/public_html/content/mu-plugins/miscellaneous.php
@@ -13,7 +13,31 @@ use DateTime;
 
 defined( 'WPINC' ) or die();
 
+add_filter( 'map_meta_cap',  __NAMESPACE__ . '\allow_css_editing', 10, 2   );
 add_filter( 'tggr_end_date', __NAMESPACE__ . '\set_tagregator_cutoff_date' );
+
+
+/**
+ * Allow admins to use Additional CSS, despite `DISALLOW_UNFILTERED_HTML`.
+ *
+ * The admins on this site are trusted, so `DISALLOW_UNFILTERED_HTML` is mostly in place to enforce best practices,
+ * -- like placing JavaScript in a plugin instead of `post_content` -- rather than to prevent malicious code. CSS
+ * is an exception to that rule, though; it's perfectly acceptable to store minor tweaks in Additional CSS, that's
+ * what it's for.
+ *
+ * @param array  $required_capabilities The primitive capabilities that are required to perform the requested meta
+ *                                      capability.
+ * @param string $requested_capability  The requested meta capability.
+ *
+ * @return array
+ */
+function allow_css_editing( $required_capabilities, $requested_capability ) {
+	if ( 'edit_css' === $requested_capability ) {
+		$required_capabilities = array( 'edit_theme_options' );
+	}
+
+	return $required_capabilities;
+}
 
 /**
  * Tell Tagregator when to stop fetching new items.
