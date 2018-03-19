c6baf668234778015640cbcaa2148277dcd9892e
Ian Dunn <iandunn@git.wordpress.org>
2018-03-19 23:06:25 +0000

    WP15: Override branding template to hardcode site description string.
    Since this site won't have a lot of written content, and it won't change often, hardcoding strings in the theme template might be the easiest way to facilitate translation via GlotPress. This commit will help give us a string to experiment with.
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@6889 74240141-8908-4e6f-9713-ba540dce6ec7

---
+++ wp15.wordpress.net/public_html/content/themes/twentyseventeen-wp15/template-parts/header/site-branding.php
@@ -0,0 +1,49 @@
+<div class="site-branding">
+	<div class="wrap">
+
+		<?php the_custom_logo(); ?>
+
+		<div class="site-branding-text">
+			<?php if ( is_front_page() ) : ?>
+				<h1 class="site-title">
+					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+						<?php bloginfo( 'name' ); ?>
+					</a>
+				</h1>
+
+			<?php else : ?>
+
+				<p class="site-title">
+					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+						<?php bloginfo( 'name' ); ?>
+					</a>
+				</p>
+			<?php endif; ?>
+
+			<p class="site-description">
+				<?php
+				// translators: Date format, see https://php.net/date.
+				$date_format    = _x( 'F dS, Y', 'Site description date format' );
+				$formatted_date = date_i18n( $date_format, strtotime( '2018-05-27' ) );
+
+				// translators: 1: the formatted date; e.g., "May 27th, 2018".
+				esc_html__(
+					printf( 'WordPress turns 15 years old on %1$s.', $formatted_date ),
+					'wp15'
+				);
+
+				?>
+			</p>
+		</div>
+
+		<?php if ( ( twentyseventeen_is_frontpage() || ( is_home() && is_front_page() ) ) && ! has_nav_menu( 'top' ) ) : ?>
+			<a href="#content" class="menu-scroll-down">
+				<?php echo twentyseventeen_get_svg( array( 'icon' => 'arrow-right' ) ); ?>
+				<span class="screen-reader-text">
+					<?php _e( 'Scroll down to content', 'twentyseventeen' ); ?>
+				</span>
+			</a>
+		<?php endif; ?>
+
+	</div>
+</div>
