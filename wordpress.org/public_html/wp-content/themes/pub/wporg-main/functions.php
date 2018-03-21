58d4324d907eaed824e8020ed1d58542a0cade22
Dion Hulse <dd32@git.wordpress.org>
2018-03-21 09:25:53 +0000

    WordPress.org Base theme, Main, Plugins, Themes: Update style cache-busters following r6902.
    See #3523.
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@6903 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-main/functions.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-main/functions.php
@@ -68,7 +68,7 @@ add_action( 'widgets_init', __NAMESPACE__ . '\widgets' );
  * Enqueue scripts and styles.
  */
 function scripts() {
-	wp_enqueue_style( 'wporg-style', get_theme_file_uri( '/css/style.css' ), [ 'dashicons', 'open-sans' ], '20180321' );
+	wp_enqueue_style( 'wporg-style', get_theme_file_uri( '/css/style.css' ), [ 'dashicons', 'open-sans' ], '20180321a' );
 	wp_style_add_data( 'wporg-style', 'rtl', 'replace' );
 
 	if ( is_page( 'stats' ) ) {
--- wordpress.org/public_html/wp-content/themes/pub/wporg/functions.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg/functions.php
@@ -79,7 +79,7 @@ add_action( 'after_setup_theme', __NAMESPACE__ . '\content_width', 0 );
  * Enqueue scripts and styles.
  */
 function scripts() {
-	wp_enqueue_style( 'wporg-style', get_theme_file_uri( '/css/style.css' ), [ 'dashicons', 'open-sans' ], '20180228' );
+	wp_enqueue_style( 'wporg-style', get_theme_file_uri( '/css/style.css' ), [ 'dashicons', 'open-sans' ], '20180321' );
 	wp_style_add_data( 'wporg-style', 'rtl', 'replace' );
 
 	// phpcs:ignore Squiz.PHP.CommentedOutCode.Found, Squiz.Commenting.InlineComment.InvalidEndChar
