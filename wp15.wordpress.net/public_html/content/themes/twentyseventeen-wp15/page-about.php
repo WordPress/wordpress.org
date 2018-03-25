3ccf3c474c0fa0856578a5b3b91483f3be0d0f9a
Ian Dunn <iandunn@git.wordpress.org>
2018-03-25 01:51:50 +0000

    WP15: Add About page template.
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@6929 74240141-8908-4e6f-9713-ba540dce6ec7

---
+++ wp15.wordpress.net/public_html/content/themes/twentyseventeen-wp15/page-about.php
@@ -0,0 +1,21 @@
+<?php get_header(); ?>
+
+<div class="wrap">
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+			<?php
+
+			while ( have_posts() ) {
+				the_post();
+				get_template_part( 'template-parts/page/content', 'page' );
+				echo do_shortcode( '[wp15_meetup_events]' );
+			}
+
+			?>
+
+		</main>
+	</div>
+</div>
+
+<?php get_footer();
