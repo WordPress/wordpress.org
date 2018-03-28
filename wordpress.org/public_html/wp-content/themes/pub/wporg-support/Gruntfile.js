0cdf1a86ea98ce89d16bdee780a933c2da7af3fb
Stephen Edgar <netweb@git.wordpress.org>
2018-03-28 06:30:14 +0000

    Support Theme: JavaScript Coding Standards for `Gruntfile.js`
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@6955 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-support/Gruntfile.js
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-support/Gruntfile.js
@@ -1,37 +1,38 @@
 module.exports = function(grunt) {
+
 	grunt.initConfig({
 		pkg: grunt.file.readJSON( 'package.json' ),
 		sass: {
 			dist: {
 				files: {
-					'style.css' : 'sass/style.scss'
+					'style.css': 'sass/style.scss',
 				},
 				options: {
 					outputStyle: 'expanded',
 					indentType: 'tab',
 					indentWidth: 1,
-					sourceMap: true
-				}
-			}
+					sourceMap: true,
+				},
+			},
 		},
 		rtlcss: {
 			dist: {
 				files: {
-					'style-rtl.css' : 'style.css'
-				}
-			}
+					'style-rtl.css': 'style.css',
+				},
+			},
 		},
 		watch: {
 			css: {
 				files: '**/*.scss',
-				tasks: ['sass', 'rtlcss']
-			}
-		}
+				tasks: ['sass', 'rtlcss'],
+			},
+		},
 	});
 	grunt.loadNpmTasks( 'grunt-sass' );
 	grunt.loadNpmTasks( 'grunt-rtlcss' );
 	grunt.loadNpmTasks( 'grunt-contrib-watch' );
 
-	grunt.registerTask( 'build', [ 'sass', 'rtlcss' ] );
-	grunt.registerTask( 'default', [ 'build' ] );
+	grunt.registerTask( 'build', [ 'sass', 'rtlcss' ]);
+	grunt.registerTask( 'default', [ 'build' ]);
 };
