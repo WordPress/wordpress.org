0011eb9a1b99f5f19d80d55bb7bf2e07bda9a85f
Ian Dunn <iandunn@git.wordpress.org>
2018-03-30 23:45:03 +0000

    WP15: Disable social media posts until moderation is in place.
    It remains visible for admins, so that Tagregator can continue importing posts.
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@6990 74240141-8908-4e6f-9713-ba540dce6ec7

--- wp15.wordpress.net/public_html/content/themes/twentyseventeen-wp15/page-live.php
+++ wp15.wordpress.net/public_html/content/themes/twentyseventeen-wp15/page-live.php
@@ -12,7 +12,10 @@
 				<div class="entry-content">
 					<?php _e( 'Join the conversation by using #WP15 on your favorite social networks.', 'wp15' ); ?>
 
+					<?php if ( current_user_can( 'manage_options' ) ) : ?>
+						<p style="color:darkred">Don't forget to enable this for logged-out visitors once moderation is in place.</p>
 					<?php echo do_shortcode( '[tagregator hashtag="#WP15"]' ); ?>
+					<?php endif; ?>
 				</div>
 
 			</article>
