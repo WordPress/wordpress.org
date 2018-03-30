3f26c7f3388d1f47e6976c9e7413f3c221f097d4
Ian Dunn <iandunn@git.wordpress.org>
2018-03-30 19:57:44 +0000

    WP15: Add template for Live page.
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@6980 74240141-8908-4e6f-9713-ba540dce6ec7

---
+++ wp15.wordpress.net/public_html/content/themes/twentyseventeen-wp15/page-live.php
@@ -0,0 +1,23 @@
+<?php get_header(); ?>
+
+<div class="wrap">
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+				<header class="entry-header">
+					<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+				</header>
+
+				<div class="entry-content">
+					<?php _e( 'Join the conversation by using #WP15 on your favorite social networks.', 'wp15' ); ?>
+
+					<?php echo do_shortcode( '[tagregator hashtag="#WP15"]' ); ?>
+				</div>
+
+			</article>
+		</main>
+	</div>
+</div>
+
+<?php get_footer();
