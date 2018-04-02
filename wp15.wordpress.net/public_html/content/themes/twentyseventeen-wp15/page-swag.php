37c45b18e295a70b6aa2a1c2dab96c777e710911
Corey McKrill <coreymckrill@git.wordpress.org>
2018-04-02 23:26:59 +0000

    WP15 theme: Updates to Swag template, remove sub-template
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@7001 74240141-8908-4e6f-9713-ba540dce6ec7

--- wp15.wordpress.net/public_html/content/themes/twentyseventeen-wp15/page-swag.php
+++ wp15.wordpress.net/public_html/content/themes/twentyseventeen-wp15/page-swag.php
@@ -1,18 +1,67 @@
+<?php
+use WP15\Theme;
+?>
+
 <?php get_header(); ?>
 
 	<div class="wrap">
 		<div id="primary" class="content-area">
 			<main id="main" class="site-main" role="main">
+				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-				<?php
+					<header class="entry-header">
+						<h1>
+							<?php
+							/* translators: "Swag" is a term for promotional items. This is the title of the page. */
+							esc_html_e( 'Swag', 'wp15' );
+							?>
+						</h1>
+					</header>
 
-				while ( have_posts() ) {
-					the_post();
-					get_template_part( 'template-parts/page/content', 'swag' );
-				}
+					<div class="entry-content">
+						<h2>
+							<?php esc_html_e( 'Print your own', 'wp15' ); ?>
+						</h2>
 
-				?>
+						<p><?php esc_html_e( 'These 15th Anniversary logos and files are available for download for folks who want to print their own swag:', 'wp15' ); ?></p>
 
+						<ul class="downloads-wrapper">
+							<?php foreach ( Theme\get_swag_download_items() as $item ) : ?>
+								<li class="downloads-item">
+									<div class="downloads-item-preview">
+										<img src="<?php echo esc_attr( $item['preview_image_url'] ); ?>" alt="<?php echo esc_attr( $item['title'] ); ?>" />
+									</div>
+									<div class="downloads-item-header">
+										<strong><?php echo esc_html( $item['title'] ); ?></strong>
+									</div>
+									<?php if ( ! empty( $item['files'] ) ) : ?>
+										<ul class="downloads-item-files">
+											<?php foreach ( $item['files'] as $file ) : ?>
+												<li>
+													<a href="<?php echo esc_attr( $file['url'] ); ?>"><?php echo esc_html( $file['name'] ); ?></a>
+												</li>
+											<?php endforeach; ?>
+										</ul>
+									<?php endif; ?>
+								</li>
+							<?php endforeach; ?>
+						</ul>
+
+						<img class="wp15-confetti-divider" src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/images/confetti-divider.svg" alt="" />
+
+						<!--
+
+						<h2>
+							<?php esc_html_e( 'Purchase', 'wp15' ); ?>
+						</h2>
+
+						<p><?php esc_html_e( 'Check out the WordPress swag store if you\'d like to purchase WordPress 15th anniversary swag.', 'wp15' ); ?></p>
+
+						-->
+
+					</div>
+
+				</article>
 			</main>
 		</div>
 	</div>
