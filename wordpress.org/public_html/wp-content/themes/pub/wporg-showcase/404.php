ff3a3f377b1a8421e0f40b53e77a2c83c33d6e4f
Samuel Wood (Otto) <otto42@git.wordpress.org>
2018-04-19 23:14:36 +0000

    Showcase: Fix closing tag and apostrophe.
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@7139 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-showcase/404.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-showcase/404.php
@@ -13,7 +13,7 @@ $wp_query = new WP_Query( array( 'no_found_rows' => true, 'post_type' => 'post',
 
 					<div class="col-5">
 						<div class="storycontent">
-								<p>Sorry, we could not not find that site in the Showcase. We do have many others available though. Here's one chosen at random!<p>
+								<p>Sorry, we could not not find that site in the Showcase. We do have many others available though. Here&#8217;s one chosen at random!</p>
 								<?php //breadcrumb(); ?>
 								<h3><?php the_title(); ?></h3>
 								<a href=' http://<?php get_site_domain( false ); ?>'>
