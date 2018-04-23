85d6a887bd100fb54c359ed267aa67a3e422bd44
JJJ <johnjamesjacoby@git.wordpress.org>
2018-04-23 01:50:45 +0000

    BuddyPress: Update codex theme headers.
    Also tweak responsive CSS to give color to hamburger icon.
    
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@7154 74240141-8908-4e6f-9713-ba540dce6ec7

--- buddypress.org/public_html/wp-content/themes/codex-bbpress-org/header-nav.php
+++ buddypress.org/public_html/wp-content/themes/codex-bbpress-org/header-nav.php
@@ -1,9 +1,12 @@
-<ul id="nav" class="menu">
-	<li><a href="https://bbpress.org/about/">About</a></li>
-	<li><a href="https://bbpress.org/plugins/">Plugins</a></li>
-	<li><a href="https://bbpress.org/themes/">Themes</a></li>
-	<li class="current"class="current"><a href="https://codex.bbpress.org/">Documentation</a></li>
-	<li><a href="https://bbpress.org/blog/">Blog</a></li>
-	<li><a href="https://bbpress.org/forums/">Support</a></li>
-	<li><a href="https://bbpress.org/download/">Download</a></li>
-</ul>
+<div id="nav">
+	<a href="#" id="bb-menu-icon"></a>
+	<ul id="bb-nav" class="menu">
+		<li><a href="https://bbpress.org/about/">About</a></li>
+		<li><a href="https://bbpress.org/plugins/">Plugins</a></li>
+		<li><a href="https://bbpress.org/themes/">Themes</a></li>
+		<li class="current"class="current"><a href="https://codex.bbpress.org/">Documentation</a></li>
+		<li><a href="https://bbpress.org/blog/">Blog</a></li>
+		<li><a href="https://bbpress.org/forums/">Support</a></li>
+		<li><a href="https://bbpress.org/download/">Download</a></li>
+	</ul>
+</div>
