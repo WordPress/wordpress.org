89b4f3915bbb25f17318bd8d7a3b73bef1a90b5a
Alex Shiels <tellyworth@git.wordpress.org>
2018-06-04 05:59:20 +0000

    Fix improper use of esc_attr_e() in [7264], thanks @dd32
    See #3640
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@7265 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-login/functions.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-login/functions.php
@@ -191,7 +191,7 @@ function wporg_login_language_switcher() {
 	<div class="language-switcher">
 		<form id="language-switcher" action="" method="GET">
 			<?php if ( !empty( $_GET['redirect_to'] ) ): ?>
-				<input type="hidden" name="redirect_to" value="<?php esc_attr_e( $_GET['redirect_to'] ); ?>" />
+				<input type="hidden" name="redirect_to" value="<?php echo esc_attr( $_GET['redirect_to'] ); ?>" />
 			<?php endif; ?>
 			<label for="language-switcher-locales">
 				<span aria-hidden="true" class="dashicons dashicons-translation"></span>
