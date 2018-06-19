50841430f33827f5395dc7e9c7d552d6657eabed
Kónståntîn Obénland <obenland@git.wordpress.org>
2018-06-19 10:59:04 +0000

    Showcase: Remove ratings aggregations.
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@7320 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-showcase/sidebar-right.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-showcase/sidebar-right.php
@@ -11,15 +11,5 @@
 				<br />
 				<?php tags_with_count( 'list', '<h4>' . __( 'Tags', 'wporg-showcase' ) . '</h4><ul>', '', '</ul>' ); ?>
 			</div>
-
-			<h4><?php _e( 'Top Rated', 'wporg-showcase' ); ?></h4>
-			<table class="top-rated">
-				<?php get_highest_rated( 'post', 10, 10 ); ?>
-			</table>
-
-			<h4><?php _e( 'Most Votes', 'wporg-showcase' ); ?></h4>
-			<table class="most-votes">
-				<?php get_most_rated(); ?>
-			</table>
 		</div>
 	</div>
