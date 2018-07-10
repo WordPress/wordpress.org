644caf755a62db06b1359923769e0b28ed89719e
obenland <obenland@git.wordpress.org>
2018-07-10 23:45:28 +0000

    Showcase: Cache bump
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@7417 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-showcase/header.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-showcase/header.php
@@ -3,7 +3,7 @@ $GLOBALS['pagetitle'] = wp_get_document_title();
 
 $prefix = is_ssl() ? 'https://' : 'http://s.';
 wp_enqueue_style( 'blog-wp4', $prefix.'wordpress.org/style/blog-wp4.css', array(), 4 );
-wp_enqueue_style( 'showcase', $prefix.'wordpress.org/wp-content/themes/pub/wporg-showcase/style.css', array(), 16 );
+wp_enqueue_style( 'showcase', $prefix.'wordpress.org/wp-content/themes/pub/wporg-showcase/style.css', array(), 17 );
 require WPORGPATH . 'header.php';
 ?>
 <div id="headline">
