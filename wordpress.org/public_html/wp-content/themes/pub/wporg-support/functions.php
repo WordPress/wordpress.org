6c71458266a0c847f22431f6ab3328ebbbe38bc8
obenland <obenland@git.wordpress.org>
2018-07-13 00:14:44 +0000

    Support: Cache bump
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@7455 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-support/functions.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-support/functions.php
@@ -20,7 +20,7 @@ add_filter( 'bbp_show_lead_topic', '__return_true' );
  */
 function wporg_support_scripts() {
 
-	wp_enqueue_style( 'forum-wp4-style', get_stylesheet_uri(), [], '20180712' );
+	wp_enqueue_style( 'forum-wp4-style', get_stylesheet_uri(), [], '20180713' );
 	wp_style_add_data( 'forum-wp4-style', 'rtl', 'replace' );
 
 	wp_enqueue_script( 'wporg-support-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151217', true );
