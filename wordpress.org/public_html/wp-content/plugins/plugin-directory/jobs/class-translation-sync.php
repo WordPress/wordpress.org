932e50e4f5bfe43152e6955308d469ff91772dab
Alex Shiels <tellyworth@git.wordpress.org>
2018-07-19 07:56:25 +0000

    Plugin dir: new file needed by translation sync job. Props @noisysocks
    See #3169
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@7477 74240141-8908-4e6f-9713-ba540dce6ec7

---
+++ wordpress.org/public_html/wp-content/plugins/plugin-directory/jobs/class-translation-sync.php
@@ -0,0 +1,41 @@
+<?php
+namespace WordPressdotorg\Plugin_Directory\Jobs;
+
+use WordPressdotorg\Plugin_Directory;
+
+/**
+ * Sync plugin translations from glotpress to each plugin's postmeta. This is
+ * intended to be run every day.
+ *
+ * @package WordPressdotorg\Plugin_Directory\Jobs
+ */
+class Translation_Sync {
+	/**
+	 * Cron trigger that syncs plugin translations from glotpress to each plugin's
+	 * postmeta.
+	 */
+	public static function cron_trigger() {
+		$args = array(
+			'post_type' => 'plugin',
+			'posts_per_page' => 100,
+			'offset' => 0,
+		);
+
+		$directory = Plugin_Directory\Plugin_Directory::instance();
+
+		while ( $posts = get_posts( $args ) ) {
+			foreach ( $posts as $post ) {
+				$directory->sync_all_translations_to_meta( $post->ID );
+			}
+
+			// Make sure the cache doesn't exhaust memory
+			global $wp_object_cache;
+			if ( is_object( $wp_object_cache ) ) {
+				$wp_object_cache->cache = array();
+				$wp_object_cache->stats = array();
+			}
+
+			$args['offset'] += $args['posts_per_page'];
+		}
+	}
+}
