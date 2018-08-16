2d898fc9c332b140201e1c4e917232f43182093b
obenland <obenland@git.wordpress.org>
2018-08-16 17:19:19 +0000

    Main: Enable links to sha1 hashes on Rosetta sites
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@7616 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-main/inc/template-tags.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-main/inc/template-tags.php
@@ -15,18 +15,17 @@ namespace WordPressdotorg\MainTheme;
  * @param array $release Release to be processed.
  */
 function release_row( $release ) {
-	$is_rosetta = defined( 'IS_ROSETTA_NETWORK' ) && IS_ROSETTA_NETWORK;
 	?>
 	<tr>
 		<td><?php echo esc_html( $release['version'] ); ?></td>
 		<td><?php echo esc_html( date_i18n( get_option( 'date_format' ), $release['builton'] ) ); ?></td>
 		<td><a href="<?php echo esc_url( $release['zip_url'] ); ?>">zip</a><br>
-			<small>(<a href="<?php echo esc_url( $release['zip_url'] . '.md5' ); ?>">md5</a><?php if ( ! $is_rosetta ) : ?> | <a href="<?php echo esc_url( $release['zip_url'] . '.sha1' ); ?>">sha1</a><?php endif; ?>)</small>
+			<small>(<a href="<?php echo esc_url( $release['zip_url'] . '.md5' ); ?>">md5</a> | <a href="<?php echo esc_url( $release['zip_url'] . '.sha1' ); ?>">sha1</a>)</small>
 		</td>
 		<td><a href="<?php echo esc_url( $release['targz_url'] ); ?>">tar.gz</a><br>
-			<small>(<a href="<?php echo esc_url( $release['targz_url'] . '.md5' ); ?>">md5</a><?php if ( ! $is_rosetta ) : ?> | <a href="<?php echo esc_url( $release['targz_url'] . '.sha1' ); ?>">sha1</a><?php endif; ?>)</small>
+			<small>(<a href="<?php echo esc_url( $release['targz_url'] . '.md5' ); ?>">md5</a> | <a href="<?php echo esc_url( $release['targz_url'] . '.sha1' ); ?>">sha1</a>)</small>
 		</td>
-		<?php if ( ! $is_rosetta ) : ?>
+		<?php if ( ! defined( 'IS_ROSETTA_NETWORK' ) || ! IS_ROSETTA_NETWORK ) : ?>
 			<td>
 				<?php if ( ! empty( $release['iis_url'] ) ) : ?>
 					<a href="<?php echo esc_url( $release['iis_url'] ); ?>">IIS zip</a><br>
