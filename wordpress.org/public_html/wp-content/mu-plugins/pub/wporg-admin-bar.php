43b76d9ec930553a2144c4f0ffcac23e99b1bf0f
Dion Hulse <dd32@git.wordpress.org>
2018-09-19 03:33:33 +0000

    Toolbar: Link the (w) and (w)->About links to the localised WordPress.org About pages.
    Previously the logged out versions of these were redirected to `/about/` which didn't make sense on the make blog or other non-rosetta subdomains.
    
    Fixes #3812.
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@7674 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/mu-plugins/pub/wporg-admin-bar.php
+++ wordpress.org/public_html/wp-content/mu-plugins/pub/wporg-admin-bar.php
@@ -4,24 +4,26 @@
  */
 
 /**
- * Makes Logo and About menu items link to the w.org/about/ page for logged-out users.
+ * Makes Logo and About menu items link to the localised w.org/about/ page for all users.
  *
  * @param WP_Admin_Bar $wp_admin_bar WP_Admin_Bar instance, passed by reference.
  */
-function wporg_logged_out_admin_bar( $wp_admin_bar ) {
-	if ( is_user_logged_in() ) {
-		return;
-	}
-
+function wporg_about_links_in_admin_bar( $wp_admin_bar ) {
 	$nodes = [
 		$wp_admin_bar->get_node( 'wp-logo' ),
 		$wp_admin_bar->get_node( 'about' ),
 	];
 
+	$about_url = 'https://wordpress.org/about/';
+
+	if ( class_exists( 'Rosetta_Sites' ) && is_object( $GLOBALS['rosetta'] ) ) {
+		$about_url = 'https://' . $GLOBALS['rosetta']->current_site_domain . '/about/';
+	}
+
 	foreach ( $nodes as $node ) {
-		$node->href = '/about/';
+		$node->href = $about_url;
 
 		$wp_admin_bar->add_node( $node );
 	}
 }
-add_action( 'admin_bar_menu', 'wporg_logged_out_admin_bar', 11 );
+add_action( 'admin_bar_menu', 'wporg_about_links_in_admin_bar', 11 );
