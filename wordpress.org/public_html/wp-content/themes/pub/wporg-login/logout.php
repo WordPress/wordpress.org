20b4c7ad44850d7a5e30f09fa592db0fd9bc0c19
Dion Hulse <dd32@git.wordpress.org>
2018-09-24 03:46:33 +0000

    Login: Allow the `/logout` route to accept a custom `?redirect_to` parameter. This will allow non-WordPress sites (Trac) to pass their current URL.
    See #3821.
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@7696 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-login/logout.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-login/logout.php
@@ -10,10 +10,14 @@ get_header();
 <p class="intro"><?php _e( 'You are attempting to log out of WordPress.org.', 'wporg' ); ?></p>
 
 <?php
+
+// This will be validated at redirect time.
+$redirect_to = !empty( $_GET['redirect_to'] ) ? $_GET['redirect_to'] : home_url( '/loggedout/' );
+
 printf(
 	/* translators: %s: logout URL */
 	__( 'Do you really want to <a href="%s">log out</a>?', 'wporg' ),
-	wp_logout_url( home_url( '/loggedout/ ' ) )
+	wp_logout_url( $redirect_to )
 );
 ?>
 
