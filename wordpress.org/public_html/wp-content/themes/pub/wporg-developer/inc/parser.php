bc6c1b1bf8aa0a1fabe17feaca69ff49d7b5739e
Scott Reilly <coffee2code@git.wordpress.org>
2018-10-04 20:26:59 +0000

    developer.wordpress.org: Add `DevHub_Parser::cache_source_code()` for pre-caching source code.
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@7714 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-developer/inc/parser.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-developer/inc/parser.php
@@ -25,6 +25,35 @@ class DevHub_Parser {
 		add_filter( 'wp_parser_skip_duplicate_hooks', '__return_true' );
 	}
 
+	/**
+	 * Pre-caches source for parsed post types that support showing source code.
+	 *
+	 * By default, source code gets imported and cached as needed.
+	 *
+	 * Primarily intended to be run as a commandline convenience script.
+	 *
+	 * @return bool True on sucess, false on failure.
+	 */
+	public static function cache_source_code() {
+		// Ensure the parsed code source directory exists.
+		$import_dir = get_option( 'wp_parser_root_import_dir' );
+		if ( ! $import_dir || ! file_exists( $import_dir ) ) {
+			echo "Unable to cache source code; import directory does not exist: {$import_dir}\n";
+			return false;
+		}
+
+		foreach ( \DevHub\get_post_types_with_source_code() as $post_type ) {
+			$posts = get_posts( array( 'fields' => 'ids', 'post_type' => $post_type, 'posts_per_page' => '-1' ) );
+			foreach ( $posts as $post ) {
+				echo '.';
+				\DevHub\get_source_code( $post, true );
+			}
+		}
+		echo "\n";
+
+		return true;
+	}
+
 } // DevHub_Parser
 
 DevHub_Parser::init();
