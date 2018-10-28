cff6c3c68b23d65f8fc51b8a952c3ea77f5b6b6f
Dominik Schilling (ocean90) <ocean90@git.wordpress.org>
2018-10-28 14:07:44 +0000

    Translate: Extend `wporg-translate make-core-pot` command to support generating POT files for WordPress 3.7+.
    See #3748.
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@7801 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/plugins/wporg-gp-customizations/inc/cli/class-make-core-pot.php
+++ wordpress.org/public_html/wp-content/plugins/wporg-gp-customizations/inc/cli/class-make-core-pot.php
@@ -12,6 +12,20 @@ class Make_Core_Pot extends WP_CLI_Command {
 	const PACKAGE_NAME = 'WordPress';
 
 	/**
+	 * WordPress directory to scan for string extraction.
+	 *
+	 * @var string
+	 */
+	protected $source;
+
+	/**
+	 * Directory to store resulting POT files.
+	 *
+	 * @var string
+	 */
+	protected $destination;
+
+	/**
 	 * Creates the POT files for WordPress core.
 	 *
 	 * ## OPTIONS
@@ -26,8 +40,17 @@ class Make_Core_Pot extends WP_CLI_Command {
 	 * : Run without creating POT files.
 	 */
 	public function __invoke( $args, $assoc_args ) {
-		$source      = trailingslashit( realpath( $args[0] ) );
-		$destination = trailingslashit( realpath( $args[1] ) );
+		$this->source      = realpath( $args[0] );
+		$this->destination = realpath( $args[1] );
+
+		$wp_version = $this->get_wp_version();
+		if ( ! $wp_version ) {
+			WP_CLI::error( 'WordPress version not found.' );
+		}
+
+		if ( version_compare( $wp_version, '3.7-beta', '<' ) ) {
+			WP_CLI::error( 'Unsupported WordPress version. Use makepot.php.' );
+		}
 
 		$dry_run = Utils\get_flag_value( $assoc_args, 'dry-run', false );
 
@@ -42,8 +65,8 @@ class Make_Core_Pot extends WP_CLI_Command {
 		);
 
 		// Continents and cities.
-		$command  = 'i18n make-pot ' . escapeshellarg( $source );
-		$command .= ' ' . escapeshellarg( $destination . 'wordpress-continents-cities.pot' );
+		$command  = 'i18n make-pot ' . escapeshellarg( $this->source );
+		$command .= ' ' . escapeshellarg( $this->destination . '/wordpress-continents-cities.pot' );
 		$command .= ' --include="wp-admin/includes/continents-cities.php"';
 		$command .= ' --package-name=' . escapeshellarg( self::PACKAGE_NAME );
 		$command .= ' --headers=' . escapeshellarg( $headers );
@@ -55,8 +78,8 @@ class Make_Core_Pot extends WP_CLI_Command {
 		! $dry_run && WP_CLI::runcommand( $command/*, [ 'launch' => false ]*/ );
 
 		// Front end.
-		$command  = 'i18n make-pot ' . escapeshellarg( $source );
-		$command .= ' ' . escapeshellarg( $destination . 'wordpress.pot' );
+		$command  = 'i18n make-pot ' . escapeshellarg( $this->source );
+		$command .= ' ' . escapeshellarg( $this->destination . '/wordpress.pot' );
 		$command .= ' --exclude="wp-admin/*,wp-content/themes/*,wp-includes/class-pop3.php,wp-content/plugins/akismet/"';
 		$command .= ' --package-name=' . escapeshellarg( self::PACKAGE_NAME );
 		$command .= ' --headers=' . escapeshellarg( $headers );
@@ -70,7 +93,7 @@ class Make_Core_Pot extends WP_CLI_Command {
 		// Hello Dolly, included in admin.
 		$hello_dolly_pot = wp_tempnam( 'hello-dolly.pot' );
 
-		$command  = 'i18n make-pot ' . escapeshellarg( $source . 'wp-content/plugins' );
+		$command  = 'i18n make-pot ' . escapeshellarg( $this->source . '/wp-content/plugins' );
 		$command .= ' ' . escapeshellarg( $hello_dolly_pot );
 		$command .= ' --exclude="akismet/*"';
 		$command .= ' --include="hello.php"';
@@ -83,13 +106,26 @@ class Make_Core_Pot extends WP_CLI_Command {
 		WP_CLI::line( $command );
 		! $dry_run && WP_CLI::runcommand( $command/*, [ 'launch' => false ]*/ );
 
-		# Admin.
-		$command  = 'i18n make-pot ' . escapeshellarg( $source );
-		$command .= ' ' . escapeshellarg( $destination . 'wordpress-admin.pot' );
-		$command .= ' --exclude="wp-admin/network/*,wp-admin/network.php,wp-admin/includes/class-wp-ms*,wp-admin/includes/network.php,wp-admin/includes/continents-cities.php"';
+		// Admin.
+		$admin_network_files = [
+			'wp-admin/network/*',
+			'wp-admin/network.php',
+		];
+
+		// See https://core.trac.wordpress.org/ticket/34910.
+		if ( version_compare( $wp_version, '4.5-beta', '>=' ) ) {
+			$admin_network_files = array_merge( $admin_network_files, [
+				'wp-admin/includes/class-wp-ms*',
+				'wp-admin/includes/network.php',
+			] );
+		}
+
+		$command  = 'i18n make-pot ' . escapeshellarg( $this->source );
+		$command .= ' ' . escapeshellarg( $this->destination . '/wordpress-admin.pot' );
+		$command .= ' --exclude=' . escapeshellarg( implode( ',', array_merge( [ 'wp-admin/includes/continents-cities.php' ], $admin_network_files ) ) );
 		$command .= ' --include="wp-admin/*"';
 		$command .= ' --merge=' . escapeshellarg( $hello_dolly_pot );
-		$command .= ' --subtract=' . escapeshellarg( $destination . 'wordpress.pot' );
+		$command .= ' --subtract=' . escapeshellarg( $this->destination . '/wordpress.pot' );
 		$command .= ' --package-name=' . escapeshellarg( self::PACKAGE_NAME );
 		$command .= ' --headers=' . escapeshellarg( $headers );
 		$command .= ' --file-comment=' . escapeshellarg( $file_comment );
@@ -101,11 +137,11 @@ class Make_Core_Pot extends WP_CLI_Command {
 
 		unlink( $hello_dolly_pot );
 
-		# Admin Network.
-		$command  = 'i18n make-pot ' . escapeshellarg( $source );
-		$command .= ' ' . escapeshellarg( $destination . 'wordpress-admin-network.pot' );
-		$command .= ' --include="wp-admin/network/*,wp-admin/network.php,wp-admin/includes/class-wp-ms*,wp-admin/includes/network.php"';
-		$command .= ' --subtract=' . escapeshellarg( sprintf( '%1$swordpress.pot,%1$swordpress-admin.pot', $destination ) );
+		// Admin Network.
+		$command  = 'i18n make-pot ' . escapeshellarg( $this->source );
+		$command .= ' ' . escapeshellarg( $this->destination . '/wordpress-admin-network.pot' );
+		$command .= ' --include=' . escapeshellarg( implode( ',', $admin_network_files ) );
+		$command .= ' --subtract=' . escapeshellarg( sprintf( '%1$s/wordpress.pot,%1$s/wordpress-admin.pot', $this->destination ) );
 		$command .= ' --package-name=' . escapeshellarg( self::PACKAGE_NAME );
 		$command .= ' --headers=' . escapeshellarg( $headers );
 		$command .= ' --file-comment=' . escapeshellarg( $file_comment );
@@ -114,5 +150,19 @@ class Make_Core_Pot extends WP_CLI_Command {
 
 		WP_CLI::line( $command );
 		! $dry_run && WP_CLI::runcommand( $command/*, [ 'launch' => false ]*/ );
+	}
+
+	/**
+	 * Extracts the WordPress version number from wp-includes/version.php.
+	 *
+	 * @return string|false Version number on success, false otherwise.
+	 */
+	private function get_wp_version() {
+		$version_php = $this->source . '/wp-includes/version.php';
+		if ( ! file_exists( $version_php ) || ! is_readable( $version_php ) ) {
+			return false;
+		}
+
+		return preg_match( '/\$wp_version\s*=\s*\'(.*?)\';/', file_get_contents( $version_php ), $matches ) ? $matches[1] : false;
 	}
 }
