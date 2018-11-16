034b89b3e7b02c741125a9c5cdc53a0677de0a87
Scott Reilly <coffee2code@git.wordpress.org>
2018-11-16 22:10:17 +0000

    Gutenberg: Bump CSS version for cache bust.
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@7862 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/gutenberg/functions.php
+++ wordpress.org/public_html/wp-content/themes/pub/gutenberg/functions.php
@@ -431,7 +431,7 @@ function gutenbergtheme_fonts_url() {
  * Enqueue scripts and styles.
  */
 function gutenbergtheme_scripts() {
-	wp_enqueue_style( 'gutenbergtheme-style', get_stylesheet_uri(), [], 9 );
+	wp_enqueue_style( 'gutenbergtheme-style', get_stylesheet_uri(), [], 10 );
 
 	wp_enqueue_style( 'gutenbergthemeblocks-style', get_template_directory_uri() . '/blocks.css');
 
