a271c972f8fb720185cc6475437f67369870c482
Dion Hulse <dd32@git.wordpress.org>
2019-01-07 02:41:41 +0000

    Gutenebrg Theme: Style the 404 page, and add a link to the Gutenberg handbook directly.
    Props mukesh27 for initial patch.
    Fixes #4043.
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@8029 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/gutenberg/404.php
+++ wordpress.org/public_html/wp-content/themes/pub/gutenberg/404.php
@@ -18,38 +18,16 @@ get_header(); ?>
 				</header><!-- .page-header -->
 
 				<div class="page-content">
-					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'gutenbergtheme' ); ?></p>
+					<p><?php
+						printf(
+							__( 'It looks like nothing was found at this location. Maybe try the <a href="%s">Gutenberg Handbook</a> or a search?', 'gutenbergtheme' ),
+							home_url( '/handbook/' )
+						);
+					?></p>
 
 					<?php
 						get_search_form();
-
-						the_widget( 'WP_Widget_Recent_Posts' );
 					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'gutenbergtheme' ); ?></h2>
-						<ul>
-						<?php
-							wp_list_categories( array(
-								'orderby'    => 'count',
-								'order'      => 'DESC',
-								'show_count' => 1,
-								'title_li'   => '',
-								'number'     => 10,
-							) );
-						?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-
-						/* translators: %1$s: smiley */
-						$archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'gutenbergtheme' ), convert_smilies( ':)' ) ) . '</p>';
-						the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
-
-						the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
-
 				</div><!-- .page-content -->
 			</section><!-- .error-404 -->
 
