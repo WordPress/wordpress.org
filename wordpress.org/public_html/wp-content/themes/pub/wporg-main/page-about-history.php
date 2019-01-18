9706ac1b9db2cc9b3290d375239634ad4258da91
Sergey Biryukov <sergeybiryukov@git.wordpress.org>
2019-01-18 10:07:39 +0000

    WordPress.org Main Theme: Move release history table from Roadmap to History page.
    Props joostdevalk.
    Fixes #4064.
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@8098 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-main/page-about-history.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-main/page-about-history.php
@@ -65,6 +65,197 @@ the_post();
 							</a>
 						</li>
 					</ul>
+
+					<h3><?php esc_html_e( 'Jazzers and Release Dates', 'wporg' ); ?></h3>
+
+					<p>
+						<?php
+						/* translators: 1: Link to playlist */
+						printf( wp_kses_post( __( 'WordPress core developers share a love of jazz music, and all our major releases are named in honor of jazz musicians we personally admire. Here&#8217;s a list of releases and the musicians they were named for. <a href="%s">You can listen to a Last.fm station of all the musicians we named a release for</a>.', 'wporg' ) ), esc_url( 'http://www.last.fm/tag/wordpress-release-jazz' ) );
+						?>
+					</p>
+
+					<table>
+						<thead>
+							<tr>
+								<th><?php esc_html_e( 'Version', 'wporg' ); ?></th>
+								<th><?php esc_html_e( 'Musician', 'wporg' ); ?></th>
+								<th><?php esc_html_e( 'Date', 'wporg' ); ?></th>
+							</tr>
+						</thead>
+						<tbody>
+						<tr>
+							<th><a href="https://wordpress.org/news/2003/05/wordpress-now-available/">.70</a></th>
+							<td><?php esc_html_e( 'No musician chosen.', 'wporg' ); ?></td>
+							<td><?php echo esc_html( date_i18n( $date_format, strtotime( 'May 27, 2003' ) ) ); ?></td>
+						</tr>
+						<tr>
+							<th><a href="https://wordpress.org/news/2004/01/wordpress-10/">1.0</a></th>
+							<td>Miles Davis</td>
+							<td><?php echo esc_html( date_i18n( $date_format, strtotime( 'January 3, 2004' ) ) ); ?></td>
+						</tr>
+						<tr>
+							<th><a href="https://wordpress.org/news/2004/05/heres-the-beef/">1.2</a></th>
+							<td>Charles Mingus</td>
+							<td><?php echo esc_html( date_i18n( $date_format, strtotime( 'May 22, 2004' ) ) ); ?></td>
+						</tr>
+						<tr>
+							<th><a href="https://wordpress.org/news/2005/02/strayhorn/">1.5</a></th>
+							<td>Billy Strayhorn</td>
+							<td><?php echo esc_html( date_i18n( $date_format, strtotime( 'February 17, 2005' ) ) ); ?></td>
+						</tr>
+						<tr>
+							<th><a href="https://wordpress.org/news/2005/12/wp2/">2.0</a></th>
+							<td>Duke Ellington</td>
+							<td><?php echo esc_html( date_i18n( $date_format, strtotime( 'December 31, 2005' ) ) ); ?></td>
+						</tr>
+						<tr>
+							<th><a href="https://wordpress.org/news/2007/01/ella-21/">2.1</a></th>
+							<td>Ella Fitzgerald</td>
+							<td><?php echo esc_html( date_i18n( $date_format, strtotime( 'January 22, 2007' ) ) ); ?></td>
+						</tr>
+						<tr>
+							<th><a href="https://wordpress.org/news/2007/05/wordpress-22/">2.2</a></th>
+							<td>Stan Getz</td>
+							<td><?php echo esc_html( date_i18n( $date_format, strtotime( 'May 16, 2007' ) ) ); ?></td>
+						</tr>
+						<tr>
+							<th><a href="https://wordpress.org/news/2007/09/wordpress-23/">2.3</a></th>
+							<td>Dexter Gordon</td>
+							<td><?php echo esc_html( date_i18n( $date_format, strtotime( 'September 24, 2007' ) ) ); ?></td>
+						</tr>
+						<tr>
+							<th><a href="https://wordpress.org/news/2008/03/wordpress-25-brecker/">2.5</a></th>
+							<td>Michael Brecker</td>
+							<td><?php echo esc_html( date_i18n( $date_format, strtotime( 'March 29, 2008' ) ) ); ?></td>
+						</tr>
+						<tr>
+							<th><a href="https://wordpress.org/news/2008/07/wordpress-26-tyner/">2.6</a></th>
+							<td>McCoy Tyner</td>
+							<td><?php echo esc_html( date_i18n( $date_format, strtotime( 'July 15, 2008' ) ) ); ?></td>
+						</tr>
+						<tr>
+							<th><a href="https://wordpress.org/news/2008/12/coltrane/">2.7</a></th>
+							<td>John Coltrane</td>
+							<td><?php echo esc_html( date_i18n( $date_format, strtotime( 'December 10, 2008' ) ) ); ?></td>
+						</tr>
+						<tr>
+							<th><a href="https://wordpress.org/news/2009/06/wordpress-28/">2.8</a></th>
+							<td>Chet Baker</td>
+							<td><?php echo esc_html( date_i18n( $date_format, strtotime( 'June 11, 2009' ) ) ); ?></td>
+						</tr>
+						<tr>
+							<th><a href="https://wordpress.org/news/2009/12/wordpress-2-9/">2.9</a></th>
+							<td>Carmen McRae</td>
+							<td><?php echo esc_html( date_i18n( $date_format, strtotime( 'December 18, 2009' ) ) ); ?></td>
+						</tr>
+						<tr>
+							<th><a href="https://wordpress.org/news/2010/06/thelonious/">3.0</a></th>
+							<td>Thelonious Monk</td>
+							<td><?php echo esc_html( date_i18n( $date_format, strtotime( 'June 17, 2010' ) ) ); ?></td>
+						</tr>
+						<tr>
+							<th><a href="https://wordpress.org/news/2011/02/threeone/">3.1</a></th>
+							<td>Django Reinhardt</td>
+							<td><?php echo esc_html( date_i18n( $date_format, strtotime( 'February 23, 2011' ) ) ); ?></td>
+						</tr>
+						<tr>
+							<th><a href="https://wordpress.org/news/2011/07/gershwin/">3.2</a></th>
+							<td>George Gershwin</td>
+							<td><?php echo esc_html( date_i18n( $date_format, strtotime( 'July 4, 2011' ) ) ); ?></td>
+						</tr>
+						<tr>
+							<th><a href="https://wordpress.org/news/2011/12/sonny/">3.3</a></th>
+							<td>Sonny Stitt</td>
+							<td><?php echo esc_html( date_i18n( $date_format, strtotime( 'December 12, 2011' ) ) ); ?></td>
+						</tr>
+						<tr>
+							<th><a href="https://wordpress.org/news/2012/06/green/">3.4</a></th>
+							<td>Grant Green</td>
+							<td><?php echo esc_html( date_i18n( $date_format, strtotime( 'June 13, 2012' ) ) ); ?></td>
+						</tr>
+						<tr>
+							<th><a href="https://wordpress.org/news/2012/12/elvin/">3.5</a></th>
+							<td>Elvin Jones</td>
+							<td><?php echo esc_html( date_i18n( $date_format, strtotime( 'December 11, 2012' ) ) ); ?></td>
+						</tr>
+						<tr>
+							<th><a href="https://wordpress.org/news/2013/08/oscar/">3.6</a></th>
+							<td>Oscar Peterson</td>
+							<td><?php echo esc_html( date_i18n( $date_format, strtotime( 'August 1, 2013' ) ) ); ?></td>
+						</tr>
+						<tr class="alt">
+							<th><a href="https://wordpress.org/news/2013/10/basie/">3.7</a></th>
+							<td>Count Basie</td>
+							<td><?php echo esc_html( date_i18n( $date_format, strtotime( 'October 24, 2013' ) ) ); ?></td>
+						</tr>
+						<tr>
+							<th><a href="https://wordpress.org/news/2013/12/parker/">3.8</a></th>
+							<td>Charlie Parker</td>
+							<td><?php echo esc_html( date_i18n( $date_format, strtotime( 'December 12, 2013' ) ) ); ?></td>
+						</tr>
+						<tr>
+							<th><a href="https://wordpress.org/news/2014/04/smith/">3.9</a></th>
+							<td>Jimmy Smith</td>
+							<td><?php echo esc_html( date_i18n( $date_format, strtotime( 'April 16, 2014' ) ) ); ?></td>
+						</tr>
+						<tr>
+							<th><a href="https://wordpress.org/news/2014/09/benny/">4.0</a></th>
+							<td>Benny Goodman</td>
+							<td><?php echo esc_html( date_i18n( $date_format, strtotime( 'September 4, 2014' ) ) ); ?></td>
+						</tr>
+						<tr>
+							<th><a href="https://wordpress.org/news/2014/12/dinah/">4.1</a></th>
+							<td>Dinah Washington</td>
+							<td><?php echo esc_html( date_i18n( $date_format, strtotime( 'December 18, 2014' ) ) ); ?></td>
+						</tr>
+						<tr>
+							<th><a href="https://wordpress.org/news/2015/04/powell/">4.2</a></th>
+							<td>Bud Powell</td>
+							<td><?php echo esc_html( date_i18n( $date_format, strtotime( 'April 23, 2015' ) ) ); ?></td>
+						</tr>
+						<tr>
+							<th><a href="https://wordpress.org/news/2015/08/billie/">4.3</a></th>
+							<td>Billie Holiday</td>
+							<td><?php echo esc_html( date_i18n( $date_format, strtotime( 'August 18, 2015' ) ) ); ?></td>
+						</tr>
+						<tr>
+							<th><a href="https://wordpress.org/news/2015/12/clifford/">4.4</a></th>
+							<td>Clifford Brown</td>
+							<td><?php echo esc_html( date_i18n( $date_format, strtotime( 'December 8, 2015' ) ) ); ?></td>
+						</tr>
+						<tr>
+							<th><a href="https://wordpress.org/news/2016/04/coleman/">4.5</a></th>
+							<td>Coleman Hawkins</td>
+							<td><?php echo esc_html( date_i18n( $date_format, strtotime( 'April 12, 2016' ) ) ); ?></td>
+						</tr>
+						<tr>
+							<th><a href="https://wordpress.org/news/2016/08/pepper/">4.6</a></th>
+							<td>Pepper Adams</td>
+							<td><?php echo esc_html( date_i18n( $date_format, strtotime( 'August 16, 2016' ) ) ); ?></td>
+						</tr>
+						<tr>
+							<th><a href="https://wordpress.org/news/2016/12/vaughan/">4.7</a></th>
+							<td>Sarah Vaughan</td>
+							<td><?php echo esc_html( date_i18n( $date_format, strtotime( 'December 6, 2016' ) ) ); ?></td>
+						</tr>
+						<tr>
+							<th><a href="https://wordpress.org/news/2017/06/evans/">4.8</a></th>
+							<td>Bill Evans</td>
+							<td><?php echo esc_html( date_i18n( $date_format, strtotime( 'June 8, 2017' ) ) ); ?></td>
+						</tr>
+						<tr>
+							<th><a href="https://wordpress.org/news/2017/11/tipton/">4.9</a></th>
+							<td>Billy Tipton</td>
+							<td><?php echo esc_html( date_i18n( $date_format, strtotime( 'November 15, 2017' ) ) ); ?></td>
+						</tr>
+						<tr>
+							<th><a href="https://wordpress.org/news/2018/12/bebo/">5.0</a></th>
+							<td>Bebo Valdés</td>
+							<td><?php echo esc_html( date_i18n( $date_format, strtotime( 'December 6, 2018' ) ) ); ?></td>
+						</tr>
+						</tbody>
+					</table>
 				</section>
 			</div><!-- .entry-content -->
 		</article><!-- #post-## -->
