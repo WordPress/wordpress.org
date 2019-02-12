cfc921857ff83c19eb97032d5fca642eaf8ef56b
Scott Reilly <coffee2code@git.wordpress.org>
2019-02-12 22:24:11 +0000

    Jobs Theme: Update URLs to HTTPS where possible or to new URLs for those that now redirect.
    Props umangvaghela123, coffee2code.
    See #3120.
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@8232 74240141-8908-4e6f-9713-ba540dce6ec7

--- jobs.wordpress.net/public_html/wp-content/themes/jobswp/functions.php
+++ jobs.wordpress.net/public_html/wp-content/themes/jobswp/functions.php
@@ -37,7 +37,7 @@ function jobswp_setup() {
 	/**
 	 * Enable support for Post Thumbnails on posts and pages
 	 *
-	 * @link http://codex.wordpress.org/Function_Reference/add_theme_support#Post_Thumbnails
+	 * @link https://developer.wordpress.org/reference/functions/add_theme_support/#post-thumbnails
 	 */
 	//add_theme_support( 'post-thumbnails' );
 
