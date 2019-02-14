039380f41f916e136d3334350a326b7a10731de7
Alex Shiels <tellyworth@git.wordpress.org>
2019-02-14 00:17:58 +0000

    Main: move skip-to-content link so it's the first focusable element.
    Needs CSS work.
    
    See #4109
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@8241 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg/header.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg/header.php
@@ -11,9 +11,10 @@
 
 namespace WordPressdotorg\Theme;
 
+$GLOBALS['wporg_global_header_options'] = array(
+	'in_wrapper' => '<a class="skip-link screen-reader-text" href="#content">' . esc_html( 'Skip to content', 'wporg' ) . '</a>',
+);
 get_template_part( 'header', 'wporg' );
 ?>
 <div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#main"><?php esc_html_e( 'Skip to content', 'wporg' ); ?></a>
-
 	<div id="content" class="site-content row gutters">
