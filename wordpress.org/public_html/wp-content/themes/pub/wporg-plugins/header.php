9de8b7f92e19a95f9834192af2e8c29c34a6d437
Alex Shiels <tellyworth@git.wordpress.org>
2019-02-14 05:15:39 +0000

    Plugins theme: don't overwrite in_wrapper if already set.
    See #4174, #4109
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@8248 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-plugins/header.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-plugins/header.php
@@ -18,9 +18,10 @@ $menu_items = array(
 );
 
 $GLOBALS['pagetitle'] = wp_get_document_title();
-$GLOBALS['wporg_global_header_options'] = array(
-	'in_wrapper' => '<a class="skip-link screen-reader-text" href="#main">' . esc_html( 'Skip to content', 'wporg-plugins' ) . '</a>',
-);
+global $wporg_global_header_options;
+if ( !isset( $wporg_global_header_options['in_wrapper'] ) )
+	$wporg_global_header_options['in_wrapper'] = '';
+$wporg_global_header_options['in_wrapper'] .= '<a class="skip-link screen-reader-text" href="#main">' . esc_html( 'Skip to content', 'wporg-plugins' ) . '</a>';
 require WPORGPATH . 'header.php';
 ?>
 <div id="page" class="site">
