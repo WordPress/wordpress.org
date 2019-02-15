299f3117e97af65d31e6b6fa81bc3fb06658bf9c
Alex Shiels <tellyworth@git.wordpress.org>
2019-02-15 08:14:13 +0000

    Themes: should be esc_html__(), not esc_html()
    See #4109
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@8259 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-themes/header.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-themes/header.php
@@ -9,7 +9,7 @@ $GLOBALS['pagetitle'] = wp_get_document_title();
 global $wporg_global_header_options;
 if ( !isset( $wporg_global_header_options['in_wrapper'] ) )
 	$wporg_global_header_options['in_wrapper'] = '';
-$wporg_global_header_options['in_wrapper'] .= '<a class="skip-link screen-reader-text" href="#themes">' . esc_html( 'Skip to content', 'wporg-themes' ) . '</a>';
+$wporg_global_header_options['in_wrapper'] .= '<a class="skip-link screen-reader-text" href="#themes">' . esc_html__( 'Skip to content', 'wporg-themes' ) . '</a>';
 
 require WPORGPATH . 'header.php';
 ?>
