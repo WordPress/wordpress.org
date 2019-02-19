e9ad0c495f060235908f17637e7404367394052d
Dion Hulse <dd32@git.wordpress.org>
2019-02-19 04:45:34 +0000

    Login: Add Google Tag Manager.
    Fixes #4185.
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@8276 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-login/header.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-login/header.php
@@ -13,9 +13,16 @@
 <title><?php _e( 'WordPress.org Login', 'wporg' ); ?></title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-P24PF4B');</script>
+
 <?php wp_head(); ?>
 </head>
 <body <?php body_class( 'wp-core-ui login' ); ?>>
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P24PF4B" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 
 <div id="login">
 	<h1><a href="https://wordpress.org/" title="WordPress.org" tabindex="-1"><?php _e( 'WordPress.org Login', 'wporg' ); ?></a></h1>
