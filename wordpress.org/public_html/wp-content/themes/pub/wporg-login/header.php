e542a53f1634e39fd444818911a2a5b1a3dec7d0
Scott Reilly <coffee2code@git.wordpress.org>
2019-02-19 21:21:11 +0000

    Login: Add DNS prefetch for www.googletagmanager.com.
    See #4189.
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@8278 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-login/header.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-login/header.php
@@ -12,6 +12,7 @@
 <meta http-equiv="x-ua-compatible" content="ie=edge">
 <title><?php _e( 'WordPress.org Login', 'wporg' ); ?></title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="dns-prefetch" href="//www.googletagmanager.com">
 
 <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
