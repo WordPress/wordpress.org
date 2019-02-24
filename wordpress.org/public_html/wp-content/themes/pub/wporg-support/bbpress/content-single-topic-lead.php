ca19a012bdea583f731c04c50a9b004e789a328f
SergeyBiryukov <SergeyBiryukov@74240141-8908-4e6f-9713-ba540dce6ec7>
2019-02-24 06:31:01 +0000

    Support Theme: Remove extra `<br />` tag in topic author block added in [7518].
    See #2576.
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@8334 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-support/bbpress/content-single-topic-lead.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-support/bbpress/content-single-topic-lead.php
@@ -49,10 +49,8 @@ if ( bbp_is_single_view() || bbp_is_search_results() ) : ?>
 				<?php bbp_user_nicename( bbp_get_topic_author_id(), array( 'before' => '<p class="bbp-user-nicename">(@', 'after' => ')</p><br />' ) ); ?>
 
 				<?php if ( current_user_can( 'moderate', bbp_get_topic_id() ) && 'bbp_blocked' === bbp_get_user_role( bbp_get_topic_author_id() ) ) : ?>
-					<p class="wporg-bbp-user-is-blocked">[<?php esc_attr_e( 'This user is blocked', 'wporg-forums' ); ?>]</p>
+					<p class="wporg-bbp-user-is-blocked">[<?php esc_attr_e( 'This user is blocked', 'wporg-forums' ); ?>]</p><br />
 				<?php endif; ?>
-
-				<br>
 
 				<?php if ( $title = get_user_option( 'title', bbp_get_topic_author_id() ) ) : ?>
 
