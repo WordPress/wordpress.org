6fd58ba9a072ecc8bcf825de864fdb9c54a1016f
Dion Hulse <dd32@git.wordpress.org>
2019-03-01 07:43:24 +0000

    WordPress.TV: Add GTM.
    Fixes #4207.
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@8377 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.tv/public_html/wp-content/themes/wptv2/header.php
+++ wordpress.tv/public_html/wp-content/themes/wptv2/header.php
@@ -15,6 +15,12 @@ global $wptv;
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+	})(window,document,'script','dataLayer','GTM-P24PF4B');</script>
+
 	<link rel="alternate" type="application/rss+xml" title="<?php esc_attr_e( 'WordPress.tv RSS Feed', 'wptv' ); ?>" href="https://wordpress.tv/feed/" />
 	<link rel="alternate" type="application/rss+xml" title="<?php esc_attr_e( 'WordPress.tv Blog RSS Feed', 'wptv' ); ?>" href="https://blog.wordpress.tv/feed/" />
 	<link rel="profile" href="http://gmpg.org/xfn/11">
@@ -26,6 +32,7 @@ global $wptv;
 </head>
 
 <body <?php body_class(); ?>>
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P24PF4B" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <div id="page">
 	<div id="header">
 		<div class="sleeve">
