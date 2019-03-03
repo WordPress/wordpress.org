d16e0aed6fd748e7132532e5f86fba9eb581645a
Dominik Schilling (ocean90) <ocean90@git.wordpress.org>
2019-03-03 16:38:21 +0000

    Translate: Add templates to start customizing default translation rows.
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@8386 74240141-8908-4e6f-9713-ba540dce6ec7

---
+++ wordpress.org/public_html/wp-content/plugins/wporg-gp-customizations/templates/translation-row.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Template for a single translation row in a translation set display.
+ */
+
+gp_tmpl_load( 'translation-row-preview', get_defined_vars() );
+gp_tmpl_load( 'translation-row-editor', get_defined_vars() );
