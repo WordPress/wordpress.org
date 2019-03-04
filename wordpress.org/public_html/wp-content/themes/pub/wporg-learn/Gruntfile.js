79b3166a38834ee223c2db740cf9b7a944b5223f
Dion Hulse <dd32@git.wordpress.org>
2019-03-04 05:50:34 +0000

    Learn: Minify CSS.
    See #4233.
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@8391 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-learn/Gruntfile.js
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-learn/Gruntfile.js
@@ -22,7 +22,10 @@ module.exports = function( grunt ) {
 						],
 						cascade: false
 					} ),
-					require( 'pixrem' )
+					require( 'pixrem' ),
+					require('cssnano')({
+						mergeRules: false
+					})
 				]
 			},
 			dist: {
