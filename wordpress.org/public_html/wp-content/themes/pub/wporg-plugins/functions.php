be2c9a0e5fdbbba936267eefdc8fc4caf053e4c8
Dion Hulse <dd32@git.wordpress.org>
2019-03-04 06:09:36 +0000

    WordPress.org Main, Plugins, and Themes themes: Bump cache buster after [8392].
    Fixes #4233.
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@8393 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-plugins/functions.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-plugins/functions.php
@@ -70,7 +70,7 @@ add_action( 'after_setup_theme', __NAMESPACE__ . '\content_width', 0 );
  * Enqueue scripts and styles.
  */
 function scripts() {
-	wp_enqueue_style( 'wporg-style', get_theme_file_uri( '/css/style.css' ), [ 'dashicons', 'open-sans' ], '20190107' );
+	wp_enqueue_style( 'wporg-style', get_theme_file_uri( '/css/style.css' ), [ 'dashicons', 'open-sans' ], '20190304' );
 	wp_style_add_data( 'wporg-style', 'rtl', 'replace' );
 
 	// Make jQuery a footer script.
