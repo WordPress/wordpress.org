df433222452b7775655b63c65cc0957f70a5b2d3
Sergey Biryukov <sergeybiryukov@git.wordpress.org>
2019-03-16 21:35:34 +0000

    Plugin Directory, Readme Validator: Clean up some strings to be less confusing to non-native English speakers.
    Props Ipstenu.
    Fixes #4291.
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@8469 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/plugins/plugin-directory/readme/class-validator.php
+++ wordpress.org/public_html/wp-content/plugins/plugin-directory/readme/class-validator.php
@@ -31,8 +31,11 @@ class Validator {
 		$url = esc_url_raw( $url );
 
 		if ( strtolower( substr( $url, -10 ) ) != 'readme.txt' ) {
-			/* translators: %s: readme.txt */
-			$error = sprintf( __( 'URL must end in %s!', 'wporg-plugins' ), '<code>readme.txt</code>' );
+			$error = sprintf(
+				/* translators: %s: readme.txt */
+				__( 'URL must end in %s!', 'wporg-plugins' ),
+				'<code>readme.txt</code>'
+			);
 			return array(
 				'errors' => array( $error ),
 			);
@@ -63,77 +66,118 @@ class Validator {
 
 		// Fatal errors.
 		if ( empty( $readme->name ) ) {
-			/* translators: 1: 'Plugin Name' section title, 2: 'Plugin Name' */
-			$errors[] = sprintf( __( 'No plugin name detected. Plugin names look like: %1$s. Please change %2$s to reflect the actual name of your plugin.', 'wporg-plugins' ), '<code>=== Plugin Name ===</code>', '<code>Plugin Name</code>' );
+			$errors[] = sprintf(
+				/* translators: 1: 'Plugin Name' section title, 2: 'Plugin Name' */
+				__( 'We cannot find a plugin name in your readme. Plugin names look like: %1$s. Please change %2$s to reflect the actual name of your plugin.', 'wporg-plugins' ),
+				'<code>=== Plugin Name ===</code>',
+				'<code>Plugin Name</code>'
+			);
 		}
 
 		// Warnings.
 		if ( isset( $readme->warnings['requires_header_ignored'] ) ) {
 			$latest_wordpress_version = defined( 'WP_CORE_STABLE_BRANCH' ) ? WP_CORE_STABLE_BRANCH : '5.0';
 
-			/* translators: 1: plugin header tag; 2: Example version 5.0. 3: Example version 4.9. */
 			$warnings[] = sprintf(
-				__( 'The Requires at least field was ignored. %1$s field should only contain a valid WordPress version such as %2$s or %3$s.', 'wporg-plugins' ),
+				/* translators: 1: plugin header tag; 2: Example version 5.0. 3: Example version 4.9. */
+				__( 'The %1$s field was ignored. This field should only contain a valid WordPress version such as %2$s or %3$s.', 'wporg-plugins' ),
 				'<code>Requires at least</code>',
 				'<code>' . number_format( $latest_wordpress_version, 1 ) . '</code>',
 				'<code>' . number_format( $latest_wordpress_version - 0.1, 1 ) . '</code>'
 			);
 		} elseif ( empty( $readme->requires ) ) {
-			/* translators: %s: plugin header tag */
-			$warnings[] = sprintf( __( '%s field is missing.', 'wporg-plugins' ), '<code>Requires at least</code>' );
+			$warnings[] = sprintf(
+				/* translators: %s: plugin header tag */
+				__( 'The %s field is missing.', 'wporg-plugins' ),
+				'<code>Requires at least</code>'
+			);
 		}
 
 		if ( isset( $readme->warnings['tested_header_ignored'] ) ) {
 			$latest_wordpress_version = defined( 'WP_CORE_STABLE_BRANCH' ) ? WP_CORE_STABLE_BRANCH : '5.0';
 
-			/* translators: 1: plugin header tag; 2: Example version 5.0. 3: Example version 5.1. */
 			$warnings[] = sprintf(
-				__( 'The Tested up to field was ignored. %1$s field should only contain a valid WordPress version such as %2$s or %3$s.', 'wporg-plugins' ),
+				/* translators: 1: plugin header tag; 2: Example version 5.0. 3: Example version 5.1. */
+				__( 'The %1$s field was ignored. This field should only contain a valid WordPress version such as %2$s or %3$s.', 'wporg-plugins' ),
 				'<code>Tested up to</code>',
 				'<code>' . number_format( $latest_wordpress_version, 1 ) . '</code>',
 				'<code>' . number_format( $latest_wordpress_version + 0.1, 1 ) . '</code>'
 			);
 		} elseif ( empty( $readme->tested ) ) {
-			/* translators: %s: plugin header tag */
-			$warnings[] = sprintf( __( '%s field is missing.', 'wporg-plugins' ), '<code>Tested up to</code>' );
+			$warnings[] = sprintf(
+				/* translators: %s: plugin header tag */
+				__( 'The %s field is missing.', 'wporg-plugins' ),
+				'<code>Tested up to</code>'
+			);
 		}
 
 		if ( isset( $readme->warnings['requires_php_header_ignored'] ) ) {
-			/* translators: 1: plugin header tag; 2: Example version 5.2.4. 3: Example version 7.0. */
-			$warnings[] = sprintf( __( 'The Requires PHP field was ignored. %1$s field should only contain a PHP version such as %2$s or %3$s.', 'wporg-plugins' ), '<code>Requires PHP</code>', '<code>5.2.4</code>', '<code>7.0</code>' );
+			$warnings[] = sprintf(
+				/* translators: 1: plugin header tag; 2: Example version 5.2.4. 3: Example version 7.0. */
+				__( 'The %1$s field was ignored. This field should only contain a PHP version such as %2$s or %3$s.', 'wporg-plugins' ),
+				'<code>Requires PHP</code>',
+				'<code>5.2.4</code>',
+				'<code>7.0</code>'
+			);
 		} elseif ( empty( $readme->requires_php ) ) {
-			/* translators: %s: plugin header tag */
-			$warnings[] = sprintf( __( '%s field is missing.', 'wporg-plugins' ), '<code>Requires PHP</code>' );
+			$warnings[] = sprintf(
+				/* translators: %s: plugin header tag */
+				__( 'The %s field is missing.', 'wporg-plugins' ),
+				'<code>Requires PHP</code>'
+			);
 		}
 		if ( empty( $readme->stable_tag ) ) {
-			/* translators: 1: 'Stable tag', 2: /trunk/ SVN directory, 3: 'Stable tag: trunk' */
-			$warnings[] = sprintf( __( '%1$s field is missing.  Hint: If you treat %2$s as stable, put %3$s.', 'wporg-plugins' ), '<code>Stable tag</code>', '<code>/trunk/</code>', '<code>Stable tag: trunk</code>' );
+			$warnings[] = sprintf(
+				/* translators: 1: 'Stable tag', 2: /trunk/ SVN directory, 3: 'Stable tag: trunk' */
+				__( 'The %1$s field is missing.  Hint: If you treat %2$s as stable, put %3$s.', 'wporg-plugins' ),
+				'<code>Stable tag</code>',
+				'<code>/trunk/</code>',
+				'<code>Stable tag: trunk</code>'
+			);
 		}
 
 		if ( isset( $readme->warnings['contributor_ignored'] ) ) {
-			/* translators: %s: plugin header tag */
-			$warnings[] = sprintf( __( 'One or more contributors listed were ignored. %s field should only contain WordPress.org usernames.', 'wporg-plugins' ), '<code>Contributors</code>' );
+			$warnings[] = sprintf(
+				/* translators: %s: plugin header tag */
+				__( 'One or more contributors listed were ignored. The %s field should only contain WordPress.org usernames. Remember that usernames are case-sensitive.', 'wporg-plugins' ),
+				'<code>Contributors</code>'
+			);
 		} elseif ( ! count( $readme->contributors ) ) {
-			/* translators: %s: plugin header tag */
-			$warnings[] = sprintf( __( '%s field is missing.', 'wporg-plugins' ), '<code>Contributors</code>' );
+			$warnings[] = sprintf(
+				/* translators: %s: plugin header tag */
+				__( 'The %s field is missing.', 'wporg-plugins' ),
+				'<code>Contributors</code>'
+			);
 		}
 
 		// Notes.
 		if ( empty( $readme->sections['faq'] ) ) {
-			/* translators: %s: section title */
-			$notes[] = sprintf( __( 'No %s section was found', 'wporg-plugins' ), '<code>== Frequently Asked Questions ==</code>' );
+			$notes[] = sprintf(
+				/* translators: %s: section title */
+				__( 'No %s section was found', 'wporg-plugins' ),
+				'<code>== Frequently Asked Questions ==</code>'
+			);
 		}
 		if ( empty( $readme->sections['changelog'] ) ) {
-			/* translators: %s: section title */
-			$notes[] = sprintf( __( 'No %s section was found', 'wporg-plugins' ), '<code>== Changelog ==</code>' );
+			$notes[] = sprintf(
+				/* translators: %s: section title */
+				__( 'No %s section was found', 'wporg-plugins' ),
+				'<code>== Changelog ==</code>'
+			);
 		}
 		if ( empty( $readme->upgrade_notice ) ) {
-			/* translators: %s: section title */
-			$notes[] = sprintf( __( 'No %s section was found', 'wporg-plugins' ), '<code>== Upgrade Notice ==</code>' );
+			$notes[] = sprintf(
+				/* translators: %s: section title */
+				__( 'No %s section was found', 'wporg-plugins' ),
+				'<code>== Upgrade Notice ==</code>'
+			);
 		}
 		if ( empty( $readme->screenshots ) ) {
-			/* translators: %s: section title */
-			$notes[] = sprintf( __( 'No %s section was found', 'wporg-plugins' ), '<code>== Screenshots ==</code>' );
+			$notes[] = sprintf(
+				/* translators: %s: section title */
+				__( 'No %s section was found', 'wporg-plugins' ),
+				'<code>== Screenshots ==</code>'
+			);
 		}
 		if ( empty( $readme->donate_link ) ) {
 			$notes[] = __( 'No donate link was found', 'wporg-plugins' );
