56edf7b268e47d2891c2c48c96b998b42b6de5df
Dion Hulse <dd32@git.wordpress.org>
2019-04-08 08:14:09 +0000

    Login: Add GTM code to more login pages.
    See #4371.
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@8606 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-login/functions.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-login/functions.php
@@ -83,6 +83,20 @@ function wporg_login_register_scripts() {
 }
 add_action( 'init', 'wporg_login_register_scripts' );
 
+function wporg_login_gtm() {
+	?>
+	<link rel="dns-prefetch" href="//www.googletagmanager.com">
+
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+	})(window,document,'script','dataLayer','GTM-P24PF4B');</script>
+	<?php
+}
+add_action( 'wp_head', 'wporg_login_gtm' );
+add_action( 'login_head', 'wporg_login_gtm' );
+
 /**
  * Avoid sending a 404 header but send a 200 with nocache headers.
  */
