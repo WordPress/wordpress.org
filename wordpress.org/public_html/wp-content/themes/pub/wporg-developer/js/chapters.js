5db327f813697b4d000624f1ab88c18310c9bce8
Scott Reilly <coffee2code@git.wordpress.org>
2019-06-03 20:46:22 +0000

    Developer: Add ability for second-level handbook sidebar menu items with sub-items to be collapsible/expandable.
    Nested menu items are all initially collapsed.
    
    Props welcher.
    Fixes #4476.
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@8910 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-developer/js/chapters.js
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-developer/js/chapters.js
@@ -1,5 +1,5 @@
 // Mobile Subnav open/close
-jQuery(document).ready(function() {
+jQuery( document ).ready( function () {
 
 	jQuery( document ).on( 'click', '.code-tab', function ( e ) {
 		var $tab = jQuery( e.target );
@@ -12,30 +12,37 @@ jQuery(document).ready(function() {
 		$tab.parent().find( '.is-active, .' + lang ).toggleClass( 'is-active' );
 	} );
 
-	var tocContainer = jQuery( 'div[class*="-table-of-contents-container"]').first();
+	var tocContainer = jQuery( 'div[class*="-table-of-contents-container"]' ).first();
 
 	if ( 0 === tocContainer.length ) {
 		return;
 	}
 
 	// Add our expandable button
-	tocContainer.find( '> ul > .menu-item-has-children > a' )
+	tocContainer.find( '> ul .menu-item-has-children > a' )
 		.wrap( '<div class="expandable"></div>' )
 		.after( '<button class="dashicons dashicons-arrow-down-alt2" aria-expanded="false"></button>' );
 
-	// Invisibly open all of the submenus
-	jQuery( '.menu-item-has-children > ul ul' ).addClass( 'default-open' );
+	// Invisibly hide all of the submenus
+	jQuery( '.menu-item-has-children > ul ul' ).hide();
 
 	// Open the current menu
 	tocContainer.find( '.current-menu-item a' ).first()
 		.addClass( 'active' )
 		.parents( '.menu-item-has-children' )
-			.toggleClass( 'open' )
+		.toggleClass( 'open' )
 		.find( '> div > .dashicons' )
-			.attr( 'aria-expanded', true );
+		.attr( 'aria-expanded', true );
 
+	// Open the current submenu
+	$secondary_menu = tocContainer.find( '.current-menu-item > ul' );
+	if ( $secondary_menu.length ) {
+		$secondary_menu.show();
+	} else {
+		tocContainer.find( '.current-menu-item' ).parents( 'ul' ).show();
+	}
 	// Or if wrapped in a div.expandable
-	jQuery( '.menu-item-has-children > div > .dashicons' ).click( function() {
+	jQuery( '.menu-item-has-children > div > .dashicons' ).click( function () {
 		var menuToggle = jQuery( this ).closest( '.menu-item-has-children' );
 
 		jQuery( this ).parent().siblings( '.sub-menu' ).length
