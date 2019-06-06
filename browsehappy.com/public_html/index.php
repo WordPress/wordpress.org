82743bc4f63694b8310e9e030ba535d020555b56
Scott Reilly <coffee2code@git.wordpress.org>
2019-06-06 18:58:21 +0000

    Browse Happy: Automatically wrap site name in header for languages that translate into long words.
    Fixes https://github.com/WordPress/browsehappy/issues/57.
    Fixes https://github.com/WordPress/browsehappy/pull/58.
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@8931 74240141-8908-4e6f-9713-ba540dce6ec7

--- browsehappy.com/public_html/index.php
+++ browsehappy.com/public_html/index.php
@@ -14,7 +14,7 @@
 	<meta name="author" content="WordPress" />
 	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css?9" />
+	<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css?10" />
 	<script src="<?php echo get_template_directory_uri(); ?>/js/modernizr-1.6.min.js"></script>
 	<script src="https://use.typekit.com/lsw6yis.js"></script>
 	<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
