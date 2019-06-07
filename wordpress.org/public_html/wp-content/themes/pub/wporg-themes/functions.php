7fc02ef354f8e91084051e8a8e7227bc86bd6df6
dd32 <dd32@74240141-8908-4e6f-9713-ba540dce6ec7>
2019-06-07 04:26:01 +0000

    Theme Directory: Cache bump after r8932.
    See #4499.
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@8933 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-themes/functions.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-themes/functions.php
@@ -83,7 +83,7 @@ function wporg_themes_scripts() {
 		$GLOBALS['concatenate_scripts'] = true;
 	}
 
-	wp_enqueue_style( 'wporg-themes', get_theme_file_uri( '/css/style.css' ), [ 'dashicons', 'open-sans' ], '20190530' );
+	wp_enqueue_style( 'wporg-themes', get_theme_file_uri( '/css/style.css' ), [ 'dashicons', 'open-sans' ], '20190607' );
 	wp_style_add_data( 'wporg-themes', 'rtl', 'replace' );
 
 	if ( ! is_singular( 'page' ) ) {
