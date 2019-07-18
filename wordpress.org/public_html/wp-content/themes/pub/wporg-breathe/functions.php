1b350e53263c10c7019dd9e36a404b8bf8f34745
Scott Reilly <coffee2code@git.wordpress.org>
2019-07-18 22:38:25 +0000

    Breathe: Bump CSS version for cache bust.
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@9062 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-breathe/functions.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-breathe/functions.php
@@ -58,7 +58,7 @@ function styles() {
 	wp_enqueue_style( 'p2-breathe', get_template_directory_uri() . '/style.css' );
 
 	// Cacheing hack
-	wp_enqueue_style( 'wporg-breathe', get_stylesheet_uri(), array( 'p2-breathe' ), '20190117' );
+	wp_enqueue_style( 'wporg-breathe', get_stylesheet_uri(), array( 'p2-breathe' ), '20190718' );
 }
 add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\styles', 11 );
 
