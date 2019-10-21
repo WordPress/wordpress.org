9951e2b668aae77a9c9bbb0206f9d3765f0a64c3
Dion Hulse <dd32@git.wordpress.org>
2019-10-21 03:08:32 +0000

    Theme Directory: Cache bump after r9212.
    See #4680.
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@9213 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-themes/functions.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-themes/functions.php
@@ -103,7 +103,7 @@ function wporg_themes_scripts() {
 
 	if ( ! is_singular( 'page' ) ) {
 		wp_enqueue_script( 'google-charts-loader', 'https://www.gstatic.com/charts/loader.js', array(), null, true );
-		wp_enqueue_script( 'wporg-theme', get_template_directory_uri() . "/js/theme{$suffix}.js", array( 'wp-backbone' ), '20191016', true );
+		wp_enqueue_script( 'wporg-theme', get_template_directory_uri() . "/js/theme{$suffix}.js", array( 'wp-backbone' ), '20191021', true );
 
 		// Use the Rosetta-specific site name. Ie. "WordPress.org $LOCALE"
 		$title_suffix = isset( $GLOBALS['wporg_global_header_options']['rosetta_title'] ) ? $GLOBALS['wporg_global_header_options']['rosetta_title'] : 'WordPess.org';
