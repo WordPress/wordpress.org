1e88318fc1fc286d63838a120ac832cd4b3b7800
Dion Hulse <dd32@git.wordpress.org>
2019-10-21 03:15:34 +0000

    Support Forums: Use the Rosetta Site Name instead of just 'WordPress.org' on locale forums.
    See #4680.
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@9214 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-support/functions.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-support/functions.php
@@ -187,7 +187,15 @@ add_filter( 'excerpt_length', 'wporg_support_excerpt_length' );
  * @package WPBBP
  */
 function wporg_get_global_header() {
-	$GLOBALS['pagetitle'] = wp_title( '&#124;', false, 'right' ) . __( 'WordPress.org', 'wporg-forums' );
+
+	$GLOBALS['pagetitle'] = wp_title( '&#124;', false, 'right' );
+	// Suffix either "WordPress.org $LOCALE" or WordPress.org.
+	if ( isset( $GLOBALS['wporg_global_header_options']['rosetta_title'] ) ) {
+		$GLOBALS['pagetitle'] .= $GLOBALS['wporg_global_header_options']['rosetta_title'];
+	} else {
+		$GLOBALS['pagetitle'] .= __( 'WordPress.org', 'wporg-forums' );
+	}
+
 	require WPORGPATH . 'header.php';
 }
 
