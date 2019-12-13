9bac8395b872c5803d193a951e0251949203b721
John James Jacoby <johnjamesjacoby@git.wordpress.org>
2019-12-13 19:43:25 +0000

    bbPress: update the tool that forum moderators use to mark Users as spammers.
    This commit brings the bespoke Moderator tool up-to-speed with changes coming in bbPress 2.6.4.
    
    See: https://bbpress.trac.wordpress.org/ticket/3305
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@9342 74240141-8908-4e6f-9713-ba540dce6ec7

--- buddypress.org/public_html/wp-content/plugins/bbpress-org/tools.php
+++ buddypress.org/public_html/wp-content/plugins/bbpress-org/tools.php
@@ -10,37 +10,44 @@
 function bbporg_make_user_spammer() {
 
 	// Bail if not capable
-	if ( ! current_user_can( 'moderate' ) )
+	if ( ! current_user_can( 'moderate' ) ) {
 		return;
+	}
 
 	// Bail if not viewing a user
-	if ( ! bbp_is_single_user() )
+	if ( ! bbp_is_single_user() ) {
 		return;
+	}
 
 	// Bail if no refresh
-	if ( empty( $_GET['spammer'] ) || ( 'true' != $_GET['spammer'] ) )
+	if ( empty( $_GET['spammer'] ) || ( 'true' != $_GET['spammer'] ) ) {
 		return;
+	}
 
 	// Get the user ID
 	$user_id = bbp_get_displayed_user_id();
 
-	// Bail if empty and protect super admins
-	if ( empty( $user_id ) || is_super_admin( $user_id ) )
+	// Bail if empty, and protect keymasters
+	if ( empty( $user_id ) || bbp_is_user_keymaster( $user_id ) ) {
 		return;
+	}
 
 	global $wpdb;
 
 	// Make array of post types to mark as spam
 	$post_types  = array( bbp_get_topic_post_type(), bbp_get_reply_post_type() );
 	$post_types  = "'" . implode( "', '", $post_types ) . "'";
-	$status      = array( bbp_get_public_status_id(), bbp_get_pending_status_id() );
+
+	// Make array of post statuses to mark as spam
+	$status      = array( bbp_get_public_status_id(), bbp_get_closed_status_id(), bbp_get_pending_status_id() );
+	$status      = "'" . implode( "', '", $status ) . "'";
 
 	// Loop through blogs and remove their posts
 	// Get topics and replies
-	$posts = $wpdb->get_col( "SELECT ID FROM {$wpdb->posts} WHERE post_author = {$user_id} AND post_status = '{$status}' AND post_type IN ({$post_types})" );
+	$posts = $wpdb->get_col( "SELECT ID FROM {$wpdb->posts} WHERE post_author = {$user_id} AND post_status IN ( '{$status}' ) AND post_type IN ({$post_types})" );
 
 	// Loop through posts and spam them
-	if ( !empty( $posts ) ) {
+	if ( ! empty( $posts ) ) {
 		foreach ( $posts as $post_id ) {
 
 			// The routines for topics ang replies are different, so use the
@@ -57,17 +64,23 @@ function bbporg_make_user_spammer() {
 		}
 	}
 
-	// Delete the users description from their profile
-	$user_description = get_user_meta( $user_id, 'description');
-	if ( !empty( $user_description ) ) {
-		delete_user_meta( $user_id, 'description' );
-	}
+	// Delete user options
+	bbp_delete_user_options( $user_id );
+
+	// Remove their name & descriptive info
+	wp_update_user( array(
+		'ID'           => $user_id,
+		'description'  => '',
+		'first_name'   => '',
+		'last_name'    => '',
+		'display_name' => '',
+		'user_url'     => ''
+	) );
 
 	// Block the user
 	bbp_set_user_role( $user_id, bbp_get_blocked_role() );
 
 	// Redirect without _GET
-	wp_safe_redirect( bbp_get_user_profile_url( $user_id ) );
-	die;
+	bbp_redirect( bbp_get_user_profile_url( $user_id ) );
 }
 add_action( 'bbp_template_redirect', 'bbporg_make_user_spammer' );
