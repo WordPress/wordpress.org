2c6f50c918647c3f1c88a67d9397da026e59c258
Dion hulse <dd32@git.wordpress.org>
2019-12-24 00:40:38 +0000

    WordPress.org: add a redirect for `/license.txt`.
    See https://core.trac.wordpress.org/ticket/49072
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@9382 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-main/functions.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-main/functions.php
@@ -253,6 +253,12 @@ function old_page_redirects() {
 		wp_safe_redirect( '/about/license/', 301 );
 		die();
 	}
+
+	// https://*/license.txt > about/license
+	if ( '/license.txt' === $_SERVER['REQUEST_URI'] ) {
+		wp_safe_redirect( '/about/license/', 301 );
+                die();
+	}
 }
 add_filter( 'template_redirect', __NAMESPACE__ . '\old_page_redirects' );
 
