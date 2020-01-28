fea7bf81986a0e75f7dfeae34953300331cd95ed
Scott Reilly <coffee2code@git.wordpress.org>
2020-01-28 19:35:23 +0000

    Developer theme: Bump CSS and JS versions for cache bust.
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@9433 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-developer/inc/user-content.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-developer/inc/user-content.php
@@ -167,7 +167,7 @@ class DevHub_User_Submitted_Content {
 			wp_enqueue_style( 'syntaxhighlighter-core' );
 			wp_enqueue_style( 'syntaxhighlighter-theme-default' );
 
-			wp_enqueue_script( 'wporg-developer-user-notes', get_template_directory_uri() . '/js/user-notes.js', array( 'jquery', 'quicktags' ), '20181019', true );
+			wp_enqueue_script( 'wporg-developer-user-notes', get_template_directory_uri() . '/js/user-notes.js', array( 'jquery', 'quicktags' ), '20200110', true );
 			wp_enqueue_script( 'wporg-developer-user-notes-feedback', get_template_directory_uri() . '/js/user-notes-feedback.js', array( 'jquery', 'quicktags' ), '20181023', true );
 			wp_localize_script( 'wporg-developer-user-notes-feedback', 'wporg_note_feedback', array(
 				'show'             => __( 'Show Feedback', 'wporg' ),
