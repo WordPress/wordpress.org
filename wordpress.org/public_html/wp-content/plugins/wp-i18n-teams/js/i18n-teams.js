c88fabb7eae52446679ffc4a2da1b1840b0249c0
Dominik Schilling <ocean90@git.wordpress.org>
2020-03-01 12:33:33 +0000

    WP I18N Teams: Fix locale filters for teams page on initial page load.
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@9554 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/plugins/wp-i18n-teams/js/i18n-teams.js
+++ wordpress.org/public_html/wp-content/plugins/wp-i18n-teams/js/i18n-teams.js
@@ -1,17 +1,22 @@
 ( function( $ ){
-	var currentHash = window.location.hash.replace( /[^a-z0-9-#]/gi, '' );
-
 	$( function() {
 		$( document.body ).on( 'click', '.i18n-filter', function( event ) {
 			event.preventDefault();
-			$( '.current-filter' ).removeClass( 'current-filter' );
-			$( '.translators-info' )[0].className = 'translators-info show-' + $( this ).data( 'filter' );
-			$( this ).addClass( 'current-filter' );
-		});
 
+			var $el = $( this );
+			var filter = $el.data( 'filter' ).replace( /[^a-z0-9-#]/gi, '' );
+			window.location.hash = '#' + filter;
+
+			$( '.current-filter' ).removeClass( 'current-filter' );
+			$( '.translators-info' )[0].className = 'translators-info show-' + filter;
+			$el.addClass( 'current-filter' );
+		});
+	});
+
+	jQuery( document ).one( 'ready.o2', function() {
+		var currentHash = window.location.hash.replace( /[^a-z0-9-#]/gi, '' );
 		if ( currentHash ) {
 			$( '.locale-filters' ).find( '[href="' + currentHash + '"]' ).trigger( 'click' );
 		}
-	});
-
+	} );
 })( jQuery );
