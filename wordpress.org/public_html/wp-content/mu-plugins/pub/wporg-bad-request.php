811bfa8fa23e1b488f32b393100cae42357bb063
Dion Hulse <dd32@git.wordpress.org>
2020-04-23 06:07:35 +0000

    Add an MU plugin which returns a `400 Bad Request` when a HTTP request is invalid and will produce PHP warnings.
    Regular clients should never trigger these checks, it should be almost exclusively vulnerability scaners and bots, but any blocked request is logged for review.
    
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@9771 74240141-8908-4e6f-9713-ba540dce6ec7

---
+++ wordpress.org/public_html/wp-content/mu-plugins/pub/wporg-bad-request.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * Plugin Name: WordPress.org 400 Bad Request
+ * Description: Throw a 400 Bad Request for bad requests. This shouldn't be needed, but vulnerability scanners exist and it makes a mess of logs.
+ * Version:     1.0
+ * Author:      WordPress.org
+ * Author URI:  https://wordpress.org/
+ * License:     GPLv2 or later
+ *
+ * @package WordPressdotorg\BadRequest
+ */
+
+namespace WordPressdotorg\BadRequest;
+
+/**
+ * Detect invalid form field values in login requests.
+ */
+add_action( 'login_init', function() {
+	$expected_string_fields = [
+		'log', 'pwd', 'redirect_to', 'rememberme',
+		'_wp_http_referer', '_wpnonce',
+		'locale' // WordPress.org login localisation.
+	];
+
+	foreach ( $expected_string_fields as $field ) {
+		if ( isset( $_REQUEST[ $field ] ) && ! is_scalar( $_REQUEST[ $field ] ) ) {
+			die_bad_request( "non-scalar $field in login \$_REQUEST" );
+		}
+	}
+} );
+
+/**
+ * Detect invalid query parameters being passed in Core query fields.
+ * Generally causes WP_Query to throw a PHP Warning.
+ */
+add_action( 'send_headers', function( $wp ) {
+	// Assumption: WP::$public_query_vars will only ever contain non-array query vars.
+	foreach ( (new \WP)->public_query_vars as $field ) {
+		if ( isset( $wp->query_vars[ $field ] ) && ! is_scalar( $wp->query_vars[ $field ] ) ) {
+			die_bad_request( "non-scalar $field in \$public_query_vars" );
+		}
+	}
+} );
+
+/**
+ * Detect invalid parameters being passed to REST API Endpoints.
+ * Not all API endpoints sanitization callbacks check variable types.
+ */
+add_action( 'rest_api_init', function( $wp_rest_server ) {
+	global $wp;
+
+	// oEmbed endpoint has some not-so-great sanitize callbacks specified
+	if ( '/oembed/1.0/embed' === $wp->query_vars['rest_route'] ) {
+		foreach ( [ 'url', 'maxwidth' ] as $field ) {
+			if ( isset( $_REQUEST[ $field ] ) && ! is_scalar( $_REQUEST[ $field ] ) ) {
+				die_bad_request( "non-scalar $field in oEmbed call" );
+			}
+		}
+	}
+
+} );
+
+/**
+ * Die with a 400 Bad Request.
+ * 
+ * @param string $reference A unique identifying string to make it easier to read logs.
+ */
+function die_bad_request( $reference = '') {
+	header( $_SERVER['SERVER_PROTOCOL'] . ' 400 Bad Request' );
+
+	// Use a prettier error page on WordPress.org
+	if ( defined( 'WPORGPATH' ) && file_exists( WPORGPATH . '/403.php' ) ) {
+		$header_set_for_403 = true;
+		include WPORGPATH . '/403.php';
+
+		// Log it if possible.
+		if ( function_exists( 'wporg_error_reporter' ) ) {
+			wporg_error_reporter( E_USER_NOTICE, "400 Bad Request: $reference", __FILE__, __LINE__ );
+		}
+		exit;
+	}
+
+	\wp_die( 'Bad Request', 'Bad Request', [ 'code' => 400 ] );
+}
