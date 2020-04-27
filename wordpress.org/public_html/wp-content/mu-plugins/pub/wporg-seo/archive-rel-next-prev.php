54fcbe5e807240cc89c10d2e2a012ecba6ef9a48
Dion Hulse <dion@wordpress.org>
2020-04-27 02:37:51 +0000

    Reformat with tabs not spaces

--- wordpress.org/public_html/wp-content/mu-plugins/pub/wporg-seo/archive-rel-next-prev.php
+++ wordpress.org/public_html/wp-content/mu-plugins/pub/wporg-seo/archive-rel-next-prev.php
@@ -8,26 +8,26 @@ use WordPressdotorg\SEO\Canonical\get_canonical_url;
  * @static
  */
 function output_rel_prev_next_links() {
-    global $paged, $wp_query, $wp_rewrite;
-    if ( ! is_archive() && ! is_search() ) {
-        return;
-    }
+	global $paged, $wp_query, $wp_rewrite;
+	if ( ! is_archive() && ! is_search() ) {
+		return;
+	}
 
-    $current_url = get_canonical_url();
-    if ( ! $current_url ) {
-        return;
-    }
+	$current_url = get_canonical_url();
+	if ( ! $current_url ) {
+		return;
+	}
 
-    $max_page = $wp_query->max_num_pages;
-    if ( ! $paged ) {
-        $paged = 1;
-    }
+	$max_page = $wp_query->max_num_pages;
+	if ( ! $paged ) {
+		$paged = 1;
+	}
 
-    $nextpage = intval( $paged ) + 1;
-    $prevpage = intval( $paged ) - 1;
+	$nextpage = intval( $paged ) + 1;
+	$prevpage = intval( $paged ) - 1;
 
-    $current_url = remove_query_arg( 'paged', $current_url );
-    $current_url = preg_replace( "|{$wp_rewrite->pagination_base}/\d+/?($|?)|", '', $current_url );
+	$current_url = remove_query_arg( 'paged', $current_url );
+	$current_url = preg_replace( "|{$wp_rewrite->pagination_base}/\d+/?($|?)|", '', $current_url );
 
 	// Support Canonical URLs with query parameters.
 	$current_url_query = '';
@@ -38,22 +38,22 @@ function output_rel_prev_next_links() {
 		}
 	}
 
-    // Just assume rewrites are in use everywhere.
-    $next_url = $current_url . "{$wp_rewrite->pagination_base}/{$nextpage}/" . $current_url_query;
-    $prev_url = $current_url . ( $prevpage > 1 ? "{$wp_rewrite->pagination_base}/{$prevpage}/" : '' ) . $current_url_query;
+	// Just assume rewrites are in use everywhere.
+	$next_url = $current_url . "{$wp_rewrite->pagination_base}/{$nextpage}/" . $current_url_query;
+	$prev_url = $current_url . ( $prevpage > 1 ? "{$wp_rewrite->pagination_base}/{$prevpage}/" : '' ) . $current_url_query;
 
-    if ( $prevpage >= 1 ) {
-        printf(
-            '<link rel="prev" href="%s">' . "\n",
-            esc_url( $prev_url )
-        );
-    }
+	if ( $prevpage >= 1 ) {
+		printf(
+			'<link rel="prev" href="%s">' . "\n",
+			esc_url( $prev_url )
+		);
+	}
 
-    if ( $nextpage <= $max_page ) {
-        printf(
-            '<link rel="next" href="%s">' . "\n",
-            esc_url( $next_url )
-        );
-    }
+	if ( $nextpage <= $max_page ) {
+		printf(
+			'<link rel="next" href="%s">' . "\n",
+			esc_url( $next_url )
+		);
+	}
 }
 add_action( 'wp_head', __NAMESPACE__ . '\output_rel_prev_next_links' );
