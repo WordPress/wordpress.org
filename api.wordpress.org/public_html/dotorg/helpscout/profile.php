52430f464b9dcd35d9f3ae6efeaceb4ed9ed0351
Samuel Wood (Otto) <otto42@git.wordpress.org>
2020-05-01 16:23:41 +0000

    API: Add endpoint for HelpScout API
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@9808 74240141-8908-4e6f-9713-ba540dce6ec7

---
+++ api.wordpress.org/public_html/dotorg/helpscout/profile.php
@@ -0,0 +1,44 @@
+<?php
+// Simple profile lookup for HelpScout sidebar. Returns w.org links to HS for emails received.
+
+$base_dir = dirname( dirname( __DIR__ ) );
+require( $base_dir . '/wp-init.php' );
+
+// function to verify signature from HelpScout
+function isFromHelpScout($data, $signature) {
+	$calculated = base64_encode( hash_hmac( 'sha1', $data, HELPSCOUT_SECRET_KEY, true ) );
+	var_dump($calculated);
+	return hash_equals( $signature, $calculated );
+}
+
+//  HelpScout sends json data in the POST, so grab it from the input directly
+$data = file_get_contents( 'php://input' );
+
+// check the signature header
+$signature = $_SERVER['HTTP_X_HELPSCOUT_SIGNATURE'];
+if ( ! isFromHelpScout( $data, $signature ) ) {
+	// failure = no response
+	exit;
+}
+
+// get the info from HS
+$request = json_decode( $data );
+
+// Look up a user based on email address
+if ( isset ( $request->customer->email ) ) {
+
+	// look up profile url for email
+	$user = get_user_by( 'email', $request->customer->email );
+	if ( isset( $user->user_nicename ) ) {
+		$profilelink = '<a href="https://profiles.wordpress.org/' . $user->user_nicename . '">'. $user->user_nicename .'</a>';
+	} else {
+		$profilelink = 'No profile found';
+	}
+
+}
+
+// response to HS is just HTML to display in the sidebar
+$response = array ('html' => $profilelink);
+
+echo json_encode( $response );
+
