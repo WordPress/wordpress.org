4b45d6d386b69a692cdaa81f73ec70130895860f
Dion Hulse <dion@wordpress.org>
2020-05-01 05:52:09 +0000

    SEO: Add a placeholder for Canonical Redirects

--- wordpress.org/public_html/wp-content/mu-plugins/pub/wporg-seo/wporg-seo.php
+++ wordpress.org/public_html/wp-content/mu-plugins/pub/wporg-seo/wporg-seo.php
@@ -8,4 +8,5 @@ include __DIR__ . '/robots.php';
 include __DIR__ . '/redirects.php';
 include __DIR__ . '/canonical.php';
 include __DIR__ . '/archive-rel-next-prev.php';
-include __DIR__ . '/hreflang.php';
+// include __DIR__ . '/hreflang.php';
+// include __DIR__ . '/canonical-redirects.php';
