361505015a010f28e395953afd509bea7759a23a
Samuel Wood (Otto) <otto42@git.wordpress.org>
2020-05-01 16:32:42 +0000

    API: clean up response to HS, eliminate errors on some requests
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@9810 74240141-8908-4e6f-9713-ba540dce6ec7

--- api.wordpress.org/public_html/dotorg/helpscout/profile.php
+++ api.wordpress.org/public_html/dotorg/helpscout/profile.php
@@ -23,21 +23,24 @@ if ( ! isFromHelpScout( $data, $signature ) ) {
 // get the info from HS
 $request = json_decode( $data );
 
+// default empty output
+$html = '';
+
 // Look up a user based on email address
 if ( isset ( $request->customer->email ) ) {
 
-	// look up profile url for email
+	// look up profile url by email
 	$user = get_user_by( 'email', $request->customer->email );
 	if ( isset( $user->user_nicename ) ) {
-		$profilelink = '<a href="https://profiles.wordpress.org/' . $user->user_nicename . '">'. $user->user_nicename .'</a>';
+		$html .= '<p>Profile: <a href="https://profiles.wordpress.org/' . $user->user_nicename . '">'. $user->user_nicename .'</a></p>';
 	} else {
-		$profilelink = 'No profile found';
+		$html .= '<p>No profile found</p>';
 	}
 
 }
 
 // response to HS is just HTML to display in the sidebar
-$response = array ('html' => $profilelink);
+$response = array ('html' => $html);
 
 echo json_encode( $response );
 
