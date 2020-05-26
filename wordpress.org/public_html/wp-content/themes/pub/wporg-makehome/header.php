c41577e29dbf848de5d76f87cd9375b5050a2bb0
Steve Dufresne <dufresnesteven@git.wordpress.org>
2020-05-26 00:31:05 +0000

    Accessibility: Add skip links that moves users to #headline.
    See: https://meta.trac.wordpress.org/ticket/4109
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@9927 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-makehome/header.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-makehome/header.php
@@ -1,5 +1,10 @@
 <?php
 $GLOBALS['pagetitle'] = wp_get_document_title();
+global $wporg_global_header_options;
+if ( !isset( $wporg_global_header_options['in_wrapper'] ) ) {
+	$wporg_global_header_options['in_wrapper'] = '';
+}
+$wporg_global_header_options['in_wrapper'] .= '<a class="skip-link screen-reader-text" href="#headline">' . esc_html( 'Skip to content', 'make-wporg' ) . '</a>';
 require( WPORGPATH . 'header.php' );
 ?>
 
