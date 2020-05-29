1b92e1ba41b0d82f5f2144fada0d5942f890ff8a
Dion Hulse <dd32@git.wordpress.org>
2020-05-29 03:31:32 +0000

    Login: When a login is for a non-wordpress.org domain, include some basic text to make it clear that their WordPress.org account is intended to be used for the site.
    See #5239.
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@9944 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-login/login.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-login/login.php
@@ -6,8 +6,27 @@
  */
 
 get_header();
+
+$from = $_GET['from'] ?? 'wordpress.org';
 ?>
-<p class="intro"><?php _e( 'Log in to your WordPress.org account to contribute to WordPress, get help in the support forum, or rate and review themes and plugins.', 'wporg' ); ?></p>
+<p class="intro"><?php
+if ( false !== stripos( $from, 'buddypress.org' ) ) {
+	echo '<strong>' . __( 'BuddyPress is part of WordPress.org', 'wporg' ) . '</strong><br>';
+	_e( 'Log in to your WordPress.org account to contribute to BuddyPress, or get help in the support forums.', 'wporg' );
+
+} elseif ( false !== stripos( $from, 'bbpress.org' ) ) {
+	echo '<strong>' . __( 'bbPress is part of WordPress.org', 'wporg' ) . '</strong><br>';
+	_e( 'Log in to your WordPress.org account to contribute to bbPress, or get help in the support forums.', 'wporg' );
+
+} elseif ( false !== stripos( $from, 'wordcamp.org' ) ) {
+	echo '<strong>' . __( 'WordCamp is part of WordPress.org', 'wporg' ) . '</strong><br>';
+	_e( 'Log in to your WordPress.org account to contribute to WordCamps and meetups around the globe.', 'wporg' );
+
+} else {
+	_e( 'Log in to your WordPress.org account to contribute to WordPress, get help in the support forum, or rate and review themes and plugins.', 'wporg' );
+}
+?>
+</p>
 
 <?php wp_login_form(); ?>
 
