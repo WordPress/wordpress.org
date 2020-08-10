176a57f15decaa3d57f262b9432d441d3d9073ab
Dion Hulse <dion@wordpress.org>
2020-08-10 06:06:32 +0000

    Add initial admin overview table

--- wordpress.org/public_html/wp-content/plugins/plugin-directory/admin/metabox/class-release-confirmation.php
+++ wordpress.org/public_html/wp-content/plugins/plugin-directory/admin/metabox/class-release-confirmation.php
@@ -12,9 +12,53 @@ class Release_Confirmation {
 	static function display() {
 		global $post;
 
-		echo 'Release Confirmation: <strong>' . ( $post->release_confirmation_enabled ? 'Enabled' : 'Disabled' ) . '</strong>';
+		$confirmations_required = $post->release_confirmation_enabled;
+		$confirmed_releases     = get_post_meta( $post->ID, 'confirmed_releases', true );
 
-		var_dump( get_post_meta( $post->ID, 'confirmed_releases', true ) );
+		echo 'Release Confirmation: <strong>' . ( $confirmations_required ? 'Enabled' : 'Disabled' ) . '</strong>';
 
+		echo '<table class="widefat">
+		<thead>
+			<tr>
+				<th>Tag</th>
+				<th>Date</th>
+				<th>Committer</th>
+				<th>Approval</th>
+				<th>Override</th>;
+		</thead>
+		';
+		foreach ( $confirmed_releases as $tag => $data ) {
+			$data['confirmations_required'] = $confirmations_required;
+
+			printf(
+				'<tr>
+					<td>%s</td>
+					<td title="%s">%s</td>
+					<td>%s</td>
+					<td>%s</td>
+					<td>%s</td>
+				</tr>',
+				esc_html( $tag ),
+				esc_attr( gmdate( 'Y-m-d H:i:s', $data['date'] ) ),
+				esc_html( human_time_diff( $data['date'] ) ),
+				esc_html( $data['committer'] ),
+				self::get_approval_text( $data ),
+				self::get_actions( $data )
+			);
+		}
+		echo '</table>';
+	}
+
+	protected static function get_approval_text( $data ) {
+		if ( ! $data['confirmations_required'] ) {
+			return 'Not required.';
+		}
+
+		return count( $data['confirmations'] ?? [] ) . '/' . $data['confirmations_required'];
+	}
+
+	protected static function get_actions( $data ) {
+		return '<button class="button button-primary override-approve-release">Approve</button>' . '&nbsp' .
+			'<button class="button button-secondary override-deny-release">Deny</button>';
 	}
 }
