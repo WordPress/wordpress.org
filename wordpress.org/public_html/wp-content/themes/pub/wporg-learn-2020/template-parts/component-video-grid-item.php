b32b30d4d21b8965dde6fbb0f930347d08d90865
dd32 <dd32@git.wordpress.org>
2020-08-13 05:43:27 +0000

    WordPress.org Learn: Sync with GitHub.
    https://github.com/WordPress/learn/compare/147b403c92721341038870274678169d9fc103d2...493304c8075e98b40168455c0fd49b5549238d22
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@10161 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-learn-2020/template-parts/component-video-grid-item.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-learn-2020/template-parts/component-video-grid-item.php
@@ -10,10 +10,8 @@
 ?>
 
 <li class="col-4 video-grid_item">
-	<?php if( has_post_thumbnail() ) : ?>
-		<?php echo the_post_thumbnail( 'medium' ); ?>
-	<?php else : ?>
-		<div class="video-grid_item--no-image"></div>
-	<? endif; ?>
-	<a class="video-grid_item_link" href="<?php echo esc_url( get_the_permalink() ); ?>"><?php the_title(); ?></a>
+	<a class="video-grid_item_link" href="<?php echo esc_url( get_the_permalink() ); ?>">
+		<?php echo wporg_get_post_thumbnail( $post, 'medium' ); ?>
+		<?php the_title(); ?>
+	</a>
 </li>
