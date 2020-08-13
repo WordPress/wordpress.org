ff0033fc8222ff7632f6aa1d4a0faf0f272ae1ef
Dion Hulse <dion@wordpress.org>
2020-08-13 08:12:22 +0000

    Add a REST API endpoint to confirm a release

--- wordpress.org/public_html/wp-content/plugins/plugin-directory/api/routes/class-plugin-release-confirmation.php
+++ wordpress.org/public_html/wp-content/plugins/plugin-directory/api/routes/class-plugin-release-confirmation.php
@@ -4,6 +4,7 @@ namespace WordPressdotorg\Plugin_Directory\API\Routes;
 use WordPressdotorg\Plugin_Directory\Plugin_Directory;
 use WordPressdotorg\Plugin_Directory\API\Base;
 use WordPressdotorg\Plugin_Directory\Tools;
+use WordPressdotorg\Plugin_Directory\Jobs\Plugin_Import;
 use WordPressdotorg\Plugin_Directory\Email\Release_Confirmation_Enabled as Release_Confirmation_Enabled_Email;
 
 /**
@@ -29,6 +30,24 @@ class Plugin_Release_Confirmation extends Base {
 			},
 		] );
 
+		register_rest_route( 'plugins/v1', '/plugin/(?P<plugin_slug>[^/]+)/release-confirmation/(?P<plugin_tag>[^/]+)', [
+			'methods'             => \WP_REST_Server::READABLE, // TODO: This really should be a POST
+			'callback'            => [ $this, 'confirm_release' ],
+			'args'                => [
+				'plugin_slug' => [
+					'validate_callback' => [ $this, 'validate_plugin_slug_callback' ],
+				],
+				'plugin_tag' => [
+					'validate_callback' => [ $this, 'validate_plugin_tag_callback' ],
+				]
+			],
+			'permission_callback' => function( $request ) {
+				$plugin = Plugin_Directory::get_plugin_post( $request['plugin_slug'] );
+
+				return current_user_can( 'plugin_admin_edit', $plugin ) && 'publish' === $plugin->post_status;
+			},
+		] );
+
 		add_filter( 'rest_pre_echo_response', [ $this, 'override_cookie_expired_message' ], 10, 3 );
 	}
 
@@ -38,7 +57,7 @@ class Plugin_Release_Confirmation extends Base {
 	function override_cookie_expired_message( $result, $obj, $request ) {
 		if (
 			is_array( $result ) && isset( $result['code'] ) &&
-			preg_match( '!^/plugins/v1/plugin/([^/]+)/release-confirmation$!', $request->get_route(), $m )
+			preg_match( '!^/plugins/v1/plugin/([^/]+)/release-confirmation(/[^/]+)?$!', $request->get_route(), $m )
 		) {
 			if ( 'rest_cookie_invalid_nonce' == $result['code'] ) {
 				wp_die( 'The link you have followed has expired.' );
@@ -87,4 +106,70 @@ class Plugin_Release_Confirmation extends Base {
 		return $result;
 	}
 
+	/**
+	 * A simple endpoint to confirm a release.
+	 */
+	public function confirm_release( $request ) {
+		$plugin   = Plugin_Directory::get_plugin_post( $request['plugin_slug'] );
+		$releases = get_post_meta( $plugin->ID, 'confirmed_releases', true ) ?: [];
+		$tag      = $request['plugin_tag'];
+
+		$location = wp_get_referer( $plugin ) ?: home_url( '/developers/releases/');
+		header( "Location: $location" );
+
+		$result = [
+			'location' => $location,
+		];
+
+		$user_login = wp_get_current_user()->user_login;
+
+		if ( ! empty( $releases[ $tag ]['confirmed'][ $user_login ] ) ) {
+			// Already confirmed.
+			$result['confirmed'] = false;
+			return $result;
+		}
+
+		// Record this user as confirming the release.
+		$releases[ $tag ]['confirmations'][ $user_login ] = time();
+		$result['confirmed']                              = true;
+
+		// Mark the release as confirmed if enough confirmations.
+		if ( count( $releases[ $tag ]['confirmations'] ) >= $plugin->release_confirmation ) {
+			$releases[ $tag ]['confirmed'] = true;
+			$result['fully_confirmed']     = true;
+		}
+
+		if ( ! update_post_meta( $plugin->ID, 'confirmed_releases', $releases ) ) {
+			$result['confirmed'] = false;
+			unset( $result['fully_confirmed'] );
+			return $result;
+		}
+
+		// Trigger the import for the plugin.
+		Plugin_Import::queue(
+			$plugin->post_name,
+			// TODO this is not 100% right... but will probably work.
+			[
+				'tags_touched'   => [
+					'trunk',
+					$tag
+				],
+				// Assume everything was modified.
+				'readme_touched' => true,
+				'code_touched'   => true,
+				'assets_touched' => true,
+				'revisions'      => $releases[ $tag ]['revision'],
+			]
+		);
+
+		return $result;
+	}
+
+	public function validate_plugin_tag_callback( $tag, $request ) {
+		$plugin = Plugin_Directory::get_plugin_post( $request['plugin_slug'] );
+
+		$releases = get_post_meta( $plugin->ID, 'confirmed_releases', true ) ?: [];
+
+		return !empty( $releases[ $tag ] );
+	}
 }
--- wordpress.org/public_html/wp-content/plugins/plugin-directory/class-template.php
+++ wordpress.org/public_html/wp-content/plugins/plugin-directory/class-template.php
@@ -816,6 +816,21 @@ class Template {
 	}
 
 	/**
+	 * Generates a link to confirm a release.
+	 *
+	 * @param int|\WP_Post|null $post Optional. Post ID or post object. Defaults to global $post.
+	 * @return string URL to enable confirmations.
+	 */
+	public static function get_release_confirmation_link( $tag, $post = null) {
+		$post = get_post( $post );
+
+		return add_query_arg(
+			array( '_wpnonce' => wp_create_nonce( 'wp_rest' ) ),
+			home_url( 'wp-json/plugins/v1/plugin/' . $post->post_name . '/release-confirmation/' . $tag )
+		);
+	}
+
+	/**
 	 * Returns the reasons for closing or disabling a plugin.
 	 *
 	 * @return array Close/disable reason labels.
