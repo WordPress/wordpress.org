2766f0d42dd15a5c4c3b5011768cd5b3bf2fa3bf
Dion Hulse <dion@wordpress.org>
2020-08-13 04:57:18 +0000

    Allow plugin admins to enable/disable release approvals.

--- wordpress.org/public_html/wp-content/plugins/plugin-directory/admin/metabox/class-release-confirmation.php
+++ wordpress.org/public_html/wp-content/plugins/plugin-directory/admin/metabox/class-release-confirmation.php
@@ -15,8 +15,32 @@ class Release_Confirmation {
 
 		$confirmations_required = $post->release_confirmation;
 		$confirmed_releases     = get_post_meta( $post->ID, 'confirmed_releases', true ) ?: [];
-		echo 'Release Confirmation: <strong>' . ( $confirmations_required ? 'Enabled' : 'Disabled' ) . '</strong>';
+
+		echo '<p><select name="release_confirmation" onchange="jQuery(this).next().removeClass(\'hidden\');">';
+		foreach ( [
+			0 => 'No approval required',
+			1 => 'One confirmation required',
+			2 => 'Two confirmations required'
+		] as $num => $text ) {
+			printf(
+				'<option value="%s" %s>%s</option>',
+				$num,
+				selected( $confirmations_required, $num, false ),
+				$text
+			);
+		}
+		echo "</select><span class='hidden'>&nbsp;Don't forget to save the changes!</span></p>";
 
 		Release_Confirmation_Shortcode::single_plugin_row( $post, $include_header = false );
+
+	}
+
+	// Save the selection.
+	static function save_post( $post_id ) {
+		if ( isset( $_REQUEST['release_confirmation'] ) && is_numeric( $_REQUEST['release_confirmation'] ) ) {
+			if ( current_user_can( 'plugin_admin_edit', $post_id ) ) {
+				update_post_meta( $post_id, 'release_confirmation', (int) $_REQUEST['release_confirmation'] );
+			}
+		}
 	}
 }
