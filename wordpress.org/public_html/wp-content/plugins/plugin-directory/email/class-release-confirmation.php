83fe8e5a30e91b323482b076bbf8d37b3499e6d0
Dion Hulse <dion@wordpress.org>
2020-08-21 04:10:36 +0000

    Add the release confirmation email text

--- wordpress.org/public_html/wp-content/plugins/plugin-directory/email/class-release-confirmation.php
+++ wordpress.org/public_html/wp-content/plugins/plugin-directory/email/class-release-confirmation.php
@@ -2,10 +2,13 @@
 namespace WordPressdotorg\Plugin_Directory\Email;
 
 use WordPressdotorg\Plugin_Directory\Tools;
+use WordPressdotorg\Plugin_Directory\Shortcodes\Release_Confirmation as Release_Confirmation_Shortcode;
 
 class Release_Confirmation extends Base {
 	protected $required_args = [
-		'readme'
+		'who',
+		'readme',
+		'headers'
 	];
 
 	function subject() {
@@ -17,16 +20,19 @@ class Release_Confirmation extends Base {
 	}
 
 	function body() {
-		/* translators: 1: plugin name, 2: plugin author's username, 3: plugin slug */
-		$email_text = "Pending release TODO Body.";
+		return sprintf(
+			/* translators: 1: Username, 2: Plugin Name, 3: Version identifier, 4: Access URL */
+			__( '%1$s has committed a new version of %2$s - %3$s.
 
-		return $email_text . print_r( $this->args, true );
+An email confirmation is required before the new version will be released.
 
-	/*	return sprintf(
-			$email_text,
-			$this->plugin->post_title,
-			$this->user->user_login,
-			$this->plugin->post_name
-		); */
+Follow the link below to login and confirm the release.
+
+<%4$s>', 'wporg-plugins' ),
+			$this->args['who'],
+			$this->args['readme']->name,
+			$this->args['headers']->Version,
+			esc_url( Release_Confirmation_Shortcode::generate_access_url( $this->user ) )
+		);
 	}
 }
