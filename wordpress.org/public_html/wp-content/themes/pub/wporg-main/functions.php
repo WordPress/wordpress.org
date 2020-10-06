dd84129960400bd158d8bcb507821d8ef1177bb2
dd32 <dd32@git.wordpress.org>
2020-10-06 04:19:36 +0000

    WordPress.org Theme: Don't attempt to use opengraph data for page embeds when we don't have a page to embed.
    Fixes some PHP Notices on embeds.
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@10350 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-main/functions.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-main/functions.php
@@ -201,7 +201,7 @@ add_filter( 'page_template_hierarchy', __NAMESPACE__ . '\child_page_templates' )
 function use_opengraph_data_for_embed_template() {
 	global $post;
 
-	if ( 'page' !== $post->post_type || ! $post->page_template || 'default' === $post->page_template ) {
+	if ( ! $post || 'page' !== $post->post_type || ! $post->page_template || 'default' === $post->page_template ) {
 		return;
 	}
 
