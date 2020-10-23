dad6594923a359bdd30a18367ecfd88509732a7f
dd32 <dd32@git.wordpress.org>
2020-10-23 04:15:45 +0000

    Trac Links: Inverse the matching to split by finding tags first and processing content outside of tags, rather than searching for text outside of tags.
    This resolves some performance issues with long posts containing many links, causing increasing pageload delays based on the number of tags present.
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@10401 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/plugins/trac-links/trac-links.php
+++ wordpress.org/public_html/wp-content/plugins/trac-links/trac-links.php
@@ -1,12 +1,12 @@
 <?php
 /*
-Plugin Name: Trac Links
-Plugin URI: http://wordpress.org/
-Description: Link ticket and changeset numbers to core.trac.
-Version: 1.0
-Author: Automattic
-Author URI: http://wordpress.org/
-*/
+ * Plugin Name: Trac Links
+ * Plugin URI: https://wordpress.org/
+ * Description: Link ticket and changeset numbers to core.trac.
+ * Version: 1.0
+ * Author: WordPress.org
+ * Author URI: https://wordpress.org/
+ */
 
 add_filter( 'the_content', 'markup_wporg_links', 0, 1 );
 add_filter( 'comment_text', 'markup_wporg_links', 0, 1 );
@@ -32,11 +32,13 @@ function markup_wporg_links( $content ) {
 			break;
 	}
 
+	$tracs = 'core|blackberry|nokia|webos|plugins|bbpress|buddypress|supportpress|glotpress|backpress|windows|themes|meta';
+
 	$find = array(
-		'/(\s|^|\()(#(\d{4,5})-(core|blackberry|nokia|webos|plugins|bbpress|buddypress|supportpress|glotpress|backpress|windows|themes|meta))(\b|$)/im', // trac ticket #1234-plugins in http://plugins.trac.wordpress.org/ticket/1234
-		'/(\s|^|\()(r(\d{4,5})-(core|blackberry|nokia|webos|plugins|bbpress|buddypress|supportpress|glotpress|backpress|windows|themes|meta))(\b|$)/im', // changeset r1234-plugins in http://plugins.trac.wordpress.org/changeset/1234
-		'/(\s|^|\()(#(core|blackberry|nokia|webos|plugins|bbpress|buddypress|supportpress|glotpress|backpress|windows|themes|meta)(\d{4,5}))(\b|$)/im', // trac ticket #1234-plugins in http://plugins.trac.wordpress.org/ticket/1234
-		'/(\s|^|\()(r(core|blackberry|nokia|webos|plugins|bbpress|buddypress|supportpress|glotpress|backpress|windows|themes|meta)(\d{4,5}))(\b|$)/im', // changeset r1234-plugins in http://plugins.trac.wordpress.org/changeset/1234
+		'/(\s|^|\()(#(\d{4,5})-(' . $tracs . '))(\b|$)/im', // trac ticket #1234-plugins in http://plugins.trac.wordpress.org/ticket/1234
+		'/(\s|^|\()(r(\d{4,5})-(' . $tracs . '))(\b|$)/im', // changeset r1234-plugins in http://plugins.trac.wordpress.org/changeset/1234
+		'/(\s|^|\()(#(' . $tracs . ')(\d{4,5}))(\b|$)/im', // trac ticket #1234-plugins in http://plugins.trac.wordpress.org/ticket/1234
+		'/(\s|^|\()(r(' . $tracs . ')(\d{4,5}))(\b|$)/im', // changeset r1234-plugins in http://plugins.trac.wordpress.org/changeset/1234
 		'/(\s|^|\()(#(\d{4,5}))(\b|$)/im', // core trac ticket #1234 in http://core.trac.wordpress.org/ticket/1234
 		'/(\s|^|\()(r(\d{4,5}))(\b|$)/im', // core changeset r1234 in http://core.trac.wordpress.org/changeset/1234
 		'/(?<!\w)\[(\d{4,5})\](?!\w)/im', // core changeset [12345]
@@ -56,10 +58,36 @@ function markup_wporg_links( $content ) {
 		'$1<a href="https://'. $trac .'.trac.wordpress.org/changeset?new=$4&old=$3">$2</a>', // diff
 	);
 
-	preg_match_all('#[^>]+(?=<[^/]*[^a])|[^>]+$#', $content, $matches, PREG_SET_ORDER);
+	// Regex and loop based on convert_smilies();
+	$arr = preg_split( '/(<.*>)/U', $content, -1, PREG_SPLIT_DELIM_CAPTURE );
 
-	foreach ( $matches as $val ) {
-		$content = str_replace($val[0],preg_replace($find,$replace,$val[0]),$content);
+	$tags_to_ignore = 'a|code|pre';
+	$ignore_element = false;
+	$len            = count( $arr );
+	
+	for ( $i = 0; $i <= $len; $i++ ) {
+		$text   = $arr[ $i ];
+		$is_tag = '<' === $text[0];
+
+		// Is this a HTML tag we want to skip the contents of?
+		if (
+			$is_tag &&
+			! $ignore_element &&
+			preg_match( '/^<(' . $tags_to_ignore . ')[^>]*>/', $text, $matches )
+		) {
+			$ignore_element = $matches[1];
+			$closing_tag    = '</' . $ignore_element . '>';
+		}
+
+		// Process this stand-alone chunk of text.
+		if ( ! $is_tag && ! $ignore_element && strlen( $text ) ) {
+			$content = str_replace( $text, preg_replace( $find, $replace, $text ), $content );
+		}
+
+		// Did we leave the ignored tag?
+		if ( $is_tag && $ignore_element && $closing_tag === $text ) {
+			$ignore_element = false;
+		}
 	}
 
 	return $content;
