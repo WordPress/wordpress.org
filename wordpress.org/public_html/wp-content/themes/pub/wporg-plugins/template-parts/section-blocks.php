a513a57bebcfe642039916520b8d20281b8f2111
ryelle <ryelle@git.wordpress.org>
2020-11-11 16:23:45 +0000

    Plugin Directory: Show available block names, icons, and descriptions.
    Props michael-arestad for design.
    Fixes #5411.
    
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@10440 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-plugins/template-parts/section-blocks.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-plugins/template-parts/section-blocks.php
@@ -13,16 +13,62 @@ $prefix = in_array( $section_slug, array( 'screenshots', 'faq', 'blocks' ), true
 
 $classes = [ 'plugin-' . $section_slug, 'section' ];
 $classes = implode( ' ', $classes );
+
+$allowed_svg = array(
+	'svg'   => array(
+		'class' => true,
+		'aria-hidden' => true,
+		'aria-labelledby' => true,
+		'role' => true,
+		'xmlns' => true,
+		'width' => true,
+		'height' => true,
+		'viewbox' => true,
+	),
+	'g'     => array( 'fill' => true ),
+	'title' => array( 'title' => true ),
+	'path'  => array(
+		'd' => true,
+		'fill' => true,
+		'transform' => true,
+	),
+);
 ?>
 
 <div id="<?php echo esc_attr( $prefix . $section_slug ); ?>" class="<?php echo esc_attr( $classes ); ?>">
 	<h2 id="<?php echo esc_attr( $section_slug . '-header' ); ?>"><?php echo esc_html( $section['title'] ); ?></h2>
 
 	<p><?php printf( esc_html( _n( 'This plugin provides %d block.', 'This plugin provides %d blocks.', count( $section_content ), 'wporg-plugins'  ) ), count( $section_content ) ); ?></p>
-	<dl>
-		<?php foreach ( $section_content as $block ) : ?>
-			<dt><?php if ( isset( $block->name ) ) echo esc_html( $block->name ); ?></dt>
-				<dd><?php if ( isset( $block->title ) ) echo esc_html( $block->title ); ?></dd>
+	<ul class="plugin-blocks-list">
+		<?php
+		foreach ( $section_content as $block ) :
+			$block_name = isset( $block->title ) ? $block->title : false;
+			if ( ! $block_name ) {
+				$block_name = isset( $block->name ) ? $block->name : false;
+			}
+			if ( ! $block_name ) {
+				// If we still have no name, we don't have a valid block.
+				continue;
+			}
+			$block_icon = isset( $block->icon ) ? $block->icon : '';
+			$block_classes = 'plugin-blocks-list-item';
+			$block_classes .= isset( $block->description ) ? ' has-description' : '';
+			?>
+			<li class="<?php echo esc_attr( $block_classes ); ?>">
+				<?php if ( false !== strpos( $block_icon, '<svg' ) ) : ?>
+					<span class="block-icon">
+						<?php echo wp_kses( str_replace( '<svg ', '<svg role="img" aria-hidden="true" focusable="false" ', $block_icon ), $allowed_svg ); ?>
+					</span>
+				<?php elseif ( $block_icon ) : ?>
+					<span class="block-icon dashicons dashicons-<?php echo esc_attr( $block->icon ); ?>"></span>
+				<?php else : ?>
+					<span class="block-icon dashicons dashicons-block-default"></span>
+				<?php endif; ?>
+				<span class="block-title"><?php echo esc_html( $block_name ); ?></span>
+				<?php if ( isset( $block->description ) ) : ?>
+					<span class="block-description"><?php echo esc_html( $block->description ); ?></dd>
+				<?php endif; ?>
+			</li>
 		<?php endforeach; ?>
-	</dl>
+	</ul>
 </div>
