2ac48950db56cabb282ea482a11d65dbcb1f6a7f
ryelle <ryelle@git.wordpress.org>
2020-11-18 15:57:04 +0000

    Theme Directory: Bump cachebuster
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@10452 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-themes/functions.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-themes/functions.php
@@ -105,12 +105,12 @@ function wporg_themes_scripts() {
 		$GLOBALS['concatenate_scripts'] = true;
 	}
 
-	wp_enqueue_style( 'wporg-themes', get_theme_file_uri( '/css/style.css' ), [ 'dashicons', 'open-sans' ], '20200708a' );
+	wp_enqueue_style( 'wporg-themes', get_theme_file_uri( '/css/style.css' ), [ 'dashicons', 'open-sans' ], '20201118' );
 	wp_style_add_data( 'wporg-themes', 'rtl', 'replace' );
 
 	if ( ! is_singular( 'page' ) ) {
 		wp_enqueue_script( 'google-charts-loader', 'https://www.gstatic.com/charts/loader.js', array(), null, true );
-		wp_enqueue_script( 'wporg-theme', get_template_directory_uri() . "/js/theme{$suffix}.js", array( 'wp-backbone' ), '20200415', true );
+		wp_enqueue_script( 'wporg-theme', get_template_directory_uri() . "/js/theme{$suffix}.js", array( 'wp-backbone' ), '20201118', true );
 
 		// Use the Rosetta-specific site name. Ie. "WordPress.org $LOCALE"
 		$title_suffix = isset( $GLOBALS['wporg_global_header_options']['rosetta_title'] ) ? $GLOBALS['wporg_global_header_options']['rosetta_title'] : 'WordPress.org';
