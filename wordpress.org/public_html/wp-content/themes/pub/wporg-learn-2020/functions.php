fdaec75c2f8c6f458c6f43da3a2ccbb8e27d5235
Corey McKrill <coreymckrill@git.wordpress.org>
2020-11-20 00:07:27 +0000

    WordPress.org Learn: Sync with GitHub
    https://github.com/WordPress/learn/compare/a6529c2090ca8a1dcc9e36e27460f6cbb84e02ef...2c9bbc5fc4eefb1f1a367f7bb8afcd3da6ef32b0
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@10458 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-learn-2020/functions.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-learn-2020/functions.php
@@ -204,24 +204,6 @@ function wporg_post_type_is_lesson() {
 }
 
 /**
- * Returns the custom field view_lesson_plan_slides_url, if it doesn't exists returns false
- *
- * @return string|bool
- */
-function wporg_get_slides_url() {
-	return get_post_meta( get_the_ID(), 'view_lesson_plan_slides_url', true );
-}
-
-/**
- * Returns the custom field download_lesson_plan_slides_url, if it doesn't exists returns false
- *
- * @return string|bool
- */
-function wporg_get_download_slides_url() {
-	return get_post_meta( get_the_ID(), 'download_lesson_plan_slides_url', true );
-}
-
-/**
  * Modify the excerpt length for our custom post types.
  *
  * @param int $length Excerpt length.
@@ -621,3 +603,19 @@ function wporg_workshop_series_get_adjacent( $which, $workshop = null ) {
 
 	return $siblings[ $index ] ?? false;
 }
+
+/**
+ * Robots "noindex" rules for specific parts of the Learn site.
+ *
+ * @param bool $noindex
+ *
+ * @return bool
+ */
+function wporg_learn_noindex( $noindex ) {
+	if ( is_singular( 'quiz' ) ) {
+		$noindex = true;
+	}
+
+	return $noindex;
+}
+add_filter( 'wporg_noindex_request', 'wporg_learn_noindex' );
