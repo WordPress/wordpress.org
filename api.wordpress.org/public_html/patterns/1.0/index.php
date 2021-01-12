a643fd82fdfd7c1d75985e6a8928d919a9f02819
Ian Dunn <iandunn@git.wordpress.org>
2021-01-12 21:30:56 +0000

    Pattern API: Use `search` param instead of `/search` endpoint.
    Only one post type is being searched, so this is much simpler.
    
    See https://github.com/WordPress/gutenberg/pull/26578#issuecomment-745032557
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@10572 74240141-8908-4e6f-9713-ba540dce6ec7

--- api.wordpress.org/public_html/patterns/1.0/index.php
+++ api.wordpress.org/public_html/patterns/1.0/index.php
@@ -7,14 +7,8 @@ namespace WordPressdotorg\API\Patterns;
  * do any any object caching.
  *
  * todo
- *  publish caching sysreq once query args settled, etc -- https://make.wordpress.org/systems/wp-admin/post.php?post=1788&action=edit
- *
- */
-
-
-/*
- * @todo
- *
+ * - publish caching sysreq once query args settled, etc -- https://make.wordpress.org/systems/wp-admin/post.php?post=1788&action=edit
+ * - add docs to codex
  */
 
 main( $_SERVER['QUERY_STRING'] );
@@ -23,49 +17,34 @@ main( $_SERVER['QUERY_STRING'] );
 /**
  * Proxy w.org/patterns API endpoints for reliability.
  *
- * Core needs to send requests to api.w.org, because it has more resources and better caching than w.org.
+ * Core clients need to send requests to api.w.org, because it has more resources and better caching than w.org.
  *
  * @param string $query_string
  */
 function main( $query_string ) {
 	$api_url_base  = 'https://wordpress.org/patterns/wp-json';
 	$wp_init_query = true;
+	$endpoint      = '/wp/v2/wporg-pattern';
 
+	/*
+	 * Core clients should pass params for the desired action:
+	 *
+	 * @example Browse all:        `/patterns/1.0/`
+	 * @example Browse a category: `/patterns/1.0/?pattern-categories={id}`
+	 * @example Search:            `/patterns/1.0/?search={query}`
+	 */
 	parse_str( $query_string, $query_args );
 
-	switch ( $query_args['action'] ) {
-		// List all patterns, or all with in category.
-		// To restrict to a category, the client needs to provide `pattern-categories={id}` param.
-		default:
-		case 'get_patterns':
-			$endpoint = '/wp/v2/wporg-pattern';
+	/*
+	 * Filter the returned fields down to only the ones Core uses.
+	 *
+	 * `_links` is necessary for `wp:term` to be embedded, see https://core.trac.wordpress.org/ticket/49985.
+	 * Related https://core.trac.wordpress.org/ticket/49538.
+	 */
+	$query_args['_fields'] = 'id,title,content,meta,_links';
+	$query_args['_embed']  = 'wp:term';
 
-			// `_links` is a workaround for https://core.trac.wordpress.org/ticket/49985. Related https://core.trac.wordpress.org/ticket/49538.
-			$query_args['_fields'] = 'id,title,content,meta,_links';
-			$query_args['_embed']  = 'wp:term';
-
-			break;
-
-		// Search patterns.
-		// Client needs to provide `search={string}` param.
-		case 'query_patterns':
-			$endpoint              = '/wp/v2/search';
-			$query_args['subtype'] = 'wporg-pattern';
-
-			// `_links` is a workaround for https://core.trac.wordpress.org/ticket/49985. Related https://core.trac.wordpress.org/ticket/49538.
-			$query_args['_fields'] = '_links';
-			$query_args['_embed']  = 'self';
-
-			break;
-	}
-
-	unset( $query_args['action'] );
-
-	$wp_init_host = $api_url_base . $endpoint;
-
-	if ( $query_args ) {
-		$wp_init_host .= '?' . urldecode( http_build_query( $query_args ) );
-	}
+	$wp_init_host = $api_url_base . $endpoint . '?' . urldecode( http_build_query( $query_args ) );
 
 	// Load WordPress to process the request and output the response.
 	require_once dirname( dirname( __DIR__ ) ) . '/wp-init.php';
--- api.wordpress.org/public_html/patterns/1.0/tests/test-index.php
+++ api.wordpress.org/public_html/patterns/1.0/tests/test-index.php
@@ -39,16 +39,120 @@ class Test_Patterns extends TestCase {
 	}
 
 	/**
+	 * Asserts that an HTTP response is valid and contains a pattern.
+	 *
+	 * @param Requests_Response $response
+	 */
+	public function assertResponseHasPattern( $response ) {
+		$patterns = json_decode( $response->body );
+
+		$this->assertSame( 200, $response->status_code );
+		$this->assertIsString( $patterns[0]->title->rendered );
+		$this->assertIsInt( $patterns[0]->meta->wpop_viewport_width );
+	}
+
+	/**
+	 * Pluck term IDs from a list of patterns.
+	 *
+	 * @param object[] $patterns
+	 *
+	 * @return int[]
+	 */
+	public function get_term_ids( $patterns ) {
+		$term_ids = array();
+
+		foreach ( $patterns as $pattern ) {
+			$term_ids = array_merge(
+				$term_ids,
+				array_column( $pattern->_embedded->{'wp:term'}[0], 'id' )
+			);
+		}
+
+		return array_unique( $term_ids );
+	}
+
+	/**
 	 * @covers ::main()
 	 *
 	 * @group e2e
 	 */
-	public function test_get_all_patterns() : void {
-		$response = $this->send_request( '/?action=get_patterns' );
-		$body     = json_decode( $response->body );
+	public function test_browse_all_patterns() : void {
+		$response = $this->send_request( '/' );
+		$patterns = json_decode( $response->body );
+		$term_ids = $this->get_term_ids( $patterns );
 
-		$this->assertSame( 200, $response->status_code );
-		$this->assertIsString( $body[0]->title->rendered );
-		$this->assertIsInt( $body[0]->meta->wpop_viewport_width );
+		$this->assertResponseHasPattern( $response );
+		$this->assertGreaterThan( 1, count( $term_ids ) );
+	}
+
+	/**
+	 * @covers ::main()
+	 *
+	 * @group e2e
+	 */
+	public function test_browse_category() : void {
+		$query_term_id = 2;
+		$response      = $this->send_request( '/?pattern-categories=' . $query_term_id );
+		$patterns      = json_decode( $response->body );
+		$term_ids      = $this->get_term_ids( $patterns );
+
+		$this->assertResponseHasPattern( $response );
+		$this->assertSame( array( $query_term_id ), $term_ids );
+	}
+
+	/**
+	 * @covers ::main()
+	 *
+	 * @dataProvider data_search_patterns
+	 *
+	 * @group e2e
+	 *
+	 * @param string $search_query
+	 */
+	public function test_search_patterns( $search_term, $match_expected ) : void {
+		$response = $this->send_request( '/?search=' . $search_term );
+		$patterns = json_decode( $response->body );
+
+		if ( $match_expected ) {
+			$this->assertResponseHasPattern( $response );
+
+			$all_patterns_include_query = true;
+
+			foreach ( $patterns as $pattern ) {
+				$match_in_title       = stripos( $pattern->title->rendered, $search_term );
+				$match_in_description = stripos( $pattern->meta->wpop_description, $search_term );;
+
+				if ( ! $match_in_title && ! $match_in_description ) {
+					$all_patterns_include_query = false;
+					break;
+				}
+			}
+
+			$this->assertTrue( $all_patterns_include_query );
+
+		} else {
+			$this->assertSame( 200, $response->status_code );
+			$this->assertSame( '[]', $response->body );
+		}
+	}
+
+	public function data_search_patterns() {
+		return array(
+			'match title' => array(
+				'search_term'    => 'side by side',
+				'match_expected' => true,
+			),
+
+			// todo Enable this once https://github.com/WordPress/pattern-directory/issues/28 is done
+//			'match description' => array(
+//				'search_term'    => 'bright gradient background',
+//				'match_expected' => true,
+//			),
+
+			'no matches' => array(
+				'search_term'    => 'Supercalifragilisticexpialidocious',
+				'match_expected' => false,
+			),
+		);
 	}
 }
