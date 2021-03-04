ac7aeb2ea0225609460d9654c14f5117c1fdc4dc
Scott Reilly <coffee2code@git.wordpress.org>
2021-03-04 21:50:08 +0000

    Handbooks, Init: Improve code formatting for `WPorg_Handbook_Init`.
    * Explicitly declare methods as public
    * Use `public static` instead of the two being reversed
    * Use short array syntax
    * Add function and hook docblocks
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@10744 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/plugins/handbook/inc/init.php
+++ wordpress.org/public_html/wp-content/plugins/handbook/inc/init.php
@@ -7,35 +7,56 @@
 
 class WPorg_Handbook_Init {
 
+	/**
+	 * Returns the post types of all handbooks.
+	 *
+	 * @return array
+	 */
 	public static function get_post_types() {
+		/**
+		 * Filters the handbook post types for creating handbooks.
+		 *
+		 * @param array $handbooks Array of handbook post types. Default 'handbook'.
+		 */
 		return (array) apply_filters( 'handbook_post_types', array( 'handbook' ) );
 	}
 
-	static function init() {
-
+	/**
+	 * Initializes handbooks.
+	 */
+	public static function init() {
 		$post_types = self::get_post_types();
 
+		// Enable table of contents.
 		new WPorg_Handbook_TOC( $post_types );
 
+		// Instantiate each of the handbooks.
 		foreach ( $post_types as $type ) {
 			new WPorg_Handbook( $type );
 		}
 
+		// Enable glossary.
 		WPorg_Handbook_Glossary::init();
 
-		add_action( 'wp_enqueue_scripts', array( __CLASS__, 'enqueue_styles' ) );
-		add_action( 'wp_enqueue_scripts', array( __CLASS__, 'enqueue_scripts' ) );
+		// Enqueue styles and scripts.
+		add_action( 'wp_enqueue_scripts', [ __CLASS__, 'enqueue_styles' ] );
+		add_action( 'wp_enqueue_scripts', [ __CLASS__, 'enqueue_scripts' ] );
 	}
 
-	static public function enqueue_styles() {
-		wp_enqueue_style( 'wporg-handbook-css', plugins_url( 'stylesheets/callout-boxes.css', WPORG_HANDBOOK_PLUGIN_FILE ), array(), '20200121' );
+	/**
+	 * Enqueues styles.
+	 */
+	public static function enqueue_styles() {
+		wp_enqueue_style( 'wporg-handbook-css', plugins_url( 'stylesheets/callout-boxes.css', WPORG_HANDBOOK_PLUGIN_FILE ), [], '20200121' );
 	}
 
-	static public function enqueue_scripts() {
-		wp_enqueue_script( 'wporg-handbook', plugins_url( 'scripts/handbook.js', WPORG_HANDBOOK_PLUGIN_FILE ), array( 'jquery' ), '20150930' );
+	/**
+	 * Enqueues scripts.
+	 */
+	public static function enqueue_scripts() {
+		wp_enqueue_script( 'wporg-handbook', plugins_url( 'scripts/handbook.js', WPORG_HANDBOOK_PLUGIN_FILE ), [ 'jquery' ], '20150930' );
 	}
 
 }
 
-add_action( 'after_setup_theme', array( 'WPorg_Handbook_Init', 'init' ) );
-
+add_action( 'after_setup_theme', [ 'WPorg_Handbook_Init', 'init' ] );
