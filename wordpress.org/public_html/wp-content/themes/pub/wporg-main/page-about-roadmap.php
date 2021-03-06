5334e90ef6a8d4f6f53d3157a95fe5c5035fe975
Sergey Biryukov <sergeybiryukov@git.wordpress.org>
2021-03-06 18:58:23 +0000

    WordPress.org Main Theme: Update Roadmap page content for 2021.
    Props chanthaboune.
    Fixes #5647.
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@10790 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-main/page-about-roadmap.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-main/page-about-roadmap.php
@@ -40,9 +40,10 @@ the_post();
 					<p>
 						<?php
 						printf(
-							/* translators: %s: Link to Gutenberg demo page */
-							wp_kses_post( __( 'WordPress is continually under development. Currently, work is underway on Phase 2 of the Gutenberg project. The Gutenberg project is a reimagination of the way we manage content on the web. Its goal is to broaden access to web presence, which is a foundation of successful modern businesses. Phase 1 was the new block editor, which was released in WordPress 5.0, you can <a href="%s">see that in action here</a>. Throughout 2020 there is a focus on full site editing as we continue to progress through Phase 2.', 'wporg' ) ),
-							esc_url( home_url( '/gutenberg/' ) )
+							/* translators: 1: Link to Gutenberg demo page, 2: Link to Feature Projects Overview page */
+							wp_kses_post( __( 'WordPress is continually under development. Currently, work is underway on Phase 2 of the Gutenberg project. The Gutenberg project is a reimagination of the way we manage content on the web. Its goal is to broaden access to web presence, which is a foundation of successful modern businesses. Phase 1 was the new block editor, which was released in WordPress 5.0, you can <a href="%1$s">see that in action here</a>. In 2021 we’re focusing on merging full site editing (Phase 2) into WordPress which brings block editing to the entire site, not just posts and pages. For more information on full site editing, its components, and other active feature work, check out the <a href="%2$s">Feature Projects Overview</a> page.', 'wporg' ) ),
+							esc_url( home_url( '/gutenberg/' ) ),
+							'https://make.wordpress.org/core/features/'
 						);
 						?>
 					</p>
@@ -51,35 +52,23 @@ the_post();
 						<?php
 						printf(
 							/* translators: %s: Link to make/core blog post */
-							wp_kses_post( __( 'For 2020 the project also has the following 7 priorities, as outlined in <a href="%s">this post</a> by project executive director Josepha Haden:', 'wporg' ) ),
-							'https://make.wordpress.org/core/2019/12/06/update-9-projects-for-2019/'
+							wp_kses_post( __( 'For 2021 the project has some big picture goals, as outlined in <a href="%s">this post</a>:', 'wporg' ) ),
+							'https://make.wordpress.org/updates/2021/01/21/big-picture-goals-2021/'
 						);
 						?>
 					</p>
 
 					<ul>
-						<li><?php esc_html_e( 'Creating a block for navigation menus.', 'wporg' ); ?></li>
-						<li><?php esc_html_e( 'Build a WordPress.org directory for discovering blocks, and a way to seamlessly install them.', 'wporg' ); ?></li>
-						<li><?php esc_html_e( 'Provide a way for users to opt-in to automatic plugin and theme updates.', 'wporg' ); ?></li>
-						<li><?php esc_html_e( 'Provide a way for themes to visually register content areas, and expose them in Gutenberg.', 'wporg' ); ?></li>
-						<li><?php esc_html_e( 'Upgrade the widgets-editing areas and the Customizer to support blocks.', 'wporg' ); ?></li>
-						<li><?php esc_html_e( 'Provide a way for users to opt-in to automatic updates of major Core releases.', 'wporg' ); ?></li>
-						<li>
-							<?php
-							printf(
-								/* translators: %s: Link to open tickets in Core Trac */
-								wp_kses_post( __( 'Form a Triage team to tackle our <a href="%s">6,500 open issues on Trac</a>.', 'wporg' ) ),
-								'https://core.trac.wordpress.org/query?status=!closed'
-							);
-							?>
-						</li>
+						<li><?php echo wp_kses_post( __( '<strong>Full site editing</strong>: Bring into the Gutenberg plugin, and subsequently WordPress Core, the ability to edit all elements of a site using Gutenberg blocks. This will include all in-progress features designed to help existing users transition to Gutenberg as well. Scope/Timeline: MVP in the plugin by April 2021, v1 in Core by WordPress 5.8.', 'wporg' ) ); ?></li>
+						<li><?php echo wp_kses_post( __( '<strong>LearnWP</strong>: Enable WordPress skills-leveling by providing workshops, pre-recorded trainings, and self-serve learning opportunities on learn.wordpress.org. Scope/Timeline: regularly publish new workshops and lesson plans, maintain a high pass rate on workshop quizzes to establish learner success and comprehension.', 'wporg' ) ); ?></li>
+						<li><?php echo wp_kses_post( __( '<strong>Contributor tools</strong>: Decrease the manual overhead of maintenance work for teams through better tooling. Scope/Timeline: Varied, and pending additional testing.', 'wporg' ) ); ?></li>
 					</ul>
 
 					<p>
 						<?php
 						printf(
 							/* translators: %s: https://make.wordpress.org/ */
-							wp_kses_post( __( 'Want to get involved? Head on over to <a href="%s">Make WordPress</a>! We can always use more people to help translate, design, document, develop and market WordPress.', 'wporg' ) ),
+							wp_kses_post( __( 'Want to get involved? Head on over to <a href="%s">Make WordPress</a>! We can always use more people to help translate, design, document, develop, and market WordPress.', 'wporg' ) ),
 							'https://make.wordpress.org/'
 						);
 						?>
@@ -113,14 +102,10 @@ the_post();
 						</tr>
 						<tr>
 							<th>5.8</th>
-							<td><?php echo date_i18n( 'F Y', strtotime( 'Jun 2021' ) ); ?></td>
+							<td><?php echo date_i18n( 'F Y', strtotime( 'Jul 2021' ) ); ?></td>
 						</tr>
 						<tr>
 							<th>5.9</th>
-							<td><?php echo date_i18n( 'F Y', strtotime( 'Sep 2021' ) ); ?></td>
-						</tr>
-						<tr>
-							<th>6.0</th>
 							<td><?php echo date_i18n( 'F Y', strtotime( 'Dec 2021' ) ); ?></td>
 						</tr>
 						</tbody>
@@ -141,7 +126,7 @@ the_post();
 						<?php
 						printf(
 							/* translators: %s: Link to History page */
-							wp_kses_post( __( 'You can see an overview of previous releases on our <a href="%s">history page</a>.', 'wporg' ) ),
+							wp_kses_post( __( 'You can see an overview of past releases on our <a href="%s">history page</a>.', 'wporg' ) ),
 							esc_url( home_url( '/about/history/' ) )
 						);
 						?>
@@ -149,13 +134,7 @@ the_post();
 
 					<h2><?php esc_html_e( 'Long term roadmap', 'wporg' ); ?></h2>
 
-					<p><?php
-						printf(
-							/* translators: %s: link to wordpress.tv State of the Word 2019 */
-							wp_kses_post( __( 'While Phase 2 of Gutenberg is expected to continue at least through 2020, there are already plans for Phase 3 and 4. During the <a href="%s">State of the Word from WordCamp US 2019</a>, Matt shared the following vision for phases in Gutenberg:', 'wporg' ) ),
-							'https://wordpress.tv/2019/11/03/2019-state-of-the-word/' );
-						?>
-					</p>
+					<p><?php esc_html_e( 'Phase 2 of Gutenberg won’t be finished when it’s merged into WordPress. The work to gather feedback and iterate based on user needs will continue after WordPress 5.8 is released. As a reminder, these are the four phases outlined in the Gutenberg project:', 'wporg' ); ?></p>
 
 						<p><?php esc_html_e( 'The Four Phases of Gutenberg', 'wporg' ); ?></p>
 						<ol>
