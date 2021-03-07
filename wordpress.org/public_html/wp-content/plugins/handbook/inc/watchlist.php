e312ad03b8f6fe12bcbc83278eb00d4c9e8fb91b
Scott Reilly <coffee2code@git.wordpress.org>
2021-03-07 20:38:58 +0000

    Handbooks, Watchlist: Improve code formatting and docs for `WPorg_Handbook_Watchlist`.
    * Add and improve docblocks
    * Use short array syntax
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@10791 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/plugins/handbook/inc/watchlist.php
+++ wordpress.org/public_html/wp-content/plugins/handbook/inc/watchlist.php
@@ -1,23 +1,42 @@
 <?php
+/**
+ * Class providing P2/O2 watchlist functionality.
+ *
+ * @package handbook
+ */
 
 class WPorg_Handbook_Watchlist {
 
+	/**
+	 * Memoized array of handbook post types.
+	 *
+	 * @var array
+	 */
 	private static $post_types;
 
+	/**
+	 * Initializes actions.
+	 */
 	public static function init() {
-		add_action( 'init', array( __CLASS__, 'on_init' ) );
-	}
-
-	public static function on_init() {
-		self::$post_types = WPorg_Handbook_Init::get_post_types();
-
-		add_action( 'p2_action_links', array(__CLASS__, 'display_action_link'), 100 );
-		add_filter( 'o2_filter_post_actions', array( __CLASS__, 'add_o2_action_link' ) );
-		add_filter( 'o2_filter_post_action_html', array( __CLASS__, 'get_o2_action_link' ), 10, 2 );
+		add_action( 'init', [ __CLASS__, 'on_init' ] );
 	}
 
 	/**
-	 * Adds a 'Watch' action link to O2
+	 * Performs actions intended to occur during 'init' action.
+	 */
+	public static function on_init() {
+		self::$post_types = WPorg_Handbook_Init::get_post_types();
+
+		add_action( 'p2_action_links',            [ __CLASS__, 'display_action_link' ], 100 );
+		add_filter( 'o2_filter_post_actions',     [ __CLASS__, 'add_o2_action_link' ] );
+		add_filter( 'o2_filter_post_action_html', [ __CLASS__, 'get_o2_action_link' ], 10, 2 );
+	}
+
+	/**
+	 * Adds a 'Watch' action link to O2.
+	 *
+	 * @param array $actions Array of O2 actions.
+	 * @return array
 	 */
 	public static function add_o2_action_link( $actions ) {
 		if ( ! is_user_logged_in() ) {
@@ -50,11 +69,16 @@ class WPorg_Handbook_Watchlist {
 				];
 			}
 		}
+
 		return $actions;
 	}
 
 	/**
-	 * Create the HTML for the watch o2 post action.
+	 * Returns the HTML for the watch o2 post action.
+	 *
+	 * @param string $html   The HTML for the given action.
+	 * @param array  $action Data about the action.
+	 * @return string
 	 */
 	public static function get_o2_action_link( $html, $action ) {
 		if ( 'watch' === $action['action'] ) {
@@ -71,10 +95,9 @@ class WPorg_Handbook_Watchlist {
 	}
 
 	/**
-	 * Adds a 'Watch' action link to P2
+	 * Outputs a 'Watch' action link to P2.
 	 */
 	public static function display_action_link() {
-
 		if ( ! is_user_logged_in() ) {
 			return;
 		}
@@ -82,7 +105,6 @@ class WPorg_Handbook_Watchlist {
 		$post = get_post();
 
 		if ( 'page' == $post->post_type || ( in_array( $post->post_type, self::$post_types ) && ! is_post_type_archive( self::$post_types ) ) ) {
-
 			$watchlist = get_post_meta( $post->ID, '_wporg_watchlist', true );
 
 			echo ' | ';
@@ -100,12 +122,9 @@ class WPorg_Handbook_Watchlist {
 					esc_attr__( 'Get notified about changes to this page', 'wporg' )
 				);
 			}
-
 		}
-
 	}
 
 }
 
 WPorg_Handbook_Watchlist::init();
-
