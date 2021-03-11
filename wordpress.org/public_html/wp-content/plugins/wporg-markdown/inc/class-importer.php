b4faf31e74f111ef6cf7a21c3fee0feb74a20f1a
Scott Reilly <coffee2code@git.wordpress.org>
2021-03-11 20:48:14 +0000

    Markdown Importer: Recognize 'welcome' as a valid root page slug.
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@10810 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/plugins/wporg-markdown/inc/class-importer.php
+++ wordpress.org/public_html/wp-content/plugins/wporg-markdown/inc/class-importer.php
@@ -85,7 +85,7 @@ abstract class Importer {
 		$key = rtrim( str_replace( $this->get_base(), '', get_permalink( $post->ID ) ), '/' );
 		// Account for potential handbook landing page, which results in an empty $key.
 		if ( ! $key ) {
-			if ( in_array( $post->post_name, [ 'handbook', $post->post_type, "{$post->post_type}-handbook" ] ) ) {
+			if ( in_array( $post->post_name, [ 'handbook', $post->post_type, "{$post->post_type}-handbook", 'welcome' ] ) ) {
 				$key = $post->post_name;
 			}
 		}
