67da4dae96fe4b9d8d770d0eaea78bc97877398f
dd32 <dd32@git.wordpress.org>
2021-03-18 03:11:43 +0000

    Slack: Update the `/subgroup` command, take three.
    Shift the response to the `/subgroup` command to async `ephemeral` responses. Unfortunately the `replace_original` flag seems not to work.
    Slack requires that the response to the slash command be a 200 response within 3000ms, unfortunately due to the migration to their newer API's this API now often takes longer than that.
    
    See [10823] and [10824].
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@10828 74240141-8908-4e6f-9713-ba540dce6ec7

--- api.wordpress.org/public_html/dotorg/slack/subgroup.php
+++ api.wordpress.org/public_html/dotorg/slack/subgroup.php
@@ -20,10 +20,45 @@ function api_call( $method, $content = array() ) {
 	return json_decode( $response, true );
 }
 
+// WARNING: Can only be called FIVE times per request.
+function update_text( $text ) {
+	if ( empty( $_POST['response_url'] ) ) {
+		die( "response_url missing." );
+	}
+
+	$payload = [
+		'response_type'    => 'ephemeral',
+		'replace_original' => 'true', // Doesn't work.
+		'text'             => $text,
+	];
+
+	$context = stream_context_create( [ 'http' => [
+		'method'  => 'POST',
+		'header'  => 'Content-Type: application/json',
+		'content' => json_encode( $payload ),
+	] ] );
+
+	file_get_contents( $_POST['response_url'], false, $context );
+}
+
+function die_text( $text ) {
+	update_text( $text );
+	die();
+}
+
 // Confirm it came from Slack.
 if ( $_POST['token'] !== WEBHOOK_TOKEN ) {
 	die( "Invalid Token" );
 }
+
+// This API needs to return a successful 200 ASAP, within 3 seconds, and most of the API calls below will take longer than.
+header( 'HTTP/1.0 200 OK' );
+flush();
+if ( function_exists( 'fastcgi_finish_request' ) ) {
+	fastcgi_finish_request();
+}
+
+update_text( 'Please wait.. This could take a moment..' );
 
 $current_group_id = $_POST['channel_id'];
 
@@ -51,12 +86,12 @@ foreach ( $groups as $group ) {
 
 // Sorry - @wordpressdotorg isn't in this group.
 if ( empty( $found ) ) {
-	die( "@wordpressdotorg isn't here." );
+	die_text( "@wordpressdotorg isn't here." );
 }
 
 // Can't call this from a subgroup.
 if ( $pos = strpos( $group['name'], '-' ) ) {
-	die( sprintf( 'Call this from the main `%s` group.', substr( $group['name'], 0, $pos ) ) );
+	die_text( sprintf( 'Call this from the main `%s` group.', substr( $group['name'], 0, $pos ) ) );
 }
 
 // Get the current groups members.
@@ -71,7 +106,7 @@ $members = api_call(
 
 // Ensure the calling user is in the main group.
 if ( ! in_array( $_POST['user_id'], $members, true ) ) {
-	die;
+	die_text( 'Unauthorized' );
 }
 
 // Commands: create, join, list (default)
@@ -80,7 +115,7 @@ list( $command, $subcommand ) = explode( ' ', $_POST['text'] );
 switch ( $command ) {
 	case 'create':
 		if ( 0 !== strpos( $subcommand, $group['name'] . '-' ) ) {
-			die( "Must create a group that starts with `{$group['name']}-`." );
+			die_text( "Must create a group that starts with `{$group['name']}-`." );
 		}
 
 		// Create the new private channel.
@@ -92,7 +127,7 @@ switch ( $command ) {
 			]
 		);
 		if ( empty( $new_group['ok'] ) ) {
-			die( "Group creation failed. Does it already exist?" );
+			die_text( "Group creation failed. Does it already exist?" );
 		}
 
 		// Post a message to the parent channel about this channels creation.
@@ -121,7 +156,7 @@ switch ( $command ) {
 			]
 		);
 
-		die( sprintf( "Group %s created.", $new_group['group']['name'] ) );
+		die_text( sprintf( "Group %s created.", $new_group['group']['name'] ) );
 
 	case 'invite':
 	case 'join':
@@ -136,11 +171,11 @@ switch ( $command ) {
 					]
 				);
 
-				die( "Invited to {$group['name']}." );
+				die_text( "Invited to {$group['name']}." );
 			}
 		}
 
-		die( "$subcommand group not found." );
+		die_text( "$subcommand group not found." );
 
 	case 'list':
 	default:
@@ -166,11 +201,11 @@ switch ( $command ) {
 		}
 
 		if ( $groups_to_add ) {
-			echo "You may join any of these groups:\n -- `" . implode( "`\n -- `", $groups_to_add ) . "`\n\n\n";
+			$text = "You may join any of these groups:\n -- `" . implode( "`\n -- `", $groups_to_add ) . "`\n\n\n";
 		} else {
-			echo "You are in all {$parent_group['name']} subgroups that the @wordpressdotorg user knows about.\n\n";
+			$text = "You are in all {$parent_group['name']} subgroups that the @wordpressdotorg user knows about.\n\n";
 		}
 
-		die( "*Help:*\n`/subgroup list` - display this message\n`/subgroup join {name}` - join a subgroup\n`/subgroup create {name}` - create a subgroup" );
+		die_text( $text . "*Help:*\n`/subgroup list` - display this message\n`/subgroup join {name}` - join a subgroup\n`/subgroup create {name}` - create a subgroup" );
 }
 
