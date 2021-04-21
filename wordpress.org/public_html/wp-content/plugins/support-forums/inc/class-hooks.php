1700e58c5b77172dc118670903f7636d592a0a46
dd32 <dd32@git.wordpress.org>
2021-04-21 07:58:50 +0000

    Support: Break a users password if they're blocked already and are being re-blocked and the password isn't broken.
    Also, `WP_User::has_role()` is not a method, call `WP_User::has_cap()` instead.
    See https://core.trac.wordpress.org/ticket/53066.
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@10918 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/plugins/support-forums/inc/class-hooks.php
+++ wordpress.org/public_html/wp-content/plugins/support-forums/inc/class-hooks.php
@@ -457,7 +457,7 @@ class Hooks {
 			$robots = true;
 		} elseif ( bbp_is_single_user() ) {
 			// Blocked users are not indexed.
-			if ( bbpress()->displayed_user && bbpress()->displayed_user->has_cap( 'bbp_blocked' ) ) {
+			if ( bbpress()->displayed_user && bbpress()->displayed_user->has_cap( bbp_get_blocked_role() ) ) {
 				$robots = true;
 
 			// Users with no Topics/Replies/Reviews are not indexed.
@@ -1315,7 +1315,10 @@ Log in and visit the topic to reply to the topic or unsubscribe from these email
 		$password_broken = ( 0 === strpos( $user->user_pass, $blocked_prefix ) );
 		$note_text       = false;
 
-		if ( $blocked_role === $new_role && ! $password_broken ) {
+		if (
+			( $blocked_role === $new_role || $user->has_cap( $blocked_role ) ) &&
+			! $password_broken
+		) {
 			// User has been blocked, break their password and sessions.
 			// WordPress doesn't have a way to edit a user password without re-hashing it.
 			$wpdb->update(
@@ -1341,7 +1344,7 @@ Log in and visit the topic to reply to the topic or unsubscribe from these email
 			);
 		} else if (
 			$password_broken &&
-			! $user->has_role( $blocked_role )
+			! $user->has_cap( $blocked_role )
 		) {
 			// User was blocked (broken password) but no longer is.
 			// WordPress doesn't have a way to edit a user password without re-hashing it.
