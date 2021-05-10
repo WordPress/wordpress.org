WordPress/wordpress.org

commit a8c8f6e8058ce6f946b61de2e493108b787f9f48

Login: When blocking users from the Admin UI, reference them by ID rather than by email to simplify some logic.
This also allows switching prior to including bbPress, avoiding issues related to code that sets all users as `bbp_participant` & [10939].

Follow up to [10928].


git-svn-id: https://meta.svn.wordpress.org/sites/trunk@10940 74240141-8908-4e6f-9713-ba540dce6ec7

wordpress.org/public_html/wp-content/themes/pub/wporg-login/admin/ui.php
@@ -203,21 +203,26 @@ add_action( 'admin_post_login_block_account', function() {
 		wp_die();
 	}
 
-	$email = $_REQUEST['email'] ?? '';
+	if ( empty( $_REQUEST['user_id'] ) ) {
+		die();
+	}
 
-	check_admin_referer( 'block_account_' . $email );
+	$user_id = (int) $_REQUEST['user_id'];
 
-	$user = get_user_by( 'email', $email );
-	if ( $user && defined( 'WPORG_SUPPORT_FORUMS_BLOGID' ) ) {
+	check_admin_referer( 'block_account_' . $user_id );
+
+	if ( $user_id && defined( 'WPORG_SUPPORT_FORUMS_BLOGID' ) ) {
+
+		// Switch first so that bbPress loads with the correct context.
+		// This also ensures that the bbp_participant code doesn't kick in.
+		switch_to_blog( WPORG_SUPPORT_FORUMS_BLOGID );
+
 		// Load the support forums.. 
 		include_once WP_PLUGIN_DIR . '/bbpress/bbpress.php';
 		include_once WP_PLUGIN_DIR . '/support-forums/support-forums.php';
 
-		// Then switch to it (Must be done after bbPress is loaded to get roles)
-		switch_to_blog( WPORG_SUPPORT_FORUMS_BLOGID );
-
 		// Set the user to blocked. Support forum hooks will take care of the rest.
-		bbp_set_user_role( $user->ID, bbp_get_blocked_role() );
+		bbp_set_user_role( $user_id, bbp_get_blocked_role() );
 
 		restore_current_blog();
 	}

wordpress.org/public_html/wp-content/themes/pub/wporg-login/admin/class-user-registrations-list-table.php
@@ -243,17 +243,17 @@ class User_Registrations_List_Table extends WP_List_Table {
 			$row_actions['delete'] = '<a href="' . esc_url( $url ) . '">Delete</a>';
 
 		} else {
+			// Account created, find the user.
+			$user = get_user_by( 'login', $item->user_login );
+
 			$url = add_query_arg(
-				'email',
-				urlencode( $item->user_email ),
+				'user_id',
+				urlencode( $user->ID ),
 				admin_url( 'admin-post.php?action=login_block_account' )
 			);
-			$url = wp_nonce_url( $url, 'block_account_' . $item->user_email );
+			$url = wp_nonce_url( $url, 'block_account_' . $user->ID );
 
-			if (
-				! ( $user = get_user_by( 'login', $item->user_login ) ) ||
-				'BLOCKED' !== substr( $user->user_pass, 0, 7 )
-			) {
+			if ( $user && 'BLOCKED' !== substr( $user->user_pass, 0, 7 ) ) {
 				$row_actions['block-account'] = '<a href="' . esc_url( $url ) . '">Block Account</a>';
 			}
 

commit 2ff83e3da3da1642d7ddf2b59142351a3580b817

Login: Fix the block account action to actually apply the bbp_blocked role to the user, in addition to breaking their password.
This is needed as bbPress doesn't appear to set the role correctly without an additional switch, unsure why.. but it works.

Broken in r10940, despite the warning I included originally to flag the bug about needing to switch after inclusion :)


git-svn-id: https://meta.svn.wordpress.org/sites/trunk@10962 74240141-8908-4e6f-9713-ba540dce6ec7

wordpress.org/public_html/wp-content/themes/pub/wporg-login/admin/ui.php
@@ -223,6 +223,11 @@ add_action( 'admin_post_login_block_account', function() {
 		include_once WP_PLUGIN_DIR . '/bbpress/bbpress.php';
 		include_once WP_PLUGIN_DIR . '/support-forums/support-forums.php';
 
+		// bbPress roles still aren't quite right, need to switch away and back..
+		// This is hacky, but otherwise the bbp_set_user_role() call below will appear to succeed, but no role aleration will actually happen.
+		restore_current_blog();
+		switch_to_blog( WPORG_SUPPORT_FORUMS_BLOGID );
+
 		// Set the user to blocked. Support forum hooks will take care of the rest.
 		bbp_set_user_role( $user_id, bbp_get_blocked_role() );
 