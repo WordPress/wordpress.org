b20e80fa2eb6c2235f22e4891e51cfd60a20b373
Dion Hulse <dd32@git.wordpress.org>
2021-05-13 01:58:45 +0000

    Theme Previewer: Vary some transients by theme.
    This varies the `global_styles` transient by theme, as Gutenberg doesn't key it by the active theme.
    
    See https://wordpress.slack.com/archives/C02QB8GMM/p1620836577000400
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@10969 74240141-8908-4e6f-9713-ba540dce6ec7

---
+++ wp-themes.com/public_html/wp-content/mu-plugins/pub/namespace-transients.php
@@ -0,0 +1,35 @@
+<?php
+namespace WordPressdotorg\Theme_Preview\Namespace_Transients;
+/**
+ * Plugin Name: Namespace transients
+ * Description: Namespaces some transients that need to differ between themes.
+ */
+
+function namespace_transients() {
+	$transients_to_key = array(
+		// Gutenberg uses the global_styles transient, but doesn't vary by theme.
+		'global_styles',
+	);
+	foreach ( $transients_to_key as $transient ) {
+		add_filter( "pre_transient_{$transient}",     __NAMESPACE__ . '\get', 10, 2 );
+		add_filter( "pre_set_transient_{$transient}", __NAMESPACE__ . '\set', 10, 3 );
+	}
+}
+namespace_transients();
+
+/**
+ * Namespace a transient to be unique per theme.
+ */
+function get( $value, $transient ) {
+	return get_transient( get_option( 'stylesheet' ) . '_' . $transient ) ?: '';
+}
+
+/**
+ * Namespace a transient set to be unique per theme.
+ */
+function set( $value, $expiration, $transient ) {
+	set_transient( get_option( 'stylesheet' ) . '_' . $transient, $value, $expiration );
+
+	// Return false to cache false in the original transient, can't avoid it being set.
+	return false;
+}
