c6491272cf94921b5de7cfa6b2141b31b751e32a
Dhruv Bhanushali <dhruv_b@live.com>
2021-06-23 19:39:00 +0530

    Add missing punctuation

--- wordpress.org/public_html/wp-content/themes/pub/wporg-openverse/js/iframe_nav.js
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-openverse/js/iframe_nav.js
@@ -2,9 +2,9 @@ document.addEventListener('DOMContentLoaded', () => {
   const { pathname: path, search: query } = window.location; // starts with /
 
   let iframePath = path.replace(openverseSubpath, '').replace(/^\/$/, ''); // Remove Openverse site subpath
-  iframePath = `${openverseUrl}${iframePath}${query}` // Add domain and query
+  iframePath = `${openverseUrl}${iframePath}${query}`; // Add domain and query
 
-  console.log(`Navigating iframe to ${iframePath}`)
+  console.log(`Navigating iframe to ${iframePath}`);
   const iframe = document.getElementById('openverse_embed');
-  iframe.src = iframePath
-})
+  iframe.src = iframePath;
+});
