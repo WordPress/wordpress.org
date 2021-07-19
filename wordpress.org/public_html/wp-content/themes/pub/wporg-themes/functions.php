63bbe8904bf3ba159ed46cd30f81eee85616b7ee
Scott Reilly <coffee2code@git.wordpress.org>
2021-07-19 23:00:30 +0000

    Theme Directory: Bump CSS version for cache bust after [11128].
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@11134 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-themes/functions.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-themes/functions.php
@@ -105,7 +105,7 @@ function wporg_themes_scripts() {
 		$GLOBALS['concatenate_scripts'] = true;
 	}
 
-	wp_enqueue_style( 'wporg-themes', get_theme_file_uri( '/css/style.css' ), [ 'dashicons', 'open-sans' ], '20201118a' );
+	wp_enqueue_style( 'wporg-themes', get_theme_file_uri( '/css/style.css' ), [ 'dashicons', 'open-sans' ], '20210719' );
 	wp_style_add_data( 'wporg-themes', 'rtl', 'replace' );
 
 	if ( ! is_singular( 'page' ) ) {
