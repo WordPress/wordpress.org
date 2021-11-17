814fd5ddd2083c57d4ef3b89eb35ec2cdd7a1045
Kelly Choyce-Dwan <ryelle@git.wordpress.org>
2021-11-17 21:56:16 +0000

    Plugin Directory: Render screenshot gallery directly into page.
    Fixes an issue where the attached click events were not registered, breaking the navigation between images in the slider.
    
    Props sterndata, yaniiliev, paulbigai, collizo4sky, jsmoriss, fernandot, shetheliving, bedas, webdevmattcrom, fierevere.
    Fixes #5952.
    
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@11334 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-plugins/client/screenshots/index.js
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-plugins/client/screenshots/index.js
@@ -1,17 +1,15 @@
 /* global localeData */
 /**
- * External dependencies.
- */
-import PropTypes from 'prop-types';
-import { identity, map } from 'lodash';
-
-/**
  * Internal dependencies.
  */
 import ImageGallery from './image-gallery';
 
-export const Screenshots = ( { screenshots } ) => {
-	const items = map( screenshots, ( { caption, src } ) => ( {
+export const Screenshots = ( { screenshots = [] } ) => {
+	if ( ! screenshots ) {
+		return null;
+	}
+
+	const items = screenshots.map( ( { caption, src } ) => ( {
 		original: src,
 		originalAlt: '',
 		thumbnail: src,
@@ -19,27 +17,12 @@ export const Screenshots = ( { screenshots } ) => {
 		description: caption || false,
 	} ) );
 
-	if ( items ) {
-		return (
-			<div id="screenshots" className="plugin-screenshots">
-				<h2>{ localeData.screenshots }</h2>
-				<ImageGallery items={ items } />
-			</div>
-		);
-	}
-
-	return null;
+	return (
+		<div id="screenshots" className="plugin-screenshots">
+			<h2>{ localeData.screenshots }</h2>
+			<ImageGallery items={ items } />
+		</div>
+	);
 };
 
-Screenshots.propTypes = {
-	screenshots: PropTypes.arrayOf( PropTypes.object ),
-	translate: PropTypes.func,
-};
-
-Screenshots.defaultProps = {
-	screenshots: [],
-	translate: identity,
-};
-
-//export default localize( Screenshots );
 export default Screenshots;
