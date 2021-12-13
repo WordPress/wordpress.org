2d233c33d0d0606494968c145c4e287ba35986b7
Dion Hulse <dd32@git.wordpress.org>
2021-12-13 05:22:29 +0000

    Trac / SVN / Make: Monitor develop.svn.wordpress.org & meta.svn.wordpress.org and import revisions into a custom table, parsing out prop'd users.
    Included are various reports and some work to capture typo's of prop'd users.
    
    The code is definately not DRY, clean, or named properly - but it works.
    
    See #5978.
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@11360 74240141-8908-4e6f-9713-ba540dce6ec7

---
+++ wordpress.org/public_html/wp-content/plugins/admin/list-table.php
@@ -0,0 +1,393 @@
+<?php
+namespace WordPressdotorg\Trac\Watcher;
+use function WordPressdotorg\Trac\Watcher\Trac\format_trac_markup as format_for_trac;
+use WP_List_Table;
+
+if ( ! class_exists( 'WP_List_Table' ) ) {
+    require_once ABSPATH . 'wp-admin/includes/class-wp-list-table.php';
+}
+
+class Commits_List_Table extends WP_List_Table {
+
+	protected $svn;
+
+	public function __construct( $svn_details ) {
+		$this->svn = $svn_details;
+
+		parent::__construct();
+	}
+
+	public function prepare_items( $args = array() ) {
+		global $wpdb;
+
+		$this->_column_headers = [
+			$this->get_columns(),
+			$this->get_hidden_columns(),
+			$this->get_sortable_columns()
+		];
+
+		$rev_table   = $this->svn['rev_table'];
+		$props_table = $this->svn['props_table'];
+		$page        = max( 0, ($args['paged'] ?? 0) -1 );
+		$screen      = get_current_screen();
+		$per_page    = $screen ? $screen->get_option( 'per_page', 'default' ) : 200;
+		$offset      = $per_page * $page;
+
+	//		var_dump( $per_page, get_current_screen()->get_option( 'per_page' ) );
+
+		$o_field = 'id';
+		$o_order = 'DESC';
+		if ( isset( $args['orderby'] ) && !empty( $this->get_sortable_columns()[ $args['orderby'] ] ) ) {
+			$o_field = $args['orderby'];
+		}
+		if ( isset( $args['order'] ) && strtoupper( $args['order'] ) === 'ASC' ) {
+			$o_order = 'ASC';
+		}
+
+		$join  = '';
+		$where = '';
+
+		// Filter to unknown props only.
+		if ( ! empty( $args['unknown-props'] ) ) {
+			$join .= "LEFT JOIN {$props_table} p ON r.id = p.revision";
+			$where .= "AND p.id IS NOT NULL AND p.user_id IS NULL";
+		}
+
+		if ( ! empty( $args['revision'] ) ) {
+			$where .= $wpdb->prepare( ' AND r.id = %d ', $args['revision'] );
+		}
+
+		if ( ! empty( $args['version'] ) ) {
+			$where .= $wpdb->prepare( ' AND version LIKE %s', $wpdb->esc_like( $args['version'] ) . '%' );
+		}
+		if ( ! empty( $args['branch'] ) ) {
+			$where .= $wpdb->prepare( ' AND branch LIKE %s', $wpdb->esc_like( $args['branch'] ) );
+		}
+
+		if ( ! empty( $args['revisions'] ) && preg_match( '!(?P<start>\d+):(?P<end>\d+)!', $args['revisions'], $m ) ) {
+			$where .= $wpdb->prepare( ' AND id BETWEEN %d AND %d', $m['start'], $m['end'] );
+		}
+		if ( ! empty( $args['revisions'] ) && preg_match( '!^[\d,]+$!', $args['revisions'], $m ) ) {
+			$ids = implode(',', array_map( 'intval', explode( ',', $args['revisions'] ) ) );
+			$where .= " AND id IN({$ids})";
+		}
+
+		if ( ! empty( $args['author'] ) ) {
+			$where .= $wpdb->prepare( ' AND author = %s', $args['author'] );
+		}
+
+		if ( ! empty( $args['s'] ) ) {
+			if ( ! $join ) {
+				$join .= "LEFT JOIN {$props_table} p ON r.id = p.revision";
+			}
+			$where .= $wpdb->prepare(
+				' AND ( message LIKE %s OR p.prop_name LIKE %s )',
+				'%' . $wpdb->esc_like( $args['s'] ) . '%',
+				'%' . $wpdb->esc_like( $args['s'] ) . '%',
+			);
+		}
+
+	//		$where .= ' AND r.id IN( 46290, 51195, 50933, 50810 )';
+
+		$this->items = $wpdb->get_results(
+			"SELECT SQL_CALC_FOUND_ROWS r.* FROM {$rev_table} r
+			{$join}
+			WHERE 1=1 {$where}
+			GROUP BY r.id
+			ORDER BY r.{$o_field} {$o_order}
+			LIMIT {$offset},{$per_page}"
+		);
+		$total_items = $wpdb->get_var( 'SELECT FOUND_ROWS()' );
+		$this->fill_props();
+
+		$this->set_pagination_args( [
+			'total_items' => $total_items,
+			'per_page'    => $per_page,
+		] );
+	}
+
+	protected function fill_props() {
+		global $wpdb;
+
+		if ( ! $this->items ) {
+			return;
+		}
+
+		$props_table = $this->svn['props_table'];
+
+		$revisions = wp_list_pluck( $this->items, 'id' );
+		$revisions = implode( ',', array_map( 'intval', $revisions ) );
+
+		$props_list = $wpdb->get_results( $wpdb->prepare(
+			"SELECT revision,user_id,prop_name
+			FROM {$props_table}
+			WHERE revision IN({$revisions})
+			ORDER BY LENGTH(prop_name) DESC
+			"
+		) );
+
+		foreach ( $this->items as $i => $details ) {
+			$this->items[$i]->props = wp_list_pluck(
+				wp_list_filter(
+					$props_list,
+					[
+						'revision' => $details->id,
+					]
+				),
+				'user_id',
+				'prop_name'
+			);
+		}
+
+	}
+
+	public function extra_tablenav( $which ) {
+		global $wpdb;
+		$views = $this->get_views();
+
+		if ( empty( $views ) )
+			return;
+
+		$this->views();
+
+		if ( 'top' === $which ) {
+			echo '<div class="actions alignleft">';
+			if ( 'core' === $this->svn['slug'] ) {
+				echo '<select name="version"><option value="">Version</option>';
+				foreach ( get_wordpress_versions() as $v ) {
+					printf(
+						'<option value="%s" %s>%s</option>',
+						esc_attr( $v ),
+						selected( $_REQUEST['version'] ?? '', $v ),
+						esc_html( $v )
+					);
+				}
+				
+				echo '</select>';
+			}
+
+			$branches = get_branches_for( $this->svn );
+			if ( $branches ) {
+				echo '<select name="branch"><option value="">Branch</option>';
+				foreach ( $branches as $b ) {
+					printf(
+						'<option value="%s" %s>%s</option>',
+						esc_attr( $b ),
+						selected( $_REQUEST['branch'] ?? '', $b ),
+						esc_html( $b )
+					);
+				}
+				echo '</select>';
+			}
+
+			echo '<select name="author"><option value="">Author</option>';
+			$authors = get_transient( $this->svn['slug'] . '_authors' );
+			if ( ! $authors ) {
+				$authors = $wpdb->get_results( 'SELECT author, count(*) as count, max(date), max(date) > DATE_SUB( NOW(), INTERVAL 1 YEAR) as active FROM ' . $this->svn['rev_table'] . ' GROUP BY author ORDER BY author ASC' );
+				set_transient( $this->svn['slug'] . '_authors', $authors, DAY_IN_SECONDS );
+			}
+			$last = 1;
+
+			foreach ( [ 1 => 'Recently Active', 0 => 'Inactive' ] as $active_val => $group ) {
+				printf( '<optgroup label="%s">', esc_attr( $group ) );
+				$_authors = wp_list_filter( $authors, [ 'active' => $active_val ] );
+				foreach ( $_authors as $a ) {
+					printf(
+						'<option value="%s" %s>%s (%s)</option>',
+						esc_attr( $a->author ),
+						selected( $_REQUEST['author'] ?? '', $a->author ),
+						esc_html( $a->author ),
+						esc_html( number_format_i18n( $a->count ) )
+					);
+				}
+				echo '</optgroup>';
+			}
+			echo '</select>';
+
+			echo '<input type="text" name="revisions" placeholder="Revs: 1:HEAD or 1,2,4,5" value="' . esc_attr( $_REQUEST['revisions'] ?? '' ) .'">';
+
+			echo '<input type="submit" class="button button-secondary" value="Filter">';
+			echo '</div>';
+		}
+	}
+
+	function get_views() {
+		$url = add_query_arg( 'page', $_REQUEST['page'], admin_url( 'admin.php' ) );
+
+		$views = [
+			'all' => '<a href="' . esc_url( $url ) . '">All</a>',
+			'unknown-props' => '<a href="' . esc_url( add_query_arg( [ 'unknown-props' => 1 ], $url ) ) . '">Unknown Props</a>',
+		];
+
+		if ( defined( 'WP_CORE_LATEST_RELEASE' ) && 'core' === $this->svn['slug'] ) {
+			$v = sprintf( '%.1f', ((float)WP_CORE_LATEST_RELEASE+0.1) );
+			$views['commits-to-trunk'] = '<a href="' . esc_url( add_query_arg( [ 'version' => $v ], $url ) ) . '">Commits to ' . $v .'</a>';
+		}
+
+		return $views;
+	}
+
+
+	public function get_columns() {
+		$columns = array(
+			'id'      => 'Revision',
+			'author'  => 'Author',
+			'message' => 'Message',
+			'date'    => 'Date',
+			'props'   => 'Props',
+			'branch'  => 'Branch',
+			'version' => 'Version'
+		);
+
+		return $columns;
+	}
+
+	public function get_hidden_columns() {
+		return [
+			'date',
+			'author',
+			'branch',
+			'version',
+		];
+	}
+
+	public function get_sortable_columns() {
+		return [
+			'id' => [
+				'id',
+				false
+			]
+		];
+	}
+
+	public function single_row( $item ) {
+		printf(
+			'<tr class="%s" data-revision="%d" data-svn="%s">',
+			esc_attr( 'revision-' . $item->id ),
+			esc_attr( $item->id ),
+			esc_attr( $this->svn['slug'] )
+		);
+
+		$this->single_row_columns( $item );
+		echo '</tr>';
+	}
+
+	public function column_default( $item, $column_name ) {
+		switch( $column_name ) {
+			case 'id':
+				$output = '';
+				$output .= sprintf(
+					"<p><a href='%s'>%s</a></p>",
+					esc_url( $this->svn['trac'] . '/changeset/' . $item->id ),
+					"[{$item->id}]",
+				);
+				foreach ( [ 'date', 'branch', 'version' ] as $field ) {
+					if ( !empty( $item->{$field} ) ) {
+						$output .= sprintf(
+							'<p><strong>%s</strong> %s</p>',
+							$field,
+							esc_html( $item->{$field} )
+						);
+					}
+				}
+
+				$user = get_user_by( 'login', $item->author );
+				$output .= sprintf(
+					'<br><a href="https://profiles.wordpress.org/%s/">%s %s<br>%s</a>',
+					$user->user_nicename,
+					get_avatar( $user, 32 ),
+					esc_html( $user->display_name ),
+					esc_html( $user->user_login )
+				);
+
+
+				return $output;
+
+			case 'author':
+				$user = get_user_by( 'login', $item->author );
+				return sprintf(
+					'<a href="https://profiles.wordpress.org/%s">%s %s</a>',
+					$user->user_nicename,
+					get_avatar( $user, 32 ),
+					$user->display_name
+				);
+
+			case 'message':
+				$message = format_for_trac( $item->message );
+
+				// Highlight props.
+				foreach ( $item->props as $prop => $user_id ) {
+					$user = $user_id ? get_user_by( 'ID', $user_id ) : false;
+
+					if ( false === stripos( $message, $prop ) ) {
+						$message .= "<em>Missed Prop: $prop</em> ";
+					}
+
+					if ( $user && strtolower( $prop ) != strtolower( $user->user_login ) && strtolower( $prop ) != strtolower( $user->user_nicename ) ) {
+						// User is a typo or mis-prop.
+						$message = str_ireplace( $prop, "<del class='replace'>{$prop}</del><ins>{$user->user_nicename}</ins>", $message );
+					} else {
+						// All else.
+						$tag     = $user ? 'ins' : 'del';
+						$message = str_ireplace( $prop, "<{$tag}>{$prop}</{$tag}>", $message );
+					}
+				}
+
+				return "<div>{$message}</div>";
+
+			case 'props':
+				$output = '<div class="propslist">';
+
+				foreach ( $item->props as $prop => $user_id ) {
+					$user    = $user_id ? get_user_by( 'ID', $user_id ) : false;
+					$avatar  = $user_id ? get_avatar( $user, 32 ) : '<span class="dashicons dashicons-editor-help"></span>';
+					$profile = $user ? 'https://profiles.wordpress.org/' . $user->user_nicename . '/' : '';
+
+					$output .= sprintf(
+						'<span class="user" data-prop="%s" data-user="%s">',
+						esc_attr( $prop ),
+						esc_attr( $user->user_login )
+					);
+					if ( $user ) {
+						$output .= '<a href="' . esc_url( $profile ) . '" target="_blank">';
+					}
+
+					$prop_is_different_from_user = ( $user && strtolower( $prop ) != strtolower( $user->user_login ) && strtolower( $prop ) != strtolower( $user->user_nicename ) );
+					$prop_display_name_different = ( $user && $user->display_name != $prop );
+
+					$output .= $avatar;
+					$output .= $user ? $user->display_name : $prop;
+
+					if ( $prop_is_different_from_user ) {
+						$output .= " <em>typo</em>";
+					} elseif ( false === stripos( $item->message, $prop ) ) {
+						$output .= " <em>missed</em>";
+					}
+
+					if ( $user ) {
+						$output .= '</a>';
+					}
+					$output .= '<div class="overlay"><div class="actions"><a href="#" class="edit dashicons dashicons-edit"></a></div></div>';
+
+					if ( $prop_is_different_from_user || $prop_display_name_different ) {
+						$output .= sprintf(
+							'<br><a href="%s" target="_blank">%s</a>',
+							esc_url( $profile ),
+							esc_html( $user->user_login )
+						);
+					}
+
+					$output .= '</span>';
+				}
+				$output .= '<span class="user add"><a href="#" class="add dashicons dashicons-plus"></a>&nbsp;<a href="#" class="add">Add new</a></span>';
+				$output .= '</div>';
+
+				$output .= '<p class="actions"><a href="#" class="reparse">Reparse</a></p>';
+
+				return $output;
+
+			default:
+				return esc_html( $item->{$column_name} );
+		}
+	}
+}
---
+++ wordpress.org/public_html/wp-content/plugins/admin/reports-page.php
@@ -0,0 +1,309 @@
+<?php
+namespace WordPressdotorg\Trac\Watcher;
+
+function display_reports_page( $details ) {
+	global $wpdb;
+	$url       = add_query_arg( 'page', $_REQUEST['page'], admin_url( 'admin.php' ) );
+	$what      = $_REQUEST['what'] ?? '';
+	$version   = $_REQUEST['version'] ?? (WP_CORE_LATEST_RELEASE+0.1);
+	$revisions = $_REQUEST['revisions'] ?? '';
+	$branch    = $_REQUEST['branch'] ?? '';
+
+	$url = add_query_arg( 'version', $version, $url );
+	?>
+	<div class="wrap">
+		<h2>Reports: <?php echo esc_html( $details['name'] ); ?></h2>
+		<ol>
+			<li><a href="<?php echo $url; ?>&what=contributors">All props matching filter</a></li>
+			<li><a href="<?php echo $url; ?>&what=committers">All Committers matching filter</a></li>
+			<li><a href="<?php echo $url; ?>&what=cloud">Cloud of Props matching filter</a></li>
+			<li><a href="<?php echo $url; ?>&what=typos">Props typos matching filter</a></li>
+			<li><a href="<?php echo $url; ?>&what=raw-contributors-and-committers">All Props+Committers matching filter grouped together</a></li>
+			
+			<li><a href="<?php echo $url; ?>&what=versions-contributed">Versions which users have contributed to. Ignores filter.</a></li>
+		</ol>
+
+		<form>
+			<input type="hidden" name="page" value="<?php echo esc_attr( $_REQUEST['page'] ); ?>">
+			<input type="hidden" name="what" value="<?php echo esc_attr( $what ); ?>">
+
+		<?php
+		if ( 'core' === $details['slug'] ) {
+			echo '<select name="version"><option value="">Version</option>';
+			foreach ( get_wordpress_versions() as $v ) {
+				printf(
+					'<option value="%s" %s>%s</option>',
+					esc_attr( $v ),
+					selected( $version ?? '', $v ),
+					esc_html( $v )
+				);
+			}
+			echo '</select>';
+		}
+
+		$branches = get_branches_for( $details );
+		if ( $branches ) {
+			echo '<select name="branch"><option value="">Branch</option>';
+			foreach ( $branches as $b ) {
+				printf(
+					'<option value="%s" %s>%s</option>',
+					esc_attr( $b ),
+					selected( $branch, $b ),
+					esc_html( $b )
+				);
+			}
+			echo '</select>';
+		}
+
+		// Revision range.
+		echo '<input type="text" name="revisions" placeholder="Revs: 1:50 or 1,2,4,5" value="' . esc_attr( $revisions ) .'">';
+
+		echo '<input type="submit" class="button button-primary" value="Filter">';
+
+		echo '</form>';
+
+		$where = '1=1';
+		if ( ! empty( $version ) ) {
+			$where .= $wpdb->prepare( ' AND r.version LIKE %s', $wpdb->esc_like( $version ) . '%' );
+		}
+
+		// Revisions.
+		if ( ! empty( $revisions ) ) {
+			if ( false !== strpos( $revisions, ':' ) || false !== strpos( $revisions, '-' )  ) {
+				$where .= $wpdb->prepare( ' AND r.id BETWEEN %d AND %d', preg_split( '![-:]!', $revisions ) );
+			} elseif ( false !== strpos( $revisions, ',' ) ) {
+				$ids = implode( ',', array_map( 'intval', explode( ',', $revisions ) ) );
+				$where .= " AND r.id IN({$ids})";
+			}
+		}
+
+		switch( $what ) {
+			case 'cloud':
+				// Get all contributors by Name & Count.
+				$details = $wpdb->get_results(
+					"SELECT u.user_nicename, u.display_name, u.ID, count(*) as count
+					FROM {$details['props_table']} p
+					LEFT JOIN {$details['rev_table']} r ON p.revision = r.id
+					JOIN $wpdb->users u ON p.user_id = u.ID
+					WHERE $where
+					GROUP BY p.user_id"
+				);
+
+				$counts = [];
+				foreach ( $details as $r ) {
+					$counts[] = (object)[
+						'id' => $r->id,
+						'name' => $r->display_name ?: $r->user_nicename,
+						'link' => 'https://profiles.wordpress.org/' . $r->user_nicename,
+						'count' => $r->count
+					];
+				}
+
+				echo wp_generate_tag_cloud( $counts );
+				break;
+			case 'committers':
+				$details = $wpdb->get_results(
+					"SELECT ifnull(u.user_login,r.author) as user_login, ifnull(u.user_nicename,r.author) as user_nicename, u.display_name, u.ID, count(*) as count
+					FROM {$details['rev_table']} r 
+						LEFT JOIN $wpdb->users u ON r.author = u.user_login
+					WHERE $where
+					GROUP BY r.author
+					ORDER BY count DESC"
+				);
+
+				echo '<table class="widefat striped">';
+				echo '<thead><tr><th>Comitter</th><th>Count</th></tr></thead>';
+				foreach ( $details as $c ) {
+					$link = add_query_arg(
+						[
+							'page' => str_replace( 'reports', 'edit', $_REQUEST['page'] ),
+							'author' => $c->user_login,
+						],
+						admin_url( 'admin.php' )
+					);
+
+					printf(
+						'<tr><td><a href="%s">%s</a></td><td><a href="%s">%s</a></td></tr>',
+						'https://profiles.wordpress.org/' . $c->user_nicename . '/',
+						get_avatar( $c->ID, 32 ) . ' ' . ( $c->display_name ?: $c->user_nicename ),
+						$link,
+						$c->count,
+					);
+				}
+				echo '</table>';
+
+
+				break;
+			case 'contributors':
+				$details = $wpdb->get_results(
+					"SELECT p.prop_name, u.user_nicename, u.display_name, u.ID, count(*) as count,
+						GROUP_CONCAT( p.revision ORDER BY p.revision ASC ) as revisions,
+						IFNULL(p.user_id,p.prop_name) as _groupby
+					FROM {$details['props_table']} p
+						LEFT JOIN {$details['rev_table']} r ON p.revision = r.id
+						LEFT JOIN $wpdb->users u ON p.user_id = u.ID
+					WHERE $where
+					GROUP BY _groupby
+					ORDER BY count DESC"
+				);
+
+				echo '<table class="widefat striped">';
+				echo '<thead><tr><th>Contributor</th><th>Count</th><th>Revisions</th></tr></thead>';
+				foreach ( $details as $c ) {
+					$link = add_query_arg(
+						[
+							'page' => str_replace( 'reports', 'edit', $_REQUEST['page'] ),
+							'revisions' => $c->revisions
+						],
+						admin_url( 'admin.php' )
+					);
+					if ( ! $c->ID ) {
+						printf(
+							'<tr><td>%s</td><td>%s</td><td><a href="%s">%s</a></td></tr>',
+							$c->prop_name,
+							$c->count,
+							$link,
+							'[' . str_replace( ',', '] [', $c->revisions ) . ']'
+						);
+						continue;
+					}
+					printf(
+						'<tr><td><a href="%s">%s</a></td><td>%s</td><td><a href="%s">%s</a></td></tr>',
+						'https://profiles.wordpress.org/' . $c->user_nicename . '/',
+						get_avatar( $c->ID, 32 ) . ' ' . ($c->display_name ?: $c->user_nicename),
+						$c->count,
+						$link,
+						'[' . str_replace( ',', '] [', $c->revisions ) . ']'
+					);
+				}
+				echo '</table>';
+
+				break;
+			case 'versions-contributed':
+					$details = $wpdb->get_results(
+						"SELECT prop_name, user_id, COUNT(*) as count, group_concat( version ORDER BY version ASC  SEPARATOR ', ' ) as versions FROM (
+							SELECT distinct LEFT(version, 3) as version, prop_name, p.user_id
+							FROM {$details['props_table']} p
+								JOIN {$details['rev_table']} r ON p.revision = r.id
+							group by prop_name, version
+						) a
+						group by prop_name HAVING COUNT(*) > 2
+						ORDER BY COUNT(*) DESC"
+					);
+
+					echo '<table class="widefat striped">';
+					echo '<thead><tr><th>Prop</th><th>Count</th><th>Versions</th></tr></thead>';
+					foreach ( $details as $p ) {
+						$link = add_query_arg(
+							[
+								'page' => str_replace( 'reports', 'edit', $_REQUEST['page'] ),
+								's' => $p->prop_name,
+							],
+							admin_url( 'admin.php' )
+						);
+	
+						$profile = $p->prop_name;
+						if ( $p->user_id ) {
+							$u = get_user_by( 'ID', $p->user_id );
+							$profile = "<A href='https://profiles.wordpress.org/{$u->user_nicename}/'>" . ( $u->display_name ?: $u->user_login ) . "</a>";
+						}
+
+						printf(
+							'<tr><td>%s</td><td>%s</td><td>%s</td></tr>',
+							$profile,
+							$p->count,
+							$p->versions
+						);
+					}
+					echo '</table>';
+			case 'typos':
+				$details = $wpdb->get_results(
+					"SELECT u.user_login, u.user_nicename,
+						COUNT( * ) as count,
+						group_concat( distinct p.prop_name SEPARATOR ', ' ) as typos,
+						group_concat( p.revision ORDER BY p.revision ASC ) as revisions
+					FROM {$details['props_table']} p
+						LEFT JOIN {$details['rev_table']} r ON p.revision = r.id
+						LEFT JOIN {$wpdb->users} u ON p.user_id = u.ID
+					WHERE $where AND p.prop_name != u.user_login AND p.prop_name != u.user_nicename AND p.prop_name != u.display_name
+					GROUP BY p.user_id
+					ORDER BY count DESC"
+				);
+
+				echo "<p>Props where what's in the commit doesn't match the user Login, Slug, or Display Name.</p>";
+
+				echo '<table class="widefat striped">';
+				echo '<thead><tr><th>Contributor</th><th>Typos</th><th>Count</th><th>Revisions</th></tr></thead>';
+				foreach ( $details as $c ) {
+					$link = add_query_arg(
+						[
+							'page' => str_replace( 'reports', 'edit', $_REQUEST['page'] ),
+							'revisions' => $c->revisions
+						],
+						admin_url( 'admin.php' )
+					);
+					printf(
+						'<tr><td><a href="%s">%s</a></td><td>%s</td><td>%s</td><td><a href="%s">%s</a></td></tr>',
+						'https://profiles.wordpress.org/' . $c->user_nicename . '/',
+						$c->display_name ?: $c->user_nicename,
+						$c->typos,
+						$c->count,
+						$link,
+						'[' . str_replace( ',', '] [', $c->revisions ) . ']'
+					);
+				}
+				echo '</table>';
+
+				break;
+			case 'raw-contributors-and-committers':
+				$details = $wpdb->get_results(
+					"SELECT p.prop_name, u.user_nicename, u.display_name, u.ID,
+					IFNULL(p.user_id,p.prop_name) as _groupby
+						FROM {$details['props_table']} p
+							LEFT JOIN {$details['rev_table']} r ON p.revision = r.id
+							LEFT JOIN {$wpdb->users} u ON p.user_id = u.ID
+						WHERE $where
+						GROUP BY _groupby
+					UNION
+						SELECT r.author as prop_name, u.user_nicename, u.display_name, u.ID, u.ID as _groupby
+						FROM {$details['rev_table']} r
+							LEFT JOIN {$wpdb->users} u ON r.author = u.user_login
+						WHERE $where
+						ORDER BY prop_name ASC"
+				);
+
+
+				echo "<p>Props (Contributors + Committers bunched together) designed to be copy-pasted elsewhere. Set gravatar size via adding <a href='$url&size=96'>&size=96</a> to this URL.</p>";
+
+				echo '<table class="widefat striped">';
+				echo '<thead><tr><th>ID</th><th>Name</th><th>DisplayName</th><th>Profile URL</th><th>Gravatar</th><th>GravURL</th></tr></thead>';
+				foreach ( $details as $c ) {
+					$link = add_query_arg(
+						[
+							'page' => str_replace( 'reports', 'edit', $_REQUEST['page'] ),
+							'revisions' => $c->revisions
+						],
+						admin_url( 'admin.php' )
+					);
+					printf(
+						'<tr><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td></tr>',
+						$c->ID,
+						$c->prop_name,
+						$c->display_name,
+						make_clickable( $c->user_nicename ? 'https://profile.wordpress.org/' . $c->user_nicename . '/' : '' ),
+						$c->ID ? get_avatar( $c->ID, min( 96, $_GET['size'] ?? 32 ) ) : '',
+						make_clickable( $c->ID ? get_avatar_url( $c->ID, [ 'size' => $_GET['size'] ?? 64 ] ) : '' )
+					);
+				}
+				echo '</table>';
+
+				break;
+			default:
+				echo '<p>Nothing but fishies here today.</p>';
+				break;
+		}
+
+		?>
+	</div>
+	<?php
+}
