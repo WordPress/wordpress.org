36326f099399a4bb5bcfe581844e31d51dfdda49
Dion Hulse <dd32@git.wordpress.org>
2022-01-11 08:16:27 +0000

    Main: Remove wporg_meta_robots(), the header no longer supports this.
    See https://github.com/WordPress/wporg-mu-plugins/issues/42
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@11428 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-main/functions.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-main/functions.php
@@ -304,8 +304,3 @@ require_once __DIR__ . '/inc/recaptcha.php';
  * Include the Privacy request functions.
  */
 require_once __DIR__ . '/inc/privacy-functions.php';
-
-/**
- * Include any global functions needed.
- */
-require_once __DIR__ . '/inc/globals.php';
