28faa3d42336e9b4e531681eb558304cd48051a9
Dion Hulse <dd32@git.wordpress.org>
2022-02-15 03:20:14 +0000

    Make: Fix the global header font size to be as specified, rather than 1.05em.
    Also removes the old header CSS overrides.
    
    Fixes https://github.com/WordPress/wporg-mu-plugins/issues/157
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@11555 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-breathe/functions.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-breathe/functions.php
@@ -67,7 +67,7 @@ function styles() {
 	wp_dequeue_style( 'breathe-style' );
 	wp_enqueue_style( 'p2-breathe', get_template_directory_uri() . '/style.css' );
 
-	wp_enqueue_style( 'wporg-breathe', get_stylesheet_uri(), array( 'p2-breathe' ), '20211217' );
+	wp_enqueue_style( 'wporg-breathe', get_stylesheet_uri(), array( 'p2-breathe' ), '20220215' );
 }
 add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\styles', 11 );
 
