ddb27c7ba28275610a0670d5208c69aba9b5d938
Dion Hulse <dd32@git.wordpress.org>
2022-02-21 04:44:13 +0000

    Bad Requests: Update it to account for the new WordPress.org header/footer.
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@11585 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/mu-plugins/pub/wporg-bad-request.php
+++ wordpress.org/public_html/wp-content/mu-plugins/pub/wporg-bad-request.php
@@ -47,6 +47,9 @@ function check_for_invalid_query_vars( $vars, $ref = '$public_query_vars' ) {
 	$query_vars = (new \WP)->public_query_vars;
 	$query_vars[] = 'customize_changeset_uuid';
 	$query_vars[] = 'forum';
+	$query_vars[] = 'action';
+	$query_vars[] = 'bbp_search';
+	$query_vars[] = 'url';
 
 	// Assumption: WP::$public_query_vars will only ever contain non-array query vars.
 	// Assumption invalid. Some fields are valid.
@@ -93,11 +96,16 @@ function check_for_invalid_query_vars( $vars, $ref = '$public_query_vars' ) {
  * Detect invalid parameters being passed to o2.
  */
 add_action( 'wp_ajax_nopriv_o2_read', function() {
-	foreach ( array( 'postId', 'rando', 'scripts', 'styles', 'since', 'method' ) as $field ) {
+	foreach ( array( 'rando', 'scripts', 'styles', 'since', 'method' ) as $field ) {
 		if ( !empty( $_REQUEST[ $field ] ) && ! is_scalar( $_REQUEST[ $field ] ) ) {
 			die_bad_request( "non-scalar input to o2" );
 		}
 	}
+
+	if ( !empty( $_REQUEST['postId'] ) && ! is_numeric( $_REQUEST['postId'] ) ) {
+		die_bad_request( "Bad post ID to o2" );
+	}
+
 	if ( isset( $_REQUEST['queryVars'] ) ) {
 		check_for_invalid_query_vars( $_REQUEST['queryVars'], 'o2 queryVars' );
 	}
@@ -120,51 +128,13 @@ add_action( 'xmlrpc_call', function() {
 /**
  * Detect invalid requests from over hungry vulnerability scanners.
  */
-add_action( 'setup_theme', function() {
+add_action( 'send_headers', function() {
 	if ( isset( $_REQUEST['EGOTEC'] ) ) {
 		die_bad_request( 'EGOTEC request parameter set' );
+	} elseif ( str_contains( $_SERVER['REQUEST_URI'], 'acunetix' ) ) {
+		die_bad_request( 'acunetix request' );
 	}
-
-	// Sorry, this shouldn't be needed.
-	// rate limit bots requesting abnormally-high POST/GET vars
-	// these often include 'pagename' and cause responses to change,
-	// causing the scanner to target even more junk requests to the url.
-	$items = max( count( $_GET ), count( $_POST ) );
-	if (
-		$items >= 8 &&
-		! is_user_logged_in() &&
-		empty( $_SERVER['HTTP_AUTHORIZATION'] ) &&
-		wp_using_ext_object_cache()
-	) {
-		$key   = 'scanner:' . $_SERVER['REMOTE_ADDR'];
-		$group = 'ip-ratelimit-bp2';
-
-		if ( function_exists( 'wp_cache_add_global_groups' ) ) {
-			wp_cache_add_global_groups( $group );
-		}
-
-		$hits = wp_cache_get( $key, $group );
-		if ( false === $hits ) {
-			wp_cache_add( $key, 1, $group, HOUR_IN_SECONDS );
-			return;
-		}
-
-		// Only increment it for high counts, but block on low counts if exceeded.
-		if ( $items > 32 || $hits > 20 ) {
-			$hits = wp_cache_incr( $key, 1, $group );
-		}
-
-		// Abnormally high numbers to be on the 'safe' side.
-		if ( $hits >= 20 ) {
-			// Since they hit the threshold and still kept going.. longer.
-			if ( $hits == 50 ) {
-				wp_cache_set( $key, $hits, $group, DAY_IN_SECONDS );
-			}
-
-			die_bad_request( "Bulk POST query scanner: $key" );
-		}
-	}
-}, 0 );
+} );
 
 /**
  * Detect invalid requests from vulnerability scanners to Jetpack Share by Email forms.
@@ -189,6 +159,23 @@ add_action( 'send_headers', function() {
 } );
 
 /**
+ * Detect invalid requests from vulnerability scanners to Jetpack Contact forms.
+ */
+add_action( 'send_headers', function() {
+	if ( ! $_POST || ! isset( $_REQUEST['action'] ) || 'grunion-contact-form' !== $_REQUEST['action'] ) {
+		return;
+	}
+
+	$combined = serialize( $_POST );
+	if (
+		false !== stripos( $combined, 'email.tst' ) ||
+		false !== stripos( $combined, 'vulnweb' )
+	) {
+		die_bad_request( "Vulnerability testing spam input to Contact Form." );
+	}
+} );
+
+/**
  * Detect invalid admin ajax requests.
  */
 if ( defined( 'DOING_AJAX' ) && DOING_AJAX ) {
@@ -206,26 +193,28 @@ if ( defined( 'DOING_AJAX' ) && DOING_AJAX ) {
  */
 function die_bad_request( $reference = '' ) {
 	// When the user is logged in, log it if possible
-	if ( ! defined( 'WPORG_SANDBOXED' ) || ! WPORG_SANDBOXED ) {
-		if ( function_exists( 'wporg_error_reporter' ) && ! empty( $_COOKIE['wporg_logged_in'] ) ) {
-			wporg_error_reporter( E_USER_NOTICE, "400 Bad Request: $reference", __FILE__, __LINE__ );
-		}
+	if (
+		'production' === wp_get_environment_type() &&
+		function_exists( 'wporg_error_reporter' ) &&
+		! empty( $_COOKIE['wporg_logged_in'] )
+	) {
+		wporg_error_reporter( E_USER_NOTICE, "400 Bad Request: $reference", __FILE__, __LINE__ );
 	}
 
 	// Use a prettier error page on WordPress.org
 	if (
-		false !== stripos( $_SERVER['HTTP_HOST'], 'wordpress.org' ) &&
-		defined( 'WPORGPATH' ) && file_exists( WPORGPATH . '/403.php' ) &&
+		str_contains( $_SERVER['HTTP_HOST'], 'wordpress.org' ) &&
 		! defined( 'XMLRPC_REQUEST' ) && ! defined( 'REST_REQUEST' )
 	) {
-		// Bare header & footer, we don't need all the extras.
-		remove_all_actions( 'wp_head' );
-		remove_all_actions( 'wp_footer' );
-		remove_all_actions( 'body_class' );
-
 		status_header( 400 );
-		$header_set_for_403 = true;
-		include WPORGPATH . '/403.php';
+		get_header();
+
+		echo '<div>
+			<h1>Bad Request</h1>
+			<p>Your request contained query variables that are unexpected. Please contact #meta.</p>
+		</div>';
+
+		get_footer();
 	} else {
 		\wp_die( 'Bad Request: Your request contained query variables that are unexpected. Please contact #meta.', 'Bad Request', [ 'response' => 400 ] );
 	}
