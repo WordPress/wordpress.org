2b87f9b023a4774eb7d8570226060e127df11682
Ian Dunn <iandunn@git.wordpress.org>
2022-04-11 21:14:50 +0000

    Slack: Apply coding standards.
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@11762 74240141-8908-4e6f-9713-ba540dce6ec7

--- api.wordpress.org/public_html/dotorg/slack/props.php
+++ api.wordpress.org/public_html/dotorg/slack/props.php
@@ -7,32 +7,31 @@
  */
 
 namespace {
-	require dirname( dirname( __DIR__ ) ) . '/includes/hyperdb/bb-10-hyper-db.php';
-	require dirname( dirname( __DIR__ ) ) . '/includes/slack-config.php';
+	require dirname( __DIR__, 2 ) . '/includes/hyperdb/bb-10-hyper-db.php';
+	require dirname( __DIR__, 2 ) . '/includes/slack-config.php';
 }
 
 namespace Dotorg\Slack\Props {
+	require dirname( __DIR__, 2 ) . '/includes/slack/props/lib.php';
 
-require dirname( dirname( __DIR__ ) ) . '/includes/slack/props/lib.php';
+	function get_avatar( $username, $slack_id, $team_id ) {
+		global $wpdb;
 
-function get_avatar( $username, $slack_id, $team_id ) {
-	global $wpdb;
+		$wp_user_id = $wpdb->get_var( $wpdb->prepare(
+			"SELECT user_id FROM slack_users WHERE slack_id = %s",
+			$slack_id
+		) );
 
-	$wp_user_id = $wpdb->get_var( $wpdb->prepare(
-		"SELECT user_id FROM slack_users WHERE slack_id = %s",
-		$slack_id
-	) );
+		$email = $wpdb->get_var( $wpdb->prepare(
+			"SELECT user_email FROM $wpdb->users WHERE ID = %d",
+			$wp_user_id
+		) );
 
-	$email = $wpdb->get_var( $wpdb->prepare(
-		"SELECT user_email FROM $wpdb->users WHERE ID = %d",
-		$wp_user_id
-	) );
+		$hash = md5( strtolower( trim( $email ) ) );
+		return sprintf( 'https://secure.gravatar.com/avatar/%s?s=96d=mm&r=G&%s', $hash, time() );
+	}
 
-	$hash = md5( strtolower( trim( $email ) ) );
-	return sprintf( 'https://secure.gravatar.com/avatar/%s?s=96d=mm&r=G&%s', $hash, time() );
-}
-
-if ( constant( __NAMESPACE__ . '\\WEBHOOK_TOKEN' ) === $_POST['token'] ) {
-	echo run( $_POST );
-}
+	if ( constant( __NAMESPACE__ . '\\WEBHOOK_TOKEN' ) === $_POST['token'] ) {
+		echo run( $_POST );
+	}
 }
