861a8dbf0d63635f2cdb3f6ab29047bd06d664c9
Ian Dunn <iandunn@git.wordpress.org>
2022-04-22 19:55:13 +0000

    Props: Add `#props` messages to w.org profiles.
    This helps to give more recogntion for non-code contributions, which we don't track as much as we should.
    
    This won't be enabled until `dotorg/props.php` calls the handler (next week).
    
    See https://github.com/WordPress/five-for-the-future/issues/169
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@11783 74240141-8908-4e6f-9713-ba540dce6ec7

--- common/includes/slack/props/lib.php
+++ common/includes/slack/props/lib.php
@@ -2,6 +2,7 @@
 
 namespace Dotorg\Slack\Props;
 use Dotorg\Slack\Send;
+use function Dotorg\Profiles\{ post as profiles_post };
 
 function show_error( $user ) {
 	return "Please use `/props SLACK_USERNAME MESSAGE` to give props.\n";
@@ -9,6 +10,8 @@ function show_error( $user ) {
 
 /**
  * Receive `/props` request and send to `#props`.
+ *
+ * This is being deprecated in favor of `handle_props_message()`.
  *
  * @param array $data
  * @param bool  $force_test Send to test channel instead of #props
@@ -55,4 +58,179 @@ function run( $data, $force_test = false ) {
 	$send->send( '#props' );
 
 	return sprintf( "Your props to @%s have been sent.\n", $receiver );
+}
+
+/**
+ * Adds props in Slack to w.org profiles.
+ *
+ * Receives webhook notifications for all new messages in `#props`,
+ */
+function handle_props_message( object $request ) : string {
+	if ( ! is_valid_props( $request->event ) ) {
+		return 'Invalid props';
+	}
+
+	$giver_user          = map_slack_users_to_wporg( array( $request->event->user ) )[0];
+	$recipient_slack_ids = get_recipient_slack_ids( $request->event->blocks );
+
+	if ( empty( $recipient_slack_ids ) ) {
+		return 'Nobody was mentioned';
+	}
+
+	$recipient_users = map_slack_users_to_wporg( $recipient_slack_ids );
+	$recipient_ids   = array_column( $recipient_users, 'id' );
+
+	$url = sprintf(
+		'https://wordpress.slack.com/archives/%s/p%s',
+		$request->event->channel,
+		$request->event->ts
+	);
+	$url = filter_var( $url, FILTER_SANITIZE_URL );
+
+	if ( empty( $recipient_ids ) ) {
+		return 'No recipients';
+	}
+
+	// This is Slack's unintuitive way of giving messages a unique ID :|
+	// https://api.slack.com/messaging/retrieving#individual_messages
+	$message_id = sprintf( '%s-%s', $request->event->channel, $request->event->ts );
+	$message    = prepare_message( $request->event->text, $recipient_users );
+
+	add_activity_to_profile( compact( 'giver_user', 'recipient_ids', 'url', 'message_id', 'message' ) );
+
+	// The request was successful from Slack's perspective as long as we received and validated it. Any errors
+	// that occurred when pushing to Profiles are only significant to us.
+	return 'Success';
+}
+
+/**
+ * Determine if this is an event that we should handle.
+ */
+function is_valid_props( object $event ) : bool {
+	$valid_channels = array(
+		'C0FRG66LR'  #props
+	);
+
+	if ( defined( 'WPORG_SANDBOXED' ) && WPORG_SANDBOXED ) {
+		$valid_channels[] = 'C03AKLN7P9U'; #iandunn-testing
+	}
+
+	$has_required_params = isset( $event->channel, $event->blocks, $event->type ) && is_array( $event->blocks );
+	$in_valid_channel    = in_array( $event->channel, $valid_channels, true );
+
+	$is_correct_type =
+		'message' === $event->type &&
+		empty( $event->subtype ) && // e.g., `message.deleted`, `message.changed`.
+		empty( $event->hidden ) &&
+		empty( $event->thread_ts );
+
+	if ( $is_correct_type && $has_required_params && $in_valid_channel ) {
+		return true;
+	}
+
+	return false;
+}
+
+/**
+ * Parse the mentioned Slack user IDs from a message event.
+ *
+ * This assumes that the app is configured to escape usernames.
+ */
+function get_recipient_slack_ids( array $blocks ) : array {
+	$ids = array();
+
+	foreach ( $blocks as $block ) {
+		foreach ( $block->elements as $element ) {
+			foreach ( $element->elements as $inner_element ) {
+				if ( 'user' !== $inner_element->type ) {
+					continue;
+				}
+
+				$ids[] = $inner_element->user_id;
+			}
+		}
+	}
+
+	return $ids;
+}
+
+/**
+ * Find the w.org users associated with the given slack accounts.
+ */
+function map_slack_users_to_wporg( array $slack_ids ) : array {
+	global $wpdb;
+
+	if ( empty( $slack_ids ) ) {
+		return array();
+	}
+
+	$wporg_users     = array();
+	$id_placeholders = implode( ', ', array_fill( 0, count( $slack_ids ), '%s' ) );
+
+	$query = $wpdb->prepare( "
+		SELECT
+			su.slack_id, su.user_id AS wporg_id,
+			mu.user_login
+		FROM `slack_users` su
+			JOIN `minibb_users` mu ON su.user_id = mu.ID
+		WHERE `slack_id` IN( $id_placeholders )",
+		$slack_ids
+	);
+
+	$results = $wpdb->get_results( $query, ARRAY_A );
+
+	foreach ( $results as $user ) {
+		$wporg_users[ $user['slack_id'] ] = array(
+			'id'         => (int) $user['wporg_id'],
+			'user_login' => $user['user_login'],
+		);
+	}
+
+	return $wporg_users;
+}
+
+/**
+ * Replace Slack IDs with w.org usernames, to better fit w.org profiles.
+ */
+function prepare_message( string $original, array $user_map ) : string {
+	$search  = array();
+	$replace = array();
+
+	foreach ( $user_map as $slack_id => $wporg_user ) {
+		$search[]  = sprintf( '<@%s>', $slack_id );
+		$replace[] = '@' . $wporg_user['user_login'];
+	}
+
+	return str_replace( $search, $replace, $original );
+}
+
+/**
+ * Send a request to Profiles to add the activity.
+ *
+ * See `handle_slack_activity()` in `buddypress.org/.../wporg-profiles-activity-handler.php` for the needed args.
+ */
+function add_activity_to_profile( array $request_args ) : bool {
+	require_once dirname( __DIR__, 2 ) . '/profiles/profiles.php';
+
+	$request_args = array_merge(
+		$request_args,
+		array(
+			'action'   => 'wporg_handle_activity',
+			'source'   => 'slack',
+			'activity' => "props_given",
+		)
+	);
+
+	$response_body = profiles_post( $request_args );
+
+	if ( is_numeric( $response_body ) && (int) $response_body > 0 ) {
+		$success = true;
+
+	} else {
+		$success = false;
+
+		trigger_error( 'Adding activity failed with error: ' . $response_body, E_USER_WARNING );
+	}
+
+	return $success;
 }
--- common/includes/slack/props/tests/test-lib.php
+++ common/includes/slack/props/tests/test-lib.php
@@ -1,16 +1,23 @@
 <?php
 
 namespace Dotorg\Slack\Props\Tests;
+use wpdbStub;
 use PHPUnit\Framework\TestCase;
-use function Dotorg\Slack\Props\{ run };
+use function Dotorg\Slack\Props\{ run, is_valid_props, get_recipient_slack_ids, map_slack_users_to_wporg, prepare_message };
 
 /**
  * @group slack
  * @group props
  */
-class Test_Props extends TestCase {
+class Test_Props_Lib extends TestCase {
 	public static function setUpBeforeClass() : void {
 		require_once dirname( __DIR__ ) . '/lib.php';
+	}
+
+	protected static function get_valid_request() {
+		$json = file_get_contents( __DIR__ . '/valid-request.json' );
+
+		return json_decode( $json );
 	}
 
 	/**
@@ -84,6 +91,281 @@ class Test_Props extends TestCase {
 			'username without @' => array(
 				'request'  => $valid_request,
 				'expected' => $success,
+			),
+		);
+
+		return $cases;
+	}
+
+	/**
+	 * @covers ::is_valid_props
+	 * @dataProvider data_is_valid_props
+	 * @group unit
+	 */
+	public function test_is_valid_props( object $event, bool $expected ) : void {
+		$actual = is_valid_props( $event );
+
+		$this->assertSame( $expected, $actual );
+	}
+
+	public function data_is_valid_props() : array {
+		$valid_request = self::get_valid_request();
+
+		$wrong_channel_event = json_decode( json_encode( $valid_request->event ) );
+		$wrong_channel_event->channel = 'C01234567';
+
+		$reaction_event = json_decode( json_encode( $valid_request->event ) );
+		$reaction_event->type = 'reaction_added';
+
+		$deleted_event = json_decode( json_encode( $valid_request->event ) );
+		$deleted_event->subtype = 'message_deleted';
+
+		$hidden_event = json_decode( json_encode( $valid_request->event ) );
+		$hidden_event->hidden = true;
+
+		$thread_event = json_decode( json_encode( $valid_request->event ) );
+		$thread_event->thread_ts = $valid_request->event->ts;
+
+		$cases = array(
+			'missing critical properties' => array(
+				'request'  => (object) array( 'foo' => 'bar' ),
+				'expected' => false,
+			),
+
+			'wrong channel' => array(
+				'request'  => $wrong_channel_event,
+				'expected' => false,
+			),
+
+			'wrong type' => array(
+				'request'  => $reaction_event,
+				'expected' => false,
+			),
+
+			'wrong subtype' => array(
+				'request'  => $deleted_event,
+				'expected' => false,
+			),
+
+			'hidden' => array(
+				'request'  => $hidden_event,
+				'expected' => false,
+			),
+
+			'reply in thread' => array(
+				'request'  => $thread_event,
+				'expected' => false,
+			),
+
+			'valid event' => array(
+				'request'  => $valid_request->event,
+				'expected' => true,
+			),
+		);
+
+		return $cases;
+	}
+
+	/**
+	 * @covers ::get_recipient_slack_ids
+	 * @dataProvider data_get_recipient_slack_ids
+	 * @group unit
+	 */
+	public function test_get_recipient_slack_ids( array $blocks, array $expected ) : void {
+		$actual = get_recipient_slack_ids( $blocks );
+
+		$this->assertSame( $expected, $actual );
+	}
+
+	public function data_get_recipient_slack_ids() : array {
+		$valid_request = self::get_valid_request();
+
+		$cases = array(
+			'empty' => array(
+				'blocks'   => array(),
+				'expected' => array(),
+			),
+
+			'valid' => array(
+				'blocks'   => $valid_request->event->blocks,
+				'expected' => array(
+					'U02RR6SGY',
+					'U02RQHNND',
+					'U3KJ0TK4L',
+					'U4L99HZB6',
+					'U024MFP4L',
+					'U6R2E3Y9Y',
+					'U023GFZJ07L',
+					'U1E5RLU1L',
+				),
+			),
+		);
+
+		return $cases;
+	}
+
+	/**
+	 * @covers ::map_slack_users_to_wporg
+	 * @dataProvider data_map_slack_users_to_wporg
+	 * @group unit
+	 */
+	public function test_map_slack_users_to_wporg( array $slack_ids, array $db_results, array $expected ) : void {
+		global $wpdb;
+
+		$wpdb = $this->createStub( wpdbStub::class );
+		$wpdb->method( 'get_results' )->willReturn( $db_results );
+
+		$actual = map_slack_users_to_wporg( $slack_ids );
+
+		$this->assertSame( $expected, $actual );
+	}
+
+	public function data_map_slack_users_to_wporg() : array {
+		$cases = array(
+			'empty' => array(
+				'slack_ids'  => array(),
+				'db_results' => array(),
+				'expected'   => array(),
+			),
+
+			'valid giver' => array(
+				'slack_ids' => array( 'U02QCF502' ),
+
+				'db_results' => array(
+					array(
+						'slack_id'   => 'U02QCF502',
+						'wporg_id'   => '33690',
+						'user_login' => 'iandunn',
+					),
+				),
+
+				'expected' => array(
+					'U02QCF502' => array(
+						'id'         => 33690,
+						'user_login' => 'iandunn',
+					),
+				),
+			),
+
+			'valid receivers' => array(
+				'slack_ids' => array( 'U02RQHNND', 'U02RR6SGY', 'U3KJ0TK4L', 'U4L99HZB6' ),
+
+				'db_results' => array(
+					array(
+						'slack_id'   => 'U02RQHNND',
+						'wporg_id'   => '297445',
+						'user_login' => 'SergeyBiryukov',
+					),
+
+					array(
+						'slack_id'   => 'U02RR6SGY',
+						'wporg_id'   => '2255796',
+						'user_login' => 'Mamaduka',
+					),
+
+
+					array(
+						'slack_id'   => 'U3KJ0TK4L',
+						'wporg_id'   => '15049054',
+						'user_login' => 'davidbaumwald',
+					),
+
+					array(
+						'slack_id'   => 'U4L99HZB6',
+						'wporg_id'   => '8976791',
+						'user_login' => 'pbiron',
+					),
+				),
+
+				'expected' => array(
+					'U02RQHNND' => array(
+						'id'         => 297445,
+						'user_login' => 'SergeyBiryukov',
+					),
+					'U02RR6SGY' => array(
+						'id'         => 2255796,
+						'user_login' => 'Mamaduka',
+					),
+					'U3KJ0TK4L' => array(
+						'id'         => 15049054,
+						'user_login' => 'davidbaumwald',
+					),
+
+					'U4L99HZB6' => array(
+						'id'         => 8976791,
+						'user_login' => 'pbiron',
+					),
+				),
+			),
+		);
+
+		return $cases;
+	}
+
+	/**
+	 * @covers ::prepare_message
+	 * @dataProvider data_prepare_message
+	 * @group unit
+	 */
+	public function test_prepare_message( string $text, array $user_map, string $expected ) : void {
+		$actual = prepare_message( $text, $user_map );
+
+		$this->assertSame( $expected, $actual );
+	}
+
+	public function data_prepare_message() : array {
+		$valid_request = self::get_valid_request();
+
+		$cases = array(
+			'empty' => array(
+				'text'     => '',
+				'user_map' => array(),
+				'expected' => '',
+			),
+
+			'valid' => array(
+				'text' => $valid_request->event->text,
+				'user_map' => array(
+					'U023GFZJ07L' => array(
+						'id' => 18752239,
+						'user_login' => 'costdev',
+					),
+					'U024MFP4L' => array(
+						'id' => 2545,
+						'user_login' => 'markjaquith',
+					),
+
+					'U02RQHNND' => array(
+						'id' => 297445,
+						'user_login' => 'SergeyBiryukov',
+					),
+
+					'U02RR6SGY' => array(
+						'id' => 2255796,
+						'user_login' => 'Mamaduka',
+					),
+
+					'U1E5RLU1L' => array(
+						'id' => 15152479,
+						'user_login' => 'jeroenrotty',
+					),
+
+					'U3KJ0TK4L' => array(
+						'id' => 15049054,
+						'user_login' => 'davidbaumwald',
+					),
+
+					'U4L99HZB6' => array(
+						'id' => 8976791,
+						'user_login' => 'pbiron',
+					),
+
+					'U6R2E3Y9Y' => array(
+						'id' => 15524609,
+						'user_login' => 'webcommsat',
+					),
+				),
+				'expected' => 'props to @Mamaduka for co-leading 5.9.3 RC 1, to @SergeyBiryukov for running mission control and to @davidbaumwald @pbiron @markjaquith @webcommsat @costdev @jeroenrotty for their help testing the release package :community: :wordpress:',
 			),
 		);
 
