9b58ba265a66832d3eed2fbefdf83ceabc2ae9c7
Ian Dunn <iandunn@git.wordpress.org>
2022-04-25 19:56:34 +0000

    Props: Enable the Props app.
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@11785 74240141-8908-4e6f-9713-ba540dce6ec7

--- api.wordpress.org/public_html/dotorg/slack/props.php
+++ api.wordpress.org/public_html/dotorg/slack/props.php
@@ -1,9 +1,7 @@
 <?php
 
 /*
- * This endpoint receives payloads from the `/props` slash command in `#props`.
- *
- * To manage, browse to https://wordpress.slack.com/apps/manage/custom-integrations > Slash Commands > `/props`.
+ * This endpoint receives requests from the `#props - production` app.
  */
 
 namespace {
@@ -12,6 +10,10 @@ namespace {
 }
 
 namespace Dotorg\Slack\Props {
+	use Exception;
+	use function Dotorg\Slack\{ is_valid_request };
+
+	require dirname( __DIR__, 2 ) . '/includes/slack/helpers.php';
 	require dirname( __DIR__, 2 ) . '/includes/slack/props/lib.php';
 
 	function get_avatar( $username, $slack_id, $team_id ) {
@@ -28,10 +30,28 @@ namespace Dotorg\Slack\Props {
 		) );
 
 		$hash = md5( strtolower( trim( $email ) ) );
+
 		return sprintf( 'https://secure.gravatar.com/avatar/%s?s=96d=mm&r=G&%s', $hash, time() );
 	}
 
 	if ( constant( __NAMESPACE__ . '\\WEBHOOK_TOKEN' ) === $_POST['token'] ) {
-		echo run( $_POST );
+		die( run( $_POST ) );
+	}
+
+	try {
+		$result              = '';
+		$request_body_raw    = file_get_contents( 'php://input' );
+		$request_body_parsed = json_decode( $request_body_raw );
+		$valid_request       = is_valid_request( APP_ID, APP_SIGNING_SECRET, getallheaders(), $request_body_raw );
+
+		if ( $valid_request ) {
+			// Replying with the challenge verifies this handler to Slack.
+			$result = $request_body_parsed->challenge ?? handle_props_message( $request_body_parsed );
+		}
+
+		die( $result );
+
+	} catch ( Exception $exception ) {
+		trigger_error( $exception->getMessage(), E_USER_WARNING );
 	}
 }
