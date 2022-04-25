d18b60589e2358d4b395b52b93d061ee44e8fb46
Ian Dunn <iandunn@git.wordpress.org>
2022-04-25 22:24:42 +0000

    Props: Remove old `/props` handler and tests.
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@11787 74240141-8908-4e6f-9713-ba540dce6ec7

--- common/includes/slack/props/lib.php
+++ common/includes/slack/props/lib.php
@@ -1,64 +1,8 @@
 <?php
 
 namespace Dotorg\Slack\Props;
-use Dotorg\Slack\Send;
+use Exception;
 use function Dotorg\Profiles\{ post as profiles_post };
-
-function show_error( $user ) {
-	return "Please use `/props SLACK_USERNAME MESSAGE` to give props.\n";
-}
-
-/**
- * Receive `/props` request and send to `#props`.
- *
- * This is being deprecated in favor of `handle_props_message()`.
- *
- * @param array $data
- * @param bool  $force_test Send to test channel instead of #props
- *
- * @return string
- */
-function run( $data, $force_test = false ) {
-	$sender = $data['user_name'];
-
-	if ( $data['command'] !== '/props' ) {
-		return "???\n";
-	}
-
-	if ( empty( $data['text'] ) ) {
-		return show_error( $sender );
-	}
-
-	list( $receiver, $message ) = @preg_split( '/\s+/', trim( $data['text'] ), 2 );
-
-	$receiver = ltrim( $receiver, '@' );
-
-	if ( ! strlen( $receiver ) || ! strlen( $message ) ) {
-		return show_error( $sender );
-	}
-
-	// TODO: Add WordPress.org username to $text if different than Slack username.
-	$text = sprintf( "Props to @%s: %s", $receiver, $message );
-
-	$send = new Send( \Dotorg\Slack\Send\WEBHOOK );
-	$send->set_username( $sender );
-	$send->set_text( $text );
-	$send->set_link_names( true ); // We want to the person getting props!
-
-	$get_avatar = __NAMESPACE__ . '\\' . 'get_avatar';
-
-	if ( function_exists( $get_avatar ) ) {
-		$send->set_icon( call_user_func( $get_avatar, $sender, $data['user_id'], $data['team_id'] ) );
-	}
-
-	if ( $force_test ) {
-		$send->testing( true );
-	}
-
-	$send->send( '#props' );
-
-	return sprintf( "Your props to @%s have been sent.\n", $receiver );
-}
 
 /**
  * Adds props in Slack to w.org profiles.
--- common/includes/slack/props/tests/test-lib.php
+++ common/includes/slack/props/tests/test-lib.php
@@ -3,7 +3,7 @@
 namespace Dotorg\Slack\Props\Tests;
 use wpdbStub;
 use PHPUnit\Framework\TestCase;
-use function Dotorg\Slack\Props\{ run, is_valid_props, get_recipient_slack_ids, map_slack_users_to_wporg, prepare_message };
+use function Dotorg\Slack\Props\{ is_valid_props, get_recipient_slack_ids, map_slack_users_to_wporg, prepare_message };
 
 /**
  * @group slack
@@ -18,83 +18,6 @@ class Test_Props_Lib extends TestCase {
 		$json = file_get_contents( __DIR__ . '/valid-request.json' );
 
 		return json_decode( $json );
-	}
-
-	/**
-	 * @covers ::run
-	 * @dataProvider data_run
-	 * @group unit
-	 */
-	public function test_run( array $request, $expected ) : void {
-		$actual = run( $request, true );
-
-		$this->assertSame( $expected, $actual );
-	}
-
-	public function data_run() : array {
-		$valid_request = array(
-			'user_name' => 'iandunn',
-			'text'      => 'kimparsell Thanks for being so welcoming at WordCamp Columbus',
-			'command'   => '/props',
-			'user_id'   => 'U02QCF502',
-			'team_id'   => '1',
-		);
-
-		// Can't call show_error() here since data providers run before `setUp()`.
-		$usage   = "Please use `/props SLACK_USERNAME MESSAGE` to give props.\n";
-		$success = "Your props to @kimparsell have been sent.\n";
-
-		$cases = array(
-			'wrong command' => array(
-				'request' => array_merge(
-					$valid_request,
-					array(
-						'command' => '/here',
-					)
-				),
-
-				'expected' => "???\n",
-			),
-
-			'empty text' => array(
-				'request' => array_merge(
-					$valid_request,
-					array(
-						'text' => '',
-					)
-				),
-
-				'expected' => $usage,
-			),
-
-			'username but no message, or vice versa' => array(
-				'request' => array_merge(
-					$valid_request,
-					array(
-						'text' => 'kimparsell',
-					)
-				),
-
-				'expected' => $usage,
-			),
-
-			'username with @' => array(
-				'request' => array_merge(
-					$valid_request,
-					array(
-						'text' => '@' . $valid_request['text'],
-					)
-				),
-				'expected' => $success,
-			),
-
-			'username without @' => array(
-				'request'  => $valid_request,
-				'expected' => $success,
-			),
-		);
-
-		return $cases;
 	}
 
 	/**
