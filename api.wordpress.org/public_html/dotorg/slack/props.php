eaf1597cc0a650c6490ad60c0e9490e5a55a5256
Ian Dunn <iandunn@git.wordpress.org>
2022-04-25 22:25:48 +0000

    Props: Remove caller for deprecated `/props` command.
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@11788 74240141-8908-4e6f-9713-ba540dce6ec7

--- api.wordpress.org/public_html/dotorg/slack/props.php
+++ api.wordpress.org/public_html/dotorg/slack/props.php
@@ -16,28 +16,6 @@ namespace Dotorg\Slack\Props {
 	require dirname( __DIR__, 2 ) . '/includes/slack/helpers.php';
 	require dirname( __DIR__, 2 ) . '/includes/slack/props/lib.php';
 
-	function get_avatar( $username, $slack_id, $team_id ) {
-		global $wpdb;
-
-		$wp_user_id = $wpdb->get_var( $wpdb->prepare(
-			"SELECT user_id FROM slack_users WHERE slack_id = %s",
-			$slack_id
-		) );
-
-		$email = $wpdb->get_var( $wpdb->prepare(
-			"SELECT user_email FROM $wpdb->users WHERE ID = %d",
-			$wp_user_id
-		) );
-
-		$hash = md5( strtolower( trim( $email ) ) );
-
-		return sprintf( 'https://secure.gravatar.com/avatar/%s?s=96d=mm&r=G&%s', $hash, time() );
-	}
-
-	if ( constant( __NAMESPACE__ . '\\WEBHOOK_TOKEN' ) === $_POST['token'] ) {
-		die( run( $_POST ) );
-	}
-
 	try {
 		$result              = '';
 		$request_body_raw    = file_get_contents( 'php://input' );
