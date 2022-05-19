5f49d763fbec9f7f615bc50da9d8dc56e15f1342
dufresnesteven <steve.dufresne@automattic.com>
2022-05-19 15:17:51 +0900

    Use i18n for label copy.

--- wordpress.org/public_html/wp-content/themes/pub/wporg-themes/blocks/wporg-chart-block/src/frontend/block/actions/index.js
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-themes/blocks/wporg-chart-block/src/frontend/block/actions/index.js
@@ -1,13 +1,18 @@
 /**
+ * WordPress dependencies
+ */
+
+/**
  * Internal dependencies
  */
 import { useAppContext } from '../store/context';
+import { __ } from '@wordpress/i18n';
 
 export default () => {
 	const { startDate, setStartDate } = useAppContext();
 	return (
 		<div className="wporg-chart-block__actions">
-			<label htmlFor="startDate">From:</label>
+			<label htmlFor="startDate">{ __( 'From:', 'wporg' ) }</label>
 			<input
 				type="month"
 				id="startDate"
