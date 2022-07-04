171dee3e8e49a1e9cb91550b5df6b2e5f6f04b14
Ian Dunn <iandunn@git.wordpress.org>
2022-07-04 16:40:35 +0000

    GP Profiles: Ignore duplicate translation actions.
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@11935 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/plugins/wporg-gp-profiles/profiles.php
+++ wordpress.org/public_html/wp-content/plugins/wporg-gp-profiles/profiles.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Plugin name: GlotPress Profiles Activity Notifier
+ * Plugin name: GlotPress: Profiles Activity Notifier
  * Description: Adds activity to profiles.wordpress.org when contributors do noteworthy things.
  * Author:      WordPress.org
  * License:     GPLv2 or later
@@ -9,7 +9,7 @@
 namespace WordPressdotorg\GlotPress\Profiles;
 
 use WordPressdotorg\Profiles as Profiles_API;
-use GP_Translation;
+use GP, GP_Translation;
 
 defined( 'WPINC' ) || die();
 
@@ -23,15 +23,26 @@ if ( 'local' === wp_get_environment_type() ) {
 }
 
 add_action( 'gp_translation_created', __NAMESPACE__ . '\add_translation_activity' );
-add_action( 'gp_translation_saved', __NAMESPACE__ . '\add_translation_activity' );
+add_action( 'gp_translation_saved', __NAMESPACE__ . '\add_translation_activity', 10, 2 );
 
 /**
  * Add a activity when strings are suggested and approved.
  */
-function add_translation_activity( GP_Translation $translation ) : void {
-	if ( 'waiting' === $translation->status ) {
+function add_translation_activity( GP_Translation $new_translation, GP_Translation $previous_translation = null ) : void {
+	$current_user_is_editor = GP::$permission->current_user_can(
+		'approve',
+		'translation-set',
+		$new_translation->translation_set_id
+	);
+
+	/*
+	 * `GP_Route_Translation::translations_post` saves the translation as `waiting` for all users. Then, if the
+	 * user is an editor, it saves it second time to update the status to `current`. We can ignore the first save
+	 * since two avoid two bumps for a single action.
+	 */
+	if ( 'waiting' === $new_translation->status && ! $current_user_is_editor ) {
 		$type = 'glotpress_translation_suggested';
-	} elseif ( 'current' === $translation->status ) {
+	} elseif ( 'current' === $new_translation->status && 'current' !== $previous_translation->status ) {
 		$type = 'glotpress_translation_approved';
 	} else {
 		return;
@@ -41,7 +52,7 @@ function add_translation_activity( GP_Translation $translation ) : void {
 		'action'    => 'wporg_handle_activity',
 		'component' => 'glotpress',
 		'type'      => $type,
-		'user_id'   => $translation->user_id,
+		'user_id'   => $new_translation->user_id,
 	);
 
 	Profiles_API\api( $request_body );
