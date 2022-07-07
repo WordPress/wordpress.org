05b0ae3c891e791b4171ee2495712f25dd717d02
Alex Kirk <akirk@git.wordpress.org>
2022-07-07 13:02:36 +0000

    Translate: Fix broken taxonomy creation for original shadow post
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@11948 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/plugins/gp-translation-helpers/helpers/helper-translation-discussion.php
+++ wordpress.org/public_html/wp-content/plugins/gp-translation-helpers/helpers/helper-translation-discussion.php
@@ -199,7 +199,7 @@ class Helper_Translation_Discussion extends GP_Translation_Helper {
 			return $allcaps;
 		}
 
-		if ( in_array( 'read', $caps, true ) && is_user_logged_in() && ! is_admin() ) {
+		if ( in_array( 'read', $caps, true ) && is_user_logged_in() && ( ! is_admin() || wp_doing_ajax() ) ) {
 			$allcaps['read'] = true;
 		}
 
