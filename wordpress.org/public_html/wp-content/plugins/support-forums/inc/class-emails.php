ebbbd4848b1302424e7b406267165d51e7c0cc63
Dion Hulse <dd32@git.wordpress.org>
2022-07-19 06:01:45 +0000

    Support Forums: Attempt to send notifications for new topics/replies when it's caught within the moderation or spam queue.
    bbPress subscription notifications (and WordPress.org's) are hooked to the `bbp_new_topic` action, however they also verify that the thread is public before notifying.
    Unfortunately, bbPress does not trigger the notifications upon the thread being unspammed/approved, and neither do the WordPress.org notifications.
    
    This fixes this condition by triggering the "new topic created" hook after a topic/reply is unspammed/approved for the first time, causing the subscriptions codes to run as if it's a brand-new thread.
    
    This should fix plugin subscription notifications not being sent to some plugin authors, and topic authors who are unaware of a reply because the responding author was caught in a moderation queue.
    
    It's worth noting that replies still only trigger notifications (matchers, topic/forum subscriptions, @mention's, etc) if the topic is public AND the reply will be public.
    If a reply is added to a pending topic, notifications for that reply will NOT be re-sent, unless the reply was also in a pending state unrelated to the topic and is then approved after the topic is.
    
    See #3674.
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@11978 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/plugins/support-forums/inc/class-emails.php
+++ wordpress.org/public_html/wp-content/plugins/support-forums/inc/class-emails.php
@@ -4,9 +4,12 @@ namespace WordPressdotorg\Forums;
 /**
  * Support Forum Email modifications.
  * 
- * This filters outgoing bbPress emails to 'unroll' subscription emails from a set of BCC's to individual emails.
+ * This provides some overrides to work around limitations / bugs in bbPress related to email.
+ *  1. Filters outgoing bbPress emails to 'unroll' subscription emails from a set of BCC's to individual emails.
+ *  2. Sends subscription emails upon approval for topics/replies.
  */
 class Emails {
+	const SUBSCRIPTIONS_TRIGGER_KEY = '_wporg_trigger_notifications_on_approve';
 
 	public function __construct() {
 		// Forum subscribers. This is also for Term (Plugin/Theme/Tag) subscriptions new topics.
@@ -16,7 +19,21 @@ class Emails {
 		// Topic subscribers. This is also for Term (Plugin/Theme/Tag) subscriptions new replies.
 		add_action( 'bbp_pre_notify_subscribers',  [ $this, 'start_unroll' ] );
 		add_action( 'bbp_post_notify_subscribers', [ $this, 'stop_unroll' ] );
+
+		// Subscriptions - Send subscriptions after approval/unspam.
+		add_action( 'bbp_new_topic', [ $this, 'bbp_new_topic' ], 20, 2 );
+		add_action( 'bbp_new_reply', [ $this, 'bbp_new_reply' ], 20, 3 );
+
+		add_action( 'bbp_unspammed_topic', [ $this, 'maybe_trigger_bbp_new_topic' ], 10, 1 );
+		add_action( 'bbp_approved_topic',  [ $this, 'maybe_trigger_bbp_new_topic' ], 10, 1 );
+		//add_action( 'bbp_untrashed_topic', [ $this, 'maybe_trigger_bbp_new_topic' ], 10, 1 );
+
+		add_action( 'bbp_unspammed_reply', [ $this, 'maybe_trigger_bbp_new_reply' ], 10, 1 );
+		add_action( 'bbp_approved_reply',  [ $this, 'maybe_trigger_bbp_new_reply' ], 10, 1 );
+		//add_action( 'bbp_untrashed_reply', [ $this, 'maybe_trigger_bbp_new_reply' ], 10, 1 );
 	}
+
+	// --- Email BCC unrolling...
 
 	/**
 	 * Attached to the filter to start filtering outgoing emails on.
@@ -107,4 +124,80 @@ class Emails {
 		// We've sent the emails, we return the last emails result here, even tho calling function probably won't look at it.
 		return $filter_return;
 	}
+
+	// --- bbPress not sending notifications if it's caught in spam/moderation queue
+
+	/**
+	 * Add postmeta to new unpublished replies, to send notifications once it's published.
+	 */
+	public function bbp_new_reply( $reply_id, $topic_id, $forum_id ) {
+		if ( bbp_is_reply_published( $reply_id ) && bbp_is_topic_public( $topic_id ) ) {
+			return;
+		}
+
+		add_post_meta( $reply_id, self::SUBSCRIPTIONS_TRIGGER_KEY, 1 );
+	}
+
+	/**
+	 * Add postmeta to new unpublished topics, to send notifications once it's published.
+	 */
+	public function bbp_new_topic( $topic_id, $forum_id ) {
+		if ( bbp_is_topic_public( $topic_id ) ) {
+			return;
+		}
+
+		add_post_meta( $topic_id, self::SUBSCRIPTIONS_TRIGGER_KEY, 1 );
+	}
+
+	/**
+	 * Send forum subscriptions for a previously-unpublished topic.
+	 */
+	public function maybe_trigger_bbp_new_topic( $topic_id ) {
+		if ( ! get_post_meta( $topic_id, self::SUBSCRIPTIONS_TRIGGER_KEY, true) ) {
+			return;
+		}
+
+		if ( ! bbp_is_topic_public( $topic_id ) ) {
+			return;
+		}
+
+		$forum_id     = bbp_get_topic_forum_id( $topic_id );
+		$topic_author = bbp_get_reply_author_id( $reply_id );
+
+		// Remove the bbPress topic update handler
+		remove_action( 'bbp_new_topic', 'bbp_update_topic' );
+
+		// Call the bbp_new_topic action..
+		do_action( 'bbp_new_topic', $topic_id, $forum_id, array(), $topic_author );
+
+		add_action( 'bbp_new_topic', 'bbp_update_topic', 10, 5 ); // bbPress requests 5, but calls it with 4..
+
+		delete_post_meta( $topic_id, self::SUBSCRIPTIONS_TRIGGER_KEY );
+	}
+
+	/**
+	 * Send topic subscriptions for a previously-unpublished topic.
+	 */
+	public function maybe_trigger_bbp_new_reply( $reply_id ) {
+		if ( ! get_post_meta( $reply_id, self::SUBSCRIPTIONS_TRIGGER_KEY, true ) ) {
+			return;
+		}
+
+		$topic_id     = bbp_get_reply_topic_id( $reply_id );
+		$forum_id     = bbp_get_topic_forum_id( $topic_id );
+		$reply_author = bbp_get_reply_author_id( $reply_id );
+
+		if ( ! bbp_is_reply_published( $reply_id ) || ! bbp_is_topic_public( $topic_id ) ) {
+			return;
+		}
+
+		remove_action( 'bbp_new_reply', 'bbp_update_reply' );
+
+		do_action( 'bbp_new_reply', $reply_id, $topic_id, $forum_id, array(), $reply_author, false, false );
+
+		add_action( 'bbp_new_reply',  'bbp_update_reply', 10, 7 );
+
+		delete_post_meta( $reply_id, self::SUBSCRIPTIONS_TRIGGER_KEY );
+	}
+
 }
