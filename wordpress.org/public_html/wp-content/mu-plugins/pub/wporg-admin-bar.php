43144eb3135c556d2e4468b1670724de7073fda6
Dion Hulse <contact@dd32.id.au>
2022-08-30 17:03:50 +1000

    Remove the Logo and Search nodes from the admin bar

--- wordpress.org/public_html/wp-content/mu-plugins/pub/wporg-admin-bar.php
+++ wordpress.org/public_html/wp-content/mu-plugins/pub/wporg-admin-bar.php
@@ -1,29 +1,20 @@
 <?php
+namespace WordPressdotorg\MU_Plugins\Admin_Bar;
 /**
  * Customizations to the Admin Bar across all networks.
  */
 
 /**
- * Makes Logo and About menu items link to the localised w.org/about/ page for all users.
+ * Removes the Search, Logo, and About menu from the admin bar for all users.
  *
  * @param WP_Admin_Bar $wp_admin_bar WP_Admin_Bar instance, passed by reference.
  */
-function wporg_about_links_in_admin_bar( $wp_admin_bar ) {
-	$nodes = [
-		$wp_admin_bar->get_node( 'wp-logo' ),
-		$wp_admin_bar->get_node( 'about' ),
-	];
-
-	$about_url = 'https://wordpress.org/about/';
-
-	if ( class_exists( 'Rosetta_Sites' ) && is_object( $GLOBALS['rosetta'] ) ) {
-		$about_url = 'https://' . $GLOBALS['rosetta']->current_site_domain . '/about/';
+function admin_bar_menu( $wp_admin_bar ) {
+	if ( is_admin() ) {
+		return;
 	}
 
-	foreach ( $nodes as $node ) {
-		$node->href = $about_url;
-
-		$wp_admin_bar->add_node( $node );
-	}
+	$wp_admin_bar->remove_node( 'wp-logo' );
+	$wp_admin_bar->remove_node( 'search' );
 }
-add_action( 'admin_bar_menu', 'wporg_about_links_in_admin_bar', 11 );
+add_action( 'admin_bar_menu', __NAMESPACE__ . '\admin_bar_menu', 11 );
