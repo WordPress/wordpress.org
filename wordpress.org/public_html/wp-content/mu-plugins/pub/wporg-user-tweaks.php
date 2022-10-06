f8df1f6b802432439388d35c1324220d10d4dc61
Dion Hulse <dd32@git.wordpress.org>
2022-10-06 01:52:36 +0000

    Support Forums: Ensure that reported topics have a value displayed for who reported a topic.
    This fixes `get_the_author_meta( 'display_name' )` not returning a value when the WP_User object has an empty display name.
    
    See #4644.
    Fixes #6500.
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@12103 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/mu-plugins/pub/wporg-user-tweaks.php
+++ wordpress.org/public_html/wp-content/mu-plugins/pub/wporg-user-tweaks.php
@@ -22,6 +22,17 @@ add_filter( 'user_display_name', function( $name, $user_id, $context ) {
 	return $name;
 }, 1, 3 );
 
+// Filter `get_the_author_meta()`.
+add_filter( 'get_the_author_display_name', function( $name, $user_id ) {
+	if ( '' === $name ) {
+		$name = get_user_by( 'id', $user_id )->user_nicename;
+	}
+
+	$name = maybe_replace_blocked_user_name( $name, $user_id );
+
+	return $name;
+}, 1, 2 );
+
 // bbPress skips user filtering and does it's own
 add_filter( 'bbp_get_displayed_user_field', function( $value, $field, $filter ) {
 	if ( 'edit' === $filter ) {
