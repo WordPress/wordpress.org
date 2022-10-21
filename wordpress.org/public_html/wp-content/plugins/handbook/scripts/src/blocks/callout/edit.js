35b70ab8ac7a938cc89b05d85ddb57a97482d787
Jonathan Bossenger <jonathanbossenger@gmail.com>
2022-10-21 13:38:25 +0200

    Adds support for lists to callout blocks
    When using callout blocks on learn.wordpress.org, it would be ideal if they supported lists as well as paragraphs.
    Sometimes it is necessary to use lists of instructions in a callout block, for a tutorial or lesson, and adding list support would
    greatly improve the benefit of using a callout.

--- wordpress.org/public_html/wp-content/plugins/handbook/scripts/src/blocks/callout/edit.js
+++ wordpress.org/public_html/wp-content/plugins/handbook/scripts/src/blocks/callout/edit.js
@@ -7,7 +7,7 @@ const CalloutEdit = ( { attributes } ) => {
 	const className = `callout callout-${ attributes.type }`;
 	return (
 		<div className={ className }>
-			<InnerBlocks allowedBlocks={ [ 'core/paragraph' ] } />
+			<InnerBlocks allowedBlocks={ [ 'core/paragraph', 'core/list' ] } />
 		</div>
 	);
 };
