31d89e3f2671fa092c0341db88a0303efcfb4f4a
Dion Hulse <dd32@git.wordpress.org>
2022-10-31 05:21:27 +0000

    Plugin Directory: Define some default values for the Plugin_Import cron tasks. Not all triggers pass all these fields which are later relied upon.
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@12167 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/plugins/plugin-directory/jobs/class-plugin-import.php
+++ wordpress.org/public_html/wp-content/plugins/plugin-directory/jobs/class-plugin-import.php
@@ -33,13 +33,12 @@ class Plugin_Import {
 	public static function cron_trigger( $plugin_data ) {
 		$plugin_slug  = $plugin_data['plugin'];
 
-		if ( ! isset( $plugin_data['tags_touched'] ) ) {
-			$plugin_data['tags_touched'] = array( 'trunk' );
-		}
-
-		if ( ! isset( $plugin_data['revisions'] ) ) {
-			$plugin_data['revisions'] = [ 0 ];
-		}
+		// Set some default values if not included from the caller.
+		$plugin_data['tags_touched']   ??= array( 'trunk' );
+		$plugin_data['revisions']      ??= [ 0 ];
+		$plugin_data['readme_touched'] ??= true;
+		$plugin_data['code_touched']   ??= true;
+		$plugin_data['assets_touched'] ??= true;
 
 		$tags_touched = $plugin_data['tags_touched'];
 		$revision     = max( (array) $plugin_data['revisions'] );
