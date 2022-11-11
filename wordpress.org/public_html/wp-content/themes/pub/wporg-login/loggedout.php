5a573d2a6a4c8fdb48e22787226618e6308d8ce1
Dion Hulse <dd32@git.wordpress.org>
2022-11-11 05:18:42 +0000

    Login: On the logout confirmation screen, link back to where the user previously was.
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@12241 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-login/loggedout.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-login/loggedout.php
@@ -10,6 +10,26 @@ get_header();
 
 <p class="center"><?php _e( 'You are now logged out.', 'wporg' ); ?></p>
 
+<?php
+	$redirect_to = wp_unslash( $_REQUEST['redirect_to'] ?? '' );
+	$redirect_to = wp_sanitize_redirect( $redirect_to );
+	$redirect_to = wp_validate_redirect( $redirect_to );
+
+	if ( $redirect_to ) {
+		$hostname = parse_url( $redirect_to, PHP_URL_HOST );
+
+		echo '<p>&nbsp;</p>';
+		echo '<p class="center">';
+		printf(
+			/* translators: 1: url, 2: Hostname, ie. wordcamp.org */
+			__( 'Return to <a href="%1$s">%2$s</a>.', 'wporg' ),
+			esc_url( $redirect_to ),
+			esc_html( $hostname )
+		);
+		echo '<p>';
+	}
+?>
+
 <p id="nav">
 	<a href="/"><?php _e( '&larr; Back to login', 'wporg' ); ?></a> &nbsp; • &nbsp;
 	<a href="<?php echo wporg_login_wordpress_url(); ?>"><?php _e( 'WordPress.org', 'wporg' ); ?></a>
