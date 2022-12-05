df75854cf9c4326517d626f23ec8fd77b5d0ce97
Dion Hulse <dd32@git.wordpress.org>
2022-12-05 02:17:16 +0000

    Support Forums: Customise the Blocks Everywhere plugin slightly, and shift enabling the plugin from constants to filters.
    Props johnny5 for filters.
    See #6608.
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@12294 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/plugins/support-forums/inc/class-blocks.php
+++ wordpress.org/public_html/wp-content/plugins/support-forums/inc/class-blocks.php
@@ -2,12 +2,73 @@
 
 namespace WordPressdotorg\Forums;
 
+/**
+ * Customizations for the Support Forum and the Blocks Everywhere plugin.
+ *
+ * To enable this file to be loaded on a bbPress install, activate the Blocks Everywhere plugin.
+ */
 class Blocks {
 	public function __construct() {
-		add_action( 'wp_enqueue_scripts', array( $this, 'enqueue_scripts' ) );
+		// Enable bbPress support.
+		add_filter( 'blocks_everywhere_bbpress', '__return_true' );
+
+		// Enable block processing in emails.
+		add_filter( 'blocks_everywhere_email', '__return_true' );
+
+		// Enable theme compatibility CSS.
+		add_filter( 'blocks_everywhere_theme_compat', '__return_true' );
+
+		// Any Javascript / CSS tweaks needed.
+		add_action( 'wp_enqueue_scripts', [ $this, 'enqueue_scripts' ] );
+
+		// Theme Tweaks, these should be moved to the theme.
+		add_filter( 'after_setup_theme', [ $this, 'after_setup_theme' ] );
+
+		// Customize blocks for the Support Forums.
+		add_filter( 'blocks_everywhere_editor_settings', [ $this, 'editor_settings' ] );
 	}
 
 	public function enqueue_scripts() {
-		wp_enqueue_script( 'wporg-bbp-blocks', plugins_url( '/js/blocks.js', __DIR__ ), array( 'wp-block-editor', 'jquery' ), filemtime( dirname( __DIR__ ) . '/js/blocks.js' ), true );
+		wp_enqueue_script(
+			'wporg-bbp-blocks',
+			plugins_url( '/js/blocks.js', __DIR__ ),
+			[ 'wp-block-editor', 'jquery' ],
+			filemtime( dirname( __DIR__ ) . '/js/blocks.js' ),
+			true
+		);
+	}
+
+	public function after_setup_theme() {
+		// This will make embeds resize properly.
+		add_theme_support( 'responsive-embeds' );
+	}
+
+	public function editor_settings( $settings ) {
+		// This adds the image block, but only with 'add from url' as an option.
+		$settings['iso']['blocks']['allowBlocks'][] = 'core/image';
+
+		// Allows embeds and might fix pasting links sometimes not working.
+		$settings['iso']['blocks']['allowBlocks'][] = 'core/embed';
+
+		// Adds a table of contents button in the toolbar.
+		$settings['toolbar']['toc'] = true;
+
+		// Adds a navigation button in the toolbar.
+		$settings['toolbar']['navigation'] = true;
+
+		// This will display a support link in an ellipsis menu in the top right of the editor.
+		$settings['iso']['moreMenu'] = true;
+		$settings['iso']['linkMenu'] = [
+			[
+				/* translators: Link title to the WordPress Editor support article. */
+				'title' => __( 'Help & Support', 'wporg' ),
+				/* translators: Link to the WordPress Editor article, used as the forum 'Help & Support' destination. */
+				'link' => __( 'https://wordpress.org/support/article/wordpress-editor/', 'wporg' ),
+			]
+		];
+
+		// TODO: To modify the available embeds modify $settings['iso']['allowEmbeds']
+
+		return $settings;
 	}
 }
