2ab68c51734809b9200a6f157bf523e59e3e4acb
Dion Hulse <dd32@git.wordpress.org>
2022-12-20 05:41:00 +0000

    wp-themes.com: Don't display patterns with third party blocks.
    Unprops dd32 for missing this in [12349].
    
    Props dufresnesteven.
    See #6420.
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@12351 74240141-8908-4e6f-9713-ba540dce6ec7

--- wp-themes.com/public_html/wp-content/plugins/pattern-page/inc/pattern-endpoint.php
+++ wp-themes.com/public_html/wp-content/plugins/pattern-page/inc/pattern-endpoint.php
@@ -2,15 +2,91 @@
 
 namespace WordPressdotorg\PatternPreview\PatternEndpoint;
 
-function endpoint_handler() {
-	$patterns = \WP_Block_Patterns_Registry::get_instance()->get_all_registered();
+/**
+ * Recurses the blocks and returns block names.
+ *
+ * @param WP_Block $block
+ * @param array    $names
+ * @return array List of block names.
+ */
+function get_all_block_names( $block, $names = array() ) {
+	// HTML blocks can have a null block name
+	// It won't have any innerBlocks.
+	if ( empty( $block['blockName'] ) ) {
+		return $names;
+	}
 
-	$theme_patterns = array_filter(
-		$patterns,
-		function ( $pattern ) {
-			return ! str_starts_with( $pattern['name'], 'core/' ) && ( ! isset( $pattern['inserter'] ) || false !== $pattern['inserter'] );
+	$names[] = $block['blockName'];
+
+	// Blocks have inner blocks that we need to recurse through
+	foreach ( $block['innerBlocks'] as $inner_block ) {
+		return get_all_block_names( $inner_block, $names );
+	}
+
+	return $names;
+}
+
+/**
+ * Return whether the block or its innerBlocks contains a non core block.
+ *
+ * @param WP_Block $block
+ * @return boolean
+ */
+function has_unsupported_block( $block ) {
+	$block_names = get_all_block_names( $block );
+
+	// We only want "core" blocks.
+	$unsupported = array_filter(
+		$block_names,
+		function ( $block_name ) {
+			return ! str_starts_with( $block_name, 'core' );
 		}
 	);
+
+	return ! empty( $unsupported );
+}
+
+/**
+ * Returns patterns that only contain core blocks.
+ *
+ * @param array[] $patterns List of block patterns
+ * @return array[]
+ */
+function get_supported_patterns( $patterns ) {
+	$supported_patterns = array();
+
+	foreach ( $patterns as $pattern ) {
+		$blocks       = parse_blocks( $pattern['content'] );
+		$is_supported = true;
+
+		// Filter out any core patterns
+		if ( str_starts_with( $pattern['name'], 'core/' ) ) {
+			$is_supported = false;
+		}
+
+		// Filter out any "no inserter" patterns
+		if ( isset( $pattern['inserter'] ) && false === $pattern['inserter'] ) {
+			$is_supported = false;
+		}
+
+		// Filter out patterns with unsupported blocks
+		foreach ( $blocks as $block ) {
+			if ( has_unsupported_block( $block ) ) {
+				$is_supported = false;
+			}
+		}
+
+		if ( $is_supported ) {
+			$supported_patterns[] = $pattern;
+		}
+	}
+
+	return $supported_patterns;
+}
+
+function endpoint_handler() {
+	$patterns = \WP_Block_Patterns_Registry::get_instance()->get_all_registered();
+	$theme_patterns = get_supported_patterns( $patterns );
 
 	/**
 	 * Add links since the links have internal business logic.
