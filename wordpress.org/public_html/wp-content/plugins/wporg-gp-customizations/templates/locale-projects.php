17c659687f9777bf5b0345ebc52b996097606cd3
JesÃºs Amieiro <amieiro@git.wordpress.org>
2023-03-02 10:40:31 +0000

    Translate: Update the URL in the filter for the main pattern project
    If the user opens the main project, we will not use any URL in the 
    GlotPress filter.
    
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@12439 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/plugins/wporg-gp-customizations/templates/locale-projects.php
+++ wordpress.org/public_html/wp-content/plugins/wporg-gp-customizations/templates/locale-projects.php
@@ -186,7 +186,12 @@ if ( isset( $pages ) && $pages['pages'] > 1 ) {
 			}
 		} elseif ( 'Patterns' == $project->name ) {
 			$prefix = 'https://translate.wordpress.org/projects/patterns/core';
-			$suffix = '?filters%5Bterm%5D=https%3A%2F%2Fwordpress.org%2Fpatterns%2Fpattern%2F' . $sub_project->slug . '%2F&filters%5Bterm_scope%5D=scope_any&filters%5Bstatus%5D=current_or_waiting_or_fuzzy_or_untranslated_or_rejected_or_changesrequested_or_old&filters%5Buser_login%5D=&filter=Apply+Filters&sort%5Bby%5D=priority&sort%5Bhow%5D=desc';
+			if ( 'patterns' == $sub_project->slug ) {
+				// Remove the URL from the filter for the main Patterns project so that it shows all strings.
+				$suffix = '?filters%5Bterm%5D=&filters%5Bterm_scope%5D=scope_any&filters%5Bstatus%5D=current_or_waiting_or_fuzzy_or_untranslated_or_rejected_or_changesrequested_or_old&filters%5Buser_login%5D=&filter=Apply+Filters&sort%5Bby%5D=priority&sort%5Bhow%5D=desc';
+			} else {
+	 			$suffix = '?filters%5Bterm%5D=https%3A%2F%2Fwordpress.org%2Fpatterns%2Fpattern%2F' . $sub_project->slug . '%2F&filters%5Bterm_scope%5D=scope_any&filters%5Bstatus%5D=current_or_waiting_or_fuzzy_or_untranslated_or_rejected_or_changesrequested_or_old&filters%5Buser_login%5D=&filter=Apply+Filters&sort%5Bby%5D=priority&sort%5Bhow%5D=desc';
+			}
 			$project_url = $prefix . '/' . $locale_slug . '/' . $set_slug . '/' . $suffix;
 			$project_name = $sub_project->name;
 		} else {
