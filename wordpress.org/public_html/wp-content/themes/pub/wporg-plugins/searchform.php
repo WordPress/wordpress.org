3edaa8714fffb80ba5d5d3970d90c4d601d7803d
Kelly Dwan <ryelle@users.noreply.github.com>
2023-05-03 18:24:21 -0400

    Plugins: Add a filter bar to switch between business models

--- wordpress.org/public_html/wp-content/themes/pub/wporg-plugins/searchform.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-plugins/searchform.php
@@ -14,5 +14,9 @@ namespace WordPressdotorg\Plugin_Directory\Theme;
 	<?php if ( get_query_var( 'block_search' ) || 'block' === get_query_var( 'browse' ) ) : ?>
 		<input type="hidden" value="1" name="block_search" />
 	<?php endif; ?>
-	<button class="button button-primary button-search"><i class="dashicons dashicons-search"></i><span class="screen-reader-text"><?php esc_html_e( 'Search plugins', 'wporg-plugins' ); ?></span></button>
+	<button class="button button-primary button-search">
+		<svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" focusable="false"><path fill-rule="evenodd" clip-rule="evenodd" d="M17.5 11.5a4 4 0 1 1-8 0 4 4 0 0 1 8 0Zm1.5 0a5.5 5.5 0 0 1-9.142 4.121l-3.364 2.943-.988-1.128 3.373-2.952A5.5 5.5 0 1 1 19 11.5Z" /></svg>
+		<span class="screen-reader-text"><?php esc_html_e( 'Search plugins', 'wporg-plugins' ); ?></span>
+	</button>
 </form>
+
