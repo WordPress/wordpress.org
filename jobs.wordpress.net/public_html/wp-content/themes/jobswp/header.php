dc227a8e0d3b1281b6315d6ed77c0e04dad7c9af
Scott Reilly <coffee2code@git.wordpress.org>
2023-06-13 18:51:50 +0000

    Jobs theme: Add GTM.
    Props joostdevalk.
    See #6483.
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@12654 74240141-8908-4e6f-9713-ba540dce6ec7

--- jobs.wordpress.net/public_html/wp-content/themes/jobswp/header.php
+++ jobs.wordpress.net/public_html/wp-content/themes/jobswp/header.php
@@ -14,11 +14,20 @@
 <title><?php wp_title( '|', true, 'right' ); ?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+<!-- Google Tag Manager -->
+<link rel="dns-prefetch" href="//www.googletagmanager.com"/>
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-P24PF4B');</script>
+<!-- End Google Tag Manager -->
 
 <?php wp_head(); ?>
 </head>
 
 <body <?php body_class(); ?>>
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P24PF4B" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <div id="page" class="hfeed site">
 	<?php do_action( 'before' ); ?>
 	<header id="masthead" class="site-header" role="banner">
