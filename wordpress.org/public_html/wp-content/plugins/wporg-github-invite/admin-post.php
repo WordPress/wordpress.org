43b4702528199d97573b59d52df2ec18c0c945fa
Dion Hulse <dd32@git.wordpress.org>
2023-06-28 02:11:16 +0000

    Make: GitHub Invite Tool: Allow specifying a GitHub user URL, and if the user is a member of the org already, just add them to the team(s).
    See #7082.
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@12678 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/plugins/wporg-github-invite/admin-post.php
+++ wordpress.org/public_html/wp-content/plugins/wporg-github-invite/admin-post.php
@@ -20,44 +20,62 @@ add_action( 'admin_post_github_invite', function() {
 	$team_ids = array_intersect( $team_ids, get_allowed_teams() );
 	$team_ids = array_map( 'intval', $team_ids );
 
-	$updated = 'success';
-	$message = null;
-	$invite  = false;
+	$updated         = 'success';
+	$message         = null;
+	$invite          = false;
+	$github_username = false;
 
 	if ( ! $team_ids ) {
 		$updated = 'error';
 		$message = 'No teams selected';
-	} elseif (
-		preg_match( '!^https://profiles.wordpress.org/(?<slug>[^/]+)!i', $input, $m ) ||
-		! is_email( $input )
-	) {
-		$user           = get_user_by( 'slug', $m['slug'] ?? $input );
-		$github_details = json_decode( $wpdb->get_var( $wpdb->prepare(
-			'SELECT user_details FROM wporg_github_users WHERE user_id = %d',
-			$user->ID
-		) ) );
-
-		if ( ! $user || ! $github_details ) {
-			$updated = 'no-github';
-		} else {
-			$invite = $github_details->id;
-		}
 	} elseif ( is_email( $input ) ) {
 		$invite = $input;
+	} elseif ( preg_match( '!^https://github.com/(?<slug>[^/]+)!i', $input, $m ) ) {
+		$gh_user         = api( '/users/' . $m['slug'] );
+		$github_username = $gh_user->login ?? false;
+		$invite          = $gh_user->id ?? 0;
 	} else {
-		$updated = 'error';
+		preg_match( '!^https://profiles.wordpress.org/(?<slug>[^/]+)!i', $input, $m );
+
+		$user           = get_user_by( 'slug', $m['slug'] ?? $input );
+		$github_details = $wpdb->get_row( $wpdb->prepare(
+			'SELECT github_user, user_details FROM wporg_github_users WHERE user_id = %d',
+			$user->ID
+		) );
+		if ( $github_details ) {
+			$github_details->user_details = json_decode( $github_details->user_details );
+		}
+
+		if ( ! $user || ! $github_details || ! $github_details->user_details ) {
+			$updated = 'no-github';
+		} else {
+			$invite          = $github_details->user_details->id;
+			$github_username = $github_details->github_user;
+		}
 	}
 
 	if ( $invite ) {
 		$result = invite_member( $invite, $team_ids );
 
-		if ( $result->id ) {
-			// Note that it was invited via this site..
-			$invited_gh_users = get_option( 'invited_gh_users', [] );
-			$invited_gh_users[] = $result->id;
-			update_option( 'invited_gh_users', $invited_gh_users );
+		if (
+			is_wp_error( $result ) &&
+			$github_username &&
+			'Invitee is already a part of this org' === $result->get_error_data( 'api_error' )->errors[0]->message ?? ''
+		) {
+			// Already a GitHub member, just add them to the team.
+			$result  = add_to_team( $github_username, $team_ids );
+			$message = 'User already a GitHub member, Added to team(s).';
+		}
 
-			delete_site_transient( 'gh_invites' );
+		if ( ! is_wp_error( $result ) ) {
+			if ( ! empty( $result->id ) ) {
+				// Note that it was invited via this site..
+				$invited_gh_users = get_option( 'invited_gh_users', [] );
+				$invited_gh_users[] = $result->id;
+				update_option( 'invited_gh_users', $invited_gh_users );
+
+				delete_site_transient( 'gh_invites' );
+			}
 
 			// Log it to Slack.
 			$teams          = get_teams();
