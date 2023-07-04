88ba1bd37f5e24437c751f74cd462469cb8880d9
Sergey Biryukov <sergeybiryukov@git.wordpress.org>
2023-07-04 21:50:30 +0000

    Slack: Give announce to cbringmann, chanthaboune, francina, and metalandcoffee in `#6-4-release-leads`, as Release Coordinators for WordPress 6.4.
    See https://wordpress.slack.com/archives/C02QB8GMM/p1688493168237659.
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@12710 74240141-8908-4e6f-9713-ba540dce6ec7

--- common/includes/slack/announce/config.php
+++ common/includes/slack/announce/config.php
@@ -37,6 +37,12 @@ function get_whitelist() {
 	);
 
 	return array(
+		'6-4-release-leads' => array(
+			'cbringmann', // @Chloé Bringmann on Slack
+			'chanthaboune',
+			'francina',
+			'metalandcoffee',
+		),
 		'accessibility' => array(
 			// #core (inc committers) already included via get_parent_channel().
 			'afercia',
