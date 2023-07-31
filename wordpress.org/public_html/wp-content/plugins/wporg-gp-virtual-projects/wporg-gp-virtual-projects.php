31ab241b69dffc291060c3a73b6ebe65cab021fc
Jesús Amieiro <1667814+amieiro@users.noreply.github.com>
2023-07-31 17:29:34 +0200

    Update the public methods to use with the new filter and action

--- wordpress.org/public_html/wp-content/plugins/wporg-gp-virtual-projects/wporg-gp-virtual-projects.php
+++ wordpress.org/public_html/wp-content/plugins/wporg-gp-virtual-projects/wporg-gp-virtual-projects.php
@@ -12,8 +12,8 @@ class WPorg_Virtual_Projects {
 	public function __construct() {
 		add_filter( 'gp_subprojects', array( $this, 'add_virtual_projects' ), 10, 1 );
 		add_filter( 'gp_get_project_status', array( $this, 'gp_get_project_status' ), 10, 4 );
-		add_filter( 'gp_translation_table_classes', array( $this, 'gp_translation_table_classes' ) );
-		add_action( 'gp_translation_table_after', array( $this, 'add_pattern_preview' ), 10, 2 );
+		add_filter( 'gp_translation_table_classes', array( $this, 'add_translation_table_classes' ), 10, 2 );
+		add_action( 'gp_after_translation_table', array( $this, 'add_pattern_preview' ), 10, 1 );
 	}
 
 	/**
@@ -155,57 +155,83 @@ class WPorg_Virtual_Projects {
 	}
 
 	/**
+	 * Adds a class to the translations table for the preview of the virtual projects.
+	 *
+	 * @param string $classes  The classes for the table.
+	 * @param array  $def_vars The defined variables.
+	 *
+	 * @return mixed|string
+	 */
+	public function add_translation_table_classes( $classes, $def_vars ) {
+		$project_name = $def_vars['project']->name;
+		$search_term  = $def_vars['filters']['term'];
+		if ( ! $this->is_pattern_project( $project_name, $search_term ) ) {
+			return $classes;
+		}
+
+		return $classes . ' table-virtual-pattern';
+	}
+
+	/**
 	 * Adds a pattern preview in the translations table.
 	 *
-	 * @param array $get_defined_vars   The defined variables.
+	 * @param array $get_defined_vars The defined variables.
 	 *
-	 * @return string
+	 * @return void
 	 */
-	public function add_pattern_preview( $get_defined_vars ) {
-		$translations_table = $get_defined_vars['translations_table'];
-		$search_term        = $get_defined_vars['filters']['term'];
-		$project_name       = $get_defined_vars['project']->name;
+	public function add_pattern_preview( array $get_defined_vars ) {
+		$search_term  = $get_defined_vars['filters']['term'];
+		$project_name = $get_defined_vars['project']->name;
 
-		if ( ! $_GET['pattern_preview'] ) {
-			return $translations_table;
-		}
-
-		if ( '' == $search_term || 'Patterns' != $project_name ) {
-			return $translations_table;
-		}
-
-		// If the search term is not a pattern, return the original table.
-		if ( false === strpos( $search_term, 'https://wordpress.org/patterns/pattern/' ) ) {
-			return $translations_table;
+		if ( ! $this->is_pattern_project( $project_name, $search_term ) ) {
+			return;
 		}
 
 		// Get the content for the pattern preview.
 		$preview_pattern_url = $search_term . '?view=true';
 		$preview_content     = @file_get_contents( $preview_pattern_url );
 		if ( ! $preview_content ) {
-			return $translations_table;
+			return;
 		}
 
-		// Add the 'table-virtual-pattern' class to the table.
-		$dom = new DomDocument();
-		@$dom->loadHTML( $translations_table, LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD );
-		$table           = $dom->getElementById( 'translations' );
-		$current_classes = $table->getAttribute( 'class' );
-		$table->setAttribute( 'class', $current_classes . ' table-virtual-pattern' );
-
-		// Merge the table with the preview.
-		$new_content  = utf8_decode( $dom->saveHTML( $dom->documentElement ) );
-		$new_content .= '<div class="preview-virtual-pattern">';
+		// Create the div with the pattern preview.
+		$new_content  = '<div class="preview-virtual-pattern">';
 		$new_content .= '<h2>' . esc_html__( 'Pattern preview', 'glotpress' ) . '</h2>';
 		$new_content .= '<p class="link-to-pattern"><a href="' . $search_term . '" target="_blank">' . esc_html__( 'View the pattern in a new tab', 'glotpress' ) . '</a></p>';
 		$new_content .= $preview_content;
 		$new_content .= '</div>';
 		$new_content .= '<div class="clear"></div>';
-		return $new_content;
+		// phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+		echo $new_content;
 	}
 
 	/**
-	 * Gets teh number of translations in a status for a set of original ids.
+	 * Checks if the current page is ok to show a pattern preview.
+	 *
+	 * @param string $project_name The project name.
+	 * @param string $search_term  The search term.
+	 *
+	 * @return bool
+	 */
+	private function is_pattern_project( $project_name, $search_term ) {
+		if ( ! isset( $_GET['pattern_preview'] ) ) {
+			return false;
+		}
+
+		if ( '' == $search_term || 'Patterns' != $project_name ) {
+			return false;
+		}
+
+		// If the search term is not a pattern, return the original table.
+		if ( false === strpos( $search_term, 'https://wordpress.org/patterns/pattern/' ) ) {
+			return false;
+		}
+
+		return true;
+	}
+
+	/**
+	 * Gets the number of translations in a status for a set of original ids.
 	 *
 	 * @param array  $original_ids       The original id for this pattern.
 	 * @param int    $translation_set_id The translation set for the pattern project and locale.
