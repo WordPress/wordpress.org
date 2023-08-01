c49f5e666d654fa618840dd500ad5f37329c6e1b
Scott Reilly <coffee2code@git.wordpress.org>
2023-08-01 22:17:14 +0000

    Photo Directory, Admin JS/CSS: Highlight taxonomy metaboxes lacking any set terms.
    See #7171.
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@12775 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/plugins/photo-directory/assets/js/admin.js
+++ wordpress.org/public_html/wp-content/plugins/photo-directory/assets/js/admin.js
@@ -22,4 +22,72 @@ document.addEventListener('DOMContentLoaded', function () {
 	if ( orientation_value ) {
 		document.getElementById('photo_orientationdiv').hidden = true;
 	}
+
+	// Highlight custom taxonomies lacking terms.
+	highlightCustomTaxonomiesWithoutTerms();
 }, false);
+
+/**
+ * Highlights when a custom taxonomy lacks at least one term assignment.
+ */
+function highlightCustomTaxonomiesWithoutTerms() {
+	// Highlight/unhighlight custom taxonomies lacking any term assignments.
+	const customTaxMetaboxes = [
+		['photo_categorydiv', 'tax_input[photo_category][]', 'checkbox'],
+		['photo_colordiv', 'tax_input[photo_color][]', 'checkbox'],
+		['photo_orientationdiv', 'tax_input[photo_orientation][]', 'checkbox'],
+		['tagsdiv-photo_tag', 'newtag[photo_tag]', 'text', '.tagchecklist'],
+	];
+
+	customTaxMetaboxes.forEach(element => {
+		// Decide on highlight initially.
+		setMetaboxHighlight(element);
+
+		// Listen for changes in value to re-determine highlight.
+		document.querySelectorAll('input[name="' + element[1] + '"]').forEach(item => {
+			item.addEventListener('input', customTaxChangeCB);
+		});
+
+		// The tag list is handled differently. Re-determine as tags are added/removed.
+		if (element[3]) {
+			const observer = new MutationObserver(function(mutations_list) {
+				mutations_list.forEach(function(mutation) {
+					if (mutation.addedNodes.length > 0 || mutation.removedNodes.length > 0) {
+						setMetaboxHighlight(element);
+					}
+				});
+			});
+
+			observer.observe(document.querySelector(element[3]), { subtree: false, childList: true });
+		}
+	});
+
+	// Callback to call setMetaboxHighlight() for proper event targets.
+	function customTaxChangeCB(e) {
+		const name = e.target.name;
+		let customTaxMetabox = customTaxMetaboxes.find(n => { return n[1] === name; });
+		if (!customTaxMetabox) {
+			return;
+		}
+		setMetaboxHighlight(customTaxMetabox)
+	}
+
+	// Highlights or unhighlights a metabox based on presence of terms.
+	function setMetaboxHighlight(customTaxMetabox) {
+		[metaboxID, inputName, inputType, tagListClass] = customTaxMetabox;
+		const missingTaxClass = 'photo-missing-taxonomy';
+		const div = document.getElementById(metaboxID);
+
+		let selector = 'input[name="' + inputName + '"]';
+		if ('checkbox' === inputType) {
+			selector += ':checked';
+		}
+		let value = document.querySelector(selector)?.value;
+		// If tagListClass is present, see if it has any values if one not already found.
+		if (!value && tagListClass) {
+			value = document.querySelector(tagListClass).hasChildNodes();
+		}
+
+		value ? div?.classList.remove(missingTaxClass) : div?.classList.add(missingTaxClass);
+	}
+}
