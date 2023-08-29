add7a035edf82f971c4712dc94532aa76118ea0a
Scott Reilly <coffee2code@git.wordpress.org>
2023-08-29 19:42:59 +0000

    Photo Directory, Admin: Remove the likes column (output by Jetpack) from the listing of pending photos.
    See #7171.
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@12867 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/plugins/photo-directory/inc/admin.php
+++ wordpress.org/public_html/wp-content/plugins/photo-directory/inc/admin.php
@@ -29,7 +29,7 @@ class Admin {
 		add_action( "manage_{$post_type}_posts_custom_column", [ __CLASS__, 'handle_photo_column_data' ], 10, 2 );
 		add_filter( "manage_{$post_type}_posts_columns",       [ __CLASS__, 'add_flags_column' ] );
 		add_action( "manage_{$post_type}_posts_custom_column", [ __CLASS__, 'handle_flags_column_data' ], 10, 2 );
-		add_filter( "manage_edit-{$post_type}_columns",        [ __CLASS__, 'remove_photo_colors_column' ] );
+		add_filter( "manage_edit-{$post_type}_columns",        [ __CLASS__, 'remove_columns_from_pending_photos' ] );
 		add_filter( 'post_row_actions',                        [ __CLASS__, 'add_post_action_photo_links' ], 10, 2 );
 		add_filter( 'the_author',                              [ __CLASS__, 'add_published_photos_count_to_author' ] );
 		add_filter( 'use_block_editor_for_post_type',          [ __CLASS__, 'disable_block_editor' ], 10, 2 );
@@ -369,21 +369,24 @@ class Admin {
 	}
 
 	/**
-	 * Removes the colors column from the listing of pending photos.
+	 * Removes certain columns from the listing of pending photos.
 	 *
-	 * Currently colors aren't auto-assigned, so rarely is there antyhing
-	 * to show.
+	 * Removes these columns:
+	 * - The colors column. (Currently colors aren't auto-assigned, so rarely
+	 *   is there antyhing to show.)
+	 * - The number of likes, as provided by Jetpack.
 	 *
 	 * @param  array $columns Array of post column titles.
 	 * @return array
 	 */
-	public static function remove_photo_colors_column( $columns ) {
+	public static function remove_columns_from_pending_photos( $columns ) {
 		if (
 			filter_input( INPUT_GET, 'post_type' ) === Registrations::get_post_type()
 		&&
 			in_array( filter_input( INPUT_GET, 'post_status' ), Photo::get_pending_post_statuses() )
 		) {
 			unset( $columns[ 'taxonomy-' . Registrations::get_taxonomy( 'colors' ) ] );
+			unset( $columns['likes'] );
 		}
 
 		return $columns;
