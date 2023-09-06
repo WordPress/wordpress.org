9a9058bf779c6727bb44220c80c546788c8e7296
JesÃºs Amieiro <amieiro@git.wordpress.org>
2023-09-06 12:10:47 +0000

    Translate: Sync gp-translation-helpers with GitHub
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@12876 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/plugins/gp-translation-helpers/helpers/helper-other-locales.php
+++ wordpress.org/public_html/wp-content/plugins/gp-translation-helpers/helpers/helper-other-locales.php
@@ -42,8 +42,9 @@ class Helper_Other_Locales extends GP_Translation_Helper {
 	 * @var array
 	 */
 	public array $related_locales = array(
-		'gl'  => array( 'es', 'pt', 'pt-ao', 'pt-br', 'ca', 'it', 'fr', 'ro' ),
+		'ca'  => array( 'ca-val', 'bal', 'es', 'oci', 'an', 'gl', 'fr', 'it', 'pt', 'ro', 'la' ),
 		'es'  => array( 'gl', 'ca', 'pt', 'pt-ao', 'pt-br', 'it', 'fr', 'ro' ),
+		'gl'  => array( 'es', 'pt', 'pt-ao', 'pt-br', 'ca', 'it', 'fr', 'ro' ),
 		'oci' => array( 'ca', 'fr', 'it', 'es', 'gl' ),
 	);
 
