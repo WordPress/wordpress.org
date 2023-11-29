d0484b1abbb88220414a47ccb6f971dd95eaf4de
Alex Kirk <akirk@git.wordpress.org>
2023-11-29 13:15:56 +0000

    Tour: update from upstream
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@12982 74240141-8908-4e6f-9713-ba540dce6ec7

---
+++ wordpress.org/public_html/wp-content/plugins/tour/assets/blocks/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-server-side-render'), 'version' => 'ff020029dbdcac61b39f');
--- wordpress.org/public_html/wp-content/plugins/tour/assets/js/tour.js
+++ wordpress.org/public_html/wp-content/plugins/tour/assets/js/tour.js
@@ -1,78 +1,93 @@
-/* global , document, window, wp_tour */
+/* global tour_plugin, XMLHttpRequest */
 /* eslint camelcase: "off" */
 
-document.addEventListener('DOMContentLoaded', function() {
-	var dismissTour;
-	document.addEventListener('click', function( event ) {
+document.addEventListener( 'DOMContentLoaded', function () {
+	let dismissTour;
+	document.addEventListener( 'click', function ( event ) {
 		if ( ! event.target.matches( '.pulse' ) ) {
 			return;
 		}
 		event.preventDefault();
 		const driver = window.driver.js.driver;
 		const tourId = event.target.dataset.tourId;
-		var startStep = 0;
+		let startStep = 0;
 		if ( typeof tour_plugin.progress[ tourId ] !== 'undefined' ) {
 			startStep = tour_plugin.progress[ tourId ] - 1;
 		}
 		if ( startStep <= 0 ) {
 			startStep = 0;
 		}
-		var tourSteps = tour_plugin.tours[ tourId ].slice(1);
+		const tourSteps = tour_plugin.tours[ tourId ].slice( 1 );
 		if ( ! tourSteps.length ) {
 			return;
 		}
 
-		dismissTour = function() {
-			var xhr = new XMLHttpRequest();
-			xhr.open('POST', tour_plugin.rest_url + 'tour/v1/save-progress');
-			xhr.setRequestHeader('Content-Type', 'application/json');
-			xhr.setRequestHeader('X-WP-Nonce', tour_plugin.nonce);
-			xhr.send(JSON.stringify({
-				tour: tourId,
-				step: tour_plugin.tours[ tourId ].length
-			}));
+		dismissTour = function () {
+			const xhr = new XMLHttpRequest();
+			xhr.open( 'POST', tour_plugin.rest_url + 'tour/v1/save-progress' );
+			xhr.setRequestHeader( 'Content-Type', 'application/json' );
+			xhr.setRequestHeader( 'X-WP-Nonce', tour_plugin.nonce );
+			xhr.send(
+				JSON.stringify( {
+					tour: tourId,
+					step: tour_plugin.tours[ tourId ].length,
+				} )
+			);
 
 			driverObj.destroy();
-		}
-		tourSteps[startStep].element = event.target.closest('.pulse-wrapper');
-		var driverObj = driver( {
+		};
+		tourSteps[ startStep ].element =
+			event.target.closest( '.pulse-wrapper' );
+		const driverObj = driver( {
 			showProgress: true,
 			steps: tourSteps,
-			onHighlightStarted: function( element, step, options )  {
-				step.popover.description += '<br><a href="" class="dismiss-tour">Dismiss the tour';
+			onHighlightStarted( element, step ) {
+				step.popover.description +=
+					'<br><a href="" class="dismiss-tour">Dismiss the tour';
 			},
-			onHighlighted: function( element, step, options )  {
-				var xhr = new XMLHttpRequest();
-				xhr.open('POST', tour_plugin.rest_url + 'tour/v1/save-progress');
-				xhr.setRequestHeader('Content-Type', 'application/json');
-				xhr.setRequestHeader('X-WP-Nonce', tour_plugin.nonce);
-				xhr.send(JSON.stringify({
-					tour: tourId,
-					step: options.state.activeIndex + 1
-				}));
+			onHighlighted( element, step, options ) {
+				tour_plugin.progress[ tourId ] = options.state.activeIndex + 1;
+				const xhr = new XMLHttpRequest();
+				xhr.open(
+					'POST',
+					tour_plugin.rest_url + 'tour/v1/save-progress'
+				);
+				xhr.setRequestHeader( 'Content-Type', 'application/json' );
+				xhr.setRequestHeader( 'X-WP-Nonce', tour_plugin.nonce );
+				xhr.send(
+					JSON.stringify( {
+						tour: tourId,
+						step: options.state.activeIndex + 1,
+					} )
+				);
 			},
-			onDestroyStarted: function( element, step, options ) {
+			onDestroyStarted( element, step, options ) {
 				if ( driverObj.hasNextStep() ) {
 					addPulse( tourId, options.state.activeIndex + 1 );
 				} else {
-					var xhr = new XMLHttpRequest();
-					xhr.open('POST', tour_plugin.rest_url + 'tour/v1/save-progress');
-					xhr.setRequestHeader('Content-Type', 'application/json');
-					xhr.setRequestHeader('X-WP-Nonce', tour_plugin.nonce);
-					xhr.send(JSON.stringify({
-						tour: tourId,
-						step: tour_plugin.tours[ tourId ].length
-					}));
+					const xhr = new XMLHttpRequest();
+					xhr.open(
+						'POST',
+						tour_plugin.rest_url + 'tour/v1/save-progress'
+					);
+					xhr.setRequestHeader( 'Content-Type', 'application/json' );
+					xhr.setRequestHeader( 'X-WP-Nonce', tour_plugin.nonce );
+					xhr.send(
+						JSON.stringify( {
+							tour: tourId,
+							step: tour_plugin.tours[ tourId ].length,
+						} )
+					);
 				}
 				driverObj.destroy();
-			}
+			},
 		} );
 		driverObj.drive( startStep );
-		const pulse = tourSteps[startStep].element.querySelector('.pulse');
+		const pulse = tourSteps[ startStep ].element.querySelector( '.pulse' );
 		pulse.parentNode.removeChild( pulse );
 	} );
 
-	document.addEventListener('click', function( event ) {
+	document.addEventListener( 'click', function ( event ) {
 		if ( ! event.target.matches( '.dismiss-tour' ) ) {
 			return;
 		}
@@ -81,7 +96,6 @@ document.addEventListener('DOMContentLoaded', function() {
 			dismissTour();
 		}
 	} );
-
 
 	function addPulse( tourId, startStep ) {
 		let fields;
@@ -102,66 +116,99 @@ document.addEventListener('DOMContentLoaded', function() {
 			fields = [ selector ];
 		}
 
-		for (let i = 0; i < fields.length; i++) {
-			let field = fields[i];
-			let wrapper = field.closest('.pulse-wrapper');
-			if (!wrapper) {
+		for ( let i = 0; i < fields.length; i++ ) {
+			const field = fields[ i ];
+			let wrapper = field.closest( '.pulse-wrapper' );
+			if ( ! wrapper ) {
 				if ( field.hasChildNodes() ) {
 					wrapper = field;
 				} else {
-					wrapper = document.createElement('div');
-					field.parentNode.insertBefore(wrapper, field);
-					wrapper.appendChild(field);
+					wrapper = document.createElement( 'div' );
+					field.parentNode.insertBefore( wrapper, field );
+					wrapper.appendChild( field );
 				}
-				wrapper.classList.add("pulse-wrapper");
+				wrapper.classList.add( 'pulse-wrapper' );
 			}
-			if ( ! wrapper.querySelector('.pulse') ) {
-				const pulse = document.createElement('div');
-				pulse.classList.add("pulse");
-				pulse.classList.add("tour-" + tourId);
+			if ( ! wrapper.querySelector( '.pulse' ) ) {
+				const pulse = document.createElement( 'div' );
+				pulse.classList.add( 'pulse' );
+				pulse.classList.add( 'tour-' + tourId );
 				pulse.dataset.tourId = tourId;
-				pulse.dataset.tourId = tourId;
+				pulse.dataset.tourTitle =
+					tour_plugin.tours[ tourId ][ 0 ].title;
 				if ( field.hasChildNodes() ) {
-					wrapper.insertBefore(pulse,wrapper.firstChild);
+					wrapper.insertBefore( pulse, wrapper.firstChild );
 				} else {
-					wrapper.insertBefore(pulse,field);
+					wrapper.insertBefore( pulse, field );
 				}
 			}
 		}
 	}
 
-	const loadTour = function() {
-		var color1 = '';
-		var color2 = '';
-		var styleElement = document.createElement( 'style' );
-		var style;
-		var startStep;
+	const loadTour = function () {
+		const styleElement =
+			document.getElementById( 'tour-styles' ) ||
+			document.createElement( 'style' );
+		let style = null;
+		let color1 = '';
+		let color2 = '';
+		let startStep;
 
-		document.head.appendChild( styleElement );
-		style = styleElement.sheet;
+		if ( ! styleElement.id ) {
+			styleElement.id = 'tour-styles';
+			document.head.appendChild( styleElement );
+			style = styleElement.sheet;
+		}
 
 		for ( const tourId in tour_plugin.tours ) {
-			color1 = tour_plugin.tours[ tourId ][ 0 ].color;
-			color2 = tour_plugin.tours[ tourId ][ 0 ].color + 'a0';
-			style.insertRule( '@keyframes animation-' + tourId + ' {' +
-				'0% {' +
-				'box-shadow: 0 0 0 0 ' + color2 + ';' +
-				'}' +
-				'70% {' +
-				'box-shadow: 0 0 0 10px ' + color1 + '00' + ';' +
-				'}' +
-				'100% {' +
-				'box-shadow: 0 0 0 0 ' + color1 + '00' + ';' +
-				'}' +
-				'}',
-				style.cssRules.length );
+			if ( style ) {
+				color1 = tour_plugin.tours[ tourId ][ 0 ].color;
+				color2 = tour_plugin.tours[ tourId ][ 0 ].color + 'a0';
+				style.insertRule(
+					'@keyframes animation-' +
+						tourId +
+						' {' +
+						'0% {' +
+						'box-shadow: 0 0 0 0 ' +
+						color2 +
+						';' +
+						'}' +
+						'70% {' +
+						'box-shadow: 0 0 0 10px ' +
+						color1 +
+						'00' +
+						';' +
+						'}' +
+						'100% {' +
+						'box-shadow: 0 0 0 0 ' +
+						color1 +
+						'00' +
+						';' +
+						'}' +
+						'}',
+					style.cssRules.length
+				);
 
-			style.insertRule( '.tour-' + tourId + '{' +
-				'box-shadow: 0 0 0 ' + color2 + ';' +
-				'background: ' + color1 + '80' + ';' +
-				'-webkit-animation: animation-' + tourId + ' 2s infinite;' +
-				'animation: animation-' + tourId + ' 2s infinite; }',
-				style.cssRules.length );
+				style.insertRule(
+					'.tour-' +
+						tourId +
+						'{' +
+						'box-shadow: 0 0 0 ' +
+						color2 +
+						';' +
+						'background: ' +
+						color1 +
+						'80' +
+						';' +
+						'-webkit-animation: animation-' +
+						tourId +
+						' 2s infinite;' +
+						'animation: animation-' +
+						tourId +
+						' 2s infinite; }',
+					style.cssRules.length
+				);
+			}
 			startStep = 0;
 			if ( typeof tour_plugin.progress[ tourId ] !== 'undefined' ) {
 				startStep = tour_plugin.progress[ tourId ];
@@ -170,5 +217,100 @@ document.addEventListener('DOMContentLoaded', function() {
 		}
 	};
 	loadTour();
-}
-);
+	filter_available_tours();
+	setTimeout( filter_available_tours, 500 );
+
+	function filter_available_tours() {
+		const tourListItems = document.querySelectorAll( '.tour-list-item' );
+		let foundTour = false;
+
+		for ( let i = 0; i < tourListItems.length; i++ ) {
+			let tourId = tourListItems[ i ].dataset.tourId;
+			if (
+				! tourId &&
+				tourListItems[ i ].id.substr( 0, 18 ) === 'wp-admin-bar-tour-'
+			) {
+				tourId = tourListItems[ i ].id.substr( 18 );
+			}
+
+			if ( ! tour_plugin.tours[ tourId ] ) {
+				continue;
+			}
+
+			const tourIsPresent =
+				tour_plugin.tours[ tourId ][ 1 ] &&
+				document.querySelector(
+					tour_plugin.tours[ tourId ][ 1 ].element
+				);
+			if ( tourIsPresent ) {
+				foundTour = true;
+			}
+			document
+				.querySelectorAll(
+					'.tour-list-item[data-tour-id="' +
+						tourId +
+						'"], #wp-admin-bar-tour-' +
+						tourId
+				)
+				.forEach( function ( element ) {
+					element.style.display = tourIsPresent ? 'block' : 'none';
+				} );
+		}
+
+		if ( document.getElementById( 'wp-admin-bar-tour-list-default' ) ) {
+			document
+				.getElementById( 'wp-admin-bar-tour-list-default' )
+				.childNodes.forEach( function ( element ) {
+					if ( element.style.display !== 'none' ) {
+						document.getElementById(
+							'wp-admin-bar-tour-list'
+						).style.display = 'block';
+					}
+				} );
+		}
+
+		if ( foundTour ) {
+			loadTour();
+		}
+	}
+
+	document.addEventListener( 'click', function ( event ) {
+		let target = event.target;
+		if ( target.matches( '.tour-list-item a' ) ) {
+			target = target.parentNode;
+		} else if ( ! target.matches( '.tour-list-item' ) ) {
+			return true;
+		}
+		event.preventDefault();
+
+		let tourId = target.dataset.tourId;
+		if ( ! tourId && target.id.substr( 0, 18 ) === 'wp-admin-bar-tour-' ) {
+			tourId = target.id.substr( 18 );
+		}
+
+		if ( ! tour_plugin.tours[ tourId ] ) {
+			return false;
+		}
+		let pulseToClick = document.querySelector( '.pulse.tour-' + tourId );
+
+		if ( pulseToClick ) {
+			pulseToClick.click();
+		} else {
+			const xhr = new XMLHttpRequest();
+			xhr.open( 'POST', tour_plugin.rest_url + 'tour/v1/save-progress' );
+			xhr.setRequestHeader( 'Content-Type', 'application/json' );
+			xhr.setRequestHeader( 'X-WP-Nonce', tour_plugin.nonce );
+			xhr.send(
+				JSON.stringify( {
+					tour: tourId,
+					step: -1,
+				} )
+			);
+
+			delete tour_plugin.progress[ tourId ];
+			loadTour();
+			pulseToClick = document.querySelector( '.pulse.tour-' + tourId );
+			pulseToClick.click();
+		}
+	} );
+} );
