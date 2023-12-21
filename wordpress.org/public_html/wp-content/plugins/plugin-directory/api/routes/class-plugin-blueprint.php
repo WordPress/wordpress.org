c905b33829c47a5591c01b9086ee7e671917ff34
Tellyworth <tellyworth@git.wordpress.org>
2023-12-21 05:40:09 +0000

    Plugin Dir: Add Preview links for moderators in plugin queue
    This adds private Playground Preview links for the plugin review team to more easily test newly submitted plugins that have not yet been published.
    
    Plugins are loaded with plugin-check active.
    
    See #7380.
    
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@13076 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/plugins/plugin-directory/api/routes/class-plugin-blueprint.php
+++ wordpress.org/public_html/wp-content/plugins/plugin-directory/api/routes/class-plugin-blueprint.php
@@ -17,6 +17,7 @@ class Plugin_Blueprint extends Base {
 		register_rest_route( 'plugins/v1', '/plugin/(?P<plugin_slug>[^/]+)/blueprint.json', array(
 			'methods'             => array( \WP_REST_Server::READABLE, \WP_REST_Server::CREATABLE ),
 			'callback'            => array( $this, 'blueprint' ),
+			// Note: the zip part of the endpoint is also public, since playground requests blueprints without cookie credentials
 			'permission_callback' => '__return_true',
 			'args'                => array(
 				'plugin_slug' => array(
@@ -34,6 +35,54 @@ class Plugin_Blueprint extends Base {
 	 */
 	public function blueprint( $request ) {
 		$plugin = Plugin_Directory::get_plugin_post( $request['plugin_slug'] );
+
+		// Direct zip preview for plugin reviewers
+		if ( $request->get_param('zip_hash') ) {
+			foreach ( get_attached_media( 'application/zip', $plugin ) as $zip_file ) {
+				if ( hash_equals( Template::preview_link_hash( $zip_file->ID, 0 ), $request->get_param('zip_hash') ) ||
+				     hash_equals( Template::preview_link_hash( $zip_file->ID, -1 ), $request->get_param('zip_hash') ) ) {
+					$zip_url = wp_get_attachment_url( $zip_file->ID );
+					$zip_blueprint =<<<EOF
+{
+    "landingPage": "/wp-admin/plugins.php",
+    "preferredVersions": {
+        "php": "8.0",
+        "wp": "latest"
+	},
+    "phpExtensionBundles": [
+        "kitchen-sink"
+    ],
+    "features": {
+        "networking": true
+    },
+    "steps": [
+		{
+            "step": "installPlugin",
+            "pluginZipFile": {
+                "resource": "wordpress.org/plugins",
+                "slug": "plugin-check"
+            }
+        },
+        {
+            "step": "installPlugin",
+            "pluginZipFile": {
+                "resource": "url",
+                "url": "$zip_url"
+            }
+		},
+		{
+            "step": "login",
+            "username": "admin",
+            "password": "password"
+        }
+	]
+}
+EOF;
+					header( 'Access-Control-Allow-Origin: https://playground.wordpress.net' );
+					die( $zip_blueprint );
+				}
+			}
+		}
 
         $blueprints = get_post_meta( $plugin->ID, 'assets_blueprints', true );
         // Note: for now, only use a file called `blueprint.json`.
