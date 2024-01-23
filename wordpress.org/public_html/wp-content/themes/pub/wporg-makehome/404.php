c41e4269d1f112c788962356dde3f24843c231d3
Scott Reilly <coffee2code@git.wordpress.org>
2024-01-23 22:35:20 +0000

    Make home theme: Prevent 404 page message from exceeding container width.
    Props kharisblank, coffee2code.
    Fixes #6658.
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@13144 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-makehome/404.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-makehome/404.php
@@ -6,7 +6,7 @@
     <div id="404" class="post 404">
 
 		<h2 class="section-title"><?php _e( 'Not Found', 'make-wporg' ); ?></h2>
-		<article style="width:100%">
+		<article>
 			<p><?php _e( 'Are you looking to contribute to WordPress? Check out the <a href="//make.wordpress.org/">many ways to get involved</a>.', 'make-wporg' ); ?></p>
 		</article>
 	</div>
