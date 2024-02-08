b19617f91e7300e9b271631ac868db89b976870e
Dion Hulse <dion@wordpress.org>
2024-02-08 07:04:07 +0000

    Template: Support archives that have no plugins, such as a filtered tag or empty search results.

--- wordpress.org/public_html/wp-content/themes/pub/wporg-plugins/search.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-plugins/search.php
@@ -12,42 +12,41 @@ get_header();
 
 	<main id="main" class="site-main" role="main">
 
-		<?php if ( have_posts() ) : ?>
-
-			<header class="page-header">
-				<h1 class="page-title">
-					<?php
-					printf(
-						/* translators: Search query. */
-						esc_html__( 'Showing results for: %s', 'wporg-plugins' ),
-						'<strong>' . get_search_query() . '</strong>'
-					);
-					?>
-				</h1>
+		<header class="page-header">
+			<h1 class="page-title">
 				<?php
-				if ( get_query_var( 'block_search' ) ) {
-					printf(
-						/* translators: %s: Search URL */
-						'<p>' . __( 'Searching the block directory. <a href="%s">Search all plugins</a>.', 'wporg-plugins' ) . '</p>',
-						remove_query_arg( 'block_search' )
-					);
-				}
+				printf(
+					/* translators: Search query. */
+					esc_html__( 'Showing results for: %s', 'wporg-plugins' ),
+					'<strong>' . get_search_query() . '</strong>'
+				);
 				?>
-			</header><!-- .page-header -->
-
+			</h1>
 			<?php
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
+			if ( get_query_var( 'block_search' ) ) {
+				printf(
+					/* translators: %s: Search URL */
+					'<p>' . __( 'Searching the block directory. <a href="%s">Search all plugins</a>.', 'wporg-plugins' ) . '</p>',
+					remove_query_arg( 'block_search' )
+				);
+			}
+			?>
+		</header><!-- .page-header -->
 
-				get_template_part( 'template-parts/plugin', 'index' );
-			endwhile;
+		<?php
+		/* Start the Loop */
+		while ( have_posts() ) :
+			the_post();
 
-			the_posts_pagination();
+			get_template_part( 'template-parts/plugin', 'index' );
+		endwhile;
 
-		else :
-			get_template_part( 'template-parts/content', 'none' );
-		endif;
+		if ( ! have_posts() ) {
+			get_template_part( 'template-parts/no-results' );
+		}
+
+		the_posts_pagination();
+
 		?>
 
 	</main><!-- #main -->
