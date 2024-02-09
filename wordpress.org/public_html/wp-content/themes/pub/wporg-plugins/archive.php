a2671c22dfe406414126a422a0902067cf040821
Dion Hulse <dd32@git.wordpress.org>
2024-02-09 03:15:54 +0000

    Plugin Directory: Allow tags to be filtered by taxonomies.
    This adjusts the styles and templates to simplify the behaviour, allowing for an "empty" archive to be displayed.
    
    Closes https://github.com/WordPress/wordpress.org/pull/198.
    See #7430.
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@13196 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-plugins/archive.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-plugins/archive.php
@@ -20,36 +20,32 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-		<?php if ( have_posts() ) : ?>
-
-			<header class="page-header">
-				<?php
-				the_archive_title( '<h1 class="page-title">', '</h1>' );
-				the_archive_description( '<div class="taxonomy-description">', '</div>' );
-				?>
-			</header><!-- .page-header -->
-
+		<header class="page-header">
 			<?php
-			/* Start the Loop */
-			while ( have_posts() ) :
-				the_post();
+			the_archive_title( '<h1 class="page-title">', '</h1>' );
+			the_archive_description( '<div class="taxonomy-description">', '</div>' );
+			?>
+		</header><!-- .page-header -->
 
-				/*
-				 * Include the Post-Format-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/plugin' );
+		<?php
+		/* Start the Loop */
+		while ( have_posts() ) :
+			the_post();
 
-			endwhile;
+			/*
+				* Include the Post-Format-specific template for the content.
+				* If you want to override this in a child theme, then include a file
+				* called content-___.php (where ___ is the Post Format name) and that will be used instead.
+				*/
+			get_template_part( 'template-parts/plugin' );
 
-			the_posts_pagination();
+		endwhile;
+		if ( ! have_posts() ) {
+			get_template_part( 'template-parts/no-results' );
+		}
 
-		else :
+		the_posts_pagination();
 
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif;
 		?>
 
 		</main><!-- #main -->
