af6889fb36ed93f4e328e2d658d5c28477a2e8ef
Dion Hulse <dion@wordpress.org>
2024-02-15 05:47:31 +0000

    Remove the filter bar entirely.

--- wordpress.org/public_html/wp-content/themes/pub/wporg-plugins/header.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-plugins/header.php
@@ -76,9 +76,7 @@ echo do_blocks( '<!-- wp:wporg/global-header /-->' ); // phpcs:ignore
 			</div><!-- .site-branding -->
 		</header><!-- #masthead -->
 
-
 		<?php
-
 echo esc_html( $wp_query->request );
 
 echo <<<CSS
@@ -125,44 +123,3 @@ echo do_blocks( <<<FILTERBLOCKS
 	<!-- /wp:group -->
 	FILTERBLOCKS
 );
-
-?>
-
-		<?php if ( false && $show_filter_bar ) : ?>
-		<div class="wporg-filter-bar">
-			<nav class="wporg-filter-bar__navigation" aria-label="<?php esc_html_e( 'Plugin filters', 'wporg-plugins' ); ?>">
-				<ul>
-				<?php
-				foreach ( $local_nav_items as $slug => $label ) {
-					$class = '';
-					if (
-						// URL contains this filter.
-						( $slug === ( $_GET['plugin_business_model'] ?? false ) ) ||
-						// Set the All item active if no business model is selected.
-						( ! $slug && empty( $_GET['plugin_business_model'] ) )
-					) {
-						$class = 'is-active';
-					}
-
-					if ( $slug ) {
-						$url = add_query_arg( array( 'plugin_business_model' => $slug ) );
-					} else {
-						$url = remove_query_arg( 'plugin_business_model' );
-					}
-
-					// Reset pagination.
-					$url = remove_query_arg( 'paged', $url );
-					$url = preg_replace( '!/page/\d+/?!i', '/', $url );
-
-					printf(
-						'<li class="page_item"><a class="%1$s" href="%2$s">%3$s</a></li>',
-						esc_attr( $class ),
-						esc_url( $url ),
-						esc_html( $label )
-					);
-				}
-				?>
-				</ul>
-			</nav>
-		</div>
-		<?php endif; ?>
