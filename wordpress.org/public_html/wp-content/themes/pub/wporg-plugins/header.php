52560e11ad050c7ffe1c0cdf8fbf51525571b66b
Dion Hulse <dion@wordpress.org>
2024-02-19 05:27:31 +0000

    Header: Make the filterbar more production ready.

--- wordpress.org/public_html/wp-content/themes/pub/wporg-plugins/header.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-plugins/header.php
@@ -25,15 +25,6 @@ $local_nav_items = array(
 	'commercial' => __( 'Commercial', 'wporg-plugins' ),
 );
 
-global $wp_query;
-$is_beta = 'beta' === $wp_query->get( 'browse' );
-$is_favs = 'favorites' === $wp_query->get( 'browse' );
-// The filter bar should not be shown on:
-// - singular: not relevant on pages or individual plugins.
-// - beta: likely unnecessary, these are probably all "community".
-// - favorites: not necessary.
-$show_filter_bar = ! ( is_singular() || $is_beta || $is_favs );
-
 echo do_blocks( '<!-- wp:wporg/global-header /-->' ); // phpcs:ignore
 
 ?>
@@ -77,48 +68,49 @@ echo do_blocks( '<!-- wp:wporg/global-header /-->' ); // phpcs:ignore
 		</header><!-- #masthead -->
 
 		<?php
-echo esc_html( $wp_query->request );
 
-echo <<<CSS
-	<style>
-	body {
-		/* TODO: Seems whatever is using this should have a default 10px applied? */
-		--wp--preset--spacing--10: 10px;
-	}
-	.wporg-plugins__filters {
-		--wporg--oldtheme--primary-color: #2371a6;
-		--wp--custom--button--color--text: #ffffff;
-		--wp--custom--button--hover--color--text: #ffffff;
-		--wp--custom--wporg-query-filters--border--color: #e5e5e5;
-		--wp--custom--wporg-query-filters--option--active--color--background: #f0f0f0;
-		--wp--custom--wporg-query-filters--toggle--active--color--background: var(--wporg--oldtheme--primary-color);
-		--wp--custom--wporg-query-filters--toggle--hover--color--text: var(--wporg--oldtheme--primary-color);
-		--wp--custom--button--color--background: var(--wporg--oldtheme--primary-color);
-		--wp--custom--button--hover--color--background: var(--wporg--oldtheme--primary-color);
-	}
-	.wporg-plugins__filters .wporg-query-filter__modal-actions input.wporg-query-filter__modal-action-clear {
-		color: var(--wporg--oldtheme--primary-color);
-	}
-	</style>
-	CSS;
+if ( ! is_front_page() && is_archive() ) {
+	//echo esc_html( $wp_query->request );
+	echo '<style>
+		body {
+			/* TODO: Seems whatever is using this should have a default 10px applied? */
+			--wp--preset--spacing--10: 10px;
+		}
+		.wporg-plugins__filters {
+			--wporg--oldtheme--primary-color: #2371a6;
+			--wp--custom--button--color--text: #ffffff;
+			--wp--custom--button--hover--color--text: #ffffff;
+			--wp--custom--wporg-query-filters--border--color: #e5e5e5;
+			--wp--custom--wporg-query-filters--option--active--color--background: #f0f0f0;
+			--wp--custom--wporg-query-filters--toggle--active--color--background: var(--wporg--oldtheme--primary-color);
+			--wp--custom--wporg-query-filters--toggle--hover--color--text: var(--wporg--oldtheme--primary-color);
+			--wp--custom--button--color--background: var(--wporg--oldtheme--primary-color);
+			--wp--custom--button--hover--color--background: var(--wporg--oldtheme--primary-color);
+		}
+		.wporg-plugins__filters .wporg-query-filter__modal-actions input.wporg-query-filter__modal-action-clear {
+			color: var(--wporg--oldtheme--primary-color);
+		}
+		.wporg-plugins__filters .wp-block-search__inside-wrapper {
+			border: none;
+		}
+	</style>';
 
-echo do_blocks( <<<FILTERBLOCKS
-	<!-- wp:group {"align":"wide","className":"wporg-filter-bar wporg-plugins__filters wporg-plugins__filters__no-count","layout":{"type":"flex","flexWrap":"wrap","justifyContent":"space-between"}} -->
-	<div class="wp-block-group alignwide wporg-filter-bar wporg-plugins__filters wporg-plugins__filters__no-count">
-		<!-- wp:group {"className":"wporg-plugins__filters__search","layout":{"type":"flex","flexWrap":"wrap"}} -->
-		<div class="wp-block-group wporg-plugins__filters__search">
-			<!-- wp:search {"showLabel":false,"placeholder":"Search plugins...","width":100,"widthUnit":"%","buttonText":"Search","buttonPosition":"button-inside","buttonUseIcon":true,"className":"is-style-secondary-search-control"} /-->
-			<!-- wp:wporg/query-total /-->
-		</div> <!-- /wp:group -->
+	echo do_blocks( '<!-- wp:group {"align":"wide","className":"wporg-filter-bar wporg-plugins__filters wporg-plugins__filters__no-count","layout":{"type":"flex","flexWrap":"wrap","justifyContent":"space-between"}} -->
+		<div class="wp-block-group alignwide wporg-filter-bar wporg-plugins__filters wporg-plugins__filters__no-count">
+			<!-- wp:group {"className":"wporg-plugins__filters__search","layout":{"type":"flex","flexWrap":"wrap"}} -->
+			<div class="wp-block-group wporg-plugins__filters__search">
+				<!-- wp:search {"showLabel":false,"placeholder":"' . esc_attr__( 'Search plugins', 'wporg-plugins' ) . '","width":100,"widthUnit":"%","buttonText":"' . esc_attr__( 'Search plugins', 'wporg-plugins' ) . '","buttonPosition":"button-inside","buttonUseIcon":true,"className":"is-style-secondary-search-control"} /-->
+				<!-- wp:wporg/query-total /-->
+			</div> <!-- /wp:group -->
 
-		<!-- wp:group {"style":{"spacing":{"blockGap":"0"}},"className":"wporg-query-filters","layout":{"type":"flex","flexWrap":"nowrap"}} -->
-		<div class="wp-block-group wporg-query-filters">
-			<!-- wp:wporg/query-filter {"key":"plugin_category"} /-->
-			<!-- wp:wporg/query-filter {"key":"business_model","multiple":false} /-->
-			<!-- wp:wporg/query-filter {"key":"sort","multiple":false} /-->
+			<!-- wp:group {"style":{"spacing":{"blockGap":"0"}},"className":"wporg-query-filters","layout":{"type":"flex","flexWrap":"nowrap"}} -->
+			<div class="wp-block-group wporg-query-filters">
+				<!-- wp:wporg/query-filter {"key":"plugin_category"} /-->
+				<!-- wp:wporg/query-filter {"key":"business_model","multiple":false} /-->
+				<!-- wp:wporg/query-filter {"key":"sort","multiple":false} /-->
+			</div>
+			<!-- /wp:group -->
 		</div>
-		<!-- /wp:group -->
-	</div>
-	<!-- /wp:group -->
-	FILTERBLOCKS
-);
+		<!-- /wp:group -->'
+	);
+}
