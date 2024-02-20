698b2bff7517ad42d7e67e4bd1af1e48f7acbaa5
Adam Wood <1017872+adamwoodnz@users.noreply.github.com>
2024-02-21 11:39:33 +1300

    Revert changes to single forum

--- wordpress.org/public_html/wp-content/themes/pub/wporg-support/bbpress/loop-single-forum-homepage.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-support/bbpress/loop-single-forum-homepage.php
@@ -4,4 +4,22 @@
 
 	<p><?php bbp_forum_content(); ?></p>
 
+	<p>
+		<?php $subforums = bbp_forum_get_subforums( bbp_get_forum_id() ); ?>
+
+		<?php if ( $subforums ) : ?>
+
+			<?php foreach ( $subforums as $subforum ) : ?>
+				<a href="<?php bbp_forum_permalink( $subforum->ID ); ?>" title="<?php bbp_forum_title( $subforum->ID ); ?>" class="viewmore"><?php bbp_forum_title( $subforum->ID ); ?></a><br>
+			<?php endforeach; ?>
+
+		<?php endif; ?>
+
+		<?php if ( ! bbp_is_forum_category() ) : ?>
+
+			<a href="<?php bbp_forum_permalink(); ?>" title="<?php bbp_forum_title(); ?>" class="viewmore"><?php _e( 'View forum', 'wporg-forums' ); ?></a>
+
+		<?php endif; ?>
+	</p>
+
 </div><!-- #bbp-forum-<?php bbp_forum_id(); ?> -->
