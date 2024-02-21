e08e20ea5da460192855c14aff1c8b5f4cf1e0f8
Adam Wood <1017872+adamwoodnz@users.noreply.github.com>
2024-02-21 16:15:34 +1300

    Reorganize homepage content again

--- wordpress.org/public_html/wp-content/themes/pub/wporg-support/bbpress/loop-forums-homepage.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-support/bbpress/loop-forums-homepage.php
@@ -1,8 +1,10 @@
 <?php do_action( 'bbp_template_before_forums_loop' ); ?>
 
-<h2 class="has-heading-5-font-size">Forums</h2>
 
-<div id="forums-list-<?php bbp_forum_id(); ?>" class="bbp-forums wp-block-group is-style-cards-grid has-small-font-size is-layout-grid wp-block-group-is-layout-grid">
+<section>
+	<h2 class="has-heading-5-font-size">Forums</h2>
+
+	<div id="forums-list-<?php bbp_forum_id(); ?>" class="bbp-forums wp-block-group is-style-cards-grid has-small-font-size is-layout-grid wp-block-group-is-layout-grid">
 
 		<?php while ( bbp_forums() ) : bbp_the_forum(); ?>
 
@@ -10,6 +12,27 @@
 
 		<?php endwhile; ?>
 
-</div><!-- .forums-directory -->
+	</div>
+</div>
+
+<section>
+	<h2 class="has-heading-5-font-size"><?php _e( 'Topics', 'wporg-forums' ); ?></h2>
+
+	<div class="wp-block-group is-style-cards-grid has-small-font-size is-layout-grid wp-block-group-is-layout-grid">
+		<?php wporg_support_get_views(); ?>
+	</div>
+</section>
+
+<section>
+	<p><?php
+		/* translators: 1: Theme Directory URL, 2: Plugin Directory URL */
+		printf( __( 'Looking for help with a specific <a href="%1$s">Theme</a> or <a href="%2$s">Plugin</a>?', 'wporg-forums' ),
+			esc_url( __( 'https://wordpress.org/themes/', 'wporg-forums' ) ),
+			esc_url( __( 'https://wordpress.org/plugins/', 'wporg-forums' ) ),
+		);
+	?></p>
+
+	<p><?php _e( 'Every theme and plugin has their own. Head to their individual pages and click "View support forum".', 'wporg-forums' ); ?></p>
+</section>
 
 <?php do_action( 'bbp_template_after_forums_loop' ); ?>
--- wordpress.org/public_html/wp-content/themes/pub/wporg-support/template-parts/bbpress-front.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-support/template-parts/bbpress-front.php
@@ -19,42 +19,4 @@
 	<?php bbp_get_template_part( 'content', 'archive-forum' ); ?>
 </section>
 
-<section>
-	<h2 class="has-heading-5-font-size"><?php _e( 'Topics', 'wporg-forums' ); ?></h2>
-
-	<div class="wp-block-group is-style-cards-grid has-small-font-size is-layout-grid wp-block-group-is-layout-grid">
-		<?php wporg_support_get_views(); ?>
-	</div>
-</section>
-
-<section>
-	<p><?php
-		/* translators: 1: Theme Directory URL, 2: Plugin Directory URL */
-		printf( __( 'Looking for help with a specific <a href="%1$s">Theme</a> or <a href="%2$s">Plugin</a>?', 'wporg-forums' ),
-			esc_url( __( 'https://wordpress.org/themes/', 'wporg-forums' ) ),
-			esc_url( __( 'https://wordpress.org/plugins/', 'wporg-forums' ) ),
-		);
-	?></p>
-
-	<p><?php _e( 'Every theme and plugin has their own. Head to their individual pages and click "View support forum".', 'wporg-forums' ); ?></p>
-</section>
-
-<section class="forum-home-footer has-text-color has-white-color has-background has-charcoal-2-background-color has-small-font-size">
-	<h2 class="has-heading-5-font-size"><?php _e( 'More resources', 'wporg-forums' ); ?></h2>
-	<div>
-		<a href="https://wordpress.org/documentation/">
-			<h3 class="has-blueberry-2-color has-text-color has-link-color has-inter-font-family has-normal-font-size">
-				<?php _e( 'Documentation', 'wporg-forums' ); ?>
-			</h3>
-			<p><?php _e( "Your first stop where you'll find information on everything.", 'wporg-forums' ); ?></p>
-		</a>
-		<a href="https://make.wordpress.org/support/handbook/">
-			<h3 class="has-blueberry-2-color has-text-color has-link-color has-inter-font-family has-normal-font-size">
-				<?php _e( 'Support Handbook', 'wporg-forums' ); ?>
-			</h3>
-			<p><?php _e( 'Great for tips, tricks, and advice regarding giving the best support.', 'wporg-forums' ); ?></p>
-		</a>
-	</div>
-</section>
-
 <?php do_action( 'bbp_after_main_content' ); ?>
