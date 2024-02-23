b7e29725b68bd83fc8fe3e8a2f1fab28a2eefa90
Adam Wood <1017872+adamwoodnz@users.noreply.github.com>
2024-02-23 15:37:08 +1300

    Inline patterns which aren't reused

--- wordpress.org/public_html/wp-content/themes/pub/wporg-support/header.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-support/header.php
@@ -44,7 +44,41 @@ echo do_blocks( $is_forums_home
 				</div><!-- .site-branding -->
 			</header><!-- #masthead -->
 		<?php elseif ( $is_forums_home ) : ?>
-			<?php echo do_blocks( '<!-- wp:pattern {"slug":"wporg-support/forums-homepage-header"} /-->' ); ?>
+			<?php echo do_blocks(
+				sprintf(
+					'<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"right":"var:preset|spacing|edge-space","left":"var:preset|spacing|edge-space"}}},"backgroundColor":"charcoal-2","className":"has-white-color has-charcoal-2-background-color has-text-color has-background has-link-color","layout":{"type":"constrained"}} -->
+					<div class="wp-block-group alignfull has-white-color has-charcoal-2-background-color has-text-color has-background has-link-color" style="padding-right:var(--wp--preset--spacing--edge-space);padding-left:var(--wp--preset--spacing--edge-space)">
+
+						<!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|40"},"blockGap":"var:preset|spacing|30"}},"layout":{"type":"flex","flexWrap":"wrap","verticalAlignment":"bottom"}} -->
+						<div class="wp-block-group alignwide" style="padding-top:var(--wp--preset--spacing--40);padding-bottom:var(--wp--preset--spacing--40)">
+
+							<!-- wp:heading {"level":1,"style":{"typography":{"fontSize":"50px","fontStyle":"normal","fontWeight":"400"}},"fontFamily":"eb-garamond"} -->
+							<h1 class="wp-block-heading has-eb-garamond-font-family" style="font-size:50px;font-style:normal;font-weight:400">%s</h1>
+							<!-- /wp:heading -->
+
+							<!-- wp:paragraph {"style":{"typography":{"lineHeight":"1.8"}},"textColor":"white"} -->
+							<p class="has-white-color has-text-color" style="line-height:1.8">%s</p>
+							<!-- /wp:paragraph -->
+
+						</div>
+						<!-- /wp:group -->
+
+					</div>
+					<!-- /wp:group -->
+
+					<div id="lang-guess-wrap"></div>
+
+					<!-- wp:group {"layout":{"type":"constrained","justifyContent":"center"},"style":{"border":{"bottom":{"color":"var:preset|color|light-grey-1","style":"solid","width":"1px"}},"spacing":{"padding":{"left":"var:preset|spacing|edge-space","right":"var:preset|spacing|edge-space"}}}} -->
+					<div class="wp-block-group alignfull" style="padding-left:var(--wp--preset--spacing--edge-space);padding-right:var(--wp--preset--spacing--edge-space);border-bottom:1px solid var(--wp--preset--color--light-grey-1)">
+
+						<!-- wp:pattern {"slug":"wporg-support/search-field"} /-->
+
+					</div>
+					<!-- /wp:group -->',
+					esc_html__( 'Forums', 'wporg-support' ),
+					esc_html__( 'Learn how to help, or get help you need.', 'wporg-support' )
+				)
+			); ?>
 		<?php elseif ( ! $is_user_profile ) : ?>
 			<?php echo do_blocks(
 				'<!-- wp:group {"style":{"spacing":{"border":{"bottom":{"color":"var:preset|color|light-grey-1","style":"solid","width":"1px"}},"padding":{"left":"var:preset|spacing|edge-space","right":"var:preset|spacing|edge-space"}}}} -->
