cff9cad691997fb6f8c286b2364636d23803ab9e
Adam Wood <adamwood@git.wordpress.org>
2024-03-05 20:34:52 +0000

    wporg-support-2024: Add missing files
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@13281 74240141-8908-4e6f-9713-ba540dce6ec7

---
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-support-2024/patterns/forums-homepage-footer.php
@@ -0,0 +1,79 @@
+<?php
+if ( function_exists( 'register_block_pattern' ) ) {
+	register_block_pattern(
+		'wporg-support/forums-homepage-footer',
+		array(
+			'title'   => __( 'Forums Homepage Footer', 'wporg-support' ),
+			'content' => sprintf(
+				'<!-- wp:group {"align":"full","style":{"spacing":{"padding":{"right":"var:preset|spacing|edge-space","left":"var:preset|spacing|edge-space","top":"var:preset|spacing|40","bottom":"var:preset|spacing|40"}},"border":{"bottom":{"color":"var:preset|color|white-opacity-15","style":"solid","width":"1px"}},"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"backgroundColor":"charcoal-2","textColor":"white","className":"forums-homepage-footer","layout":{"type":"constrained"}} -->
+				<div class="wp-block-group alignfull forums-homepage-footer has-white-color has-charcoal-2-background-color has-text-color has-background has-link-color" style="border-bottom-color:var(--wp--preset--color--white-opacity-15);border-bottom-style:solid;border-bottom-width:1px;padding-top:var(--wp--preset--spacing--40);padding-right:var(--wp--preset--spacing--edge-space);padding-bottom:var(--wp--preset--spacing--40);padding-left:var(--wp--preset--spacing--edge-space)">
+
+					<!-- wp:columns {"align":"wide","style":{"spacing":{"blockGap":{"top":"var:preset|spacing|20"}}},"className":"is-style-default"} -->
+					<div class="wp-block-columns alignwide is-style-default">
+
+						<!-- wp:column {"verticalAlignment":"top","width":"50%%","className":"is-left-column","layout":{"inherit":false}} -->
+						<div class="wp-block-column is-vertically-aligned-top is-left-column" style="flex-basis:50%%">
+
+							<!-- wp:heading {"style":{"typography":{"fontStyle":"normal","fontWeight":"600","lineHeight":"1.3"}},"fontSize":"heading-5"} -->
+							<h2 class="wp-block-heading has-heading-5-font-size" style="font-style:normal;font-weight:600;line-height:1.3">%1$s</h2>
+							<!-- /wp:heading -->
+
+							<!-- wp:columns {"style":{"spacing":{"blockGap":{"left":"var:preset|spacing|50"}}}} -->
+							<div class="wp-block-columns">
+
+								<!-- wp:column {"style":{"spacing":{"blockGap":"var:preset|spacing|10"}}} -->
+								<div class="wp-block-column">
+
+									<!-- wp:heading {"level":3,"style":{"typography":{"fontStyle":"normal","fontWeight":"700","lineHeight":1.6},"elements":{"link":{"color":{"text":"var:preset|color|blueberry-2"}}}},"textColor":"blueberry-2","className":"is-style-short-text","fontSize":"normal","fontFamily":"inter"} -->
+									<h3 class="wp-block-heading is-style-short-text has-blueberry-2-color has-text-color has-link-color has-inter-font-family has-normal-font-size" style="font-style:normal;font-weight:700;line-height:1.6"><a href="https://make.wordpress.org/docs/handbook/">%2$s</a></h3>
+									<!-- /wp:heading -->
+
+									<!-- wp:paragraph -->
+									<p>%3$s</p>
+									<!-- /wp:paragraph -->
+
+								</div>
+								<!-- /wp:column -->
+
+								<!-- wp:column {"style":{"spacing":{"blockGap":"var:preset|spacing|10"}}} -->
+								<div class="wp-block-column">
+
+									<!-- wp:heading {"level":3,"style":{"typography":{"fontStyle":"normal","fontWeight":"700","lineHeight":1.6},"elements":{"link":{"color":{"text":"var:preset|color|blueberry-2"}}}},"textColor":"blueberry-2","className":"is-style-short-text","fontSize":"normal","fontFamily":"inter"} -->
+									<h3 class="wp-block-heading is-style-short-text has-blueberry-2-color has-text-color has-link-color has-inter-font-family has-normal-font-size" style="font-style:normal;font-weight:700;line-height:1.6"><a href="https://make.wordpress.org/core/handbook/">%4$s</a></h3>
+									<!-- /wp:heading -->
+
+									<!-- wp:paragraph -->
+									<p>%5$s</p>
+									<!-- /wp:paragraph -->
+
+								</div>
+								<!-- /wp:column -->
+
+							</div>
+							<!-- /wp:columns -->
+
+						</div>
+						<!-- /wp:column -->
+
+					<!-- wp:column {"width":"50%%","layout":{"inherit":false}} -->
+					<div class="wp-block-column" style="flex-basis:50%%">
+
+					</div>
+					<!-- /wp:column -->
+
+					</div>
+					<!-- /wp:columns -->
+
+				</div>
+				<!-- /wp:group -->',
+				esc_html__( 'More resources', 'wporg-support' ),
+				esc_html__( 'Documentation', 'wporg-support' ),
+				esc_html__( "Your first stop where you'll find information on everything.", 'wporg-support' ),
+				esc_html__( 'Support Handbook', 'wporg-support' ),
+				esc_html__( 'Great for tips, tricks, and advice regarding giving the best support.', 'wporg-support' ),
+			),
+		)
+	);
+}
+
+?>
