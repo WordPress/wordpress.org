407f3a4d0e72c8f663f9375191868e04572634ee
Adam Wood <adamwood@git.wordpress.org>
2024-03-06 04:08:27 +0000

    wporg-support-2024: i18n for local nav site title
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@13286 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-support-2024/patterns/local-nav.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-support-2024/patterns/local-nav.php
@@ -8,13 +8,14 @@ if ( function_exists( 'register_block_pattern' ) ) {
 				'<!-- wp:wporg/local-navigation-bar {"className":"has-display-contents","backgroundColor":"charcoal-2","style":{"elements":{"link":{"color":{"text":"var:preset|color|white"},":hover":{"color":{"text":"var:preset|color|white"}}}}},"textColor":"white","fontSize":"small"} -->
 
 					<!-- wp:paragraph {"fontSize":"small"} -->
-					<p class="wp-block-site-title has-small-font-size"><a href="%s">Forums</a></p>
+					<p class="wp-block-site-title has-small-font-size"><a href="%s">%s</a></p>
 					<!-- /wp:paragraph -->
 
 					<!-- wp:navigation {"icon":"menu","overlayBackgroundColor":"charcoal-2","overlayTextColor":"white","layout":{"type":"flex","orientation":"horizontal"},"fontSize":"small","menuSlug":"forums"} /-->
 
 				<!-- /wp:wporg/local-navigation-bar -->',
-				esc_url( home_url( '/forums/' ) )
+				esc_url( home_url( '/forums/' ) ),
+				esc_html__( 'Forums', 'wporg-support' ),
 			),
 		)
 	);
