ed113771a337bdbbc35a5fa645e28bf2220aeabe
Steve Dufresne <dufresnesteven@git.wordpress.org>
2024-03-15 03:48:39 +0000

    wporg-plugins-2024:  Update container styles to harmonize layout for all viewports. x
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@13343 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-plugins-2024/src/blocks/archive-page/render.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-plugins-2024/src/blocks/archive-page/render.php
@@ -10,33 +10,30 @@ if ( ! $wp_query->have_posts() ) {
 
 ?>
 
-<main id="main" class="site-main alignwide" role="main">
-
-	<header class="page-header">
-		<?php
-		the_archive_title( '<h1 class="page-title">', '</h1>' );
-		the_archive_description( '<div class="taxonomy-description">', '</div>' );
-		?>
-	</header>
-
+<header class="page-header">
 	<?php
-	echo do_blocks( <<<BLOCKS
-		<!-- wp:wporg/filter-bar /-->
-		<!-- wp:query {"tagName":"div","className":"plugin-cards"} -->
-		<div class="wp-block-query plugin-cards">
-				<!-- wp:post-template {"className":"is-style-cards-grid","layout":{"type":"grid","minimumColumnWidth":"48%"}} -->
-					<!-- wp:wporg/plugin-card /-->
-				<!-- /wp:post-template -->
-			</div>
-		<!-- /wp:query -->
-	BLOCKS
-	);
-
-	if ( ! have_posts() ) {
-		get_template_part( 'template-parts/no-results' );
-	}
-
-	the_posts_pagination();
-
+	the_archive_title( '<h1 class="page-title">', '</h1>' );
+	the_archive_description( '<div class="taxonomy-description">', '</div>' );
 	?>
-</main><!-- #main -->
+</header>
+
+<?php
+echo do_blocks( <<<BLOCKS
+	<!-- wp:wporg/filter-bar /-->
+	<!-- wp:query {"tagName":"div","className":"plugin-cards"} -->
+	<div class="wp-block-query plugin-cards">
+			<!-- wp:post-template {"className":"is-style-cards-grid","layout":{"type":"grid","minimumColumnWidth":"48%"}} -->
+				<!-- wp:wporg/plugin-card /-->
+			<!-- /wp:post-template -->
+		</div>
+	<!-- /wp:query -->
+BLOCKS
+);
+
+if ( ! have_posts() ) {
+	get_template_part( 'template-parts/no-results' );
+}
+
+the_posts_pagination();
+
+?>
