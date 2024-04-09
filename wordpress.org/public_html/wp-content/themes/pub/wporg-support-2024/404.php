cf823dcc3b1ff71de280cc9ca196cebb865f810d
Kelly Choyce-Dwan <ryelle@git.wordpress.org>
2024-04-09 18:16:57 +0000

    wporg-support-2024: Fix 404 page styles.
    Fixes https://github.com/WordPress/wordpress.org/issues/235.
    
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@13478 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-support-2024/404.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-support-2024/404.php
@@ -13,17 +13,19 @@ get_header(); ?>
 
 <main id="main" class="wp-block-group alignfull site-main is-layout-constrained wp-block-group-is-layout-constrained" role="main">
 
-		<div class="wp-block-group alignwide is-layout-flow wp-block-group-is-layout-flow">
+	<div class="wp-block-group alignwide is-layout-flow wp-block-group-is-layout-flow">
 
-	<section class="error-404 not-found">
-		<header class="page-header">
-			<h1 class="page-title"><?php _e( 'Oops! That page can&rsquo;t be found.', 'wporg-forums' ); ?></h1>
-		</header><!-- .page-header -->
+		<section class="error-404 not-found">
+			<header class="page-header">
+				<h1 class="page-title"><?php _e( 'Oops! That page can&rsquo;t be found.', 'wporg-forums' ); ?></h1>
+			</header><!-- .page-header -->
 
-		<div class="page-content">
-			<p><?php printf( __( 'Try searching from the field above, or go to the <a href="%s">home page</a>.', 'wporg-forums' ), get_home_url() ); ?></p>
-		</div>
-	</section>
+			<div class="page-content">
+				<p><?php printf( __( 'Try searching from the field above, or go to the <a href="%s">home page</a>.', 'wporg-forums' ), get_home_url() ); ?></p>
+			</div>
+		</section>
+
+		<div style="margin-top:0;height:var(--wp--preset--spacing--70);" aria-hidden="true" class="wp-block-spacer"></div>
 
 	</div>
 
