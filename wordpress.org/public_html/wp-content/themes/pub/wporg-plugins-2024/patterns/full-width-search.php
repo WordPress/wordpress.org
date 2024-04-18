d4e5713f8c62012c6a0232027e9b93d2f602c699
Steve Dufresne <dufresnesteven@git.wordpress.org>
2024-04-18 07:12:19 +0000

    wporg-plugins-2024: Add search bar to single plugin page.
    See: https://github.com/WordPress/wordpress.org/issues/252
    Props: ndiego
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@13564 74240141-8908-4e6f-9713-ba540dce6ec7

---
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-plugins-2024/patterns/full-width-search.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Title: Full width search
+ * Slug: wporg-plugins-2024/full-width-search
+ * Inserter: no
+ *
+ */
+
+?>
+
+<!-- wp:group {"align":"full","style":{"spacing":{"margin":{"top":"var:preset|spacing|20","bottom":"var:preset|spacing|20"}}}} -->
+	<div class="wp-block-group alignfull" style="margin-top:var(--wp--preset--spacing--20);margin-bottom:var(--wp--preset--spacing--20)">
+		<!-- wp:search {"label":"<?php esc_attr_e( 'Search plugins', 'wporg' ); ?>","showLabel":false,"placeholder":"<?php esc_attr_e( 'Search plugins', 'wporg' ); ?>","width":232,"widthUnit":"px","buttonText":"<?php esc_attr_e( 'Search plugins', 'wporg' ); ?>","buttonPosition":"button-inside","buttonUseIcon":true} /-->
+	</div>
+<!-- /wp:group -->
