b6c4dd2c60b9a0b191786d41a2a6e611646cc8ac
Adam Wood <adamwood@git.wordpress.org>
2024-05-14 08:45:15 +0000

    Learn: Sync with git WordPress/learn@72a68fd
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@13699 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-learn-2024/functions.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-learn-2024/functions.php
@@ -3,25 +3,104 @@
 namespace WordPressdotorg\Theme\Learn_2024;
 
 /**
+ * Shortcut to the build directory.
+ *
+ * @return string
+ */
+function get_build_path() {
+	return get_stylesheet_directory() . '/build/';
+}
+
+/**
+ * Shortcut to the build URL.
+ *
+ * @return string
+ */
+function get_build_url() {
+	return get_stylesheet_directory_uri() . '/build/';
+}
+
+/**
+ * Shortcut to the includes directory.
+ *
+ * @return string
+ */
+function get_includes_path() {
+	return get_stylesheet_directory() . '/inc/';
+}
+
+/**
+ * Shortcut to the views directory.
+ *
+ * @return string
+ */
+function get_views_path() {
+	return get_stylesheet_directory() . '/views/';
+}
+
+/**
  * Admin.
  */
-require __DIR__ . '/inc/admin.php';
+require get_includes_path() . 'admin.php';
 
 /**
  * Capabilities.
  */
-require __DIR__ . '/inc/capabilities.php';
+require get_includes_path() . 'capabilities.php';
 
 /**
  * Taxonomies.
  */
-require __DIR__ . '/inc/taxonomy.php';
+require get_includes_path() . 'taxonomy.php';
 
 /**
  * Actions and filters.
  */
+add_action( 'after_setup_theme', __NAMESPACE__ . '\setup' );
 add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\enqueue_assets' );
 add_filter( 'wporg_block_navigation_menus', __NAMESPACE__ . '\add_site_navigation_menus' );
+add_filter( 'single_template_hierarchy', __NAMESPACE__ . '\modify_single_template' );
+remove_filter( 'template_include', array( 'Sensei_Templates', 'template_loader' ), 10, 1 );
+add_filter( 'sensei_register_post_type_lesson', function( $args ) {
+	$args['has_archive'] = 'lessons';
+	return $args;
+} );
+add_filter( 'sensei_register_post_type_course', function( $args ) {
+	$args['has_archive'] = 'courses';
+	return $args;
+} );
+
+/**
+ * Modify the single template hierarchy to use customised copies of the Sensei Course Theme templates.
+ *
+ * @param array $templates Array of template files.
+ * @return array
+ */
+function modify_single_template( $templates ) {
+	if ( is_singular( 'lesson' ) ) {
+		array_unshift( $templates, 'single-lesson.html' );
+	} elseif ( is_singular( 'quiz' ) ) {
+		array_unshift( $templates, 'single-quiz.html' );
+	}
+
+	return $templates;
+}
+
+/**
+ * Sets up theme defaults and registers support for various WordPress features.
+ *
+ * Note that this function is hooked into the after_setup_theme hook, which
+ * runs before the init hook. The init hook is too late for some features, such
+ * as indicating support for post thumbnails.
+ */
+function setup() {
+	add_theme_support( 'post-thumbnails' );
+	add_theme_support( 'sensei' );
+	add_theme_support( 'responsive-embeds' );
+
+	add_filter( 'mkaz_code_syntax_force_loading', '__return_true' );
+	add_filter( 'mkaz_prism_css_path', '__return_empty_string' ); // Disable default styles to avoid conflicts.
+}
 
 /**
  * Enqueue scripts and styles.
@@ -32,9 +111,9 @@ function enqueue_assets() {
 	// stylesheet as a dependency.
 	wp_enqueue_style(
 		'wporg-learn-2024-style',
-		get_stylesheet_directory_uri() . '/build/style/style-index.css',
+		get_build_url() . 'style/style-index.css',
 		array( 'wporg-parent-2021-style', 'wporg-global-fonts' ),
-		filemtime( __DIR__ . '/build/style/style-index.css' )
+		filemtime( get_build_path() . 'style/style-index.css' )
 	);
 
 	// Preload the heading font(s).
@@ -71,4 +150,3 @@ function add_site_navigation_menus( $menus ) {
 		),
 	);
 }
-
