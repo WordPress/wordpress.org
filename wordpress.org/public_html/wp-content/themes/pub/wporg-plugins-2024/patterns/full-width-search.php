9ffb6ca549c5d12ce4ed80d028a1e24eaa72b8dc
Kelly Choyce-Dwan <ryelle@git.wordpress.org>
2024-06-24 22:45:06 +0000

    Plugin Directory: Move spacing to parent element for search bar.
    This is an alternate solution to https://github.com/WordPress/wordpress.org/issues/327. Rather than creating a duplicate container for the search bar, the left/right spacing can be added to the alignfull element directly. This adds back the expected left/right spacing now that nested alignfull elements have negative margins.
    
    See https://github.com/WordPress/wordpress.org/pull/328.
    
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@13855 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-plugins-2024/patterns/full-width-search.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-plugins-2024/patterns/full-width-search.php
@@ -3,17 +3,12 @@
  * Title: Full width search
  * Slug: wporg-plugins-2024/full-width-search
  * Inserter: no
- *
  */
 
 ?>
 
-<!-- wp:group {"align":"full","style":{"spacing":{"margin":{"top":"var:preset|spacing|20","bottom":"var:preset|spacing|20"}}}} -->
-	<div class="wp-block-group alignfull" style="margin-top:var(--wp--preset--spacing--20);margin-bottom:var(--wp--preset--spacing--20)">
-	<!-- wp:group {"tagName":"div","style":{"spacing":{"padding":{"left":"var:preset|spacing|edge-space","right":"var:preset|spacing|edge-space"}}}} -->
-		<div class="wp-block-group" style="padding-right:var(--wp--preset--spacing--edge-space);padding-left:var(--wp--preset--spacing--edge-space)">
-			<!-- wp:search {"label":"<?php esc_attr_e( 'Search plugins', 'wporg' ); ?>","showLabel":false,"placeholder":"<?php esc_attr_e( 'Search plugins', 'wporg' ); ?>","width":232,"widthUnit":"px","buttonText":"<?php esc_attr_e( 'Search plugins', 'wporg' ); ?>","buttonPosition":"button-inside","buttonUseIcon":true} /-->
-		</div>
-	<!-- /wp:group -->
-	</div>
+<!-- wp:group {"align":"full","style":{"spacing":{"margin":{"top":"var:preset|spacing|20","bottom":"var:preset|spacing|20"},"padding":{"right":"var:preset|spacing|edge-space","left":"var:preset|spacing|edge-space"}}}} -->
+<div class="wp-block-group alignfull" style="margin-top:var(--wp--preset--spacing--20);margin-bottom:var(--wp--preset--spacing--20);padding-right:var(--wp--preset--spacing--edge-space);padding-left:var(--wp--preset--spacing--edge-space)">
+	<!-- wp:search {"label":"<?php esc_attr_e( 'Search plugins', 'wporg' ); ?>","showLabel":false,"placeholder":"<?php esc_attr_e( 'Search plugins', 'wporg' ); ?>","width":232,"widthUnit":"px","buttonText":"<?php esc_attr_e( 'Search plugins', 'wporg' ); ?>","buttonPosition":"button-inside","buttonUseIcon":true} /-->
+</div>
 <!-- /wp:group -->
