171d2917f4e44ca50c0bc920055b5abbe62008d2
Kelly Choyce-Dwan <ryelle@git.wordpress.org>
2024-06-27 20:25:52 +0000

    Support Forums (2024): Update site title in local navigation.
    The site title should be visible on scroll on the homepage, and always visible on interior pages.
    
    See https://github.com/WordPress/wporg-mu-plugins/issues/535.
    
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@13866 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-support-2024/patterns/local-nav-home.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-support-2024/patterns/local-nav-home.php
@@ -4,17 +4,18 @@ if ( function_exists( 'register_block_pattern' ) ) {
 		'wporg-support/local-nav-home',
 		array(
 			'title'   => __( 'Local Nav - Home', 'wporg-forums' ),
-			'content' => '<!-- wp:wporg/local-navigation-bar {"backgroundColor":"charcoal-2","style":{"elements":{"link":{"color":{"text":"var:preset|color|white"},":hover":{"color":{"text":"var:preset|color|white"}}}}},"textColor":"white","fontSize":"small"} -->
+			'content' => sprintf(
+				'<!-- wp:wporg/local-navigation-bar {"className":"has-display-contents","backgroundColor":"charcoal-2","style":{"elements":{"link":{"color":{"text":"var:preset|color|white"},":hover":{"color":{"text":"var:preset|color|white"}}}}},"textColor":"white","fontSize":"small"} -->
 
-					<!-- wp:html -->
-					<div style="height:calc(var(--wp--custom--body--small--typography--line-height) * var(--wp--preset--font-size--small));" aria-hidden="true"></div>
-					<!-- /wp:html -->
+					<!-- wp:paragraph {"fontSize":"small","className":"wporg-local-navigation-bar__show-on-scroll"} -->
+					<p class="wp-block-site-title has-small-font-size wporg-local-navigation-bar__show-on-scroll">%s</p>
+					<!-- /wp:paragraph -->
 
 					<!-- wp:navigation {"icon":"menu","overlayBackgroundColor":"charcoal-2","overlayTextColor":"white","layout":{"type":"flex","orientation":"horizontal"},"fontSize":"small","menuSlug":"forums"} /-->
 
-				<!-- /wp:wporg/local-navigation-bar -->'
+				<!-- /wp:wporg/local-navigation-bar -->',
+				esc_html__( 'Forums', 'wporg-forums' ),
+			),
 		),
 	);
 }
-
-?>
