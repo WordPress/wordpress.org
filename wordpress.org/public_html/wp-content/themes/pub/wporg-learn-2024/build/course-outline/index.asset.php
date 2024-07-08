a2cf24e41c76f51ac6b835dc306f7b6f21b71e8e
Adam Wood <adamwood@git.wordpress.org>
2024-07-08 00:22:09 +0000

    Learn: Sync with git WordPress/learn@2354a14
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@13882 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-learn-2024/build/course-outline/index.asset.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-learn-2024/build/course-outline/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-element', 'wp-primitives'), 'version' => '3131e826d22f106afc44');
+<?php return array('dependencies' => array('react', 'wp-element', 'wp-primitives'), 'version' => 'b043040c59c33e770195');
