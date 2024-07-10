ba68512a215ae1d41c87260f15f1aa3b4d072659
Adam Wood <adamwood@git.wordpress.org>
2024-07-10 06:17:54 +0000

    Learn: Sync with git WordPress/learn@83e59bf
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@13893 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-learn-2024/patterns/sidebar-details.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-learn-2024/patterns/sidebar-details.php
@@ -11,26 +11,50 @@
 
 <!-- wp:wporg-learn/sidebar-meta-list {"type":"course","fontSize":"normal"} /-->
 
-<!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"400","lineHeight":"34px"}},"fontSize":"large","fontFamily":"inter"} -->
-<p class="has-inter-font-family has-large-font-size" id="suggestions" style="font-style:normal;font-weight:400;line-height:34px">
-	<?php esc_html_e( 'Suggestions', 'wporg-learn' ); ?>
-</p>
-<!-- /wp:paragraph -->
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group">
 
-<!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"400,"lineHeight":"26px"},"spacing":{"margin":{"bottom":"40px"}}},"fontSize":"normal","fontFamily":"inter"} -->
-<p class="has-inter-font-family has-normal-font-size" style="margin-bottom:40px;font-style:normal;font-weight:400;line-height:26px">
-	<?php esc_html_e( 'Found a typo, grammar error or outdated screenshot? ', 'wporg-learn' ); ?><a href="https://learn.wordpress.org/report-content-feedback/"><?php esc_html_e( 'Contact us', 'wporg-learn' ); ?></a>
-</p>
-<!-- /wp:paragraph -->
+	<!-- wp:heading {"level":2,"fontSize":"large"} -->
+	<h2 class="wp-block-heading has-large-font-size">
+		<?php esc_html_e( 'Suggestions', 'wporg-learn' ); ?>
+	</h2>
+	<!-- /wp:heading -->
 
-<!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"400","lineHeight":"34px"}},"fontSize":"large","fontFamily":"inter"} -->
-<p class="has-inter-font-family has-large-font-size" id="suggestions" style="font-style:normal;font-weight:400;line-height:34px">
-	<?php esc_html_e( 'License', 'wporg-learn' ); ?>
-</p>
-<!-- /wp:paragraph -->
+	<!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"400,"lineHeight":"26px"}},"fontSize":"normal","fontFamily":"inter"} -->
+	<p class="has-inter-font-family has-normal-font-size" style="font-style:normal;font-weight:400;line-height:26px">
+		<?php echo wp_kses_post(
+			sprintf(
+				/* translators: 1: Report content feedback link */
+				__( 'Found a typo, grammar error or outdated screenshot? <a href="%s">Contact us</a>.', 'wporg-learn' ),
+				'https://learn.wordpress.org/report-content-feedback/',
+			)
+		); ?>
+	</p>
+	<!-- /wp:paragraph -->
 
-<!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"400,"lineHeight":"26px"}},"fontSize":"normal","fontFamily":"inter"} -->
-<p class="has-inter-font-family has-normal-font-size" style="font-style:normal;font-weight:400;line-height:26px">
-	<?php esc_html_e( 'This work is licensed under a ', 'wporg-learn' ); ?><a href="http://creativecommons.org/licenses/by-sa/4.0/"><?php esc_html_e( 'Creative Commons Attribution-ShareAlike 4.0 International License', 'wporg-learn' ); ?></a>.
-</p>
-<!-- /wp:paragraph -->
+</div>
+<!-- /wp:group -->
+
+<!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group">
+
+	<!-- wp:heading {"level":2,"fontSize":"large"} -->
+	<h2 class="wp-block-heading has-large-font-size">
+		<?php esc_html_e( 'License', 'wporg-learn' ); ?>
+	</h2>
+	<!-- /wp:heading -->
+
+	<!-- wp:paragraph {"style":{"typography":{"fontStyle":"normal","fontWeight":"400,"lineHeight":"26px"}},"fontSize":"normal","fontFamily":"inter"} -->
+	<p class="has-inter-font-family has-normal-font-size" style="font-style:normal;font-weight:400;line-height:26px">
+		<?php echo wp_kses_post(
+			sprintf(
+				/* translators: 1: License link */
+				__( '<a href="%s">CC BY-SA 4.0 <span aria-hidden="true" class="wp-exclude-emoji">↗</span></a>', 'wporg-learn' ),
+				'http://creativecommons.org/licenses/by-sa/4.0/',
+			)
+		); ?>
+	</p>
+	<!-- /wp:paragraph -->
+
+</div>
+<!-- /wp:group -->
