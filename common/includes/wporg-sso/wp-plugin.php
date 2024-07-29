d94285582acf20ee135470e2a50e55f9934d25fe
Dion Hulse <dd32@git.wordpress.org>
2024-07-29 02:56:28 +0000

    SSO: When a user resets their password, treat that as a password change for logging purposes.
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@13932 74240141-8908-4e6f-9713-ba540dce6ec7

--- common/includes/wporg-sso/wp-plugin.php
+++ common/includes/wporg-sso/wp-plugin.php
@@ -80,6 +80,7 @@ if ( class_exists( 'WPOrg_SSO' ) && ! class_exists( 'WP_WPOrg_SSO' ) ) {
 
 				add_action( 'wp_login', array( $this, 'record_last_logged_in' ), 10, 2 );
 				add_action( 'profile_update', array( $this, 'record_last_password_change' ), 10, 3 );
+				add_action( 'wp_set_password', array( $this, 'record_last_password_change_reset' ), 10, 3 );
 
 				add_action( 'login_form_logout', array( $this, 'login_form_logout' ) );
 
@@ -866,6 +867,16 @@ if ( class_exists( 'WPOrg_SSO' ) && ! class_exists( 'WP_WPOrg_SSO' ) ) {
 		 */
 		public function record_last_password_change( $user_id, $old_data, $new_data ) {
 			if ( $old_data->user_pass !== $new_data['user_pass'] ) {
+				update_user_meta( $user_id, 'last_password_change', gmdate( 'Y-m-d H:i:s' ) );
+			}
+		}
+
+		/**
+		 * Record the last date a user changed their password via password reset.
+		 */
+		public function record_last_password_change_reset( $password, $user_id, $old_user_data ) {
+			$user = get_user_by( 'id', $user_id );
+			if ( $old_user_data->user_pass !== $user->user_pass ) {
 				update_user_meta( $user_id, 'last_password_change', gmdate( 'Y-m-d H:i:s' ) );
 			}
 		}
