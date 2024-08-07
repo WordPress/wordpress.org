dc62f7d16b34bbdd4f0f8e2ba21dc18fb9f540a0
Steve Dufresne <dufresnesteven@git.wordpress.org>
2024-08-07 05:41:58 +0000

    wporg-support: Deleting old theme. Replaced by wporg-support-2024.
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@13952 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-support/bbpress/feedback-no-search.php
+++
@@ -1,3 +0,0 @@
-<div class="bbp-template-notice">
-	<p><?php _e( 'No results found. Try a different search or start a new post.', 'wporg-forums' ); ?></p>
-</div>
--- wordpress.org/public_html/wp-content/themes/pub/wporg-support/sidebar-helphub.php
+++
@@ -1,8 +0,0 @@
-<?php
-if ( is_active_sidebar( 'helphub-sidebar' ) ) {
-	?>
-	<div class="left-sidebar">
-		<?php dynamic_sidebar( 'helphub-sidebar' ); ?>
-	</div>
-	<?php
-}
