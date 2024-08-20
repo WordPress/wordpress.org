a996633a94a17823a3b8dd6de5f3b0c0aae85463
Dion Hulse <dion@wordpress.org>
2024-08-20 12:12:03 +1000

    s/Recovery/Backup/g

--- wordpress.org/public_html/wp-content/themes/pub/wporg-login/backup-codes.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-login/backup-codes.php
@@ -1,11 +1,11 @@
 <?php
 use function WordPressdotorg\Two_Factor\get_edit_account_url;
 /**
- * The 'Recovery Codes' post-login screen.
+ * The 'Backup Codes' post-login screen.
  *
  * This template is used for two primary purposes:
- * 1. The user has logged in with a recovery code, we need to push them to verify their 2FA settings.
- * 2. The user is running low on recovery codes (or has none!), we need to remind them to generate new ones.
+ * 1. The user has logged in with a backup code, we need to push them to verify their 2FA settings.
+ * 2. The user is running low on backup codes (or has none!), we need to remind them to generate new ones.
  *
  * @package wporg-login
  */
@@ -14,9 +14,9 @@ $account_settings_url = get_edit_account_url();
 $redirect_to          = wporg_login_wordpress_url();
 $user                 = wp_get_current_user();
 $session              = WP_Session_Tokens::get_instance( $user->ID )->get( wp_get_session_token() );
-$used_recovery_code   = str_contains( $session['two-factor-provider'] ?? '', 'Backup_Codes' );
+$used_backup_code     = str_contains( $session['two-factor-provider'] ?? '', 'Backup_Codes' );
 $codes_available      = Two_Factor_Backup_Codes::codes_remaining_for_user( $user );
-$can_ignore           = ! $used_recovery_code || ( $used_recovery_code && $codes_available > 1 );
+$can_ignore           = ! $used_backup_code || ( $used_backup_code && $codes_available > 1 );
 
 if ( isset( $_REQUEST['redirect_to'] ) ) {
 	$redirect_to = wp_validate_redirect( wp_unslash( $_REQUEST['redirect_to'] ), $redirect_to );
@@ -29,34 +29,34 @@ if ( ! is_user_logged_in() || ! Two_Factor_Core::is_user_using_two_factor( $user
 }
 
 /**
- * Record the last time we nagged the user about recovery codes, as we only want to do this once per code-use.
+ * Record the last time we nagged the user about backup codes, as we only want to do this once per code-use.
  */
-update_user_meta( $user->ID, 'last_2fa_recovery_codes_nag', $codes_available );
+update_user_meta( $user->ID, 'last_2fa_backup_codes_nag', $codes_available );
 
 get_header();
 ?>
 
 <h2 class="center"><?php
 	if ( $used_recovery_code ) {
-		_e( 'Recovery Code used', 'wporg-login' );
+		_e( 'Backup Code used', 'wporg-login' );
 	} else {
-		_e( 'Account Recovery Codes', 'wporg-login' );
+		_e( 'Account Backup Codes', 'wporg-login' );
 	}
 ?></h2>
 
 <p>&nbsp;</p>
 
 <p><?php
-	if ( $used_recovery_code ) {
-		_e( "You've logged in with a Recovery Code.<br>These codes are intended to be used when you lose access to your authentication device.<br>Please take a moment to review your account settings and ensure your Two-Factor settings are up-to-date.", 'wporg-login' );
+	if ( $used_backup_code ) {
+		_e( "You've logged in with a Backup Code.<br>These codes are intended to be used when you lose access to your authentication device.<br>Please take a moment to review your account settings and ensure your Two-Factor settings are up-to-date.", 'wporg-login' );
 	} else {
 		if ( ! $codes_available ) {
-			_e( "Warning! You don't have any Recovery Codes left.", 'wporg-login' );
+			_e( "Warning! You don't have any Backup Codes left.", 'wporg-login' );
 		} else {
 			printf(
 				_n(
-					"Warning! You've only got %s Recovery Code left.",
-					"Warning! You've only got %s Recovery Codes left.",
+					"Warning! You've only got %s Backup Code left.",
+					"Warning! You've only got %s Backup Codes left.",
 					$codes_available,
 					'wporg-login'
 				),
@@ -72,7 +72,7 @@ get_header();
 <p>&nbsp;</p>
 
 <p><?php
-	_e( 'If you run out of Recovery Codes and no longer have access to your Authentication device, you are at risk of being locked out of your WordPress.org account if we are unable to verify account ownership.', 'wporg-login' );
+	_e( 'If you run out of Backup Codes and no longer have access to your Authentication device, you are at risk of being locked out of your WordPress.org account if we are unable to verify account ownership.', 'wporg-login' );
 ?></p>
 
 <p>&nbsp;</p>
