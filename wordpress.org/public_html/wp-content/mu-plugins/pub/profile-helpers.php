5c0e0c8db8f6d143929397a246ac4327944585f6
Dion Hulse <dd32@git.wordpress.org>
2024-10-17 01:37:00 +0000

    Profile Helpers: Add extra badge helpers and profile-data getters.
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@14115 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/mu-plugins/pub/profile-helpers.php
+++ wordpress.org/public_html/wp-content/mu-plugins/pub/profile-helpers.php
@@ -32,12 +32,45 @@ function remove_badge( string $badge, $users ) : bool {
 }
 
 /**
+ * Determine if a user has a given badge
+ *
+ * @param $badge string The badge slug to check for.
+ * @param $user  mixed  The user to check. A WP_User/ID/Login/Email.
+ * @return bool
+ */
+function has_badge( string $badge, $user ) : bool {
+	return isset( get_user_badges( $user )[ $badge ] );
+}
+
+/**
+ * Get a list of users with a given badge.
+ *
+ * WARNING: Uncached. Excludes dynamically allocated badges.
+ *
+ * @param $badge string The badge group we're looking for.
+ * @return array An array of user IDs.
+ */
+function get_users_with_badge( string $badge ) : array {
+	global $wpdb;
+
+	$users = $wpdb->get_col( $wpdb->prepare(
+		"SELECT user_id
+			FROM bpmain_wporg_groups_members m
+			JOIN bpmain_wporg_groups g ON m.group_id = g.id
+			WHERE g.slug = %s AND m.is_confirmed = 1 AND m.is_banned = 0",
+		$badge
+	) );
+
+	return array_map( 'intval', $users );
+}
+
+/**
  * Get a list of badges for a given user.
  *
  * WARNING: Uncached. Excludes dynamically allocated badges.
  *
  * @param $users mixed The user to fetch the badged for. A WP_User/ID/Login/Email.
- * @return array
+ * @return array An array of badge names keyed by slug.
  */
 function get_user_badges( $user ) {
 	global $wpdb;
@@ -48,12 +81,45 @@ function get_user_badges( $user ) {
 		"SELECT slug, name
 			FROM bpmain_wporg_groups_members m
 			JOIN bpmain_wporg_groups g ON m.group_id = g.id
-			WHERE m.user_id = %d
+			WHERE m.user_id = %d AND m.is_confirmed = 1 AND m.is_banned = 0
 			ORDER BY slug",
 		$user_id
 	), ARRAY_A );
 
 	return array_column( $badges, 'name', 'slug' );
+}
+
+/**
+ * Fetch the profiles data for a given user.
+ *
+ * NOTE: The returned array is currently uncached and if a field is empty/blank it's not set here.
+ *
+ * @param $user mixed The user to fetch the profile for. A WP_User/ID/Login/Email.
+ * @return array|WP_Error An array of profile fields keyed by field name.
+ */
+function get_user_details( $user ) {
+	global $wpdb;
+
+	$user_id = find_user_id( $user );
+
+	$data = $wpdb->get_results( $wpdb->prepare(
+		"SELECT f.name, d.value, f.type, d.last_updated
+			FROM bpmain_bp_xprofile_data d
+				JOIN bpmain_bp_xprofile_fields f ON d.field_id = f.id
+			WHERE d.user_id = %d",
+		$user_id
+	) );
+
+	$result = [];
+	foreach ( $data as $field ) {
+		$name = sanitize_title_with_dashes( $field->name );
+		if ( str_contains( $field->type, 'multi' ) || $field->type === 'checkbox' ) {
+			$field->value = maybe_unserialize( $field->value );
+		}
+		$result[ $name ] = $field;
+	}
+
+	return $result;
 }
 
 /**
@@ -105,7 +171,7 @@ function update_profile( $field, $value, $user ) {
  * @param $users  mixed  The user(s) to assign to. A WP_User/ID/Login/Email/Slug (or array of) of the user(s) to assign.
  * @return bool
  */
-function badge_api( string $action, string $badge, $users ) : bool {
+function badge_api( string $action, string $badge, $users = array() ) : bool {
 	$users = is_object( $users ) ? [ $users ] : (array) $users;
 	$users = array_filter( array_map( __NAMESPACE__ . '\find_user_id', $users ) );
 
@@ -117,14 +183,14 @@ function badge_api( string $action, string $badge, $users ) : bool {
 		$users = array_filter(
 			$users,
 			function( $user ) use ( $badge ) {
-				return isset( get_user_badges( $user )[ $badge ] );
+				return has_badge( $badge, $user );
 			}
 		);
 	} elseif ( 'add' === $action ) {
 		$users = array_filter(
 			$users,
 			function( $user ) use ( $badge ) {
-				return ! isset( get_user_badges( $user )[ $badge ] );
+				return ! has_badge( $badge, $user );
 			}
 		);
 	}
