4dbc67ba07a4c8b82eb7791039a6d9d75fcf2b25
Dion Hulse <dd32@git.wordpress.org>
2024-10-24 07:42:29 +0000

    Plugin Directory: Fix a typo in [14126] causing SVN creation not to be retried.
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@14142 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/plugins/plugin-directory/jobs/class-svn-repo-creation.php
+++ wordpress.org/public_html/wp-content/plugins/plugin-directory/jobs/class-svn-repo-creation.php
@@ -14,6 +14,9 @@ class SVN_Repo_Creation {
 
 	public static function cron_trigger( $post_id, $author_id = null ) {
 		$post = get_post( $post_id );
+		if ( ! $post ) {
+			throw new \Exception( 'Invalid plugin ID.' );
+		}
 
 		// Check to see if the repo exists..
 		$exists = SVN::ls( 'http://plugins.svn.wordpress.org/' . $post->post_name . '/' );
@@ -21,7 +24,7 @@ class SVN_Repo_Creation {
 			return;
 		}
 
-		$created = Status_Transitions::instance()->approved_create_svn_repo( $plugin_id, $author_id );
+		$created = Status_Transitions::instance()->approved_create_svn_repo( $post, $author_id );
 		if ( $created ) {
 			Tools::audit_log( 'Created SVN Repository.', $post->ID );
 		}
