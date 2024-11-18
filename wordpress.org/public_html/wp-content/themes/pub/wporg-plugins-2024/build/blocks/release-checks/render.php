38e8bb944d29baec34e24e8300898622a5b09f91
StevenDufresne <dufresnesteven@gmail.com>
2024-11-18 00:45:55 +0000

    Turn around if not published.

--- wordpress.org/public_html/wp-content/themes/pub/wporg-plugins-2024/build/blocks/release-checks/render.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-plugins-2024/build/blocks/release-checks/render.php
@@ -3,7 +3,22 @@
 if ( ! current_user_can( 'plugin_admin_edit', $post ) ) {
     return;
 }
+
+
+$current_post_id = $block->context['postId'];
+if ( ! $current_post_id ) {
+	return;
+}
+
+if ( 'publish' === get_post_status( $block->context['postId'] ) ) {
+    return;
+}
+
 ?>
+
+<!-- wp:heading {"level":4,"fontSize":"normal"} -->
+<h4 class="wp-block-heading has-normal-font-size"><?php esc_html_e( 'Checks', 'wporg-plugins' ); ?></h4>
+<!-- /wp:heading -->
 
 <ul>
     <li>First Item</li>
