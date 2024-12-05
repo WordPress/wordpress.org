af729aa8c94440012c15116f61af8a00dadf6c56
tellyworth <alex@automattic.com>
2024-12-05 17:14:54 +1100

    Add untested Plugin_Publish API endpoint
    As yet untested.
    
    Note that I called it Plugin_Publish rather than Plugin_Release, so as not to confuse it with the other class which is also called Plugin_Release (we're using "Release" as both a noun and verb in different places).

---
+++ wordpress.org/public_html/wp-content/plugins/plugin-directory/api/routes/class-plugin-publish.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * An API endpoint for publishing plugin releases.
+ *
+ * @package WordPressdotorg_Plugin_Directory
+ */
+
+namespace WordPressdotorg\Plugin_Directory\API\Routes;
+
+use WP_REST_Response;
+use WP_REST_Server;
+use WordPressdotorg\Plugin_Directory\Plugin_Directory;
+use WordPressdotorg\Plugin_Directory\Plugin_Directory\Plugin_Release;
+use WordPressdotorg\Plugin_Directory\API\Base;
+
+/**
+ * An API endpoint for publishing a release of a plugin.
+ *
+ * @package WordPressdotorg_Plugin_Directory
+ */
+class Plugin_Publish extends Base {
+
+	/**
+	 * Plugin_Publish constructor.
+	 */
+	public function __construct() {
+		register_rest_route(
+			'plugins/v2',
+			'/plugin/(?P<plugin_slug>[^/]+)/publish',
+			array(
+				'methods'             => \WP_REST_Server::CREATABLE,
+				'callback'            => array( $this, 'publish_release' ),
+				'args'                => array(
+					'plugin_slug' => array(
+						'validate_callback' => array( $this, 'validate_plugin_slug_callback' ),
+					),
+				),
+				'permission_callback' => array( $this, 'permission_can_access_plugin' ),
+			)
+		);
+	}
+
+	/**
+	 * Validate that the user can manage a given plugin.
+	 */
+	public function permission_can_access_plugin( $request ) {
+		$plugin = Plugin_Directory::get_plugin_post( $request['plugin_slug'] );
+		return current_user_can( 'plugin_manage_releases', $plugin );
+	}
+
+	/**
+	 * A simple endpoint to publish a release.
+	 *
+	 * @param WP_REST_Request $request The request object.
+	 * @return WP_REST_Response
+	 */
+	public function publish_release( $request ) {
+
+		$plugin = Plugin_Directory::get_plugin_post( $request['plugin_slug'] );
+		// Will return either a WP_Error, or the post ID of the published release CPT.
+		// Maybe it should return the version string instead, or an object with more details? The whole CPT?
+		$result = Plugin_Release::publish_release( $plugin );
+
+		return new WP_REST_Response( $result );
+	}
+}
