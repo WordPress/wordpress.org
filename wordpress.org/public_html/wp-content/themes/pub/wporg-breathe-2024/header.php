ac7d33bdedf92cef8788883b319318f1b0913e73
timse201 <timse201@users.noreply.github.com>
2025-02-10 15:39:44 +0100

    Add missing trailing slash
    reduces redirects to home url

--- wordpress.org/public_html/wp-content/themes/pub/wporg-breathe-2024/header.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-breathe-2024/header.php
@@ -36,7 +36,7 @@ if ( is_front_page() && is_home() ) {
 				<!-- wp:navigation {"icon":"menu","overlayBackgroundColor":"charcoal-2","overlayTextColor":"white","layout":{"type":"flex","orientation":"horizontal"},"fontSize":"small","menuSlug":"breathe"} /-->
 
 			<!-- /wp:wporg/local-navigation-bar -->',
-			esc_url( $is_updates_or_project ? $make_home_url : home_url() ),
+			esc_url( $is_updates_or_project ? $make_home_url : home_url( '/' ) ),
 			$before_name,
 			$is_updates_or_project
 				? esc_html__( 'Make WordPress', 'wporg-breathe' )
