d40ec0c03de39c9688f1ceae38ad5bfd21d2d498
Nikunj Hatkar <nhatkar@updraftplus.com>
2025-04-09 17:00:07 +0530

    Add donate button near download button

--- wordpress.org/public_html/wp-content/themes/pub/wporg-plugins-2024/template-parts/plugin-single.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-plugins-2024/template-parts/plugin-single.php
@@ -49,6 +49,7 @@ $plugin_title = $is_closed ? $post->post_name : get_the_title();
 			</div>
 			<div class="plugin-actions">
 				<?php
+				$donate_link = get_post_meta( get_the_ID(), 'donate_link', true );
 				$buttons = '<!-- wp:wporg/favorite-button /-->';
 
 				if ( 'publish' === get_post_status() || current_user_can( 'plugin_admin_view', $post ) ) {
@@ -75,6 +76,15 @@ $plugin_title = $is_closed ? $post->post_name : get_the_title();
 						<!-- /wp:button -->',
 						esc_attr( add_query_arg( array( 'preview' => 1 ), get_the_permalink() ) ),
 						esc_html__( 'Test Preview', 'wporg-plugins' )
+					);
+				}
+				if ( $donate_link ) {
+					$buttons .= sprintf(
+						'<!-- wp:button {"className":"is-small is-style-outline plugin-donate download-button"} -->
+						<div class="wp-block-button is-small is-style-outline plugin-donate download-button"><a class="wp-block-button__link wp-element-button" href="%1$s">%2$s</a></div>
+						<!-- /wp:button -->',
+						esc_url( $donate_link ),
+						esc_html__( 'Donate', 'wporg-plugins' )
 					);
 				}
 				echo do_blocks( $buttons ); // phpcs:ignore -- Output escaped while building string.
