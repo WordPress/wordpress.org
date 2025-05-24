9524aa8ed68bdc089b6982845f0074645148b424
Bademba SANGARE, laptop <sangarebademba.mail@gmail.com>
2025-05-24 03:55:47 +0200

    Fix: Make 'See all reviews' link fully visible for better localization

--- wordpress.org/public_html/wp-content/themes/pub/wporg-plugins-2024/inc/block-bindings.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-plugins-2024/inc/block-bindings.php
@@ -43,7 +43,7 @@ function get_meta_block_value( $args, $block ) {
 			return sprintf(
 				'<a href="%s">%s</a>',
 				esc_url( 'https://wordpress.org/support/plugin/' . $plugin_post->post_name . '/reviews/' ),
-				__( 'See all<span class="screen-reader-text"> reviews</span>', 'wporg-themes' )
+				__( 'See all reviews', 'wporg-themes' )
 			);
 		case 'submit-review-link':
 			return sprintf(
