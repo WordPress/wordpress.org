3ea0c0d63e2c60cbbe6513657db30e2138993871
Phuc Nguyen <manhphucofficial@gmail.com>
2025-12-10 14:23:50 +0700

    Support Forums: Add login call-to-action when no topics found. See #8141.

--- wordpress.org/public_html/wp-content/themes/pub/wporg-support/bbpress/feedback-no-topics.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-support/bbpress/feedback-no-topics.php
@@ -1,3 +1,15 @@
 <div class="bbp-template-notice">
-	<p><?php _e( 'No topics found. Select another view or start a new post.', 'wporg-forums' ); ?></p>
+	<p>
+		<?php
+		if ( is_user_logged_in() ) {
+			_e( 'No topics found. Select another view or start a new post.', 'wporg-forums' );
+		} else {
+			printf(
+			/* translators: %s: Login URL. */
+				__( 'No topics found. Select another view or start a new post. Please <a href="%s">log in</a> first.', 'wporg-forums' ),
+				esc_url( wp_login_url( bbp_get_forum_permalink() ) )
+			);
+		}
+		?>
+	</p>
 </div>
