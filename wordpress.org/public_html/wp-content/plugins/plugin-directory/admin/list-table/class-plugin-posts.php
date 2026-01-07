cab39978ab5c6cf02216c3a9f9ccd6c94a041aee
Dion Hulse <dd32@git.wordpress.org>
2026-01-07 05:47:13 +0000

    Plugin Directory: Admin: Only show the latest ZIP in the post table list.
    Props nilambar.
    Fixes https://github.com/WordPress/wordpress.org/pull/540.
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@14626 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/plugins/plugin-directory/admin/list-table/class-plugin-posts.php
+++ wordpress.org/public_html/wp-content/plugins/plugin-directory/admin/list-table/class-plugin-posts.php
@@ -706,6 +706,9 @@ class Plugin_Posts extends \WP_Posts_List_Table {
 			return;
 		}
 
+		// Only display the latest.
+		$media = array_slice( $media, -1 );
+
 		foreach ( $media as $zip_file ) {
 			$zip_size = size_format( filesize( get_attached_file( $zip_file->ID ) ), 1 );
 
@@ -717,7 +720,7 @@ class Plugin_Posts extends \WP_Posts_List_Table {
 			}
 
 			printf(
-				'<a href="%1$s">%2$s</a> v%3$s<br>%4$s<br>(<a href="%5$s" target="_blank">test</a> | <a href="%6$s" target="_blank">pcp</a>)<br></li>',
+				'<a href="%1$s">%2$s</a> v%3$s<br>%4$s<br>(<a href="%5$s" target="_blank">test</a> | <a href="%6$s" target="_blank">pcp</a>)<br>',
 				esc_url( $url ),
 				esc_html( $name ),
 				esc_html( $zip_file->version ),
