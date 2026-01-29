04e24f158f8aa7be0f92486d4d3dfb5667461e62
Dion Hulse <dd32@git.wordpress.org>
2026-01-29 02:23:12 +0000

    Plugin Directory: Beta/Featured plugins: These plugins have limited self-management capabilities, they're required to contact the plugins team for alterations.
    This makes it clear on how to contact them, and shows the limitation warning to plugin reviewers (or super-admins) to ease confusion.
    
    Props JeffPaul for suggestion.
    See #8160.
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@14644 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-plugins-2024/inc/template-tags.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-plugins-2024/inc/template-tags.php
@@ -268,12 +268,21 @@ function the_no_self_management_notice() {
 	// Check if they can access plugin management, but can't add committers.
 	// This means the plugin has limited self-management functionalities, for security.
 	if (
-		current_user_can( 'plugin_admin_edit', $post ) &&
-		! current_user_can( 'plugin_add_committer', $post )
+		(
+			current_user_can( 'plugin_admin_edit', $post ) &&
+			! current_user_can( 'plugin_add_committer', $post )
+		) || (
+			// Show the notice to plugin reviewers when it's limited. See class-capabilities.php.
+			is_object_in_term( $post->ID, 'plugin_section', array( 'beta', 'featured' ) ) &&
+			current_user_can( 'plugin_review' )
+		)
 	) {
 		printf(
 			'<div class="plugin-notice notice notice-warning notice-alt"><p>%s</p></div>',
-			__( 'Management of this plugin has been limited for security reasons. Please contact the plugins team for assistance to add/remove committers, or to perform other actions that are unavailable.', 'wporg-plugins' )
+			sprintf(
+				__( 'Management of this plugin has been limited for security reasons. Please contact the <a href="mailto:%1$s">plugins team (%1$s)</a> for assistance to add/remove committers, or to perform other actions that are unavailable.', 'wporg-plugins' ),
+				'plugins@wordpress.org'
+			)
 		);
 	}
 }
