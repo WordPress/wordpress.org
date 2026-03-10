3f6766daf5e82af627af94ffc227a59cf4c72514
dd32 <dd32@74240141-8908-4e6f-9713-ba540dce6ec7>
2026-03-10 03:33:02 +0000

    Plugin Directory: Navigation: Better support for local environments and unit tests.
    See https://github.com/WordPress/wordpress.org/pull/555
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@14685 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/themes/pub/wporg-plugins-2024/inc/block-config.php
+++ wordpress.org/public_html/wp-content/themes/pub/wporg-plugins-2024/inc/block-config.php
@@ -26,7 +26,11 @@ add_filter( 'render_block_wporg/language-suggest', __NAMESPACE__ . '\filter_lang
 function add_site_navigation_menus( $menus ) {
 	global $wp;
 
-	$url = 'https://' . $_SERVER['HTTP_HOST'] . parse_url( $_SERVER['REQUEST_URI'], PHP_URL_PATH );
+	// This accounts for test and local environments.
+	$scheme = wp_parse_url( home_url(), PHP_URL_SCHEME ) ?: 'https';
+	$host   = $_SERVER['HTTP_HOST'] ?? wp_parse_url( home_url(), PHP_URL_HOST ) ?: 'localhost';
+	$path   = parse_url( ( $_SERVER['REQUEST_URI'] ?? '/' ) ?: '/', PHP_URL_PATH );
+	$url    = $scheme . '://' . $host . $path;
 
 	$items = array(
 		'plugins' => array(
