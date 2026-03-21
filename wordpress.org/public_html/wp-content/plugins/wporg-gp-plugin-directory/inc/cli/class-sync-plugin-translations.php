6718905550586215ba7d47d66f6386b291a2a54c
timse201 <timse201@users.noreply.github.com>
2026-03-21 14:35:24 +0100

    fix invalid property
    Fix invalid property translation_status. This prevents translations from being imported with null and the fallback by GlotPress to current.

--- wordpress.org/public_html/wp-content/plugins/wporg-gp-plugin-directory/inc/cli/class-sync-plugin-translations.php
+++ wordpress.org/public_html/wp-content/plugins/wporg-gp-plugin-directory/inc/cli/class-sync-plugin-translations.php
@@ -75,7 +75,7 @@ class Sync_Plugin_Translations extends WP_CLI_Command {
 			$current_translations->add_entry( $translation );
 			$current_translations_map[ $translation->key() ] = [
 				'user_id'  => $translation->user_id,
-				'status'   => $translation->translation_status,
+				'status'   => $translation->status,
 				'warnings' => $translation->warnings,
 			];
 
