55bef84fdb8b744ed807e7b5cda6bbdc88b10112
davidperezgar <david@closemarketing.es>
2026-03-22 17:31:02 +0100

    add template email

--- wordpress.org/public_html/wp-content/plugins/plugin-directory/class-template.php
+++ wordpress.org/public_html/wp-content/plugins/plugin-directory/class-template.php
@@ -1086,7 +1086,7 @@ class Template {
 			'banned'               => 'Banned developer trying to sneak back in',
 			'author-request'       => 'Author requested not to continue',
 			'security'             => 'Security concerns',
-			'common'               => 'Common plugin',
+			'common-plugin'        => 'Common plugin',
 			'other'                => 'OTHER: See notes',
 		);
 	}
