476a89c7fefcc0e85a61c9c553103d2611040612
Dion Hulse <dd32@git.wordpress.org>
2026-03-27 08:41:11 +0000

    Plugin Directory: Admin: Add ElasticSearch index metabox and status tool.
    Adds a new metabox on plugin admin edit screens showing current ES status, to allow plugin reviewers to confirm that a plugin is indeed in the search index.
    
    Adds a new plugin admin tool to allow bulk-query of the ES index to verify the contents is correct (Note: It should be, this is just to validate that).
    
    Adds an ability to reindex plugins, by touching metadata / firing Jetpack sync actions, in the rare event it becomes out of sync.
    This would likely only occur due to downtime on WordPress.org/WordPress.com, or when we cause Jetpack sync to not work.
    
    See #8210
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@14765 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/plugins/plugin-directory/admin/class-customizations.php
+++ wordpress.org/public_html/wp-content/plugins/plugin-directory/admin/class-customizations.php
@@ -72,6 +72,8 @@ class Customizations {
 		add_action( 'wp_ajax_plugin-author-lookup', array( __NAMESPACE__ . '\Metabox\Author', 'lookup_author' ) );
 		add_action( 'wp_ajax_plugin-svn-sync', array( __NAMESPACE__ . '\Metabox\Review_Tools', 'svn_sync' ) );
 		add_action( 'wp_ajax_plugin-set-reviewer', array( __NAMESPACE__ . '\Metabox\Reviewer', 'xhr_set_reviewer' ) );
+		add_action( 'wp_ajax_plugin-elasticsearch', array( __NAMESPACE__ . '\Metabox\Elasticsearch', 'ajax_response' ) );
+		add_action( 'wp_ajax_plugin-elasticsearch-reindex', array( __NAMESPACE__ . '\Metabox\Elasticsearch', 'ajax_reindex' ) );
 
 		add_action( 'save_post', array( __NAMESPACE__ . '\Metabox\Release_Confirmation', 'save_post' ) );
 		add_action( 'save_post', array( __NAMESPACE__ . '\Metabox\Author_Notice', 'save_post' ) );
@@ -783,6 +785,13 @@ class Customizations {
 				'plugin', 'normal', 'high'
 			);
 		}
+
+		add_meta_box(
+			'plugin-elasticsearch',
+			__( 'ElasticSearch Index', 'wporg-plugins' ),
+			array( __NAMESPACE__ . '\Metabox\Elasticsearch', 'display' ),
+			'plugin', 'normal', 'low'
+		);
 
 		// Remove unnecessary metaboxes.
 		remove_meta_box( 'commentsdiv', 'plugin', 'normal' );
