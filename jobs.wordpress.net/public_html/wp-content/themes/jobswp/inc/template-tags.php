2f05bc761c4bcec4fc1c16558ec099b92a6aee95
Dion Hulse <dion@wordpress.org>
2026-03-27 18:56:59 +1000

    Simplify the posted_on logic.

--- jobs.wordpress.net/public_html/wp-content/themes/jobswp/inc/template-tags.php
+++ jobs.wordpress.net/public_html/wp-content/themes/jobswp/inc/template-tags.php
@@ -172,22 +172,12 @@ if ( ! function_exists( 'jobswp_posted_on' ) ) :
  * Prints HTML with meta information for the current post-date/time and author.
  */
 function jobswp_posted_on() {
-	$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time>';
-	if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) )
-		$time_string .= '<time class="updated" datetime="%3$s">%4$s</time>';
-
-	$time_string = sprintf( $time_string,
-		esc_attr( get_the_date( 'c' ) ),
-		esc_html( get_the_date() ),
-		esc_attr( get_the_modified_date( 'c' ) ),
-		esc_html( get_the_modified_date() )
-	);
-
-	printf( __( '<span class="posted-on">Posted %1$s</span>', 'jobswp' ),
-		sprintf( '<a href="%1$s" title="%2$s" rel="bookmark">%3$s</a>',
+	printf(
+		__( '<span class="posted-on">Posted %1$s</span>', 'jobswp' ),
+		sprintf( '<a href="%1$s" rel="bookmark"><time class="entry-date published" datetime="%2$s">%3$s</time></a>',
 			esc_url( get_permalink() ),
-			esc_attr( get_the_time() ),
-			$time_string
+			esc_attr( get_the_date( 'c' ) ),
+			esc_html( get_the_date() )
 		)
 	);
 }
