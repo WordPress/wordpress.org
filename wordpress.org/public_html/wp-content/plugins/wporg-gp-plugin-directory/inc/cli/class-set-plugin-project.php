3a4f991c01dfed81846adfa54721e1bbc8bb20a2
timse201 <timse201@users.noreply.github.com>
2026-04-03 22:38:21 +0200

    Set_Plugin_Project: Validate JSON via json_validate()

--- wordpress.org/public_html/wp-content/plugins/wporg-gp-plugin-directory/inc/cli/class-set-plugin-project.php
+++ wordpress.org/public_html/wp-content/plugins/wporg-gp-plugin-directory/inc/cli/class-set-plugin-project.php
@@ -295,9 +295,13 @@ class Set_Plugin_Project extends WP_CLI_Command {
 			)
 		);
 
-		$details = $json && '{' == $json[0] ? json_decode( $json ) : null;
+		if ( ! json_validate( $json ) ) {
+			return null;
+		}
+		
+		$details = json_decode( $json );
 
-		if ( isset( $details->error ) ) {
+		if ( ! is_object( $details ) || isset( $details->error ) ) {
 			return null;
 		}
 
