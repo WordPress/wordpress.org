fa5c3ac7fcaf877e6a50955e7d6b351ebff2563f
Jonathan Bossenger <jonathanbossenger@gmail.com>
2026-04-03 09:43:07 +0200

    Adding announce privs for neel33 in the #core-ai channel in Slack

--- common/includes/slack/announce/config.php
+++ common/includes/slack/announce/config.php
@@ -163,6 +163,7 @@ function get_whitelist() {
 		) ),
 		'core-ai' => array_merge( get_committers(), array(
 			'isotropic', // @James LePage on Slack
+            '@neel33', // @Neill McShea on Slack
 		) ),
 		'core-upgrade-install' => array_merge( get_committers(), array(
 			'afragen',
