e8f4726f5f28377c83c53bbe1d4c9e9214c4fa4c
Dion Hulse <dd32@git.wordpress.org>
2026-05-15 08:41:00 +0000

    Themes API: Don't unslash the input, the POST'd values haven't gone through WordPress bootstrap here, so they won't be slashed. Unslashing them can cause unexpected results (especially with PHP serialized payloads).
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@14891 74240141-8908-4e6f-9713-ba540dce6ec7

--- api.wordpress.org/public_html/themes/info/1.0/index.php
+++ api.wordpress.org/public_html/themes/info/1.0/index.php
@@ -8,18 +8,6 @@ require dirname( dirname( dirname( __DIR__ ) ) ) . '/wp-init-ondemand.php';
 //  wp_cache_switch_to_blog( WPORG_THEME_DIRECTORY_BLOGID ); // Uses is_multisite() which is unavailable.
 $wp_object_cache->blog_prefix = WPORG_THEME_DIRECTORY_BLOGID;
 
-// Helper methods
-function wp_unslash( $value ) {
-	if ( is_string( $value ) ) {
-		return stripslashes( $value );
-	}
-	if ( is_array( $value ) ) {
-		return array_map( __FUNCTION__, $value );
-	}
-
-	return $value;
-}
-
 /**
  * Bails out with an error message.
  *
@@ -69,10 +57,10 @@ if ( ! defined( 'THEMES_API_VERSION' ) ) {
 
 // Set up action and request information.
 if ( defined( 'JSON_RESPONSE' ) && JSON_RESPONSE ) {
-	$request = isset( $_REQUEST['request'] ) ? (object) wp_unslash( $_REQUEST['request'] ) : '';
+	$request = isset( $_REQUEST['request'] ) ? (object) $_REQUEST['request'] : '';
 	$format = 'json';
 } else {
-	$post_request = isset( $_POST['request'] ) && is_string( $_POST['request'] ) ? wp_unslash( $_POST['request'] ) : '';
+	$post_request = isset( $_POST['request'] ) && is_string( $_POST['request'] ) ? $_POST['request'] : '';
 	if ( $post_request ) {
 		// PHP Needs to get a non-urldecoded request, to avoid multibyte character malforming the request,
 		// but we need to check for malicious content with the decoded style (in addition)
