d78d6c280a5e18c37a9ee134e5911d22774928f2
Jamie Bradley <jamie@stackupdigital.co.uk>
2026-06-04 14:34:52 +0200

    fix: update comment in after-start
    Added extra detail to the comments for activating the browse happy
    theme to avoid confusion in the future.

--- environments/browsehappy/bin/after-start.sh
+++ environments/browsehappy/bin/after-start.sh
@@ -11,6 +11,9 @@ $WP wp option update blogname 'Browse Happy'
 $WP wp option update blogdescription 'Online. Worry free. Upgrade your browser today!'
 
 # Activate the browsehappy theme.
-$WP wp theme activate "Browse Happy"
+# wp-env uses the last path segment as the theme slug, so the slug is 'public_html'
+# rather than 'browsehappy' (from "themes": ["../browsehappy.com/public_html"]).
+# This theme is not pushed to the svn repo in wp-content/themes as per other sites.
+$WP wp theme activate public_html
 
 echo "Browse Happy environment ready!"
