eae5da38d4ea5a8a5c0f43a40911e43c7637cac3
Dion Hulse <dd32@dd32.id.au>
2026-06-09 13:52:00 +1000

    Apply suggestion from @dd32

--- wordpress.org/public_html/wp-content/plugins/wporg-markdown/inc/class-importer.php
+++ wordpress.org/public_html/wp-content/plugins/wporg-markdown/inc/class-importer.php
@@ -242,6 +242,7 @@ abstract class Importer {
 		if ( $sanitized !== $doc['slug'] ) {
 			return false;
 		}
+
 		$post_data = array(
 			'post_type'   => $this->get_post_type(),
 			'post_status' => 'publish',
