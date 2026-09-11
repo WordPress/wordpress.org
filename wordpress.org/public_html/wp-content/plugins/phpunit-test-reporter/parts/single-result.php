c1a901599f3f7e27a331888a8309a4d641ffe68f
KokkieH <kokkieh@git.wordpress.org>
2026-09-11 10:23:21 +0000

    PHPUnit Test Reporter: Improve single result rendering
    Props kokkieh, bor0.
    Closes https://github.com/WordPress/wordpress.org/pull/897.
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk@15181 74240141-8908-4e6f-9713-ba540dce6ec7

--- wordpress.org/public_html/wp-content/plugins/phpunit-test-reporter/parts/single-result.php
+++ wordpress.org/public_html/wp-content/plugins/phpunit-test-reporter/parts/single-result.php
@@ -36,7 +36,7 @@ if ( $user ) {
 $parent = get_post( $report->post_parent );
 if ( $parent ) :
 	?>
-<p><a href="<?php echo esc_url( get_permalink( $parent ) ); ?>">&larr; <?php echo esc_html( $parent->post_name ) . ': ' . apply_filters( 'the_title', get_the_title( $parent ) ); ?></a></p>
+<p><a href="<?php echo esc_url( get_permalink( $parent ) ); ?>">&larr; <?php echo esc_html( $parent->post_name ) . ': ' . esc_html( $parent->post_title ); ?></a></p>
 <?php endif; ?>
 
 <p><a href="<?php echo esc_url( get_permalink( $report->ID ) ); ?>" title="<?php echo esc_attr( $status_title ); ?>" class="<?php echo esc_attr( 'ptr-status-badge ptr-status-badge-' . strtolower( $status ) ); ?>"><?php echo esc_html( $status ); ?></a></p>
